9ba0adde2ee41a7ceaf540a58cadfd24e43b350c
Ihor Hranetskyi <graniecki20@gmail.com>
2020-07-16 22:16:26 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const list = document.getElementById('thumbs');
+
+list.addEventListener('click', (event) => {
+  const newSrc = event.target.parentElement.href;
+
+  event.preventDefault();
+  document.getElementById('largeImg').src = newSrc;
+});
