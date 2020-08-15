d09a7477afee366786ffcb7a1b8a8164e4318d78
Petro Mandziuk <64119700+Petro-JS-Developer@users.noreply.github.com>
2020-08-15 15:23:10 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const listUl = document.querySelector('ul');
+const largeImgId = document.getElementById('largeImg');
+
+const listU = (event) => {
+  event.preventDefault();
+
+  const item = event.target.closest('.list-item');
+
+  if (!item || !listUl.contains(item)) {
+    return;
+  }
+
+  largeImgId.setAttribute('src', item.querySelector('a').getAttribute('href'));
+};
+
+listUl.addEventListener('click', listU);
