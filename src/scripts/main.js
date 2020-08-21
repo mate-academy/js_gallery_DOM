d1a9295ef6eaa5c1cd270375bd2d5f4365dd918e
Tatiana Ivanchenko <tanyarz1996@gmail.com>
2020-08-21 15:43:43 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const galleryList = document.querySelector('.gallery__list');
+const largeImage = document.querySelector('#largeImg');
+
+galleryList.addEventListener('click', (event) => {
+  event.preventDefault();
+  largeImage.src = event.target.src.replace('-thumb.jpeg', '.png');
+});
