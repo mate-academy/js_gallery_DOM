e05f5a123f43082a4002f98325011b1755775b35
oivannikov <alex0502ivannikov@gmail.com>
2020-08-25 20:16:11 +0300

    changed the name of the varible

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,11 @@
 'use strict';
 
 const largeImage = document.querySelector('.gallery__img');
-const list = document.querySelector('ul');
+const photos = document.querySelector('ul');
 
 list.addEventListener('click', (event) => {
-  const item = event.target.closest('a');
+  const photo = event.target.closest('a');
 
   event.preventDefault();
-  largeImage.src = item.href;
+  largeImage.src = photo.href;
 });
