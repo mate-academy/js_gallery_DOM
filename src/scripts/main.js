318e1914989f96258be247a739a5f1a29b6acf0b
nanccyy <lidiapriymak@gmail.com>
2020-08-25 14:45:52 +0200

    finished

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const mainImage = document.querySelector('#largeImg');
+const thumbImages = document.querySelectorAll('.list-item__link');
+
+[...thumbImages].map(thumb => {
+  thumb.addEventListener('click', (event) => {
+    event.preventDefault();
+    mainImage.src = thumb.href;
+  });
+});
