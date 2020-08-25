13579c982226302e52ab0f59db6e9cebf6baac1e
Dennis Priadka <dennis.priadka@gmail.com>
2020-08-25 10:52:03 +0300

    +

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,11 +5,8 @@ window.onload = () => {
 
   largeImg.height = parseInt(window.getComputedStyle(largeImg).height);
 
-  document.querySelectorAll('.list-item__link').forEach((link) => {
-    link.removeAttribute('href');
-  });
-
   document.querySelector('.gallery__list').onclick = (event) => {
+    event.preventDefault();
     largeImg.src = event.target.src;
   };
 };
