3b1363e3920829abe784c37a883d93a047b069c1
mpone <m.parphenjuk@gmail.com>
2020-08-25 18:22:36 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const gallery = document.getElementById('thumbs');
+const largeImage = document.getElementById('largeImg');
+
+for (const picture of gallery.querySelectorAll('.list-item__link')) {
+  picture.addEventListener('click', (event) => {
+    event.preventDefault();
+    largeImage.src = picture.href;
+  });
+}
