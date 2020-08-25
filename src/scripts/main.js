e926f7597f73d1f07649724a2469304bee470c2a
Mikle25 <mikle2593@gmail.com>
2020-08-25 16:33:07 +0300

    v1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+window.addEventListener('load', slider());
+
+function slider() {
+  const list = document.getElementById('thumbs');
+  const largeImg = document.getElementById('largeImg');
+
+  largeImg.height = parseInt(window.getComputedStyle(largeImg).height);
+
+  list.addEventListener('click', (event) => {
+    event.preventDefault();
+    largeImg.src = event.target.src;
+  });
+}
