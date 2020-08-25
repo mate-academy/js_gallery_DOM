b01e713c6617b236a19391bf60d7a6b4e66f8a0b
Serhii Antonyshyn <serginio4422@gmail.com>
2020-08-25 20:12:49 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const listOfA = document.querySelectorAll('a');
+
+for (const keys of listOfA) {
+  keys.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const img = document.querySelector('img');
+
+    img.src = keys.href;
+  });
+}
