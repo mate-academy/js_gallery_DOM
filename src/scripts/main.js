fd0b1efbf65985875ed6caa8e1a13a2585abc03e
oivannikov <alex0502ivannikov@gmail.com>
2020-08-25 20:09:23 +0300

    fixed code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,11 @@
 'use strict';
 
-const bigImage = document.querySelector('.gallery__img');
+const largeImage = document.querySelector('.gallery__img');
 const list = document.querySelector('ul');
 
 list.addEventListener('click', (event) => {
   const item = event.target.closest('a');
 
   event.preventDefault();
-  bigImage.src = item.href;
+  largeImage.src = item.href;
 });
