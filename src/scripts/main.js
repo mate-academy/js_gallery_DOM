697e17aef4e22ae96289c7e4a45f6ba7f1ea5da8
uni-8 <julia.zhol.a@gmail.com>
2020-08-26 13:54:51 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const largeImg = document.getElementById('largeImg');
+const thumbs = document.getElementById('thumbs');
+
+thumbs.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const item = event.target.closest('.list-item__link');
+
+  if (!item || !thumbs.contains(item)) {
+    return;
+  }
+
+  largeImg.src = item.href;
+});
