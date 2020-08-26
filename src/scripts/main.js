63937ad19a6de33a4117e7c2f857f956bbddf688
DmJSCourse <dimonua@gmail.com>
2020-08-26 15:04:24 +0300

    solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const links = document.querySelector('.gallery__list');
+const frame = document.querySelector('.gallery__large-img');
+
+links.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const imageLink = event.target.closest('.list-item__link').href;
+
+  frame.src = imageLink;
+});
