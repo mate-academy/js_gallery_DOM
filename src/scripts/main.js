35696a43bc6d116bbc997c51cb0ea9b7cf5d52d3
Eduard Nazarenko <ferrarif40lemans@gmail.com>
2020-08-27 02:06:17 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const largeImage = document.getElementById('largeImg');
+const imageList = document.querySelector('.gallery__list');
+
+const imageChanger = (event) => {
+  event.preventDefault();
+  largeImage.src = event.target.closest('A').href;
+};
+
+imageList.addEventListener('click', imageChanger);
