37bb9e093229bbc133d2d958a1c73809d64a0ab2
martakupka <marta-k2008@ukr.net>
2020-08-26 13:47:04 +0300

    Item variable changed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,13 +4,9 @@ const mainImage = document.querySelector('#largeImg');
 const thumbsList = document.querySelector('#thumbs');
 
 thumbsList.addEventListener('click', (event) => {
-  const item = event.target;
+  const item = event.target.closest('.list-item__link');
 
   event.preventDefault();
 
-  if (!item.classList.contains('gallery__img')) {
-    return;
-  }
-
-  mainImage.src = item.closest('.list-item__link').href;
+  mainImage.src = item.href;
 });
