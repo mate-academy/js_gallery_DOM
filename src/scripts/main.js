664bed382bf509358f0eaf3bc5a3e6319639c405
Gabbasova Oksana <oksana.gabba@gmail.com>
2020-08-26 21:15:17 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const largeImg = document.querySelector('.gallery__large-img');
+const ul = document.querySelector('.gallery__list');
+
+ul.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const link = event.target.closest('a');
+
+  largeImg.src = link.href;
+})
