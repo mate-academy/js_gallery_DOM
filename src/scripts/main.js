48af5b08616f31955094bd6f10f0ae2c3ab82ac6
Vasyl Tykhan <tykhan@MacBook-Pro-Vasyl.local>
2020-08-27 01:44:48 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,9 @@
 'use strict';
 
-const image = document.getElementById('largeImg');
+const image = document.querySelector('largeImg');
 
 document.querySelector('.gallery').onclick = (event) => {
   event.preventDefault();
+
   image.src = event.target.closest('.list-item__link').href;
 };
