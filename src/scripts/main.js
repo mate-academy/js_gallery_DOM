37dfa98548a37c2b27137f4e1576cd141c894fdf
uni-8 <julia.zhol.a@gmail.com>
2020-08-26 16:16:45 +0300

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,11 +6,5 @@ const thumbs = document.getElementById('thumbs');
 thumbs.addEventListener('click', (event) => {
   event.preventDefault();
 
-  const item = event.target.closest('.list-item__link');
-
-  if (!item || !thumbs.contains(item)) {
-    return;
-  }
-
-  largeImg.src = item.href;
+  largeImg.src = event.target.closest('.list-item__link').href;
 });
