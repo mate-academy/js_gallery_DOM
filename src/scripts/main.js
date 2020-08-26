a9fb1ccda4895dcd7bc38fc380352333c246abc2
KatrinaSavvova <kat.savvek@gmail.com>
2020-08-26 19:29:00 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const list = document.querySelector('ul');
+const mainPicture = document.getElementById('largeImg');
+
+const getBigPicture = (event) => {
+  event.preventDefault();
+
+  const item = event.target.closest('a');
+
+  if (!item || !list.contains(item)) {
+    return;
+  }
+
+  mainPicture.src = item;
+};
+
+list.addEventListener('click', getBigPicture);
