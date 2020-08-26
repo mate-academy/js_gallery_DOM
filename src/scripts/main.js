2ed143d8116307a54b5feb820734017dccd52e1c
nznmvs <nznmvs@gmail.com>
2020-08-26 16:21:23 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const thumbnails = document.getElementById('thumbs');
+const mainPicture = document.getElementById('largeImg');
+
+thumbnails.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const pictureDir = event.target.closest('.list-item__link');
+
+  mainPicture.src = pictureDir.href;
+});
