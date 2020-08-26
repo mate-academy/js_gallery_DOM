1a808fde72bc50d344023f18fc9186eacaed6673
ChrisZinch <chrispakulyak@gmail.com>
2020-08-26 15:54:43 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const largImg = document.querySelector('#largeImg');
+const list = document.querySelector('.gallery__list');
+
+list.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const item = event.target.closest('.list-item__link');
+
+  if (!item || !list.contains(item)) {
+    return;
+  };
+
+  largImg.src = item.href;
+});
