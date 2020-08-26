c811083e1cf2cac098d259cdf3960218cc71838a
Anastasiia Bortnichuk <feilanheid@gmail.com>
2020-08-26 16:18:50 +0300

    develop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const galleryList = document.querySelector('#thumbs');
+const mainImg = document.querySelector('#largeImg');
+
+galleryList.addEventListener('click', (event) => {
+  event.preventDefault();
+});
+
+galleryList.addEventListener('click', (event) => {
+  mainImg.src = event.target.src;
+});
