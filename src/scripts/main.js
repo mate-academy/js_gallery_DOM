5f4c9664cce3b0e4fb64047ac5c35caea33cb9db
oggyrock <steelo.sun@gmail.com>
2020-08-26 11:34:52 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const thumbs = document.querySelector('#thumbs');
+const largeImg = document.querySelector('#largeImg');
+
+thumbs.addEventListener('click', (event) => {
+  const item = event.target;
+
+  event.preventDefault();
+  largeImg.src = item.parentNode.href;
+});
