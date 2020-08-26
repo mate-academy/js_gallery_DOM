cd22d427543d2b0e29d0cdf39d816bb67bb01b5c
unknown <pavlo.com.5@gmail.com>
2020-08-26 16:16:07 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const image = document.querySelectorAll('.list-item');
+
+for (const img of [...image]) {
+  img.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const photoUrl = img.querySelector('.list-item__link');
+
+    document.getElementById('largeImg').src = `${photoUrl.href}`;
+  });
+}
