dfcb11253623eeffc708f8c201004b81919ee951
Anastasiia Lukina <anlukina24@gmail.com>
2020-08-26 12:15:58 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const links = document.querySelectorAll('.list-item__link');
+const mainImage = document.getElementById('largeImg');
+
+const pathNames = [...links].map(elem => elem.pathname);
+
+[...links].forEach((link, i) => {
+  link.addEventListener('click', event => {
+    event.preventDefault();
+    mainImage.src = pathNames[i];
+  });
+});
