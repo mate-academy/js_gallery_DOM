f5fc1d566c20d148aae8747749ff2795a5689c2d
Oleksandr Rohatnov <mr.rogatnov@gmail.com>
2020-08-27 09:03:41 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const mainImg = document.getElementById('largeImg');
+const togglerImg = document.getElementById('thumbs');
+
+togglerImg.addEventListener('click', (event) => {
+  const item = event.target.closest('.list-item__link');
+
+  event.preventDefault();
+
+  if (!item || !togglerImg.contains(item)) {
+    return;
+  };
+  mainImg.src = item.href;
+});
