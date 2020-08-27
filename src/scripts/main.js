214be29478bf6394c1353f1113b66f72c2763081
yuriimovchko <movchko.yuriy@gmail.com>
2020-08-27 07:56:32 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const list = document.querySelectorAll('.list-item__link');
+const largeImg = document.querySelector('#largeImg');
+
+for (const item of list) {
+  item.addEventListener('click', (event) => {
+    event.preventDefault();
+    largeImg.src = item.href;
+  });
+}
