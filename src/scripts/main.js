89fe48c7041b776ada4cad24e6080eb5a1eb0cc6
kshvetsova <katshvetsova@gmail.com>
2020-08-30 20:13:56 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const largeImg = document.getElementById('largeImg');
+const thumbs = document.getElementById('thumbs');
+
+thumbs.addEventListener('click', (event) => {
+  const thumbnail = event.target.closest('a');
+
+  event.preventDefault();
+  largeImg.src = thumbnail.href;
+});
