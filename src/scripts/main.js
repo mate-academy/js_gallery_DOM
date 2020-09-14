d4ba7309b9d63dd18a15b870b9a606c0e2bb9224
Dima Diachenko <zarg129@gmail.com>
2020-09-14 12:36:40 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const imgList = document.querySelector('.gallery__list');
+const bigImg = document.querySelector('img');
+
+imgList.addEventListener('click', (event) => {
+  event.preventDefault();
+});
+
+imgList.addEventListener('click', (event) => {
+  bigImg.src = event.target.src;
+});
