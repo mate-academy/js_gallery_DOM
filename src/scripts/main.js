323f65568a8e4d0f8bb3f7850768833da061f452
Yazheviks <vicsergeyeva@gmail.com>
2020-10-08 17:43:48 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const galleryList = document.querySelector('.gallery__list');
+const listLinks = document.querySelectorAll('.list-item__link');
+const mainImg = document.querySelector('.gallery__large-img');
+
+listLinks.forEach(link => {
+  link.addEventListener('click', (event) => {
+    event.preventDefault();
+  });
+});
+
+galleryList.addEventListener('click', (event) => {
+  const item = event.target;
+
+  if (
+    item.classList.contains('gallery__thumb')
+    || item.classList.contains('list-item__link')
+  ) {
+    mainImg.src = item.closest('.list-item__link').getAttribute('href');
+  }
+});
