687c15f65d727e8bff42039d418add800a67d0d7
Alona <aionovaionova@gmail.com>
2020-10-08 16:47:33 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const list = document.querySelector('ul');
+const largeImage = document.querySelector('#largeImg');
+
+list.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const item = event.target.closest('.list-item');
+
+  if (!item || !list.contains(item)) {
+    return;
+  };
+  largeImage.src = item.children[0].href;
+});
