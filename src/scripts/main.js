0a1c184c5a0cde274c1a60b1084802c8d409e64c
Alina <alina90565@gmail.com>
2020-10-14 20:22:36 +0300

    done gallery task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const listOfImages = document.querySelector('.gallery__list');
+const largeImg = document.querySelector('#largeImg');
+const galleryImages = document.querySelectorAll('.gallery__thumb');
+
+listOfImages.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const closestParent = event.target.closest('.list-item__link');
+
+  if (!closestParent || !listOfImages.contains(closestParent)) {
+    return;
+  };
+
+  largeImg.src = closestParent.href;
+
+  galleryImages.forEach(img => {
+    img.style.outlineWidth = '0';
+  });
+
+  closestParent.firstElementChild.style.outline = '1px solid blue';
+});
