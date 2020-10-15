de78b85468aa461de79a53838c8e36855e9e3744
VoduDay <andriy.maniak@gmail.com>
2020-10-15 16:57:49 +0300

    gallery listener

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const galleryList = document.querySelector('.gallery__list');
+const galleryImage = document.querySelector('.gallery__img');
+
+galleryList.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const targetImage = event.target.closest('.list-item__link');
+
+  if (!targetImage.classList.contains('list-item__link')) {
+    return;
+  }
+
+  galleryImage.src = targetImage.href;
+});
