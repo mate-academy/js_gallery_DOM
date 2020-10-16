4e606b9ccb2a071f5dfea82e7ecd5d08f90770f9
Nikolay Filiuk <Filiuk98@gmail.com>
2020-10-16 17:20:30 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const imagesList = document.querySelector('.gallery__list');
+const mainImg = document.querySelector('.gallery__large-img');
+
+imagesList.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const closestParent = event.target.closest('.list-item__link');
+
+  if (!closestParent || !imagesList.contains(closestParent)) {
+    return;
+  };
+
+  mainImg.src = closestParent.href;
+});
