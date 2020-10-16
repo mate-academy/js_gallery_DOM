f0edbaae123f250ae4e777b830285a5a7ebfe14b
Andrii Medziak <58214135+Andrii1996@users.noreply.github.com>
2020-10-16 19:12:47 +0300

    gallery

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const largeImg = document.querySelector('.gallery__large-img');
+const listlImg = document.querySelector('ul');
+
+listlImg.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const image = event.target.closest('.list-item');
+
+  largeImg.src = image.children[0].href;
+});
