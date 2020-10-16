df6f2ee2af2a066acc37c8165daffda2ba4292da
andlap24 <andrew.laptev24@gmail.com>
2020-10-16 15:48:12 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const mainImage = document.querySelector('.gallery__large-img');
+const list = document.querySelector('ul');
+
+list.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const smallImage = event.target.closest('.list-item');
+
+  mainImage.src = smallImage.children[0].href;
+});
