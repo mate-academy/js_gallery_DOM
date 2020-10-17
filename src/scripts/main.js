eebd843532b68484b0c5a033140b40e7c4dc33e1
goncharukop <goncharukop@gmail.com>
2020-10-17 16:43:51 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const galleryImage = document.querySelector('.gallery__img');
+const galleryList = document.querySelector('.gallery__list');
+
+galleryList.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const imageInSlider = event.target.closest('.list-item__link');
+
+  if (!imageInSlider || !galleryList.contains(imageInSlider)) {
+    return;
+  }
+
+  galleryImage.src = imageInSlider.href;
+});
