7b15cf1049de656c56e1fe3df1f950870218c997
Yuriy Putsan <putsan57@gmail.com>
2020-10-17 12:10:26 +0300

    fix galery

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const mainImg = document.querySelector('.gallery__large-img');
+const picturesList = document.querySelector('.gallery__list');
+
+picturesList.addEventListener('click', (event) => {
+  event.preventDefault();
+  mainImg.src = event.target.closest('a').href;
+});
