d26c0cfcdd6da1a65eb6ca3f1c66fecaef4dd4e9
Liza <elizabethhoncharenko@gmail.com>
2020-10-18 14:25:33 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const list = document.querySelector('.gallery__list');
+const mainImage = document.querySelector('.gallery > div > img');
+
+list.addEventListener('click', (event) => {
+  event.preventDefault();
+  mainImage.src = event.target.src;
+});
