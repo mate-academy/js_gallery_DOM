2a5aab490bbed14218e8b29f067b778aa2e8d6fb
KhrystynaYelyseyeva <play1409@gmail.com>
2020-10-19 12:23:36 +0300

    add click event on imgs

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const list = document.querySelector('.gallery__list');
+const largeImg = document.querySelector('.gallery__large-img');
+
+list.addEventListener('click', function(e) {
+  e.preventDefault();
+
+  const item = e.target;
+
+  largeImg.src = item.closest('.list-item__link').href;
+});
