10702933a43c5e98a15688aed539e0e2f4d1dbb4
TishchenkoSergey <tishchenko.seriy@gmail.com>
2020-10-19 23:48:02 +0300

    gallery

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const largeImg = document.querySelector('.gallery__large-img');
+const list = document.querySelector('.gallery__list');
+
+list.addEventListener('click', event => {
+  event.preventDefault();
+
+  largeImg.src = event.target.closest('.list-item__link').href;
+});
