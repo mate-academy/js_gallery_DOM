ca7449cfbb2f34c22d2d3a6f8f37f5da7ea4ba07
KhrystynaYelyseyeva <play1409@gmail.com>
2020-10-19 15:29:54 +0300

    e to event

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,10 +3,10 @@
 const list = document.querySelector('.gallery__list');
 const largeImg = document.querySelector('.gallery__large-img');
 
-list.addEventListener('click', function(e) {
-  e.preventDefault();
+list.addEventListener('click', function(event) {
+  event.preventDefault();
 
-  const item = e.target;
+  const item = event.target;
 
   largeImg.src = item.closest('.list-item__link').href;
 });
