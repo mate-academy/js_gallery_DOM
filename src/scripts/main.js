ed2fa0d91a189b7ed22f3a693392fa6b0a36f910
dmitry <akulinind@gmail.com>
2020-10-19 14:35:52 +0300

    add gallery listener

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const galleryList = document.querySelector('.gallery__list');
+
+galleryList.addEventListener('click', (event) => {
+  const smallImage = event.target.closest('.list-item');
+  const bigImage = document.querySelector('#largeImg');
+
+  event.preventDefault();
+
+  if (!smallImage) {
+    return;
+  }
+
+  bigImage.src = smallImage.querySelector('.list-item__link').href;
+});
