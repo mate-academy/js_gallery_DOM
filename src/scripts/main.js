cae4e876584d9385ab8a7942f56f92733d6fd273
Yurii <yurastt1@gmail.com>
2020-10-19 23:54:44 +0300

    solutiom

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const listItem = document.querySelectorAll('.list-item');
+const mainPicture = document.querySelector('#largeImg');
+
+listItem.forEach(item => {
+  item.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    mainPicture.src = item.children[0].href;
+  });
+});
