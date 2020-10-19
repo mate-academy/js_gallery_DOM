824099cac5dd9368d80b8414ed1692543b17b2f9
KirillLutsenko <mnrovski68@gmail.com>
2020-10-19 15:59:38 +0300

    Gallery

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const mainImg = document.querySelector('.gallery__large-img');
+const galleryList = document.querySelector('.gallery__list');
+
+galleryList.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  mainImg.src = event.target.closest('a').href;
+});
