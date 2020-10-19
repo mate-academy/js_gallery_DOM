82bea807faea228076ed461a26b30b67d2ada7bd
Anton <office.sklyarov@gmail.com>
2020-10-19 11:08:25 +0300

    added changes back(test)

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,5 +8,5 @@ list.addEventListener('click', (event) => {
 
   const link = event.target.closest('.list-item__link');
 
-  largeImg.src = link.href;
+  largeImg.setAttribute('src', link.getAttribute('href'));
 });
