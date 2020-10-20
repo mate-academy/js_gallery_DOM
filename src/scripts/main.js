79babae4d11f871d59ba5d6f20796cc29efd690f
OnlyRaydek <bayramsaid00@gmail.com>
2020-10-20 16:11:17 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const mainImage = document.querySelector('.gallery__large-img');
+const imageList = document.querySelector('.gallery__list');
+
+imageList.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const image = event.target.closest('.list-item__link');
+
+  if (!image || !imageList.contains(image)) {
+    return;
+  };
+
+  mainImage.src = image.href;
+});
