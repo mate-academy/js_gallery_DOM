f285d79f7fc4042376bf773c6d42c928627ec89e
dmitry <akulinind@gmail.com>
2020-10-20 15:11:52 +0300

    refactor

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,9 @@
 'use strict';
 
 const galleryList = document.querySelector('.gallery__list');
+const bigImage = document.querySelector('#largeImg');
 
 galleryList.addEventListener('click', (event) => {
-  const smallImage = event.target.closest('.list-item');
-  const bigImage = document.querySelector('#largeImg');
-
   event.preventDefault();
-
-  if (!smallImage) {
-    return;
-  }
-
-  bigImage.src = smallImage.querySelector('.list-item__link').href;
+  bigImage.src = event.target.closest('.list-item__link').href;
 });
