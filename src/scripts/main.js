f6d496059c585ee7f47fb9c285eef16670a64722
OnlyRaydek <bayramsaid00@gmail.com>
2020-10-20 16:16:08 +0300

    Add querySelector

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,11 +6,11 @@ const imageList = document.querySelector('.gallery__list');
 imageList.addEventListener('click', (event) => {
   event.preventDefault();
 
-  const image = event.target.closest('.list-item__link');
+  const image = event.target.closest('.list-item');
 
   if (!image || !imageList.contains(image)) {
     return;
   };
 
-  mainImage.src = image.href;
+  mainImage.src = image.querySelector('.list-item__link').href;
 });
