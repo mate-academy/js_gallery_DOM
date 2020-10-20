9aa73b04a0736da641a112851ebe3c31e51344a5
Liza Krasnoshchok <nlight1122@gmail.com>
2020-10-20 13:12:09 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const images = document.querySelectorAll('a');
+const largeImg = document.getElementById('largeImg');
+
+for (const img of images) {
+  img.onclick = (event) => {
+    event.preventDefault();
+
+    largeImg.src = event.target.src;
+  };
+}
