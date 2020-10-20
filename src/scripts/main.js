557d4e09b9cd9d7e3761fcb8e9684e1d4ad7bf11
Lancaelot <Lancaelot@gmail.com>
2020-10-20 11:38:15 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const galleryImageList = document.querySelector('#thumbs');
+
+const galleryMainImage = document.querySelector('#largeImg');
+
+galleryImageList.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const eventElement = event.target.closest('.list-item__link');
+
+  galleryMainImage.src = eventElement.href;
+});
