2b4852b5e082de905196969a5fe7cc0990a9bddb
AntonChetveryk <chetverykanton92@gmail.com>
2020-10-25 10:52:04 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const ul = document.querySelector('ul');
+const largeImg = document.getElementById('largeImg');
+
+ul.addEventListener('click', (e) => {
+  const item = e.target;
+
+  e.preventDefault();
+
+  if (!item.classList.contains('gallery__img')) {
+    return;
+  }
+
+  largeImg.setAttribute('src', item.getAttribute('src'));
+});
