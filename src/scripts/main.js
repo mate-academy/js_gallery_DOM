a5326225fe4ea430e4e43b9776a13840fe77c913
y.kostenko <y.kostenko@mate.academy>
2020-11-05 15:41:02 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const largeImg = document.querySelector('#largeImg');
+const linkList = document.querySelectorAll('.list-item__link');
+
+for (const key of linkList) {
+  key.onclick = (event) => {
+    event.preventDefault();
+
+    largeImg.src = key.href;
+  };
+}
