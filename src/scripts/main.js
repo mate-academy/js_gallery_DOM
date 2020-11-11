91071363cb84a84ffde5be6be719085ff69556e2
Anna Uska <annauska95@gmail.com>
2020-11-11 23:06:01 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const thumbs = document.querySelector('.gallery__list');
+const largeImg = document.getElementById('largeImg');
+
+thumbs.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const item = event.target.closest('.list-item__link');
+
+  if (!item.classList.contains('list-item__link')) {
+    return;
+  }
+  largeImg.src = item.href;
+});
