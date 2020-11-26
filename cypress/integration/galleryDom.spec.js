0c14082ced89b6e5fd3cf4df9328ad767b372adf
y.kostenko <y.kostenko@mate.academy>
2020-11-26 12:38:13 +0200

    commands deleted

--- cypress/integration/galleryDom.spec.js
+++ cypress/integration/galleryDom.spec.js
@@ -1,35 +1,27 @@
 'use strict';
 
+const url = 'http://localhost:8080';
+const images = ['/images/landscape-first.png',
+  '/images/landscape-second.png', '/images/landscape-third.png',
+  '/images/landscape-fourth.png', '/images/landscape-fifth.png'];
+
 describe('Gallery application', () => {
   beforeEach(() => {
     cy.visit('/');
     cy.get('#largeImg').as('largeImg');
-
-    const url = 'http://localhost:8080';
-    const images = ['/images/landscape-first.png',
-      '/images/landscape-second.png', '/images/landscape-third.png',
-      '/images/landscape-fourth.png', '/images/landscape-fifth.png'];
-
-    Cypress.Commands.add('beenSelected', (selector) => {
-      for (let i = 0; i < images.length; i++) {
-        cy.get(`${selector}(${i + 1})`).click();
-        cy.get('@largeImg').should('have.attr', 'src', `${url}${images[i]}`);
-      };
-    });
-
-    Cypress.Commands.add('abeenSelected', (selector) => {
-      for (let i = 0; i < images.length; i++) {
-        cy.get(`[${selector} ${i + 1}"]`).click();
-        cy.get('@largeImg').should('have.attr', 'src', `${url}${images[i]}`);
-      };
-    });
   });
 
   it('can click on a small `img` image', () => {
-    cy.beenSelected('li:nth-child');
+    for (let i = 0; i < images.length; i++) {
+      cy.get(`li:nth-child(${i + 1})`).click();
+      cy.get('@largeImg').should('have.attr', 'src', `${url}${images[i]}`);
+    };
   });
 
   it('can click on `a` outside of image', () => {
-    cy.abeenSelected('title="Image');
+    for (let i = 0; i < images.length; i++) {
+      cy.get(`[title="Image ${i + 1}"]`).click();
+      cy.get('@largeImg').should('have.attr', 'src', `${url}${images[i]}`);
+    };
   });
 });
