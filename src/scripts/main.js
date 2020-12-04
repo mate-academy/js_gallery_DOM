c0de205ed932b356dd05ab47689244d20c68f4bd
losanov <losanov@ukr.net>
2020-12-04 11:41:26 +0200

    solution1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const largeImg = document.getElementById('largeImg');
+const imageList = document.querySelector('ul');
+
+imageList.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  largeImg.src = e.target.closest('.list-item__link').href;
+});
