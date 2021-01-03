1b749d7cabd1bddb2a538bbff9d489e6d679d822
Serhii Alieksieievych <kajot1863@gmail.com>
2021-01-04 00:02:51 +0200

    Completed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const links = [...document.querySelectorAll('.list-item__link')];
+const display = document.querySelector('#largeImg');
+
+for (const link of links) {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    display.src = e.currentTarget.href;
+  });
+}
