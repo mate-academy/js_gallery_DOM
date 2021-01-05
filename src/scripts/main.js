eb4d2fc71e4bd710965818b4c3bdcf709d890d19
Dmytro Tsehelnyk <34zimnii@gmail.com>
2021-01-05 16:04:29 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const largeImg = document.querySelector('#largeImg');
+const galleryNav = document.querySelector('#thumbs');
+
+galleryNav.addEventListener('click', action => {
+  action.preventDefault();
+
+  const currentLink = action.target.closest('.list-item__link');
+
+  if (galleryNav.contains(currentLink)) {
+    largeImg.src = currentLink.href;
+  }
+});
