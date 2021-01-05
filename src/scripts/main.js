c6858846206ae72a25f0cc969b7ea69fc054c7cb
Your Name <mogolgogol.90@gmail.com>
2021-01-05 19:03:43 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const mainImage = document.querySelector('.gallery__large-img');
+const thumbs = document.querySelector('#thumbs');
+
+thumbs.addEventListener('click', (mouseEvent) => {
+  const item = mouseEvent.target.closest('.gallery__thumb');
+
+  if (!item || !thumbs.contains(item)) {
+    return;
+  }
+  mouseEvent.preventDefault();
+  mainImage.src = mouseEvent.target.closest('.list-item__link').href;
+});
