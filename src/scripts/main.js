439603c41433cb14d111d68bcba248783b470699
NikitaTudakov <tudakov.nikita@gmail.com>
2021-01-06 17:24:17 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const largeImg = document.getElementById('largeImg');
+const gallery = document.getElementById('thumbs');
+
+const switcher = (eventNew) => {
+  event.preventDefault();
+
+  const newPic = eventNew.target.closest('.list-item__link');
+
+  if (!newPic) {
+    return;
+  }
+
+  largeImg.src = newPic.href;
+  largeImg.alt = newPic.alt;
+};
+
+gallery.addEventListener('click', switcher);
