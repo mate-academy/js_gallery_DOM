562ac814e4fe2f9e58cd74da67230c5162a901a9
Vitalii Siedykh <siedykh.v@gmail.com>
2021-01-06 14:15:47 +0200

    sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const large = document.getElementById('largeImg');
+const small = document.querySelectorAll('.list-item__link');
+
+for (const img of small) {
+  img.addEventListener('click', (x) => {
+    x.preventDefault();
+
+    large.src = img.href;
+  });
+}
