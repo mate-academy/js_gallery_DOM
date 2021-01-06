310aaceedb11d5005cf4b11b2c77965563a7d644
MarchD <dubchmar@gmail.com>
2021-01-06 15:54:00 +0200

    added solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const thumbs = document.getElementById('thumbs');
+const img = document.getElementById('largeImg');
+
+thumbs.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const item = e.target.closest('.list-item');
+
+  img.src = item.firstElementChild.href;
+});
