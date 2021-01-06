d7a54e529f5c7b350d3061c0f2e576c8d98e0ccb
Andrii Haidai <haidai.andriy@gmail.com>
2021-01-06 12:32:14 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const bigImage = document.querySelector('#largeImg');
+const galleryItem = document.querySelector('#thumbs');
+
+galleryItem.addEventListener('click', myevent => {
+  myevent.preventDefault();
+
+  const clickedImg = myevent.target.closest('.list-item__link');
+
+  if (galleryItem.contains(clickedImg)) {
+    bigImage.src = clickedImg.href;
+  }
+});
