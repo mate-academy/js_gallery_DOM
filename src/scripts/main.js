5e697abc251e8a3602d426ce4ac3e8335811fe2a
Svitlana Haiduk <svitlana.gaiduk@gmail.com>
2021-01-06 15:45:55 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const largeImage = document.querySelector('#largeImg');
+const list = document.querySelector('#thumbs');
+
+list.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const item = e.target.closest('.list-item__link');
+
+  if (list.contains(item)) {
+    largeImage.src = item.href;
+  }
+});
