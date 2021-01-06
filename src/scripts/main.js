47ac17bb9ee1828431dd622b5fc311acca481e1e
Zhenya <e.mezhueva@gmail.com>
2021-01-06 16:06:08 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const thumbs = document.querySelector('.gallery__list');
+const largeImg = document.getElementById('largeImg');
+
+thumbs.addEventListener('click', (galleryEvent) => {
+  galleryEvent.preventDefault();
+
+  const item = galleryEvent.target.closest('.list-item__link');
+
+  if (item) {
+    largeImg.src = item.href;
+  }
+});
