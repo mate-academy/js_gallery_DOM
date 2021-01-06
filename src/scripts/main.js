884361dcff7b495693c65a26dc2df8d766563d6b
Ruslan Shmadchenko <ruslanwpua@gmail.com>
2021-01-06 17:57:59 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const list = document.querySelector('.gallery');
+const largeImg = document.getElementById('largeImg');
+
+list.addEventListener('click', e => {
+  e.preventDefault();
+
+  const item = e.target;
+
+  if (!item.classList.contains('gallery__thumb')
+    && !item.classList.contains('list-item__link')) {
+    return;
+  }
+
+  const closestLink = item.closest('.list-item__link');
+  const mainLink = closestLink.href;
+
+  largeImg.src = mainLink;
+});
