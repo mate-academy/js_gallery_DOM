d244362279050675e338ba2f0b4e71ed61196dae
PaulT <ptimofejev@gmail.com>
2021-01-06 17:35:52 +0200

    Gallery solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const display = document.getElementById('largeImg');
+const imgList = document.querySelector('.gallery__list');
+
+imgList.addEventListener('click', (ev) => {
+  ev.preventDefault();
+
+  const link = (ev.target.tagName === 'IMG')
+    ? ev.target.parentElement
+    : ev.target;
+
+  display.src = link.href;
+});
