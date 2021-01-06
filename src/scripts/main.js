cb1358a3315c0cc992b4d868fd8adc830f2d912c
Layrovell <marina95prodan@gmail.com>
2021-01-06 16:44:20 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const largeImg = document.querySelector('#largeImg');
+const smallImgToChange = document.querySelectorAll('.list-item__link');
+
+for (const i of smallImgToChange) {
+  i.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    largeImg.src = i.href;
+  });
+}
