9b79846f76df98a0594e104607dfbec331bfc34a
Rostyslav Meleshko <meleshkorostyslav@gmail.com>
2021-01-07 17:57:41 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const largeImage = document.querySelector('#largeImg');
+
+const pictures = document.querySelectorAll('a');
+
+for (const picture of pictures) {
+  picture.addEventListener('click', () => {
+    event.preventDefault();
+    largeImage.src = picture.href;
+  });
+}
