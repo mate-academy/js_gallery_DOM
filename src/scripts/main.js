596e9e62eb9c98d4615368afc2af88de689fa0fe
Pavlo Terletskyi <pavloterleckij2@gmail.com>
2021-01-07 15:35:54 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const photoLinks = document.querySelectorAll('.list-item__link');
+const mainPhoto = document.getElementById('largeImg');
+
+[...photoLinks].forEach(link =>
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    mainPhoto.src = e.currentTarget.href;
+  })
+);
