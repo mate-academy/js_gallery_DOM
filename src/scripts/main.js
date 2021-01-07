a16c4503210d71aac65e9b41572fe4ed321529bf
Andrey Gavriluk <bugabuga.008@gmail.com>
2021-01-07 23:08:26 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const thumbLinks = document.querySelectorAll('.list-item__link');
+const img = document.getElementById('largeImg');
+
+thumbLinks.forEach(thumb => {
+  thumb.addEventListener('click', (ev) => {
+    ev.preventDefault();
+    img.src = thumb.href;
+  });
+});
