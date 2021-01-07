8ac13e935f0fcd13e597383980a892250ff509d9
KirillLutsenko <mnrovski68@gmail.com>
2021-01-07 18:34:56 +0200

    js gallery

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const mainPicture = document.querySelector('.gallery__large-img');
+const galleryList = document.querySelector('.gallery__list');
+
+galleryList.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  mainPicture.src = event.target.closest('.list-item__link').href;
+});
