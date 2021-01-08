4470ad4186db52bbb8a1a2d8caf4f8f6dd28a681
Yurii Fitsyk <yuriy.fitsyk@gmail.com>
2021-01-08 02:14:21 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const gallary = document.querySelector('.gallery');
+const langePhoto = document.querySelector('.gallery__large-img');
+
+gallary.addEventListener('click', (clickEvent) => {
+  const target = clickEvent.target;
+
+  clickEvent.preventDefault();
+
+  if (target.className === 'list-item__link') {
+    langePhoto.src = target.href;
+
+    return;
+  }
+
+  langePhoto.src = target.src;
+});
