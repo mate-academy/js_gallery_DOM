d1e7812c35457dc19c7f2b41f90d0c785a84674a
Nazar Valchyshyn <nvalja@gmail.com>
2021-01-08 17:30:00 +0200

    made task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const list = document.querySelector('.gallery__list');
+const picture = document.querySelector('.gallery__large-img');
+
+list.addEventListener('click', (occasion) => {
+  occasion.preventDefault();
+
+  const item = occasion.target.closest('.list-item__link');
+
+  if (!item || !list.contains(item)) {
+    return;
+  }
+
+  picture.src = item.href;
+});
