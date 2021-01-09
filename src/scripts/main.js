f7c36b8e21af6de14442c2e0199bb503210c4cb3
Oleksii Spalylo <white.rh42@gmail.com>
2021-01-09 19:08:51 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const img = document.getElementById('thumbs');
+
+img.addEventListener('click', () => {
+  event.preventDefault();
+
+  const item = event.target.closest('.list-item__link');
+  const mainImg = document.getElementById('largeImg');
+
+  mainImg.src = item.href;
+});
