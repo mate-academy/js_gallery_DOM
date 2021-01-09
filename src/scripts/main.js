60b634557fc770a5c4ec0c46a27308e8bf33c763
Olesia <olesia.tk@gmail.com>
2021-01-09 16:22:06 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const imgMain = document.querySelector('#largeImg');
+
+const imgCollection = document.querySelectorAll('.list-item__link');
+
+for (const image of imgCollection) {
+  image.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    imgMain.src = e.currentTarget.href;
+  });
+}
