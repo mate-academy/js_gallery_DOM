d43c182580e338abc22031c7cd51d619c95ad4b9
Dima <dimontregyb@email.ua>
2021-01-10 22:36:11 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const list = document.querySelector('#thumbs');
+const currentImage = document.querySelector('#largeImg');
+
+list.addEventListener('click', (e) => {
+  e.preventDefault();
+  currentImage.src = e.target.parentElement.href;
+});
