db7c0ecb65c1cf080b0acbf13f0a023f5820939f
Dima Bond <neverpooh@gmail.com>
2021-01-10 12:51:40 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const thumbList = document.querySelector('.gallery__list');
+const previewImg = document.querySelector('#largeImg');
+
+thumbList.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const clickedItem = e.target.closest('.list-item__link');
+
+  if (!clickedItem || !thumbList.contains(clickedItem)) {
+    return;
+  };
+
+  previewImg.src = clickedItem.href;
+});
