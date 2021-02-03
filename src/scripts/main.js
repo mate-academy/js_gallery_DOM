92b1cdc48c11a76783196aad3e3c5dde4f3ef7ea
Oleksandr Vasilkov <vasilkoff1337@gmail.com>
2021-02-03 13:14:22 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const largeImg = document.querySelector('#largeImg');
+const ul = document.querySelector('.gallery__list');
+
+ul.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const item = e.target;
+  const link = item.closest('A');
+
+  if (link) {
+    largeImg.src = link.href;
+    largeImg.alt = link.title;
+  }
+});
