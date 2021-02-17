48c498d806a4b08a033309179d4b02f6a0ddf926
Andriy <andriy@Air-Andriy.lan>
2021-02-17 00:42:41 +0000

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const largeImg = document.querySelector('#largeImg');
+const galleryList = document.querySelector('.gallery__list');
+
+galleryList.addEventListener('click', (event) => {
+    event.preventDefault();
+    largeImg.attributes.src.value = event.target.closest('a').attributes.href.value;
+
+});
