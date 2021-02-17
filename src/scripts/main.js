e950790739a8f0de03987687688413e5dc0107fb
Andriy <andriy@Air-Andriy.lan>
2021-02-17 00:54:10 +0000

    Fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,10 @@
 'use strict';
 
-const largeImg = document.querySelector('#largeImg');
+const largeImg = document.querySelector('#largeImg').attributes.src;
 const galleryList = document.querySelector('.gallery__list');
 
 galleryList.addEventListener('click', (event) => {
-    event.preventDefault();
-    largeImg.attributes.src.value = event.target.closest('a').attributes.href.value;
+  event.preventDefault();
 
+  largeImg.value = event.target.closest('a').attributes.href.value;
 });
