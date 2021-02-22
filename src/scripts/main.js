08e225d577b47cef9a332eff492592a1dabae092
Georgy Dzumenko <erterj05@gmail.com>
2021-02-22 18:45:29 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const list = document.querySelector('#thumbs');
+const mainImg = document.querySelector('#largeImg');
+
+const changeMainImg = (e) => {
+  const item = e.target.closest('.list-item__link');
+
+  if (!item) {
+    return;
+  }
+
+  mainImg.setAttribute('src', item.href);
+  e.preventDefault();
+};
+
+list.addEventListener('click', changeMainImg);
