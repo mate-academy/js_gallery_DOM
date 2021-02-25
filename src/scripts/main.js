32d55be338049a5cae88059fd9de38729ab3367d
Yevgeniy Podobreev <podobr_2018@ukr.net>
2021-02-25 23:35:00 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const list = document.querySelector('ul');
+const img = document.querySelector('img');
+
+list.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const item = e.target;
+
+  if (item.parentNode.href !== undefined) {
+    img.src = item.parentNode.href;
+  }
+});
