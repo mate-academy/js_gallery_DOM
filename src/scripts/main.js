d944fcacaae28181c922930f6fbd46c0622a4995
Bohdan Dobak <chudolipton@gmail.com>
2021-02-26 23:44:42 +0200

    made task gallery by using addEventListener and changing src of bigimage to element href

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const list = document.querySelector('ul');
+const bigImage = document.getElementById('largeImg');
+
+function changeImage(element) {
+  element.preventDefault();
+
+  const elementTarget = element.target.closest('a');
+
+  if (!elementTarget || !list.contains(elementTarget)) {
+    return;
+  }
+
+  bigImage.src = elementTarget.href;
+}
+
+list.addEventListener('click', changeImage);
