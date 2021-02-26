56f9f62f713a41b6b8db0228c61b315df43bb394
morozoleh <olegmoroz93@gmail.com>
2021-02-26 20:08:32 +0200

    develop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const largeImg = document.querySelector('#largeImg');
+const galleryList = document.querySelector('.gallery__list');
+
+const handler = function(e) {
+  e.preventDefault();
+
+  if (e.target.tagName === 'A') {
+    largeImg.src = e.target.href;
+  } else if (e.target.tagName === 'IMG') {
+    largeImg.src = e.path[1].href;
+  }
+};
+
+galleryList.addEventListener('click', handler);
