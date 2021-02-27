736ae7f2495e4d60aff203009c0b346300115e2f
Misha Lysak <myhaillysak@gmail.com>
2021-02-27 22:32:33 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const largeImg = document.querySelector('.gallery__large-img');
+const thumbs = document.querySelector('#thumbs');
+
+const changeBigImg = (element) => {
+  const target = element.target.closest('a');
+
+  element.preventDefault();
+
+  if (!target.closest('.list-item')) {
+    return;
+  }
+
+  largeImg.src = target.href;
+};
+
+thumbs.addEventListener('click', changeBigImg);
