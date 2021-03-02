522257f9018428ee38cdefb440f167f2e68fdc34
Taras <seman6745@gmail.com>
2021-03-02 14:17:52 +0200

    first

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const mainPicture = document.querySelector('#largeImg');
+const picturesList = document.querySelector('#thumbs');
+
+picturesList.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const item = e.target.closest('.list-item__link');
+
+  mainPicture.src = item.href;
+});
