fc89f0038463198e3c43eaf0f2d471d0b1759e70
Sofiia Asieieva <sofiia.asieieva@gmail.com>
2021-03-09 14:53:33 +0200

    created an addEventListener for the list

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const galleryImg = document.querySelector('.gallery__large-img');
+const list = document.querySelector('.gallery__list');
+
+list.addEventListener('click', () => {
+  const item = event.target.closest('.list-item__link');
+
+  event.preventDefault();
+  galleryImg.src = item.href;
+});
