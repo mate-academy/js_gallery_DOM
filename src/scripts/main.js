63a2d6e348c60e0f1446dbf7abe42c61a85f7b68
Ostap21 <57452764+Ostap21@users.noreply.github.com>
2021-03-09 22:17:23 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const ulElement = document.querySelector('.gallery__list');
+
+const mainImage = document.querySelector('#largeImg');
+
+// eslint-disable-next-line no-shadow
+ulElement.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  mainImage.setAttribute('src', event.target.parentElement.href);
+});
