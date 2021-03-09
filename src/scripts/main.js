91bbfaab8421fc2aec535e80d8abd6e1af0fb750
Olexandr Bieliaiev <Shurasava17@gmail.com>
2021-03-09 17:07:30 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const mainPicture = document.querySelector('.gallery__large-img');
+
+const imageNavigation = document.querySelector('.gallery__list');
+
+imageNavigation.addEventListener('click', (event1) => {
+  const clickedElement = event1.target.closest('.list-item__link');
+
+  event1.preventDefault();
+
+  if (!clickedElement) {
+    return 0;
+  }
+
+  mainPicture.src = clickedElement.href;
+});
