24b8aa808dd64ce2418fb5323a0aa4941f38164b
Tetiana Zotova <zotova.tetiana@gmail.com>
2021-03-10 23:52:41 +0200

    Suggestions applied

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,16 @@
 'use strict';
 
 const list = document.querySelector('#thumbs');
-const bigImageSrc = document.querySelector('#largeImg');
+const bigImageSource = document.querySelector('#largeImg');
 
 list.addEventListener('click', (eventClick) => {
   eventClick.preventDefault();
 
   const selectedImg = eventClick.target.closest('.list-item__link');
 
-  bigImageSrc.src = selectedImg.href;
+  if (!selectedImg) {
+    return;
+  }
+
+  bigImageSource.src = selectedImg.href;
 });
