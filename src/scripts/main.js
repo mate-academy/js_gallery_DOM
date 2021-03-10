1a9196db06b4b0577dc3c90824ff30cdbfa5d50e
Ivan Panchenko <ivanpanchenkorulit@gmail.com>
2021-03-10 11:53:04 +0200

    firstCommit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const largeImg = document.querySelector('#largeImg');
+const imgList = document.querySelector('#thumbs');
+
+imgList.addEventListener('click', (action) => {
+  action.preventDefault();
+
+  const imgLink = action.target.closest('.list-item__link');
+
+  if (!imgLink || imgList.contains(imgLink)) {
+    largeImg.src = imgLink.href;
+  }
+});
