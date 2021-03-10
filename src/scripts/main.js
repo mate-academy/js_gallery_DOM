46aae67cf262f48e9991e880124a7881aceedb2e
Olha Briukhanova <oaleksandrovva@gmail.com>
2021-03-10 20:41:57 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
+/* eslint-disable no-useless-return */
+/* eslint-disable no-shadow */
 'use strict';
+
+const largeImage = document.getElementById('largeImg');
+const galleryList = document.getElementById('thumbs');
+
+galleryList.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const link = event.target.closest('.list-item__link');
+
+  largeImage.src = link.href;
+});
