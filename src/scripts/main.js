8b84fa5d31fd6fd799a98801adda2d01132911ad
Olexandr Bieliaiev <Shurasava17@gmail.com>
2021-03-10 11:13:27 +0200

    edited with comments

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,13 +4,13 @@ const mainPicture = document.querySelector('.gallery__large-img');
 
 const imageNavigation = document.querySelector('.gallery__list');
 
-imageNavigation.addEventListener('click', (event1) => {
-  const clickedElement = event1.target.closest('.list-item__link');
+imageNavigation.addEventListener('click', (clickEvent) => {
+  const clickedElement = clickEvent.target.closest('.list-item__link');
 
-  event1.preventDefault();
+  clickEvent.preventDefault();
 
   if (!clickedElement) {
-    return 0;
+    return;
   }
 
   mainPicture.src = clickedElement.href;
