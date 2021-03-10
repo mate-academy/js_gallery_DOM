94b8dd52fbc17aeb509201ef2a18144258288cc2
Vitalii Skrynkovych <vitaliiskrynkovych@gmail.com>
2021-03-10 18:32:45 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const imagesList = document.body.querySelector('.gallery__list');
+const mainImg = document.body.querySelector('#largeImg');
+
+imagesList.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const closestElement = e.target.closest('.list-item__link');
+
+  mainImg.src = closestElement.href;
+});
