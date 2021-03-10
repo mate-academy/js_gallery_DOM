65fc7adf5108b283e2c4740a6fafdde664b9d33e
Владислав Сандык <nubia@MacBook-Pro-Vladislav.local>
2021-03-10 17:44:49 +0200

    added eventListener for list of gallery

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const galleryList = document.querySelector('.gallery__list');
+const mainPicture = document.querySelector('.gallery__img');
+
+galleryList.addEventListener('click', e => {
+  e.preventDefault();
+
+  const closest = e.target.closest('.list-item__link');
+
+  mainPicture.src = closest.href;
+});
