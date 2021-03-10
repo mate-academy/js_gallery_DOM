e258429d57cb3bce68bd090f4372838b07292c6f
Yurs Chabarenko <yurschabarenko@Yurss-MacBook-Pro.local>
2021-03-10 16:30:50 +0200

    first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const largeImage = document.querySelector('.gallery__large-img');
+const gallery = document.querySelector('.gallery__list');
+
+gallery.addEventListener('click', (clickEvent) => {
+  clickEvent.preventDefault();
+
+  const clickedImage = clickEvent.target.closest('.list-item__link');
+
+  if (!clickedImage) {
+    return;
+  }
+
+  largeImage.src = clickedImage.href;
+});
