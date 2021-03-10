2fdcf58624582d3e94d20544aaf52f8349ac0c2c
Sofiia Asieieva <sofiia.asieieva@gmail.com>
2021-03-10 11:17:53 +0200

    renamed the variable

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,11 @@
 'use strict';
 
-const galleryImg = document.querySelector('.gallery__large-img');
-const list = document.querySelector('.gallery__list');
+const mainImage = document.querySelector('.gallery__large-img');
+const imageNavigation = document.querySelector('.gallery__list');
 
-list.addEventListener('click', () => {
-  const item = event.target.closest('.list-item__link');
+imageNavigation.addEventListener('click', () => {
+  const selectedElement = event.target.closest('.list-item__link');
 
   event.preventDefault();
-  galleryImg.src = item.href;
+  mainImage.src = selectedElement.href;
 });
