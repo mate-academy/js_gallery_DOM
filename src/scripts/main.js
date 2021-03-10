70842f908377bdd50397812abbf78cd4f96d707c
anastasiia <anastasiia.aliinyk@gmail.com>
2021-03-10 11:56:56 +0200

    galerry

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const largeImage = document.getElementById('largeImg');
+const smallImages = document.querySelectorAll('.list-item__link');
+
+[...smallImages].forEach(link =>
+  link.addEventListener('click', (element) => {
+    element.preventDefault();
+    largeImage.src = link.href;
+  }));
