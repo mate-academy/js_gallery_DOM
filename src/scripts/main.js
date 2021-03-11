c9f442597ab2d36f3f35ec7961e67c08027b54d6
Anna <garsinaanna32778@gmail.com>
2021-03-11 15:19:30 +0200

    created gallery

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const mainImage = document.querySelector('#largeImg');
+const imagesList = document.querySelector('#thumbs');
+
+imagesList.addEventListener('click', () => {
+  event.preventDefault();
+
+  const newMainImage = event.target.closest('.list-item__link');
+
+  mainImage.src = newMainImage.href;
+});
