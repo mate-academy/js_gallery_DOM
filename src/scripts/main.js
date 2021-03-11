11223ae8d918a54ea51393bb66937e6676ff2d66
Artur Shpontak <ashpontak97@gmail.com>
2021-03-11 13:41:25 +0200

    Solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const list = document.querySelector('.gallery__list');
+const largeImg = document.getElementById('largeImg');
+
+list.addEventListener('click', (smallImg) => {
+  smallImg.preventDefault();
+
+  largeImg.src = smallImg.target.parentNode.href;
+});
