332df6eb18b3ef5526e52e7e3dff316318392bd6
Ihor Drul <druleger@gmail.com>
2021-03-24 20:31:06 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const imagesList = document.querySelector('#thumbs');
+const largeImg = document.querySelector('#largeImg');
+
+imagesList.addEventListener('click', (ev) => {
+  ev.preventDefault();
+
+  largeImg.setAttribute('src', ev.target.parentElement.getAttribute('href'));
+});
