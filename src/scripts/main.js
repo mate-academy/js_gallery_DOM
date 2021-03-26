bf15217c5080b45882dd8d95d02588f70a8ad686
Vlad Dezhniuk <vladdezhniuk@gmail.com>
2021-03-26 14:24:52 +0200

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,7 +6,7 @@ const largeImg = document.querySelector('#largeImg');
 gallery.addEventListener('click', (ev) => {
   ev.preventDefault();
 
-  const item = ev.target;
+  const item = ev.target.closest('.list-item__link');
 
-  largeImg.src = item.src;
+  largeImg.src = item.href;
 });
