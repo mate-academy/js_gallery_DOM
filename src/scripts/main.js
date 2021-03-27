bc4880c91ba485217767a218057f848f4ef209fe
Ruslan Kalyniak <krv241085krv@icloud.com>
2021-03-27 21:15:54 +0200

    test

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const listGallery = document.getElementById('thumbs');
+const largeImg = document.getElementById('largeImg');
+
+listGallery.onclick = function(click) {
+  click.preventDefault();
+  largeImg.src = click.target.closest('a');
+};
