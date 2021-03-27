4e487a6c5b98ae2d8abfb69bcf7227e3568856d1
Bohdan Prokopov <b.prokopov96@gmail.com>
2021-03-27 19:33:50 +0200

    solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const list = document.querySelector('#thumbs');
+const image = document.querySelector('#largeImg');
+
+function imageClick(eventInner) {
+  eventInner.preventDefault();
+
+  image.src = eventInner.target.closest('a');
+}
+
+list.addEventListener('click', imageClick);
