1f6dca04caf25fdc3dc7371b1a038761ef58c471
Ruslan Vasylyshyn <r.y.vasylyshyn@gmail.com>
2021-03-28 20:35:36 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const littleImg = document.getElementById('thumbs');
+const bigImg = document.getElementById('largeImg');
+
+littleImg.onclick = function(click) {
+  click.preventDefault();
+  bigImg.src = click.target.closest('a');
+};
