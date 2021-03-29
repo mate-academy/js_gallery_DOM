f42c20d3e5af43bcefb074836d2a5c76fae7bb99
Oleg Hluhk <glug.oleg@gmail.com>
2021-03-29 18:46:33 +0300

    fix comment

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,9 +3,10 @@
 const gallery = document.querySelector('.gallery__list');
 const image = document.querySelector('.gallery__large-img');
 
-gallery.addEventListener('click', (element) => {
-  const link = element.target.closest('.list-item__link');
+gallery.addEventListener('click', e => {
+  e.preventDefault();
 
-  element.preventDefault();
+  const link = e.target.closest('.list-item__link');
+
   image.src = link.href;
 });
