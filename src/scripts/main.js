58988a9400f914eb5cd62ee7fd53d0238f75e3fb
Maksym Matuliak <maksymmatuliak@gmail.com>
2021-03-30 18:59:14 +0300

    fixed if else

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,30 +5,20 @@ const gallery = document.getElementsByClassName('gallery')[0];
 gallery.addEventListener('click', (galleryEvent) => {
   galleryEvent.preventDefault();
 
-  let clickedImageSrc = null;
-
   if (galleryEvent.target.classList.contains('gallery__large-img')
     || galleryEvent.target.classList.contains('gallery')
     || galleryEvent.target.classList.contains('gallery__list')
-    || galleryEvent.target.classList.contains('gallery__img gallery__thumb')
-    || galleryEvent.target.classList.contains('list-item__link')) {
+    || galleryEvent.target.classList.contains('list-item__link')
+    || galleryEvent.target.tagName === 'DIV') {
     return;
   }
 
-  if (galleryEvent.target.parentElement.getAttribute('title') === 'Image 1') {
-    clickedImageSrc = '/images/landscape-first.png';
-  } else if (galleryEvent.target.parentElement.getAttribute('title')
-    === 'Image 2') {
-    clickedImageSrc = '/images/landscape-second.png';
-  } else if (galleryEvent.target.parentElement.getAttribute('title')
-    === 'Image 3') {
-    clickedImageSrc = '/images/landscape-third.png';
-  } else if (galleryEvent.target.parentElement.getAttribute('title')
-    === 'Image 4') {
-    clickedImageSrc = '/images/landscape-fourth.png';
-  } else if (galleryEvent.target.parentElement.getAttribute('title')
-    === 'Image 5') {
-    clickedImageSrc = '/images/landscape-fifth.png';
-  }
+  const clickedImageSrc = getSrcFromThumb(
+    galleryEvent.target.getAttribute('src'));
+
   document.getElementById('largeImg').setAttribute('src', clickedImageSrc);
 });
+
+function getSrcFromThumb(attribute) {
+  return 'images/' + attribute.replace('-thumb', '').split('.')[0] + '.png';
+}
