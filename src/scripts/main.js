c092d05fae6feb37fca805d77bec87ec9e8e0f47
Alex Fedorchenko <mralex.fed@gmail.com>
2021-04-07 19:53:42 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const largeImg = document.getElementById('largeImg');
+const minImg = document.querySelectorAll('a');
+const minImgArr = [...minImg];
+
+minImgArr.forEach((item, i) => {
+  minImg[i].addEventListener('click', (ev) => {
+    largeImg.src = item.getAttribute('href');
+    ev.preventDefault();
+  });
+});
