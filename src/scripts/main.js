d3e392d4ddc0e9d96e4c4d76456a781742c930d6
gennady.tabala <gennady.tabala@gmail.com>
2021-04-14 21:08:51 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const galleryList = document.querySelector('.gallery__list');
+const largeImage = document.querySelector('#largeImg');
+
+galleryList.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  if (e.target.tagName === 'IMG') {
+    largeImage.src = e.target.parentNode.href;
+  } else if (e.target.tagName === 'A') {
+    largeImage.src = e.target.href;
+  }
+});
