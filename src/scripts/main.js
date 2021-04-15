12269e871dbefdfc9219516d09829228211b7916
gennady.tabala <gennady.tabala@gmail.com>
2021-04-15 16:05:44 +0300

    works by `closest` now but tests is broken

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,9 +6,5 @@ const largeImage = document.querySelector('#largeImg');
 galleryList.addEventListener('click', (e) => {
   e.preventDefault();
 
-  if (e.target.tagName === 'IMG') {
-    largeImage.src = e.target.parentNode.href;
-  } else if (e.target.tagName === 'A') {
-    largeImage.src = e.target.href;
-  }
+  largeImage.src = e.target.closest('a').href;
 });
