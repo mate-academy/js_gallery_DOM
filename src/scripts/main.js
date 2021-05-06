800dd42e45811dd1034268c5b3c74ee46a7a90cf
Olga Vasylevskaya <olgavasylevskaya@ukr.net>
2021-05-06 17:27:07 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const largeImg = document.getElementById('largeImg');
+const list = document.getElementsByClassName('list-item');
+const links = document.getElementsByClassName('list-item__link');
+
+for (let i = 0; i < list.length; i++) {
+  [...list][i].addEventListener('click', (li) => {
+    li.preventDefault();
+
+    largeImg.setAttribute('src',
+      `http://localhost:8080${[...links][i].getAttribute('href')}`
+        .replace(/\\/, '/'));
+  });
+};
