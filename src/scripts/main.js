77493656581cc3373ca179542fc2251302c638bd
Kostrica <romiorw@ukr.net>
2021-05-06 21:38:13 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const gallery = document.querySelector('.gallery');
+const largeImg = document.querySelector('#largeImg');
+
+gallery.addEventListener('click', (occurrent) => {
+  occurrent.preventDefault();
+
+  const item = occurrent.target;
+
+  if (!item.classList.contains('list-item__link')
+  && !item.classList.contains('gallery__img')) {
+    return;
+  }
+
+  largeImg.src = item.src;
+});
