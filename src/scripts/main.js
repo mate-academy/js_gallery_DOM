a281304de366b1ec12c859cfe72f2d0fe6d76749
Ivan Major <tarstarcas@gmail.com>
2021-05-07 12:47:38 +0300

    Gallery

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const mainImage = document.querySelector('#largeImg');
+
+const list = document.querySelector('.gallery__list');
+
+list.addEventListener('click', (happening) => {
+  const item = happening.target.closest('a');
+
+  if (!item) {
+    return;
+  };
+
+  event.preventDefault();
+
+  mainImage.src = item.href;
+  mainImage.alt = item.title;
+});
