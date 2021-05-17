1d6ae784c2f13aa51fbdd1e9a6695d8f7e097252
romandubovskyi <rdubowskij@gmail.com>
2021-05-17 17:41:52 +0300

    added main frame change on click

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const pictureList = document.getElementById('thumbs');
+const mainPicture = document.getElementById('largeImg');
+
+pictureList.addEventListener('click', (eventTrigger) => {
+  eventTrigger.preventDefault();
+  mainPicture.src = `${eventTrigger.target.parentNode.href}`;
+});
