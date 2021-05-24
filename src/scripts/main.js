4011a6c8b3239b0380314355e3588c3891c9ceba
Kirill Osypov <Kirill.Osypov@gmail.com>
2021-05-24 17:24:05 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const largeImg = document.querySelector('#largeImg');
+const list = document.querySelector('.gallery__list');
+
+list.addEventListener('click', (act) => {
+  act.preventDefault();
+
+  if (act.target.classList.contains('gallery__img')) {
+    largeImg.src = act.target.src;
+  } else if (act.target.classList.contains('list-item__link')) {
+    largeImg.src = act.target.href;
+  }
+});
