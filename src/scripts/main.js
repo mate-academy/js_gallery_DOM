916ad86f261cc48e90ead87cf357d78ac8380a08
Kirill Osypov <Kirill.Osypov@gmail.com>
2021-05-25 11:23:22 +0300

    Fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,12 +3,7 @@
 const largeImg = document.querySelector('#largeImg');
 const list = document.querySelector('.gallery__list');
 
-list.addEventListener('click', (act) => {
-  act.preventDefault();
-
-  if (act.target.classList.contains('gallery__img')) {
-    largeImg.src = act.target.src;
-  } else if (act.target.classList.contains('list-item__link')) {
-    largeImg.src = act.target.href;
-  }
+list.addEventListener('click', (e) => {
+  e.preventDefault();
+  largeImg.src = e.target.closest('.list-item__link').href;
 });
