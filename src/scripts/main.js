8e7a824cc3ab8e463290b1b1a5d68a33a863e67c
VasylPerepichai <vperepichay@gmail.com>
2021-06-14 09:20:44 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const ul = document.querySelector('.gallery__list');
+const largeImg = document.getElementById('largeImg');
+
+ul.onclick = function(events) {
+  const target = events.target.closest('a');
+
+  if (!target) {
+    return;
+  }
+  largeImg.src = target.href;
+  events.preventDefault();
+};
