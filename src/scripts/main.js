125799756feccefe67f59d85258faaf0c3106c3c
Tolik <tolikdolinskiy@gmail.com>
2021-06-22 11:49:27 +0300

    some message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const listImg = document.querySelector('.gallery__list');
+const bigImg = document.querySelector('.gallery__large-img');
+const defaultSrc = bigImg.src;
+
+listImg.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  if (e.target.classList.contains('gallery__img')) {
+    bigImg.src = e.target.src;
+  } else {
+    bigImg.src = defaultSrc;
+  }
+});
