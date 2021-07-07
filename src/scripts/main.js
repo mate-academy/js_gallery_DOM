16661b747edfb9d207018c462ef299a56144669d
Andriy Fesych <andriyhimself@gmail.com>
2021-07-07 21:43:09 +0300

    Solution_0.2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,8 @@
 'use strict';
 
-const smallPics = Array.from(document.querySelectorAll('li'));
-const largeImg = document.querySelector('#largeImg');
-
-smallPics.forEach(li => {
+Array.from(document.querySelectorAll('li')).forEach(li => {
   li.addEventListener('click', (action) => {
     action.preventDefault();
-    largeImg.src = li.querySelector('a').href;
+    document.querySelector('#largeImg').src = li.querySelector('a').href;
   });
 });
