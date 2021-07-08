0221967fb9a497a800e2683c6bf0c9f18a4d9706
korzh <korzh.alexey@yahoo.com>
2021-07-08 14:43:06 +0300

    solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const bigImage = document.querySelector('.gallery__large-img');
+const images = document.querySelectorAll('.gallery__thumb');
+
+for (const image of images) {
+  image.addEventListener('click', (ev) => {
+    ev.preventDefault();
+
+    bigImage.src = image.src;
+  });
+}
