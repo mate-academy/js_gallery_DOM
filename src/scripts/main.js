d4a0d4e25f1878126322f5f0a01833f8862dd502
John <jozaman96@gmail.com>
2021-07-09 12:52:59 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const imageList = document.querySelector('#thumbs');
+const frontImg = document.querySelector('#largeImg');
+
+imageList.addEventListener('click', (eventObj) => {
+  eventObj.preventDefault();
+
+  const targetImgLink = eventObj.target.closest('a');
+
+  frontImg.setAttribute('src', targetImgLink.href);
+});
