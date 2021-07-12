0b40ccdaae364537544b39e9f1852acd3105caad
David Dohru <tronsmartmy@gmail.com>
2021-07-13 00:04:40 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const allGalleryImages = [ ...document.querySelectorAll('.gallery__img') ];
+const allImagesLinks = [ ...document.querySelectorAll('.list-item__link') ];
+const mainImages = document.querySelector('#largeImg');
+
+allGalleryImages.map(value => {
+  value.addEventListener('mouseup', (occasion) => {
+    value.classList.add('gallery__large-img');
+
+    if (occasion.target.src !== undefined) {
+      allImagesLinks.map(item => item.addEventListener('click', () => {
+        mainImages.src = item.href;
+      }));
+    }
+  });
+});
+
+[ ...document.querySelectorAll('.list-item__link') ].map(item => {
+  item.addEventListener('click', (occasion) => occasion.preventDefault());
+});
