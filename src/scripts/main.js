bf2306c62eb69dd0f6a3c6d60821d76b07d2bf72
Oleksandr Saulko <aleksandrsaulko@gmail.com>
2021-07-29 21:31:27 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const thumbs = document.querySelector('#thumbs');
+const rootElement = document.querySelector('.gallery').children[0].children[0];
+
+thumbs.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const el = e.target.parentElement.pathname
+    || e.target.pathname;
+
+  rootElement.src = 'http://localhost:8080' + el;
+});
