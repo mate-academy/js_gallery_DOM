d9a7e47900331ce7e3fe70acee6461ab4c66ca60
Oleksandr Saulko <aleksandrsaulko@gmail.com>
2021-07-29 22:20:51 +0300

    Galery bug fixing

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,12 +2,18 @@
 
 const thumbs = document.querySelector('#thumbs');
 const rootElement = document.querySelector('.gallery').children[0].children[0];
+// console.dir(rootElement);
 
 thumbs.addEventListener('click', (e) => {
   e.preventDefault();
+  // console.dir(e.target.parentElement);
 
   const el = e.target.parentElement.pathname
     || e.target.pathname;
 
-  rootElement.src = 'http://localhost:8080' + el;
+  const imageSrc = el.split('/').reverse()[0];
+  const tempSrc = rootElement.src.split('/');
+
+  tempSrc[tempSrc.length - 1] = imageSrc;
+  rootElement.src = tempSrc.join('/');
 });
