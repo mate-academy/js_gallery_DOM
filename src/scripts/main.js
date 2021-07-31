163149ee5d6bfcbd0696758e271bdfbfea9dbb74
Oleksandr Saulko <aleksandrsaulko@gmail.com>
2021-07-31 14:46:51 +0300

    Fixes after code review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,19 +1,13 @@
 'use strict';
 
 const thumbs = document.querySelector('#thumbs');
-const rootElement = document.querySelector('.gallery').children[0].children[0];
-// console.dir(rootElement);
+const rootElement = document.querySelector('#largeImg');
 
 thumbs.addEventListener('click', (e) => {
   e.preventDefault();
-  // console.dir(e.target.parentElement);
 
-  const el = e.target.parentElement.pathname
-    || e.target.pathname;
+  const el = e.target.parentElement.href
+    || e.target.href;
 
-  const imageSrc = el.split('/').reverse()[0];
-  const tempSrc = rootElement.src.split('/');
-
-  tempSrc[tempSrc.length - 1] = imageSrc;
-  rootElement.src = tempSrc.join('/');
+  rootElement.src = el;
 });
