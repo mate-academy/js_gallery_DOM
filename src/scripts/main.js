1ad34bd1a0fd566a558362ca87385bcfa6c331e7
Kyrylo Pyniv <pynivkm@gmail.com>
2021-08-14 20:20:06 +0300

    sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const galleryList = document.querySelector('.gallery__list');
+
+const largeImage = document.querySelector('.gallery__large-img');
+
+galleryList.addEventListener('click', (ev) => {
+  const item = ev.target.closest('.list-item__link');
+
+  ev.preventDefault();
+
+  if (!item || !galleryList.contains(item)) {
+    return false;
+  };
+
+  largeImage.src = item.href;
+});
