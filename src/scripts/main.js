75277ca28442122f7bb21ff4e63798b4778e66e3
MaximTsyhanenko <maksym.tsyganenko85@gmail.com>
2021-08-16 21:55:29 +0300

    solution v.1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const bigImage = document.querySelector('#largeImg');
+
+const links = document.querySelectorAll('.list-item__link');
+
+for (const elem of links) {
+  // eslint-disable-next-line no-shadow
+  elem.addEventListener('click', (event) => {
+    event.preventDefault();
+    bigImage.src = elem.href;
+  });
+}
