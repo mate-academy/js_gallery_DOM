ab21b75f691ea6ab33e0e6b3d5262d4d7abd2fc4
MaximTsyhanenko <maksym.tsyganenko85@gmail.com>
2021-08-17 18:28:33 +0300

    solution v.3

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,6 +7,10 @@ const galleryList = document.querySelector('.gallery__list');
 galleryList.addEventListener('click', (e) => {
   e.preventDefault();
 
+  if (e.target.classList.contains('list-item')) {
+    return;
+  }
+
   const element = e.target.closest('.list-item__link');
 
   bigImage.src = element.href;
