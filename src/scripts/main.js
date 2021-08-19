b8a53f41816f78278c09c7f3315b2f9d4d0d7cba
Svyatoslav Victor <victorsvyatoslav@gmail.com>
2021-08-19 10:27:22 +0300

    Task Done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const imageList = document.querySelector('.gallery__list');
+const largeImage = document.querySelector('#largeImg');
+const links = document.querySelectorAll('.list-item__link');
+
+for (const link of links) {
+  link.addEventListener('click', (event) => {
+    event.preventDefault();
+  });
+}
+
+imageList.addEventListener('click', (event) => {
+  const image = event.target.closest('.gallery__img');
+
+  largeImage.src = image.parentElement.href;
+});
