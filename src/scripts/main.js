bd59d86ae0bfdd1a19e9dd69da066b226c5b6804
Olga Bordiug <obordiug.gmail.com>
2021-08-22 17:16:27 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const list = document.querySelector('.gallery__list');
+const largeImg = document.querySelector('#largeImg');
+
+list.addEventListener('click', (e) => {
+  const itemLink = e.target.closest('.list-item__link');
+
+  if (!itemLink || !list.contains(itemLink)) {
+    return;
+  }
+  largeImg.src = itemLink.href;
+  e.preventDefault();
+});
