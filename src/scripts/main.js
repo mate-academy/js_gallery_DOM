43979b90100aa1a9d30f68b7f7ab5432b7f3dc89
Alexandr Somov <aleksandr.somov2011@gmail.com>
2021-08-23 22:50:52 +0300

    'solution'

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const list = document.querySelector('.gallery__list');
+const bigImg = document.querySelector('.gallery__large-img');
+
+list.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const imageLink = e.target.closest('.list-item__link');
+
+  bigImg.src = imageLink.href;
+}
+);
