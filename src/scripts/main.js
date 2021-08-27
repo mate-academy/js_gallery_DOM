95a8ddb94fd3a6a3248ccf4aa3f66883b7d50570
Roman Minchenko <minchenko.poma@gmail.com>
2021-08-27 18:17:04 +0300

    gallery-DOM-done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const img = document.querySelectorAll('.list-item');
+const largeImg = document.getElementById('largeImg');
+
+for (let i = 0; i < [...img].length; i++) {
+  img[i].addEventListener('click', () => {
+    const item = event.target.closest('.list-item__link');
+
+    event.preventDefault();
+
+    if (!item.classList.contains('list-item__link')) {
+      return;
+    }
+
+    largeImg.src = item.href;
+  });
+};
