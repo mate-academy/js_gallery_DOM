5bda2eff22b915c868303512ae7b138db8a595e9
Viktormaz <vitecc91@gmail.com>
2021-08-31 17:01:25 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const mainImg = document.getElementById('largeImg');
+const images = document.querySelectorAll('.gallery__img');
+
+for (const item of images) {
+  item.addEventListener('click', (elem) => {
+    elem.preventDefault();
+
+    mainImg.src = item.src;
+  });
+};
