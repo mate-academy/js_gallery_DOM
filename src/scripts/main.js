775f282fc4e19646b48dbe43138a67f8a3ccce72
westone <west0ne@MacBook-Pro-Oleg.local>
2021-09-09 18:28:06 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const images = document.querySelectorAll('.gallery__img');
+const mainImage = document.getElementById('largeImg');
+
+for (const image of images) {
+  image.addEventListener('click', () => {
+    event.preventDefault();
+    mainImage.src = image.src;
+  });
+};
