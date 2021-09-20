8040f7d34c37b6bca6324856869b94ef9b7d0aeb
Tykhon Shuliak <tiandzh2013@gmail.com>
2021-09-20 23:41:48 +0300

    Sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const images = document.querySelectorAll('.gallery__img');
+const bigImage = document.getElementById('largeImg');
+
+for (const image of images) {
+  image.addEventListener('click', () => {
+    event.preventDefault();
+    bigImage.src = image.src;
+  });
+}
