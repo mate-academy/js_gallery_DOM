c0022fd5c5f971074269724086287e50d23d9ba6
MaxStets <weawer458@gmail.com>
2021-09-30 15:24:19 +0300

    Solution -- 'gallery_DOM'

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const thumbs = document.querySelector('#thumbs');
+const mainImage = document.querySelector('#largeImg');
+
+thumbs.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  if (e.target.tagName === 'IMG') {
+    const linkImg = e.target.getAttribute('src');
+
+    mainImage.setAttribute('src', linkImg);
+  };
+});
