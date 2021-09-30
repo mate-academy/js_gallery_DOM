ee67f8a340603b26a4fa2e2f38b5027c18609c73
Roman Mefokov <meforoma@gmail.com>
2021-09-30 14:57:17 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const largeImg = document.querySelector('#largeImg');
+const ul = document.querySelector('#thumbs');
+
+ul.onclick = (innerEvent) => {
+  innerEvent.preventDefault();
+};
+
+const thumbToMainOnClick = (innerEvent) => {
+  const target = innerEvent.target.closest('.list-item__link');
+
+  if (!target || !ul.contains(target)) {
+    return;
+  }
+
+  largeImg.src = target.href;
+};
+
+ul.addEventListener('click', thumbToMainOnClick);
