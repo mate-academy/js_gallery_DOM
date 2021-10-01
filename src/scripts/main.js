97c64f9168b769c5a5df276d9c8ef84319bb79f7
bohdan_kochetov <bodja1208@gmail.com>
2021-10-01 15:19:56 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const largeImg = document.getElementById('largeImg');
+const gallery = document.querySelector('ul');
+
+gallery.addEventListener('click', (ev) => {
+  ev.preventDefault();
+  largeImg.src = ev.target.parentNode.href;
+});
