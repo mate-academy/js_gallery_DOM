f29475b41a2739826de8ef21a69dfe1f34b1996c
Oleh <chepil742@gmail.com>
2021-10-09 09:10:11 +0300

    new

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,6 +6,6 @@ const mainImage = document.getElementById('largeImg');
 for (const image of images) {
   image.addEventListener('click', () => {
     event.preventDefault();
-    mainImage.src = image.src;
+    mainImage.src = image.href;
   });
 };
