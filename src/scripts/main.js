4a33ad809ad4103b461d1317871c36f708f9545f
Dima Narozhnyi <dmitriy.narozhnyi@gmail.com>
2021-10-18 12:03:20 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const handler = function(e) {
+  const thumb = e.target.closest('a');
+
+  if (!thumb) {
+    return;
+  }
+
+  e.preventDefault();
+  mainImage.src = thumb.href;
+};
+const galleryList = document.getElementById('thumbs');
+const mainImage = document.getElementById('largeImg');
+
+galleryList.addEventListener('click', handler);
