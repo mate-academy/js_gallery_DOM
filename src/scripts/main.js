775ce36fe287f4f2c4cd4412d561ef906a4dca99
Dmitriy Hrytsak <hrytsakmail@gmail.com>
2021-11-15 15:18:08 +0200

    Create interactive gallery

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,21 @@
 'use strict';
+
+function changeMainImage(photoLink) {
+  const largePhoto = document.querySelector('#largeImg');
+  const newLargePhotoHref = photoLink.getAttribute('href');
+
+  largePhoto.setAttribute('src', newLargePhotoHref);
+}
+
+const thumbs = document.querySelector('#thumbs');
+
+thumbs.addEventListener('click', (event) => {
+  const photoLink = event.target.closest('.list-item__link');
+
+  if (!photoLink || !thumbs.contains(photoLink)) {
+    return;
+  }
+
+  event.preventDefault();
+  changeMainImage(photoLink);
+});
