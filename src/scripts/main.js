5d570cc7172cb9e8974223efec323dbae54426a4
Yuriy Titov <yuranus@gmail.com>
2021-11-17 03:19:19 +0200

    feat: solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
+
 'use strict';
+
+const thumbs = document.body.querySelector('#thumbs');
+
+const clickHandler = (e) => {
+  e.preventDefault();
+
+  const tag = e.target.tagName;
+
+  if (tag === 'IMG' || tag === 'A') {
+    const link = e.target.closest('.list-item__link');
+
+    if (thumbs.contains(link)) {
+      const largeImg = document.body.querySelector('#largeImg');
+
+      largeImg.src = link.href;
+    }
+  }
+};
+
+thumbs.addEventListener('click', clickHandler);
