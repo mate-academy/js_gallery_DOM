8e411fe3ac56b19669b2cced74bf9bba82dae2c1
Dariia Romanova <dariia.romanova@gmail.com>
2021-11-23 14:15:59 +0200

    Gallery

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const gallery = document.querySelector('.gallery');
+const largeImage = document.querySelector('.gallery__large-img');
+
+gallery.addEventListener('click', (clickEvent) => {
+  const item = clickEvent.target.closest('.list-item__link');
+
+  if (!item) {
+    return;
+  }
+
+  event.preventDefault();
+
+  largeImage.src = item.href;
+});
