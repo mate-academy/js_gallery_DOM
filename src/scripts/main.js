23474321149006c1156a949a1ec58d5f24c64a9f
Andrey Grigoriev <nkgrand25@gmail.com>
2021-11-26 23:43:26 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const bigImg = document.querySelector('#largeImg');
+const thumbs = document.querySelector('#thumbs');
+
+function changeImage(click) {
+  const img = click.target;
+
+  click.preventDefault();
+  bigImg.src = img.parentElement.href;
+}
+
+thumbs.addEventListener('click', changeImage);
