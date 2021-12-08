4896aa2d3bbfecfa0a5e9a31255a9b02c165ae94
Anastasiia Afanasieva <anastasiiaafanasieva@Anastasiias-MacBook-Air.local>
2021-12-08 17:05:20 +0200

    Solution js_gallery_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const list = document.querySelector('.gallery__list');
+
+list.addEventListener('click', (action) => {
+  action.preventDefault();
+
+  const item = action.target.closest('.list-item__link');
+
+  const mainImage = document.querySelector('#largeImg');
+
+  mainImage.src = item.href;
+});
