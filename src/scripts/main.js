09d1a57c8e4cbe78374c372f1e1839bc046bd661
Vasiliy <voronvasia@gmail.com>
2021-12-12 10:57:27 +0200

    complite

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const bigImg = document.querySelector('#largeImg');
+const galleryList = document.querySelector('#thumbs');
+
+galleryList.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const item = e.target.closest('.gallery__img');
+
+  if (!item || !galleryList.contains(item)) {
+    return;
+  }
+
+  bigImg.src = item.parentElement.href;
+});
