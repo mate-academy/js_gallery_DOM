2696aa0abf4d75e722a1a4220902d72197360e13
Oleksandr Leshchenko <bulochkin72@gmail.com>
2021-12-20 15:38:06 +0200

    job done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const largeImage = document.querySelector('#largeImg');
+const list = document.querySelector('#thumbs');
+
+list.addEventListener('click', (event) => {
+  event.preventDefault();
+  largeImage.src = event.target.parentElement.href;
+});
