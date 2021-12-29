fdd3478fe148313d1538bc6be54480dcea8db9f3
Виталий Пасечник <47535101+VitalyPasechnik@users.noreply.github.com>
2021-12-29 15:24:24 +0200

    Update main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,5 +5,5 @@ const bigImg = document.querySelector('#largeImg');
 
 a.addEventListener('click', (e) => {
   e.preventDefault();
-  bigImg.src = e.target.src;
+  bigImg.src = e.target.parentElement.href;
 });
