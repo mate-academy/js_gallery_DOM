0be58a895911fd8665c1bacbd1384343adea0a03
Pavlo Khashchevskyi <PikiMapo92@gmail.com>
2022-01-10 18:00:36 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const gallery = document.querySelector('.gallery__list');
+const banner = document.querySelector('#largeImg');
+
+gallery.addEventListener('click', (eventObj) => {
+  eventObj.preventDefault();
+
+  const item = eventObj.target.closest('.list-item__link');
+
+  banner.src = item.href;
+});
