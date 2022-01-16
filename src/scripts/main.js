4bbfa41c780285bc83f19f71a56b988965c4f753
Klets Serhii <sergklets75@gmail.com>
2022-01-16 11:37:20 +0200

    Solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const div = document.querySelector('.gallery');
+const frame = div.firstElementChild.querySelector('img');
+const list = div.querySelector('.gallery__list');
+let sourceOfBigImg = '';
+
+list.addEventListener('click', (ev) => {
+  const item = ev.target;
+
+  if (item.tagName === 'A') {
+    item.preventDefault();
+    sourceOfBigImg = item.href;
+  } else {
+    const a = item.closest('a');
+
+    sourceOfBigImg = a.href;
+    ev.preventDefault();
+  }
+
+  frame.src = sourceOfBigImg;
+});
