66ad651766edc2e86fdd42b77312a41c4e4ad624
Ladyha Roman <ladrom.ua@gmail.com>
2022-02-02 12:15:32 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const gallery = document.querySelector('.gallery');
+const thumbs = document.querySelector('#thumbs');
+
+thumbs.addEventListener('click', (evt) => {
+  evt.preventDefault();
+
+  const picture = evt.target.closest('.list-item');
+
+  if (!picture) {
+    return;
+  }
+
+  gallery.children[0].children[0].src = picture.children[0].href;
+});
