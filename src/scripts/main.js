281c79f0a264d036955d85a6e802ee0a2ce36ee3
Ivan Shulhan <92580512+IvanShulhan@users.noreply.github.com>
2022-02-03 14:40:34 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const largeImg = document.querySelector('#largeImg');
+const imageList = document.querySelector('#thumbs');
+
+function imgChanger(eventObject) {
+  eventObject.preventDefault();
+
+  const link = eventObject.target.closest('.list-item__link');
+
+  if (!link || !imageList.contains(link)) {
+    return;
+  };
+
+  largeImg.src = link.getAttribute('href');
+}
+
+imageList.addEventListener('click', imgChanger);
