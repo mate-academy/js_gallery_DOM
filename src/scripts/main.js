68d9e2dc18fa85f287fbee11b121adf36c2c3b62
Oleh Rak <rakole86@gmail.com>
2022-02-08 14:58:47 +0200

    gallery_dom

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const list = document.querySelector('.gallery__list');
+const large = document.getElementById('largeImg');
+
+// eslint-disable-next-line no-shadow
+list.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  // const imgLinc = event.target.parentElement.getAttribute('href');
+
+  // large.src = imgLinc;
+  const el = event.target.closest('.list-item__link');
+
+  if (!el) {
+    return;
+  }
+  large.src = el.href;
+});
