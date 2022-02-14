3a2ef7fdac5104af94cfd768dcdfad12de7137c6
Roman Kovalchuk <roma220702@gmail.com>
2022-02-14 18:00:28 +0300

    some message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const list = document.querySelector('.gallery__list');
+const img = document.querySelector('.gallery__large-img');
+
+list.addEventListener('click', (e) => {
+  const item = e.target.closest('.gallery__img');
+
+  if (!item || !list.contains(item)) {
+    return;
+  }
+
+  img.src = item.src;
+});
