e12bf110a08bb36121597ac32a2288da4a60a5b6
KristinaTar <kristina.tarashchuk@gmail.com>
2022-02-21 22:12:37 +0200

    firt commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const largeImage = document.getElementById('largeImg');
+const smallImages = document.getElementById('thumbs');
+
+smallImages.onclick = (e) => {
+  e.preventDefault();
+
+  if (!e.target.closest('a')) {
+    return;
+  };
+
+  largeImage.src = e.target.closest('a').href;
+};
