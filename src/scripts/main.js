b7a82dc1cceb4c6dd0888d2571ebcf9882fb3b86
Dima Cherednyk <dicherednyk@gmail.com>
2022-03-02 10:14:01 +0200

    some message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const list = document.querySelector('.gallery__list');
+const imgL = document.getElementById('largeImg');
+
+list.addEventListener('click', (events) => {
+  events.preventDefault();
+
+  const imgChange = events.target.closest('.list-item__link');
+
+  imgL.src = imgChange.href;
+});
