2fa9c3d2eb2238e004293cdaa46d16d91bf14b10
Mykhailo Molnar <mmolnar@atlassian.com>
2022-03-05 13:40:56 +0200

    image changing works

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const list = document.querySelector('.gallery__list');
+const mainImage = document.querySelector('#largeImg');
+
+list.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const item = event.target.closest('.list-item__link');
+
+  if (!item) {
+    return;
+  }
+
+  mainImage.src = item.href;
+})
