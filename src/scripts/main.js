37810fa90dc500a08540c3b9f1e63621fb232820
Vladyslav317 <you@example.com>
2022-03-09 16:37:15 +0200

    JS_gallery_dom

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const largeImg = document.querySelector('.gallery__large-img');
+const gallery = document.querySelector('.gallery__list');
+
+gallery.addEventListener('click', events => {
+  events.preventDefault();
+
+  const image = events.target.closest('.list-item__link').href;
+
+  largeImg.src = image;
+});
