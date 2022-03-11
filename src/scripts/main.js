f4f31d3ff37da84602018e8c8b84b366e40df73b
Дима Краснобокий <krasnoboky.dima@gmail.com>
2022-03-11 15:46:00 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const allImg = document.querySelector('#thumbs');
+const selectImg = document.querySelector('#largeImg');
+
+allImg.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  selectImg.src = e.target.src;
+});
