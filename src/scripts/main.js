6046f51b5c32fefd1f2eb4bb430cc08703580f0b
Misha Ivchenko <ivchenkomykhail@meta.ua>
2022-03-14 20:48:11 +0200

    The draft of the solution was created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const currentMainImage = document.getElementById('largeImg');
+
+let targetImage;
+
+const eventElem = document.getElementById('thumbs');
+
+alert(currentMainImage.src);
+
+eventElem.addEventListener('click', function(ourEvent) {
+  targetImage = ourEvent.target;
+  currentMainImage.src = targetImage.src;
+});
