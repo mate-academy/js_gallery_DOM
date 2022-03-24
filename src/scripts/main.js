ad694602bca8637267013103f222dead7e9d37e8
Dmytro Bychkivskyi <dbychkivskyi@cccis.com>
2022-03-24 14:57:05 +0200

    Finish hw.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const list = document.querySelector('.gallery__list');
+const largeImage = document.querySelector('.gallery__large-img');
+
+list.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const item = e.target.closest('.list-item__link');
+
+  if (!item) {
+    return;
+  }
+
+  largeImage.src = item.href;
+});
