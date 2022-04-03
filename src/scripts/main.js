2c2e1d53b2af84bc19467118dda896903f11860c
Maria Luschik <marialyschik@Marias-MacBook-Air.local>
2022-04-03 16:47:13 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const largeImg = document.querySelector('.gallery__large-img');
+const list = document.querySelector('.gallery__list');
+
+list.addEventListener('click', (action) => {
+  action.preventDefault();
+
+  let img = action.target.closest('.list-item__link');
+
+  if(!img) return;
+
+  largeImg.src = img.href;
+});
+
