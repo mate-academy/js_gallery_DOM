20668c7ea9ee7379ed8eee198426ee152853251d
Batick3737 <antonandriy@ukr.net>
2022-04-05 14:58:44 +0300

    write code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const bigPicture = document.getElementById('largeImg');
+
+const list = document.getElementById('thumbs');
+
+list.addEventListener('click', (events) => {
+  events.preventDefault();
+
+  const img = events.target.closest('.list-item__link');
+
+  bigPicture.src = img.href;
+});
