2fa895ff80438d0a2efde93151115202ae8faf60
Yevgeniy Savchuk <savchukevgeniy1@gmail.com>
2022-04-06 12:53:04 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const mainImg = document.querySelector('#largeImg');
+const listOfImg = document.getElementById('thumbs');
+
+listOfImg.addEventListener('click', (events) => {
+  events.preventDefault();
+
+  const link = events.target.closest('a');
+
+  mainImg.src = link.href;
+});
