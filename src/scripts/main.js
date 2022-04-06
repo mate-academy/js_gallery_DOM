11f9e6ad460bdeda32bbe510cf371402a7264821
Hlib Schebalkov <glebusha228@gmail.com>
2022-04-06 22:26:41 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const list = document.querySelector('.gallery__list');
+
+list.addEventListener('click', (even) => {
+  even.preventDefault();
+
+  const item = even.target;
+
+  document.getElementById('largeImg').src = item.src;
+});
