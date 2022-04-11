4022b2b3e419c21a241a1410881c4b651995f163
Avdyeyenko Vitalik <dalshe.budet.sobaka@gmail.com>
2022-04-11 17:37:33 +0300

    Image switching implemented

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const thumbs = document.querySelector('#thumbs');
+const thumbLinks = thumbs.querySelectorAll('a');
+const imageMain = document.querySelector('#largeImg');
+
+for (const thumbLink of thumbLinks) {
+  thumbLink.addEventListener('click', (e) => {
+    const imageSrc = e.currentTarget.href;
+
+    imageMain.src = imageSrc;
+    e.preventDefault();
+  });
+}
