46444a067b91009496b6ab24a00c2630d42dd750
Hlib Schebalkov <glebusha228@gmail.com>
2022-04-12 20:56:43 +0300

    delete for use closest

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,16 +2,11 @@
 
 const list = document.querySelector('.gallery__list');
 const mainImg = document.getElementById('largeImg');
-const link = document.querySelectorAll('.list-item__link');
 
 list.addEventListener('click', (event) => {
   event.preventDefault();
 
   const item = event.target;
 
-  for (let i = 0; i < link.length; i++) {
-    if (link[i].contains(item)) {
-      mainImg.src = link[i].href;
-    }
-  }
+  mainImg.src = item.closest('.list-item__link').href;
 });
