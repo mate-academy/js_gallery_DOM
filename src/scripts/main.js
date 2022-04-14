ff61358073dbe8629747885fbc8e32d3d5b8030a
Denys Lipin <lipin.transstream@gmail.com>
2022-04-14 19:44:01 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const list = document.querySelector('#thumbs');
+const largeImg = document.querySelector('#largeImg');
+
+list.addEventListener('click', (meEvent) => {
+  meEvent.preventDefault();
+
+  const item = meEvent.target;
+
+  largeImg.src = item.parentElement.href;
+});
