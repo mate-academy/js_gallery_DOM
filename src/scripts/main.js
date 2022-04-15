3335a3a58bd2e3895eb01d3057f3f41c708e8c4d
Maksym Yaremenko <restartermord123@ukr.net>
2022-04-15 18:45:36 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,41 @@
 'use strict';
+
+const largeImage = document.getElementById('largeImg');
+const imageList = document.querySelector('.gallery__list');
+
+imageList.addEventListener('click', (ev) => {
+  ev.preventDefault();
+
+  const item = ev.target;
+
+  if (item.classList.contains('list-item__link')) {
+    return;
+  }
+
+  let newLink = '/images/landscape-';
+  const targetNumber = item.closest('.list-item__link').title;
+
+  switch (targetNumber) {
+    case 'Image 1':
+      newLink += 'first.png';
+      break;
+
+    case 'Image 2':
+      newLink += 'second.png';
+      break;
+
+    case 'Image 3':
+      newLink += 'third.png';
+      break;
+
+    case 'Image 4':
+      newLink += 'fourth.png';
+      break;
+
+    case 'Image 5':
+      newLink += 'fifth.png';
+      break;
+  }
+
+  largeImage.setAttribute('src', newLink);
+});
