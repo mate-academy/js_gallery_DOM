32b9d270b06f83aa5ffdfb64d1dae16ff86af980
Max Shylenko <maxshylenko@gmail.com>
2022-04-19 21:58:23 +0200

    gallery DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
+/* eslint-disable no-undef */
 'use strict';
+
+const listOfImg = document.querySelector('#thumbs');
+const mainImg = document.querySelector('#largeImg');
+
+listOfImg.addEventListener('click', (replace) => {
+  replace.preventDefault();
+
+  if (!(replace.target.matches('.list-item__link'))
+    && !(replace.target.matches('.gallery__img'))) {
+    return;
+  }
+
+  mainImg.src = `${replace.target.parentElement.href}`;
+});
