d476a23ba4f7fdaea32d3f3cddeb76e20ad1360d
Alex Nastychenko <nastychenko1993@gmail.com>
2022-04-26 12:21:05 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const largeImg = document.getElementById('largeImg');
+const list = document.querySelector('.gallery__list');
+
+list.addEventListener('click', (e) => {
+  const item = e.target.closest('.list-item');
+
+  if (!item) {
+    return;
+  };
+
+  e.preventDefault();
+
+  largeImg.src = item.children[0].href;
+});
