c22370221800902c327787a371026fd70592cefe
Bohdan Dubravin <dubravin111@gmail.com>
2022-05-07 10:06:44 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const largeImg = document.querySelector('.gallery__large-img');
+
+const listImages = document.querySelector('.gallery__list');
+
+listImages.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const image = event.target.closest('.list-item__link');
+
+  largeImg.src = image.href;
+});
