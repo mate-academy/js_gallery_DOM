6f84c9041ffebb77555b2e7be4770601895cf677
Roman Nikitenko <Lomkaaa@ukr.net>
2022-05-15 16:10:34 +0300

    Task complete

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const largeImage = document.getElementById('largeImg');
+const links = document.querySelectorAll('.list-item__link');
+
+links.forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    largeImage.src = e.currentTarget.getAttribute('href');
+  });
+});
