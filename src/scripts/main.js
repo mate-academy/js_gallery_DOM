0a37782d4460e4c2f684aa088ad21414eb8756c3
ShapovalDenys <denys.igorevich@gmail.com>
2022-05-18 14:40:16 +0300

    solution2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const mainImg = document.getElementById('largeImg');
+const list = document.getElementById('thumbs');
+
+list.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const target = event.target.closest('.list-item__link');
+
+  if (mainImg.src !== target.href) {
+    mainImg.src = target.href;
+  };
+});
