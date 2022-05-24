0942a2b0cc6841b9aebaf28073cb3d8bbd42ad47
Anastasiia Sydorchuk <nastia.sydorchuk@gmail.com>
2022-05-24 18:41:18 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,10 +3,10 @@
 const imagesList = document.querySelector('.gallery__list');
 const mainImage = document.querySelector('.gallery__large-img');
 
-imagesList.addEventListener('click', (ev) => {
-  ev.preventDefault();
+imagesList.addEventListener('click', (browserEvent) => {
+  browserEvent.preventDefault();
 
-  const image = ev.target.closest('.list-item__link');
+  const image = browserEvent.target.closest('.list-item__link');
 
   mainImage.src = image.href;
 });
