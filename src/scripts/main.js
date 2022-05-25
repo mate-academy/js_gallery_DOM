f65f9ea67deb419f5b0a1255dcb6363c6e2f925f
Roman Lipatov <96875973+roman-lipatov@users.noreply.github.com>
2022-05-25 18:03:25 +0300

    gallery

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const bigImg = document.querySelector('.gallery__large-img');
+const list = document.querySelector('.gallery__list');
+
+list.addEventListener('click', events => {
+  events.preventDefault();
+  bigImg.src = events.target.closest('.list-item__link').href;
+});
