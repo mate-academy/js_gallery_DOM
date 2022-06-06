cad3129a7ecbab9a0d9547a2717c0f78376aa081
Katya Suvorova <katerinasuvorova97@gmail.com>
2022-06-06 11:15:58 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const list = document.querySelector('.gallery__list'); // ul
+
+const bigImg = document.getElementById('largeImg');
+
+list.addEventListener('click', (obj) => {
+  const link = obj.target.parentElement.href; // ссылка a.href
+
+  bigImg.setAttribute('src', link);
+
+  obj.preventDefault();
+});
