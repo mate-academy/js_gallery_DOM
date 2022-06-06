83a43202a0f6dc6cec64975cb2ba55db28f1dfbd
Kostiantyn <havryshkostya@gmail.com>
2022-06-06 12:50:59 +0400

    looks fine

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const bigImg = document.querySelector('.gallery__large-img');
+const links = document.querySelectorAll('a');
+
+for (const link of links) {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    bigImg.src = e.target.closest('.list-item__link').href;
+  });
+}
