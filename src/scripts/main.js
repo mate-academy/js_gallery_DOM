f5becb1ff882c577bf8a669a360632ca52853b40
Katya Suvorova <katerinasuvorova97@gmail.com>
2022-06-08 17:02:34 +0300

    Fixes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,25 @@
 'use strict';
 
-const list = document.querySelector('.gallery__list'); // ul
+const list = document.querySelector('.gallery__list');
 
 const bigImg = document.getElementById('largeImg');
+const aArray = document.querySelectorAll('.list-item__link');
+const imgArray = document.querySelectorAll('.gallery__thumb');
 
 list.addEventListener('click', (obj) => {
-  const link = obj.target.parentElement.href; // ссылка a.href
+  const link = obj.target.parentElement.href;
 
-  bigImg.setAttribute('src', link);
+  for (const img of imgArray) {
+    if (obj.target === img) {
+      bigImg.setAttribute('src', link);
+    }
+  }
+
+  for (const a of aArray) {
+    if (obj.target === a) {
+      bigImg.setAttribute('src', a.href);
+    }
+  }
 
   obj.preventDefault();
 });
