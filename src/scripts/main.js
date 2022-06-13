7ae648534cc706e73a27c445532a0d65fd7283d7
Vladyslava Buzova <dev.vladyslava@gmail.com>
2022-06-13 11:11:56 +0800

    commit message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const large = document.getElementById('largeImg');
+const thumbs = document.querySelectorAll('.gallery__img');
+
+for (let smallPic of thumbs) {
+  smallPic.addEventListener('click', (event) => {
+    event.preventDefault();
+    large.src = smallPic.src;
+  });
+
+  // smallPic.onclick = (event) => {
+  //   event.preventDefault();
+  //   large.src = smallPic.src;
+  // };
+}
