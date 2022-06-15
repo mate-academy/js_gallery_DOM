4c41b3797066f3117499c97c48a91363046bb21c
Katruk Evgeny <zaiaz_@i.ua>
2022-06-15 19:40:34 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const list = document.querySelector('#thumbs');
+const lageImg = document.querySelector('#largeImg');
+
+list.addEventListener('click', (meEvent) => {
+  meEvent.preventDefault();
+
+  const item = meEvent.target;
+
+  lageImg.src = item.parentElement.href;
+});
