6b1c058f81227e67828fa53f4e851bf38aa16626
bogdana18 <usenkobogdana19@gmail.com>
2022-06-16 21:26:38 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const largeImg = document.getElementById('largeImg');
+const list = document.getElementById('thumbs');
+
+list.addEventListener('click', events => {
+  const item = events.target.closest('.list-item');
+
+  if (!item) {
+    return;
+  }
+
+  events.preventDefault();
+  largeImg.src = item.children[0].href;
+});
