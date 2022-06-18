dcd93cbe390c5c6dc1c8126c2102f2024a99e4ee
Oleh Kalynovskyi <oleh.kalynovskyi@nure.ua>
2022-06-18 23:37:41 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const items = document.querySelectorAll('.list-item');
+const image = document.querySelector('#largeImg');
+
+for (let i = 0; i < items.length; i++) {
+  items[i].addEventListener('click', (e) => {
+    e.preventDefault();
+
+    switch (i) {
+      case 0: image.src = './images/landscape-first.png';
+        break;
+      case 1: image.src = './images/landscape-second.png';
+        break;
+      case 2: image.src = './images/landscape-third.png';
+        break;
+      case 3: image.src = './images/landscape-fourth.png';
+        break;
+      case 4: image.src = './images/landscape-fifth.png';
+        break;
+    };
+  });
+}
