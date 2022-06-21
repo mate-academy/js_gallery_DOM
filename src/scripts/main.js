281c5900ef68866a58532ba346619951c3c2c337
Oleh Kalynovskyi <oleh.kalynovskyi@nure.ua>
2022-06-22 00:12:03 +0300

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,17 +7,6 @@ for (let i = 0; i < items.length; i++) {
   items[i].addEventListener('click', (e) => {
     e.preventDefault();
 
-    switch (i) {
-      case 0: image.src = './images/landscape-first.png';
-        break;
-      case 1: image.src = './images/landscape-second.png';
-        break;
-      case 2: image.src = './images/landscape-third.png';
-        break;
-      case 3: image.src = './images/landscape-fourth.png';
-        break;
-      case 4: image.src = './images/landscape-fifth.png';
-        break;
-    };
+    image.src = items[i].children[0].children[0].src;
   });
 }
