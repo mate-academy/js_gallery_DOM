4088caf452f07c4aba474d44bd345ae5a798fad5
Kirill Polozenko <kir.polozenko@icloud.com>
2022-06-22 17:23:55 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const currentPicture = document.querySelectorAll('a');
+const mainImage = document.querySelector('#largeImg');
+
+for (let i = 0; i < currentPicture.length; i++) {
+  currentPicture[i].addEventListener('click', (e) => {
+    e.preventDefault();
+    mainImage.src = currentPicture[i].href;
+  });
+}
