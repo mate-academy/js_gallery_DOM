e2aae9e16b9c4e73e6f0ddc50a665e9f23a79288
Mikhail Shchuka <mikhail.shchuka@gmail.com>
2022-06-24 18:56:59 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const thumbs = document.querySelector('.gallery__list');
+
+thumbs.addEventListener('click', replaceImg);
+
+function replaceImg(click) {
+  click.preventDefault();
+
+  if (!click.target.closest('.list-item__link')) {
+    return false;
+  }
+
+  const largeImg = document.querySelector('#largeImg');
+
+  switch (click.target.tagName) {
+    case 'A':
+      largeImg.src = click.target.src;
+      break;
+    case 'IMG':
+      largeImg.src = click.target.parentElement.href;
+      break;
+  }
+}
