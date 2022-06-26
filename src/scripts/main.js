b72140d74281bd22ae3a378818b0cc6065bc2a13
Max <59259022+WhiteAshDc@users.noreply.github.com>
2022-06-26 23:18:02 +0200

    result

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const ul = document.querySelector('#thumbs');
+
+ul.addEventListener('click', () => {
+  const thumbs = event.target.closest('a');
+
+  showNail(thumbs.href);
+  event.preventDefault();
+});
+
+let largeImg;
+
+function showNail(href) {
+  largeImg.src = href;
+}
