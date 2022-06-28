343061969d497a7d079473ee6768a091e62a6fbc
Yaroslav Kolbaiev <103733788+YaroslavKolbaiev@users.noreply.github.com>
2022-06-28 13:42:38 +0300

    firstCommit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const mainImage = document.querySelector(
+  '.gallery__large-img');
+const smallImage = document.querySelectorAll(
+  '.list-item__link');
+
+for (const ch of smallImage) {
+
+  ch.addEventListener('click', (e) => {
+    e.preventDefault();
+    mainImage.src = e.currentTarget.href;
+  });
+}
