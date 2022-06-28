102f3b0a5320cc60ad616954aead282e630cf256
Yaroslav Kolbaiev <103733788+YaroslavKolbaiev@users.noreply.github.com>
2022-06-28 13:44:02 +0300

    secondCommit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,7 +6,6 @@ const smallImage = document.querySelectorAll(
   '.list-item__link');
 
 for (const ch of smallImage) {
-
   ch.addEventListener('click', (e) => {
     e.preventDefault();
     mainImage.src = e.currentTarget.href;
