07ebfd93c94bb1e7d1155510cfbcf89c0968b9a1
PaulRodriguez2049 <paul.rodriguez.2049@gmail.com>
2022-06-29 15:05:35 +0300

    first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const ul = document.querySelector(`.gallery__list`);
+const mainImage = document.querySelector(`#largeImg`);
+
+ul.addEventListener(`click`, function(e) {
+  e.preventDefault();
+
+  const link = e.target.closest(`.list-item__link`);
+
+  if (!link) {
+    return;
+  }
+
+  mainImage.src = link.href;
+});
