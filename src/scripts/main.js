066f31ae2a7df3d8c7c81b1cb9e7c1d1a67dc87e
Tania Troshchuk <troschuk95@gmail.com>
2022-06-30 18:29:31 +0300

    Solution Task: Create a gallery

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const list = document.querySelector('ul');
+const largeImg = document.getElementById('largeImg');
+
+list.addEventListener('click', (currentEvent) => {
+  currentEvent.preventDefault();
+
+  largeImg.src = currentEvent.target.parentElement.href;
+});
