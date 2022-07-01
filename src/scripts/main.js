78d00ca4b666eae67630e045d835e495ed286acb
Dmytro Lebedchenko <lebedchenkodmytro@gmail.com>
2022-07-01 18:32:16 +0300

    Gallery thumbnail script was created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const list = document.querySelector('#thumbs');
+const largeImg = document.querySelector('#largeImg');
+
+list.addEventListener('click', (thumbEvent) => {
+  const item = thumbEvent.target.closest('.list-item');
+  const smallImg = item.querySelector('.list-item__link');
+
+  thumbEvent.preventDefault();
+
+  largeImg.src = smallImg.getAttribute('href');
+});
