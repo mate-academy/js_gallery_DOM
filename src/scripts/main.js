6525b7c064b789f1d05a52f8d95b96202abda7ad
silchyn <ivan.silchin@gmail.com>
2022-07-06 17:40:23 +0300

    created gallery

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const large = document.querySelector('#largeImg');
+
+document.querySelector('.gallery__list').addEventListener('click', ev => {
+  ev.preventDefault();
+
+  const link = ev.target.closest('.list-item__link');
+
+  if (link) {
+    large.src = link.href;
+  }
+});
