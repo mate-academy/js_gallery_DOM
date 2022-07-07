49050daaf559af7537c1f6082e2a8984d0a686d7
Oleksii Budnik <budnikoleksii@gmail.com>
2022-07-07 07:44:01 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const imageMain = document.getElementById('largeImg');
+const galleryList = document.getElementById('thumbs');
+
+galleryList.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  imageMain.src = e.target.closest('.list-item__link');
+});
