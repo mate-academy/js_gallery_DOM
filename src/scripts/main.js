16e2295ee05876a3ec9921b2bf58db1a829463dd
Tanya Hryn <tanyagryn1984@gmail.com>
2022-07-10 23:28:20 +0300

    gallery

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const listUl = document.querySelector('.gallery__list');
+
+const bigImage = document.querySelector('#largeImg');
+
+listUl.addEventListener('click', (events) => {
+  events.preventDefault();
+
+  const link = events.target.closest('.list-item__link');
+
+  if (!link) {
+    return;
+  }
+
+  bigImage.src = link.href;
+});
