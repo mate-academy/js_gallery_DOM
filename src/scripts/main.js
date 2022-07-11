98f282d95295fe0e826789e5f77d302e0a76e9ca
Max Shylenko <maxshylenko@gmail.com>
2022-07-11 23:31:46 +0200

    gallery DOM done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const largeImg = document.getElementById('largeImg');
+const galleryList = document.getElementById('thumbs');
+
+galleryList.addEventListener('click', (eve) => {
+  eve.preventDefault();
+
+  const img = eve.target.closest('.list-item__link');
+
+  if (!img) {
+    return;
+  }
+
+  largeImg.src = img.href;
+});
