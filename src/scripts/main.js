46e237efef46213f5ec35db99b95ed222465db95
Stanislav Bzhezinskyi <stasiva28@gmail.com>
2022-07-13 12:21:17 +0300

    some message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const mainPicture = document.getElementById('largeImg');
+const list = document.querySelector('.gallery__list');
+
+list.addEventListener('click', (events) => {
+  events.preventDefault();
+
+  const item = events.target.closest('.list-item__link');
+
+  mainPicture.src = item.href;
+});
