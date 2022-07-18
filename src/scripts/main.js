ef3754462295264f84636086cbe281213036feb9
Sergii Maniurov <maniurov_sv@ukr.net>
2022-07-18 21:13:23 +0300

    added an event handler to the gallery

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const largeImage = document.querySelector('#largeImg');
+const elementUl = document.querySelector('#thumbs');
+
+elementUl.addEventListener('click', eventClick => {
+  eventClick.preventDefault();
+
+  const link = eventClick.target.closest('.list-item__link');
+
+  largeImage.src = link ? link.href : largeImage.src;
+});
