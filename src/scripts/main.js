31c51e11c2362a9da2022e00d323ffc5f6f9fadf
Denys Dmytruk <dmytrukdenysan@gmail.com>
2022-07-20 18:18:06 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const list = document.querySelector('.gallery__list');
+const mainImage = document.querySelector('.gallery__large-img');
+
+list.addEventListener('click', (clicked) => {
+  clicked.preventDefault();
+
+  const item = clicked.target.closest('.list-item');
+
+  if (!item || !list.contains(item)) {
+    return;
+  }
+
+  const itemImage = item.querySelector('.list-item__link');
+
+  mainImage.src = itemImage.href;
+});
