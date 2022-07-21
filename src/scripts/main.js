9eefa8d39ebc8884c64fa94d39c088893cc083d3
lentyaeev <daniilshaliuk@gmail.com>
2022-07-21 09:59:48 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const thumbs = document.getElementsByClassName('list-item__link');
+const bigImg = document.querySelector('#largeImg');
+
+for (const link of thumbs) {
+  link.addEventListener('click', eve => {
+    eve.preventDefault();
+    bigImg.src = link.href;
+  });
+}
