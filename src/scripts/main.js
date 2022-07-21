07acba397d1fee6454095c3ac6fc27ad8eff0554
Vladyslava Buzova <dev.vladyslava@gmail.com>
2022-07-21 19:00:40 +0800

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,14 +3,9 @@
 const large = document.getElementById('largeImg');
 const thumbs = document.querySelectorAll('.gallery__img');
 
-for (let smallPic of thumbs) {
+for (const smallPic of thumbs) {
   smallPic.addEventListener('click', (event) => {
     event.preventDefault();
     large.src = smallPic.src;
   });
-
-  // smallPic.onclick = (event) => {
-  //   event.preventDefault();
-  //   large.src = smallPic.src;
-  // };
 }
