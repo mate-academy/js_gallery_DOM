1bd9c39dddc5a3acb5f88814dfebf4bf1ede08ff
DashaPoliazz <dasha.poliazzz@gmail.com>
2022-07-22 13:31:12 +0300

    gallery has been maded

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const root = document.querySelector('#largeImg');
+
+const onClickHandler = (e) => {
+  e.preventDefault();
+
+  root.src = e.target.parentNode.href;
+};
+
+[...document.querySelectorAll('li')]
+  .forEach(item => {
+    item.addEventListener('click', onClickHandler);
+  });
