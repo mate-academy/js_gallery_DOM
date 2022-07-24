2d5ef257e27d18f304b2915aa3bb69cbb0033d74
Kostya Berehoviy <kostyaber3@gmail.com>
2022-07-25 00:52:08 +0300

    JS Gallery Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const largeImg = document.querySelector('.gallery__large-img');
+const links = document.querySelector('.gallery__list');
+
+links.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const link = e.target;
+
+  largeImg.src = link.parentElement.href;
+});
+
+// for (const link of links) {
+//   link.addEventListener('click', (event) => {
+//     event.preventDefault();
+//     largeImg.src = link.href;
+//   });
+// };
