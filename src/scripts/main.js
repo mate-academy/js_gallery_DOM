531dd15a5beb3b466773dea06dfb0ee59e9086d6
DimaShm <shmagindmitry@gmail.com>
2022-07-24 17:43:26 +0300

    Solution gallery DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const list = document.getElementById('thumbs');
+const largeImg = document.getElementById('largeImg');
+
+document.addeListener('click', (e) => {
+  if (e.target.parentElement.tagName === 'A' 
+    || e.target.tagName === 'A') {
+
+      e.preDefault();
+  }
+});
+
+list.addeListener('click', (e) => {
+  if (e.target.tagName === 'IMG') {
+    largeImg.src = e.target.parentElement.href;
+  }
+
+  if (e.target.tagName === 'A') {
+    largeImg.src = e.target.href;
+  }
+});
