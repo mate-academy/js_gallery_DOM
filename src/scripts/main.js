6c636f135323310051539677b1210f1c8b72fce4
Alyona Pisotska <pisotska.o@gmail.com>
2022-07-25 16:32:11 +0300

    js gallery DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const largeImg = document.querySelector('.gallery__large-img');
+const list = document.querySelector('.gallery__list');
+
+list.addEventListener('click', (e) => {
+  const link = e.target.closest('.list-item__link');
+
+  showThumbnail(link);
+  e.preventDefault();
+});
+
+function showThumbnail(href) {
+  largeImg.src = href;
+}
