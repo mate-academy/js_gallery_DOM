62edc80569a8da05a1c1c4cf958c7b126c620348
Olha Arama <arama_o_f@ukr.net>
2022-07-30 20:12:42 +0300

    Gallery was created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const gallery = document.querySelector('.gallery');
+const largeImg = document.getElementById('largeImg');
+
+gallery.addEventListener('click', e => {
+  const target = e.target.closest('a');
+
+  if (!target) {
+    return;
+  }
+
+  largeImg.src = target.href;
+
+  e.preventDefault();
+});
