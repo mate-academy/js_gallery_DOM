b9e6392e4e8811ef5681bbcd0e07d3815e39ec82
Vlad Skuratovskyi <vladskur0369@gmail.com>
2022-08-09 22:19:31 +0300

    Solution with scss style

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const gallery = document.querySelector('.gallery');
+const bigPicture = document.querySelector('#largeImg');
+
+gallery.addEventListener('click', (events) => {
+  events.preventDefault();
+
+  const item = events.target.closest('.list-item__link');
+
+  if (!item || !gallery.contains(item)) {
+    return;
+  }
+
+  bigPicture.src = events.target.parentElement.href;
+});
