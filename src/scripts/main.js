1c07f535248561462426f187773d1342d1c91eb5
Kostya Konev <kosgoh.214@gmail.com>
2022-08-13 20:13:58 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const mainPhoto = document.querySelector('.gallery__large-img');
+const galleryList = document.querySelector('.gallery__list');
+
+galleryList.addEventListener('click', (eventObj) => {
+  eventObj.preventDefault();
+
+  if (eventObj.target.tagName === 'A') {
+    mainPhoto.src = eventObj.target.href;
+  } else {
+    mainPhoto.src = eventObj.target.parentElement.href;
+  }
+});
