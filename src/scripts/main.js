66a5a7a045800bcffb9135d2e4206239cd530dd1
Andriy Milishchuk <andriy.milishchuk@teamvoy.com>
2022-08-15 16:56:52 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const list = document.querySelector('.gallery__list');
+const selectedImage = document.querySelector('.gallery__large-img');
+
+list.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const item = e.target.closest('.list-item__link');
+
+  if (!item || !list.contains(item)) {
+    return;
+  }
+
+  const newSelectedImageSrc = item.children[0].attributes.src.value;
+
+  selectedImage.src = newSelectedImageSrc;
+});
