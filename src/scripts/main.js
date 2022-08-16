844730b90f9809f852392df6b27fa8eb7daa5f4d
Anton Chetveryk <chetverykantonma@gmail.com>
2022-08-16 20:03:17 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const thumbs = document.getElementById('thumbs');
+const largeImg = document.getElementById('largeImg');
+
+thumbs.addEventListener('click', (e) => {
+  const item = e.target.closest('.list-item__link');
+
+  e.preventDefault();
+  largeImg.src = item.href;
+});
