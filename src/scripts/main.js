3114ff82b3739ed390c20bda41420c4f5397c7e6
Sasha Kolomiiets <kinbean444@gmail.com>
2022-08-17 21:47:46 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const links = document.querySelectorAll('.list-item__link');
+const largePhoto = document.querySelector('#largeImg');
+
+links.forEach(link =>
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    largePhoto.src = link.href;
+  })
+);
