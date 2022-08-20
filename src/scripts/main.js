38dd4d69663af9867c3d6bdc1e9ee0bead1a22e8
Roman Nikitenko <Lomkaaa@ukr.net>
2022-08-20 20:52:27 +0300

    Test task complete

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,7 @@
 
 const largeImage = document.getElementById('largeImg');
 const links = document.querySelectorAll('.list-item__link');
+const test = 1;
 
 links.forEach(link => {
   link.addEventListener('click', e => {
