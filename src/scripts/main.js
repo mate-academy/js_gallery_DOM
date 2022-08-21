1d794b21d33ad58bcd7d2aa5d3c552e63f2d67ed
Viktoria Neimesh <valencia88vika@gmail.com>
2022-08-21 07:55:18 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const thumbs = document.getElementById('thumbs');
+const largeImg = document.getElementById('largeImg');
+
+thumbs.addEventListener('click', e => {
+  const picture = e.target.closest('a');
+
+  if (!picture) {
+    return;
+  }
+
+  showPicture(picture.href, picture.title);
+  e.preventDefault();
+
+  function showPicture(href, title) {
+    largeImg.src = href;
+    largeImg.alt = title;
+  }
+});
