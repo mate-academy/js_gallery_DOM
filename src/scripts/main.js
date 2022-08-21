7b897705c389f8411ec530f51a1f67cc93ff7022
Oksana Shvets <ksenyasvatko1993@gmail.com>
2022-08-21 08:51:55 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const thumbs = document.getElementById('thumbs');
+
+thumbs.onclick = function() {
+  const thumbnail = event.target.closest('a');
+
+  if (!thumbnail) {
+    return;
+  }
+  showThumbnail(thumbnail.href, thumbnail.title);
+  event.preventDefault();
+};
+
+const largeImg = document.getElementById('largeImg');
+
+function showThumbnail(href, title) {
+  largeImg.src = href;
+  largeImg.alt = title;
+}
