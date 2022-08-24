3610cc3976c473835b6f4e083e80b7c1b1f0d13d
Yurii Kozlov <yurka22870@gmail.com>
2022-08-24 20:53:57 +0300

    gallery was created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const largeImage = document.querySelector('#largeImg');
+const list = document.querySelector('.gallery__list');
+
+// eslint-disable-next-line no-shadow
+list.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const item = event.target.closest('.list-item__link');
+
+  largeImage.src = item.href;
+});
