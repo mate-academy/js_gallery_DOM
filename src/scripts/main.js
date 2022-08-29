59c31894473fb4577fa89e2f92cde9a190e2a717
Bohdan Tron <ikwiczzts@gmail.com>
2022-08-29 16:20:00 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const items = document.querySelectorAll('.list-item');
+const largePic = document.getElementById('largeImg');
+
+items.forEach(item => {
+  item.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const source = e.target.getAttribute('src');
+
+    largePic.setAttribute('src', source);
+  });
+});
