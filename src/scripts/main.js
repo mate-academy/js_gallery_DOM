bfe3eb863ffee8e66f33e33697318d0a9ab9b1ef
Denys Topchyi <miliarder97@gmail.com>
2022-08-30 14:30:45 +0300

    gallery_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const gallery = document.querySelector('.gallery');
+
+gallery.addEventListener('click', function(e) {
+  e.preventDefault();
+
+  const item = e.target;
+
+  if (item.classList.contains('gallery__thumb')) {
+    document.querySelector('.gallery__large-img').src
+      = item.closest('.list-item__link').href;
+  }
+});
