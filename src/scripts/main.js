a1021aad6ae139631c2c7c109cf169abe965ab28
Oleksandr Malyshev <sanyokmalyshev@gmail.com>
2022-09-06 13:46:31 +0800

    Task done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const list = document.querySelector('.gallery__list');
+const largeImg = document.querySelector('#largeImg');
+
+list.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const item = e.target.closest('.list-item');
+
+  if (!item) {
+    return;
+  }
+
+  const link = item.querySelector('.list-item__link');
+
+  largeImg.src = link.href;
+});
