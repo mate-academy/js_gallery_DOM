2d0623d0532d1de8606e160aaf0c1486865d3780
Inna <dragotaiv@gmail.com>
2022-09-15 19:52:46 +0300

    first_commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const largeImg = document.querySelector('#largeImg');
+
+const listItem = document.querySelectorAll('.list-item');
+
+for (const el of [...listItem]) {
+  el.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    const a = el.querySelector('a');
+
+    largeImg.src = a.href;
+  });
+};
