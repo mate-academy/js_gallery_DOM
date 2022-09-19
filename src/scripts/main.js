839b166e53eaeea30f1012fe797f59d3aa176bf1
Max Bovtun <maxbovtun@gmail.com>
2022-09-19 21:00:07 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const items = document.querySelectorAll('a');
+const bigImg = document.querySelector('#largeImg');
+
+for (const item of items) {
+  item.addEventListener('click', (i) => {
+    i.preventDefault();
+    bigImg.src = item.getAttribute('href');
+  });
+}
