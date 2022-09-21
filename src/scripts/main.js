e13796c9fbcbb974e2feaa864d99f3655b781cf7
Yevheniia Danylchuk <shenijad1@gmail.com>
2022-09-21 20:48:11 +0300

    gallery

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const mainImg = document.querySelector('#largeImg');
+const thumbs = document.querySelector('#thumbs');
+
+thumbs.addEventListener('click', (ev) => {
+  const item = ev.target.closest('.gallery__thumb');
+
+  if (!item || !thumbs.contains(item)) {
+    return;
+  }
+
+  ev.preventDefault();
+
+  mainImg.src = item.src;
+});
