4052544fc2d992850d4bfcd5bd41b26e160d13ff
Oleh Savchuk <sav4uk.oleg@gmail.com>
2022-09-24 14:36:51 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const images = document.querySelector('.gallery__list');
+
+const bigImage = document.querySelector('.gallery__large-img');
+
+images.addEventListener('click', () => {
+  event.preventDefault();
+
+  if (event.target.classList.contains('list-item')) {
+    return;
+  }
+
+  bigImage.src = event.target.closest('.list-item__link').href;
+});
