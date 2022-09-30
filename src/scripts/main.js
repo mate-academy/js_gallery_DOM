83cb854ff8aef92e6be6eb718738a241a81cda05
Volodymyr Nosachenko <vovanosachenko@meta.ua>
2022-09-30 12:59:37 +0300

    gallery slider was implemented

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const gallery = document.querySelector('.gallery');
+const largeImg = document.querySelector('#largeImg');
+
+// eslint-disable-next-line no-shadow
+const replaceImg = (event) => {
+  event.preventDefault();
+
+  if (event.target.src) {
+    largeImg.src = event.target.src;
+  }
+};
+
+gallery.addEventListener('click', replaceImg);
