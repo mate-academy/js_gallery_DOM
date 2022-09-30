8b137645882464ff1fdd790fc02ff7f3b04ee3ca
dmitriyn3679 <dmitriyn3679@gmail.com>
2022-09-30 18:54:42 +0300

    done!

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const mainImg = document.querySelector('.gallery__large-img');
+const galleryList = document.querySelector('.gallery__list');
+
+galleryList.addEventListener('click', (myEvent) => {
+  myEvent.preventDefault();
+
+  const imgLink = myEvent.target.closest('.list-item__link');
+
+  const imgUrl = imgLink.getAttribute('href');
+
+  mainImg.setAttribute('src', imgUrl);
+});
