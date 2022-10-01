31c73f82db54183f551655dea47669fa2e37ea48
Killdex <kdcompany2018@gmail.com>
2022-10-01 18:28:00 +0300

    gallery created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const galleryList = document.querySelector('.gallery__list');
+const largeImg = document.querySelector('#largeImg');
+
+galleryList.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const target = e.target.closest('.gallery__thumb');
+  const targetSrc = target.closest('.list-item__link').href;
+
+  if (!target || !galleryList.contains(target)) {
+    return;
+  }
+
+  largeImg.src = targetSrc;
+});
