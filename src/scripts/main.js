ffbfb1e3fbae2e32d565952543acbcf99e765962
Danil <dasha8854567@gmail.com>
2022-10-07 14:05:58 +0300

    commit1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const thumbsLinks = [...document.querySelector('#thumbs')
+  .querySelectorAll('a')];
+
+thumbsLinks.forEach(a => a.addEventListener('click', (eventt, target) => {
+  eventt.preventDefault();
+
+  document.querySelector('#largeImg').src = eventt.path[1].href;
+}));
