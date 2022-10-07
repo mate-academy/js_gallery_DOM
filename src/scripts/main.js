ffd2e23ce7f948f9d2392f9648b6c1baa94fd3c7
Yevhen Baiev <baev838@gmail.com>
2022-10-07 17:05:44 +0300

    First attempt

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const largeImage = document.getElementById('largeImg');
+const images = document.querySelectorAll('.list-item');
+
+images.forEach(el => {
+  el.addEventListener('click', (ev) => {
+    ev.preventDefault();
+
+    largeImage.src = el.querySelector('a').href;
+  });
+});
