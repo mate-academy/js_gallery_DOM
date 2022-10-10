66ffd0e03b8f2aad10e048bd5a35d2db95028c75
Oleksandr Naumchak <naumchak@ukr.net>
2022-10-10 15:09:53 +0300

    JS gallery DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const thumbs = document.querySelector('#thumbs');
+const largeImg = document.querySelector('#largeImg');
+
+function showThumb(currImg) {
+  const thumb = currImg.target.closest('.list-item__link');
+
+  if (!thumb || !thumbs.contains(thumb)) {
+    return;
+  }
+
+  largeImg.src = thumb.href;
+  largeImg.alt = thumb.title;
+  currImg.preventDefault();
+}
+
+thumbs.addEventListener('click', showThumb);
