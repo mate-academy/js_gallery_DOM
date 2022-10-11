0be50cb0aea8d3f7569ac7e05eb4ed654c6aca6c
Mykhailo Oblat <moblat890@gmail.com>
2022-10-11 17:01:46 +0300

    solution gallery_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const mainImage = document.getElementById('largeImg');
+const thumbs = document.getElementById('thumbs');
+
+thumbs.addEventListener('click', (evImg) => {
+  const thumb = evImg.target.closest('.list-item__link');
+
+  if (!thumb) {
+    return;
+  };
+
+  evImg.preventDefault();
+
+  mainImage.src = thumb.href;
+});
