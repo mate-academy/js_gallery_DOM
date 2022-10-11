cad768e1290896e539cf4b01cfef4375dc515df4
Vitalii Derlemenko <vitaliidox@gmail.com>
2022-10-11 21:11:39 +0300

    create gallery

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const galleryList = document.querySelector('.gallery__list');
+const largeImg = document.querySelector('#largeImg');
+
+galleryList.addEventListener('click', (event) => {
+  const item = event.target;
+
+  event.preventDefault();
+
+  if (!item.classList.contains('gallery__thumb')) {
+    return;
+  }
+
+  largeImg.setAttribute('src', item.parentElement.getAttribute('href'));
+})
