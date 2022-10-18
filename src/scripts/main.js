dbdef6684f4605e85bf46e305fd8d56ac5585446
Kate <krasnova2912@gmail.com>
2022-10-18 23:05:05 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const thumbs = document.querySelector('#thumbs');
+const largeImg = document.querySelector('#largeImg');
+
+thumbs.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const el = e.target.closest('.list-item__link');
+
+  if (!el) {
+    return;
+  };
+
+  largeImg.setAttribute('src', el.href);
+});
