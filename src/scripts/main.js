fe9de886ba0dead45d2982406e65578031669879
Ruslan Nedaiborshch <rnedaiborshch@gmail.com>
2022-10-20 15:00:29 +0300

    cod for gallery

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const list = document.querySelector('.gallery__list');
+
+const largeImg = document.querySelector('.gallery__large-img');
+
+list.addEventListener('click', (e) => {
+  const item = e.target.closest('.list-item__link');
+
+  e.preventDefault();
+
+  largeImg.src = item.href;
+});
