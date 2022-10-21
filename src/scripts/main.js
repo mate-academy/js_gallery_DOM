9355bb053e5092382d63233da9f17679709d952f
John Rust <johnrust12@gmail.com>
2022-10-21 10:37:20 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const largeImg = document.querySelector('#largeImg');
+const galleryList = document.querySelector('.gallery__list');
+
+galleryList.addEventListener('click', (click) => {
+  click.preventDefault();
+
+  const item = click.target.closest('.list-item__link');
+
+  if (!item) {
+    return;
+  }
+
+  largeImg.src = item.href;
+});
