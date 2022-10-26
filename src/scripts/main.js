881dfed2924c67fc4f1cce80e05dabf4348687d2
Anastasia Babko <hr.anastasia.t@gmail.com>
2022-10-26 14:54:49 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const mainImage = document.querySelector('#largeImg');
+const links = document.querySelectorAll('.list-item__link');
+
+for (const link of links) {
+  link.addEventListener('click', (ev) => {
+    ev.preventDefault();
+
+    const item = ev.target.closest('.list-item__link');
+
+    if (!item) {
+      return;
+    }
+
+    mainImage.src = item.href;
+  });
+};
