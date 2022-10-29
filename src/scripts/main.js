819d82073266a5eaa53de7bf0b239dec4c113585
Anastasiya145 <asiva@ukr.net>
2022-10-29 09:26:34 +0200

    the task is done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const imgLarge = document.querySelector('#largeImg');
+const links = document.querySelectorAll('.list-item__link');
+
+for (const link of links) {
+  link.addEventListener('click', (ev) => {
+    const clickedImg = ev.target.closest('.list-item__link');
+
+    imgLarge.src = clickedImg.href;
+
+    ev.preventDefault();
+  });
+}
