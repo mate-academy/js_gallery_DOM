dfe56870268332d7a205a655c266fce734218eb0
Valentyn Ivanenko <Ivanenko1402@gmail.com>
2022-10-31 15:24:33 +0200

    commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const largeImg = document.querySelector('#largeImg');
+const list = document.querySelector('#thumbs');
+
+list.addEventListener('click', ev => {
+  const link = ev.target.closest('.list-item__link');
+
+  ev.preventDefault();
+  largeImg.src = link.href;
+});
