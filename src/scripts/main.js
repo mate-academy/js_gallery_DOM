eda75936d7c1782e53ed674d8c3cfd8025060da8
Kolya Movchan <kmovchan261@gmail.com>
2022-11-01 19:10:08 +0200

    js_gallery_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const thumbPictures = document.querySelectorAll('.list-item');
+const largePicture = document.getElementById('largeImg');
+
+[...thumbPictures].map(picture => {
+  // eslint-disable-next-line no-shadow
+  picture.addEventListener('click', (event) => {
+    event.preventDefault();
+    largePicture.setAttribute('src', picture.firstChild.nextSibling.href);
+  });
+});
