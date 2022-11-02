5114064bd8895a7c306ff761bf1ad760d32ffa83
Max Fedak <max.fedak@gmail.com>
2022-11-02 19:24:47 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const gallery = document.querySelector('.gallery__list');
+const largeImg = document.querySelector('#largeImg');
+
+gallery.addEventListener('click', (e) => {
+  // e.preventDefault();
+  const item = e.target.closest('.list-item__link');
+
+  if (!item) {
+    return;
+  }
+
+  e.preventDefault();
+
+  largeImg.src = item.href;
+});
