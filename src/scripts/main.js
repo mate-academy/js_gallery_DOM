eccd26753c31b2cdfc1270ec94dd5b4f533f690d
Oleksandr Lytvynenko <olek.lytvynenko@gmail.com>
2022-11-03 20:43:53 +0200

    solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const listItem = document.querySelectorAll('.list-item');
+const largePicture = document.getElementById('largeImg');
+
+[...listItem].map(image => {
+  image.addEventListener('click', (occurent) => {
+    occurent.preventDefault();
+    largePicture.setAttribute('src', image.firstChild.nextSibling.href);
+  });
+});
