ebde2257b1d6a70105e48d65de728e6b3645ba52
Ruslan Miroshnychenko <rusikm87@gmail.com>
2022-11-04 19:41:30 +0200

    Done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const gallery = document.getElementById('thumbs');
+const showImage = document.getElementById('largeImg');
+
+gallery.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const image = e.target.closest('.list-item__link').getAttribute('href');
+
+  showImage.setAttribute('src', image);
+});
