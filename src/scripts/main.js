99ab73a8ce1f686f54769fbc75de7a43d75daca8
Antonenko-AA <alexandr.antonenko.fr@gmail.com>
2022-11-09 23:57:47 +0200

    Gallery has been created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const mainImage = document.getElementById('largeImg');
+const list = document.getElementById('thumbs');
+
+list.addEventListener('click', (e) => {
+  const item = e.target.closest('.list-item__link');
+
+  e.preventDefault();
+  mainImage.src = item.href;
+});
