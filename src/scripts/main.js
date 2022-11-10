3a7451526473de1965d79a0adc45c3a7e4fa0f66
Denis Cherevaschenko <deniska.cherevaschenko@gmail.com>
2022-11-10 19:20:57 +0200

    solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const list = document.getElementById('thumbs');
+
+list.addEventListener('click', (eventObj) => {
+  const largeImg = document.getElementById('largeImg');
+
+  eventObj.preventDefault();
+
+  const item = eventObj.target.closest('.list-item__link');
+
+  if (!item || !list.contains(item)) {
+    return;
+  }
+
+  largeImg.src = item.href;
+});
