876f61751780cbb65ba7db7b45368c629b745f93
Bohdan Rudenko <RudBeee@gmail.com>
2022-11-22 14:45:20 +0200

    vol_1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const galleryList = document.querySelector('ul');
+const largeImg = document.querySelector('#largeImg');
+
+galleryList.addEventListener('click', (even) => {
+  even.preventDefault();
+  largeImg.src = even.target.src;
+}
+);
