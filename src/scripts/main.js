b5484b464fe632ea1ce76abf0ffa3c0aa53df841
Taras Kovalchuk <taras.kovalcuk93@gmail.com>
2022-11-23 23:25:12 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const gallery = document.querySelector('.gallery');
+const list = document.querySelector('.gallery__list');
+
+list.addEventListener('click', (events) => {
+  const item = events.target;
+  const largeImg = gallery.children[0];
+  const itemCopy = item.cloneNode(true);
+
+  events.preventDefault();
+  largeImg.firstElementChild.remove();
+  itemCopy.className = 'gallery__large-img gallery__img';
+  largeImg.append(itemCopy);
+});
