04fce0b9237df5a1654772d22bf104bb2df5519f
Oleksandr <Khabailiuk@gmail.com>
2022-11-23 21:22:35 +0100

    corrected with e

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,8 +4,8 @@ const images = document.querySelectorAll('.list-item');
 const bigImage = document.getElementById('largeImg');
 
 images.forEach(image => {
-  image.addEventListener('click', (el) => {
-    el.preventDefault();
+  image.addEventListener('click', (e) => {
+    e.preventDefault();
 
     bigImage.src = image.querySelector('a').href;
   });
