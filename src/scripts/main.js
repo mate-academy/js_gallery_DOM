057c0c0fb5cc0217575a108addc6d0dc2baf5166
Maryna Kucher <marina.kucher8@gmail.com>
2022-11-28 13:08:38 +0200

    Solution 25

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const list = document.querySelector('.gallery');
+const mainImg = document.querySelector('.gallery__large-img');
+
+list.addEventListener('click', (thumb) => {
+  const item = thumb.target.closest('.list-item__link');
+
+  if (!item || !list.contains(item)) {
+    return;
+  }
+  thumb.preventDefault();
+  mainImg.src = item.href;
+});
