0ec5859a61406b729bc600d40aa8efe836af6c55
Panianchuk M <panianchuk@gmail.com>
2022-12-02 00:27:18 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const largeImg = document.querySelector('#largeImg');
+const listSmallImg = document.querySelector('.gallery__list');
+
+listSmallImg.addEventListener('click', function(eventClick) {
+  const choiceImg = eventClick.target.closest('a');
+
+  if (!choiceImg) {
+    return;
+  }
+
+  largeImg.src = choiceImg.href;
+  eventClick.preventDefault();
+});
