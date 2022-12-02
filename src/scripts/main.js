fd71b0978cfe4efe95771f09c2a91ce4fd29765e
Sergey Mironenko <serg1prime@gmail.com>
2022-12-02 03:54:09 +0200

    Task was done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const mainImage = document.querySelector('.gallery__large-img');
+const table = document.querySelector('.gallery__list');
+
+table.addEventListener('click', (action) => {
+  const tableItem = action.target;
+
+  action.preventDefault();
+
+  if (tableItem.classList.contains('gallery__thumb')) {
+    mainImage.src = tableItem.parentElement.href;
+  }
+});
