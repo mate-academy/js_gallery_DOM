7e1dcd0237a07bc94561e2d6ab2ff93c05182348
Vlad Fedorishchev <fedorischev23@gmail.com>
2022-12-03 21:48:30 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const bigPicture = document.getElementById('largeImg');
+const smallPictures = [...document.querySelectorAll('.gallery__thumb')];
+
+smallPictures.map(picture => {
+  picture.onclick = (action) => {
+    action.preventDefault();
+
+    bigPicture.src = picture.parentElement.href;
+  };
+});
