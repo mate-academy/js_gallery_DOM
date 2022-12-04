40b27408732c82db1e533db7c01aaf95d74d9c3f
Maria Bramnik <mariabramnik@gmail.com>
2022-12-04 23:10:59 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const ul = document.querySelector('.gallery__list');
+const liImg = document.querySelector('.gallery div img');
+
+ul.addEventListener('click', (evnt) => {
+  const item = evnt.target;
+
+  evnt.preventDefault();
+
+  const itemParent = item.closest('.list-item');
+
+  if (itemParent === null) {
+    return;
+  }
+
+  const mySrc = item.src;
+
+  liImg.src = mySrc;
+});
