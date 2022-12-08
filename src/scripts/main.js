6ed1013a62f3ed41f4768b72bbd9f5127b7bbabe
Artem Hirzhev <a.hirzhev@gmail.com>
2022-12-08 21:01:06 +0200

    Initial commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const thumbs = document.querySelector('#thumbs');
+const largeImg = document.querySelector('#largeImg');
+
+thumbs.addEventListener('click', (e) => {
+  const a = e.target.closest('a');
+
+  if (!a) {
+    return;
+  }
+
+  e.preventDefault();
+  largeImg.setAttribute('src', a.href);
+});
