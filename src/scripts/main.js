0d525d1f6fea1ecddfba1b14bd4b2bc72e661e51
Nick_Radchuk <mukola.radchuk@gmail.com>
2022-12-12 20:15:36 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const largeImage = document.querySelector('.gallery__large-img');
+const smallImage = document.querySelectorAll('.list-item__link');
+
+for (const img of smallImage) {
+  img.addEventListener('click', (e) => {
+    e.preventDefault();
+    largeImage.src = e.currentTarget.href;
+  });
+}
