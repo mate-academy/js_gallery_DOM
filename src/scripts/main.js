c1dad32e20863c4b018dc83f9e0e9dbe7f9ed4e8
Mykola Hadupiak <mykola.hadupiak@gmail.com>
2022-12-12 19:50:08 +0200

    Gallery solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const list = document.querySelector('.gallery__list');
+const mainImg = document.querySelector('.gallery__large-img');
+
+list.addEventListener('click', function(eventClick) {
+  eventClick.preventDefault();
+
+  const target = eventClick.target.closest('.list-item__link');
+
+  if (!target || !list.contains(target)) {
+    return;
+  }
+
+  mainImg.src = target.href;
+});
