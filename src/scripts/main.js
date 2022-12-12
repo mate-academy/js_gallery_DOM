5ee0c0e17e0ed57695db28695b78bb689ab2503d
Elena Ver <hellena84@i.ua>
2022-12-12 16:41:15 +0100

    my commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const bigPicture = document.querySelector('#largeImg');
+
+const list = document.querySelector('#thumbs');
+
+list.addEventListener('click', (item) => {
+  const ziel = item.target.closest('.list-item__link');
+
+  item.preventDefault();
+
+  bigPicture.src = ziel.href;
+});
