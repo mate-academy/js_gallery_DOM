1c0b4b8bbafd5f0677ae82689819e2ce9abb4664
Aleks <offsset@gmail.com>
2022-12-14 14:13:29 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
+/* eslint-disable no-shadow */
 'use strict';
+
+const mainImage = document.getElementById('largeImg');
+const additionalImage = document.getElementById('thumbs');
+
+additionalImage.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const item = event.target.closest('.list-item__link');
+
+  if (!item) {
+    return;
+  }
+
+  mainImage.src = item.href;
+});
