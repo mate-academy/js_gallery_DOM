8f0047dbdaf943a065eae86b988a17ffe9759340
Oselskyi Oleksandr <oselskyi3@gmail.com>
2022-12-18 11:24:38 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const gallery = document.querySelector('.gallery');
+const largeImg = document.querySelector('.gallery__large-img');
+const list = document.querySelector('.gallery__list');
+
+gallery.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const item = e.target.closest('.gallery__thumb');
+
+  if (item || list.contains(item)) {
+    largeImg.src = e.target.parentElement.href;
+  }
+});
