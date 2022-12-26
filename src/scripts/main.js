05cf85f9dfff32fd183c9fcca72b102e71c3468d
Rafael Mamedov <rafmamedov@gmail.com>
2022-12-26 17:07:44 +0200

    removed extra variables

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,16 +3,10 @@
 const list = document.querySelector('.gallery__list');
 const largeImage = document.querySelector('.gallery__large-img');
 
-// eslint-disable-next-line no-shadow
-list.addEventListener('click', (event) => {
-  const item = event.target.closest('.list-item__link');
-  const itemImg = item.getAttribute('href');
+list.addEventListener('click', (eventClick) => {
+  const item = eventClick.target.closest('.list-item__link');
 
-  event.preventDefault();
+  eventClick.preventDefault();
 
-  if (!item || !list.contains(item)) {
-    return;
-  }
-
-  largeImage.setAttribute('src', itemImg);
+  largeImage.src = item.href;
 });
