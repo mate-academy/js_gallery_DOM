3edf4f8daccde7f72d266db47f82d010f1df87e7
Denis Bokov <denisbokov1703@gmail.com>
2022-12-26 16:22:14 +0100

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const img = document.querySelector('#largeImg');
+
+const list = document.querySelectorAll('li');
+
+list.forEach(element => {
+  element.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    img.src = event.target.closest('.list-item__link').href;
+  });
+});
