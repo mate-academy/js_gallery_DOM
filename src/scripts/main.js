93be66f85cb7c7dd948f521930962438f5db52a3
Rafael Mamedov <rafmamedov@gmail.com>
2022-12-26 16:57:12 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const list = document.querySelector('.gallery__list');
+const largeImage = document.querySelector('.gallery__large-img');
+
+// eslint-disable-next-line no-shadow
+list.addEventListener('click', (event) => {
+  const item = event.target.closest('.list-item__link');
+  const itemImg = item.getAttribute('href');
+
+  event.preventDefault();
+
+  if (!item || !list.contains(item)) {
+    return;
+  }
+
+  largeImage.setAttribute('src', itemImg);
+});
