c47306521dba9adf7784a8a6b3c320262003289e
Kukharuk <its.up.to.you.dk@gmail.com>
2022-12-29 14:23:35 +0200

    gallery

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const ul = document.querySelector('ul');
+const img = document.querySelector('#largeImg');
+const eventName = 'click';
+
+const func = (action) => {
+  action.preventDefault();
+
+  if (action.target.tagName === 'IMG') {
+    img.src = action.target.src;
+    img.style.height = '348px';
+  }
+};
+
+ul.addEventListener(eventName, func);
