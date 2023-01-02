0c122eace3871db373022e089858770425305885
Nazarii Kyselevych <nazarkysel@gmail.com>
2023-01-02 12:44:29 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const mainImage = document.querySelector('#largeImg');
+const thumbnails = document.querySelector('#thumbs');
+
+thumbnails.addEventListener('click', e => {
+  e.preventDefault();
+
+  const link = e.target.closest('.list-item__link');
+
+  if (!link) {
+    return;
+  }
+
+  mainImage.src = link.href;
+});
