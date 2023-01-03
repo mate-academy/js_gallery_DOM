b0b4ce9ce49641c80c0783e5bb19ef6236d77f49
Ivan Vyhinnyi <ivan665303@gmail.com>
2023-01-03 13:36:57 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const imageBig = document.querySelector('#largeImg');
+const links = document.querySelectorAll('.list-item');
+
+links.forEach(link => link.addEventListener('click', (ev) => {
+  ev.preventDefault();
+
+  const targetImgAttr = ev.target.parentElement;
+
+  imageBig.setAttribute('src', targetImgAttr.getAttribute('href'));
+}));
