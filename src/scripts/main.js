58afa698e1a00317ebebe9ac70a81b46ad8304a2
Nick Busarhin <busarginkolia@gmail.com>
2023-01-04 19:56:13 +0200

    gallery

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const smallImage = document.querySelector('#thumbs');
+const bigImage = document.querySelector('#largeImg');
+
+const a = (e) => {
+  e.preventDefault();
+
+  const picture
+    = e.target.className === 'gallery__img gallery__thumb'
+      ? e.target.parentElement.href
+      : e.target.href;
+
+  bigImage.src = picture;
+};
+
+smallImage.addEventListener('click', a);
