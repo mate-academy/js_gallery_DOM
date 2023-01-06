2a278a8d2ffee59ae726073c1850f64c04a2f2b3
Oleksii Mishchenko <alex.mishch@gmail.com>
2023-01-06 05:12:44 +0200

    Image gallery created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const largeImg = document.getElementById('largeImg');
+const thumbs = document.getElementById('thumbs');
+
+thumbs.addEventListener('click', (evnt) => {
+  const item = evnt.target.closest('.list-item__link');
+
+  if (!item || !thumbs.contains(item)) {
+    return;
+  }
+
+  evnt.preventDefault();
+  largeImg.src = item.href;
+});
