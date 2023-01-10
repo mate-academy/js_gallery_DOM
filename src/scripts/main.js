58f324434c3e602fe6487552cac904a287773096
Ihor Chaikovskyi <ihorchaikovskyiweb@gmail.com>
2023-01-10 17:00:56 +0200

    first attempt

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const menu = document.getElementById('thumbs');
+const mainImg = document.getElementById('largeImg');
+
+menu.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const img = e.target.closest('.list-item__link');
+
+  mainImg.src = img.href;
+});
