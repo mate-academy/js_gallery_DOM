516d84341b1df30e2a61eb450b80952506ef001c
EhorSaprykin <astralik1993@gmail.com>
2023-01-11 12:08:24 +0200

    All done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const largeImage = document.querySelector('.gallery__large-img');
+const links = document.querySelectorAll('.list-item__link');
+
+links.forEach(link => {
+  link.addEventListener('click', (item) => {
+    item.preventDefault();
+    largeImage.src = link.href;
+  });
+});
