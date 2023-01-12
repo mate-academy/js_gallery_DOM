d037467f5a8d1ef45e4847df23a87a7768e78179
divineretribution1 <danya00776@gmail.com>
2023-01-12 16:51:58 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const gallery = document.getElementById('thumbs');
+const mainImage = document.getElementById('largeImg');
+
+gallery.addEventListener('click', (ev) => {
+  ev.preventDefault();
+
+  const image = ev.target.closest('.list-item__link');
+
+  mainImage.src = image.href;
+});
