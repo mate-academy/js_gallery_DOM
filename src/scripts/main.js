b903893db329d8062bdc0d778673c5f2f21191fc
Anna Akhalmosulishvili <akhala33@gmail.com>
2023-01-13 12:25:02 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const mainImg = document.querySelector('.gallery__large-img');
+const galleryItems = [...document.querySelectorAll('.list-item')];
+
+for (const item of galleryItems) {
+  const link = item.children[0];
+  const newSrc = link.href;
+
+  link.addEventListener('click', (newEvent) => {
+    newEvent.preventDefault();
+
+    mainImg.src = newSrc;
+  });
+};
