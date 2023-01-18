8d25b890641ee4033c2af6ca31675db781458565
Kate <ekaterina2002aleks@gmail.com>
2023-01-18 21:19:10 +0200

    Solution for gallery

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const root = document.querySelector('.gallery');
+const largeImg = root.querySelector('#largeImg');
+const galleryList = root.querySelector('.gallery__list');
+const links = root.querySelectorAll('.list-item__link');
+
+for (const link of links) {
+  link.addEventListener('click', function(event_) {
+    event_.preventDefault();
+  });
+}
+
+galleryList.addEventListener('click', (event_) => {
+  if (!event_.target.matches('.gallery__thumb')) {
+    return;
+  }
+
+  const listLink = event_.target.closest('.list-item__link');
+  const listItemSrc = listLink.getAttribute('href');
+
+  largeImg.setAttribute('src', listItemSrc);
+});
