728afdc6781b68bc7ea173cd746d774f7ece4f09
Vadym Prydatok <vadymprydatok@gmail.com>
2023-01-19 20:24:22 +0200

    gallery_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const mainView = document.querySelector('#largeImg');
+const thumbsList = document.querySelector('#thumbs');
+
+thumbsList.addEventListener('click', (e) => {
+  const link = e.target.closest('.list-item__link');
+
+  e.preventDefault();
+  mainView.setAttribute('src', link.href);
+});
