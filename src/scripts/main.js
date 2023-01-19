7772501c2ae30edbcbde9682cb42cdb874bf42b2
Kate <ekaterina2002aleks@gmail.com>
2023-01-19 12:49:45 +0200

    fix event

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,17 +6,17 @@ const galleryList = root.querySelector('.gallery__list');
 const links = root.querySelectorAll('.list-item__link');
 
 for (const link of links) {
-  link.addEventListener('click', function(event_) {
-    event_.preventDefault();
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
   });
 }
 
-galleryList.addEventListener('click', (event_) => {
-  if (!event_.target.matches('.gallery__thumb')) {
+galleryList.addEventListener('click', (e) => {
+  if (!e.target.matches('.gallery__thumb')) {
     return;
   }
 
-  const listLink = event_.target.closest('.list-item__link');
+  const listLink = e.target.closest('.list-item__link');
   const listItemSrc = listLink.getAttribute('href');
 
   largeImg.setAttribute('src', listItemSrc);
