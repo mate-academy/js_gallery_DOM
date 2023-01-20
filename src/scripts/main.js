35da5a8404a4755906d9e8e5401d5ff0e66592c0
Taras Baran <baranprivat22@gmail.com>
2023-01-20 13:36:45 +0200

    finished

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,21 @@
 'use strict';
+
+window.addEventListener('DOMContentLoaded', () => {
+  const largeImg = document.querySelector('#largeImg');
+  const imgList = document.querySelector('.gallery__list');
+
+  imgList.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const target = e.target;
+    const isImg = target.classList.contains('gallery__img');
+
+    if (isImg) {
+      const imgParentHref = target
+        .closest('a.list-item__link')
+        .getAttribute('href');
+
+      largeImg.setAttribute('src', imgParentHref);
+    }
+  });
+});
