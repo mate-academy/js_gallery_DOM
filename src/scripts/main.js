33025a0f1aee5bf56a7ade355532919d97393d14
abobyr <artem.bobyr@dataart.com>
2023-01-21 20:49:30 +0200

    task is done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const images = document.querySelectorAll('a');
+const largeImage = document.getElementById('largeImg');
+
+images.forEach(image => {
+  image.addEventListener('click', e => {
+    e.preventDefault();
+    largeImage.src = image.href;
+  });
+});
