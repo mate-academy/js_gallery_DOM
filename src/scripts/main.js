210878113daf7017bfc64017f0c33f3392ed4b64
valntyn <romanenko.valentyn1@gmail.com>
2023-01-22 19:22:14 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const mainImage = document.getElementById('largeImg');
+const list = document.querySelector('ul');
+
+list.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const item = e.target.closest('.list-item');
+
+  if (!item || !list.contains(item)) {
+    return;
+  }
+
+  const imgSmall = item.querySelector('a');
+
+  mainImage.src = imgSmall.href;
+});
