fd026717858f72fa330d22bf25cb96e902a231fb
Vadym Prydatok <vadymprydatok@gmail.com>
2023-01-24 19:07:50 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,8 +4,9 @@ const mainView = document.querySelector('#largeImg');
 const thumbsList = document.querySelector('#thumbs');
 
 thumbsList.addEventListener('click', (e) => {
+  e.preventDefault();
+
   const link = e.target.closest('.list-item__link');
 
-  e.preventDefault();
   mainView.setAttribute('src', link.href);
 });
