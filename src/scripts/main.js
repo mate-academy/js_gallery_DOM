f0c1d72233478373804fd3ab91a509e369b85229
Mykhailo Orlov <mihalikmihalik85@gmail.com>
2023-01-26 22:37:11 +0200

    tests good

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const largeImg = document.querySelector('.gallery__large-img');
+const list = document.querySelector('.gallery__list');
+
+list.addEventListener('click', (event) => {
+  event.preventDefault();
+  largeImg.src = event.target.closest('a');
+});
