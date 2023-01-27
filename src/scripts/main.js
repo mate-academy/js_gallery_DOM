9b977066ea2e2de35a0f968aa97ea609ad171e35
Anastasia Radchenko <radchenckooo@gmail.com>
2023-01-27 14:29:11 +0200

    images

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const images = document.querySelectorAll('a');
+const mainImage = document.querySelector('#largeImg');
+
+images.forEach(image => {
+  image.addEventListener('click', click => {
+    click.preventDefault();
+    mainImage.src = image.href;
+  });
+});
