7cf5b1c628f9f133ece4ff1171ceb08c05e201f1
Golibroda <dgolibroda@god.de>
2023-02-02 18:50:56 +0200

    did the task, but there some bugs with connection to css file, so, you can just check the code ;)

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const list = document.body.querySelector('#thumbs');
+
+list.addEventListener('click', myEvent => {
+  if (myEvent.target === list) {
+    return;
+  }
+
+  myEvent.preventDefault();
+
+  let currentImage = myEvent.target.querySelector('img');
+
+  if (!currentImage) {
+    currentImage = myEvent.target;
+  }
+
+  const largeImage = document.body.querySelector('#largeImg');
+  const currentImageSrcBeforeChanges = currentImage.src;
+
+  currentImage.src = largeImage.src;
+  largeImage.src = currentImageSrcBeforeChanges;
+});
