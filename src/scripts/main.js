5414225a0b86822712702c946eb4c0171142128d
Yulia Borshovska <yulya.borshovska@gmail.com>
2023-02-02 21:41:46 -0800

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const largeImage = document.getElementById('largeImg');
+const thumbs = document.getElementById('thumb');
+
+thumbs.addEventListener('click', elem => {
+  elem.preventDefault();
+
+  const link = elem.target.closest('.list-item__link').href;
+
+  largeImage.src = link;
+});
