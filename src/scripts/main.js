1f1e00242c3b9c8047ed0ea7ca3ca87e780d2e0a
Ukrainian_panda <94929618+Ukrainiane-panda@users.noreply.github.com>
2023-02-03 19:56:29 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const list = document.querySelector('.gallery__list');
+const blockImg = document.querySelector('.gallery__large-img');
+
+list.addEventListener('click', (e) => {
+  const item = e.target.closest('.list-item__link');
+
+  e.preventDefault();
+
+  blockImg.src = item.href;
+});
