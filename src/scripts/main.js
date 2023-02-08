1947358a9c9d446d1dbaf6d48456494525a96a2d
Sergei Nikolaienko <serjexdeath@gmail.com>
2023-02-08 23:37:36 +0200

    Task complete

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+const mainPic = document.querySelector('.gallery__large-img');
+const previewLinks = document.querySelectorAll('list-item__link');
+
+for (const link of previewLinks) {
+  link.addEventListener('click', (item) => {
+    item.preventDefault();
+    mainPic.src = link.href;
+  });
+}
