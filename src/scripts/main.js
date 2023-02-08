6570736b182ffa5a1e4543d2630e7e436e6bcd92
haduigon <haduigon@gmail.com>
2023-02-08 23:08:47 +0200

    fisrt gallery commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+
+let bigImg = document.querySelector('.gallery__large-img');
+
+
+let imgList = document.querySelector('.gallery__list');
+
+imgList.addEventListener('click', (event) => {
+
+  event.preventDefault();
+  let closest = event.target.closest('.list-item');
+  bigImg.attributes[1].value = closest.children[0].attributes.href.value;
+
+});
+
+
+
