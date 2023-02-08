f63169c51b692136d77fb68cc6cc091118e099f5
Sergei Nikolaienko <serjexdeath@gmail.com>
2023-02-08 23:45:05 +0200

    Another solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,18 @@
 'use strict';
-const mainPic = document.querySelector('.gallery__large-img');
-const previewLinks = document.querySelectorAll('list-item__link');
 
-for (const link of previewLinks) {
-  link.addEventListener('click', (item) => {
-    item.preventDefault();
-    mainPic.src = link.href;
-  });
-}
+const mainPic = document.querySelector('.gallery__large-img');
+const list = document.querySelector('ul');
+
+list.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const item = e.target.closest('.list-item');
+
+  if (!item || !list.contains(item)) {
+    return;
+  }
+
+  const previewLink = item.querySelector('a');
+
+  mainPic.src = previewLink.href;
+});
