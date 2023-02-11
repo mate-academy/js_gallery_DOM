ba0f2b5020d08359b37e99f840a75b1ce5296ec5
Ivan <90928916+vanyachyzh@users.noreply.github.com>
2023-02-11 16:55:28 +0200

    task was done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const gallery = document.querySelector('.gallery');
+const largeImg = document.querySelector('.gallery__large-img');
+
+gallery.addEventListener('click', (e) => {
+  const target = e.target.closest('.list-item');
+  const src = target.firstElementChild.getAttribute('href');
+
+  if (!target) {
+    return;
+  }
+
+  e.preventDefault();
+  largeImg.setAttribute('src', src);
+});
