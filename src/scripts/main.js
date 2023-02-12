6efcd1b22ff6ddb079b59824b7b1e54c7e62d696
Jaxxxon78 <jaxxxon78@ukr.net>
2023-02-12 17:47:21 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const gallery = document.querySelector('.gallery__list');
+const bigImage = document.querySelector('.gallery__large-img');
+
+gallery.addEventListener('click', (el) => {
+  bigImage.src = el.target.parentNode.href;
+  el.preventDefault();
+});
