3c5fc80e3ab04ee7409bae0ea19b311de7ec3ca0
Vasyl Pavlenko <genlus@ukr.net>
2023-02-13 14:30:22 +0200

    Gallery

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const mainImage = document.querySelector('#largeImg');
+const list = document.querySelector('ul');
+
+list.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const item = e.target.closest('.list-item');
+
+  if (!item || !list.contains(item)) {
+    return;
+  }
+
+  const smallImg = item.querySelector('a');
+
+  mainImage.src = smallImg.href;
+});
