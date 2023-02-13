b574884b769b563ab761508a41a692368ee78fa2
Vitalii Borzov <amirborzo99@gmail.com>
2023-02-13 08:41:23 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const gallery = document.querySelector('.gallery');
+
+gallery.addEventListener('click', e => {
+  e.preventDefault();
+
+  const bigImg = document.querySelector('.gallery__large-img');
+
+  const smallImg = e.target.closest('.gallery__img');
+
+  if (smallImg && smallImg.id !== 'largeImg') {
+    const link = smallImg.parentElement.href;
+
+    bigImg.setAttribute('src', link);
+  }
+});
