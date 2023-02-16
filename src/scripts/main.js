a10c6bb11b4bfb3655d8f1f3c1e68b43322e0ff1
Nigga <yomzfk@gmail.com>
2023-02-16 12:48:33 +0200

    Gallery

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const largeImg = document.getElementById('largeImg');
+const thumbs = document.getElementById('thumbs');
+
+thumbs.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const link = e.target.closest('.list-item__link').href;
+
+  largeImg.src = link;
+});
