df39f70f64e2b203656aa8301d5925aa9f5fdaf6
Nick <tortickk8475@gmail.com>
2023-02-16 20:19:56 +0200

    add click on imgs

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const centralFrame = document.querySelector('.gallery__large-img');
+const images = document.querySelectorAll('.list-item__link');
+
+for (const image of images) {
+  image.addEventListener('click', (events) => {
+    const img = image.firstElementChild;
+
+    centralFrame.src = image.href;
+
+    if (events.target === img || events.target === image) {
+      events.preventDefault();
+    }
+  });
+}
