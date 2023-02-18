28368990db6f841b88dcc0b17ec731b030f0fa32
KseniyaGonchar <kseniyadf888@gmail.com>
2023-02-18 23:39:57 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const list = document.querySelector('.gallery__list');
+const largeImg = document.querySelector('#largeImg');
+
+list.addEventListener('click', (e) => {
+  const picture = e.target;
+
+  e.preventDefault();
+
+  const href = 'http://localhost:8080/images'
+  + picture.getAttribute('src').toString().split('-').slice(0, -1).join('-')
+  + '.png';
+
+  if (picture.classList.contains('.list-item__link')) {
+    largeImg.setAttribute('src', href);
+  }
+
+  largeImg.setAttribute('src', href);
+});
