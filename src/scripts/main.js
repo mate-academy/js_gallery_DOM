a2b6c9158b97e39c6f4343051fe9e6ce53643338
Nazarii AlieKsieiev <nazarmetall@gmail.com>
2023-02-18 16:46:30 +0200

    Solution gallery

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const thumbs = document.querySelector('#thumbs');
+const largeImg = document.querySelector('#largeImg');
+
+thumbs.addEventListener('click', e => {
+  e.preventDefault();
+
+  if (e.target.nodeName !== 'img') {
+    return;
+  }
+
+  largeImg.src = e.target.parentNode.href;
+});
