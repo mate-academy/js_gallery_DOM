0cf83a31d86828aa9dd849530a52cfda4f5591ed
Vadym Tsyndra <Mr.Tsyndra@gmail.com>
2023-02-19 13:00:58 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const galleryList = document.querySelector('.gallery__list');
+const largeImg = document.querySelector('.gallery__large-img');
+
+galleryList.addEventListener('click', (e) => {
+  const listItem = e.target.closest('a');
+
+  if (!listItem) {
+    return;
+  }
+
+  showThumbnail(listItem.href, listItem.title);
+  event.preventDefault();
+});
+
+function showThumbnail(href, title) {
+  largeImg.src = href;
+  largeImg.alt = title;
+}
