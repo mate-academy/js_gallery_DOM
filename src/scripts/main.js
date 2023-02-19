a679982391c0b5cae8b6a655b373a9e17314d170
Vadym Tsyndra <Mr.Tsyndra@gmail.com>
2023-02-19 18:05:31 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,13 +4,13 @@ const galleryList = document.querySelector('.gallery__list');
 const largeImg = document.querySelector('.gallery__large-img');
 
 galleryList.addEventListener('click', (e) => {
+  e.preventDefault();
+
   const listItem = e.target.closest('a');
 
   if (!listItem) {
     return;
   }
-
-  e.preventDefault();
 
   largeImg.src = listItem.href;
   largeImg.alt = listItem.title;
