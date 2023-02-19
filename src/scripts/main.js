933c1d61edeeea0f0b9876576cc532f9c12634f8
mykolachupyak <chupyak@gmail.com>
2023-02-19 21:59:55 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const largeImage = document.querySelector('#largeImg');
+const smalImages = document.querySelectorAll('a');
+
+for (const item of smalImages) {
+  // eslint-disable-next-line no-shadow
+  item.addEventListener('click', function(event) {
+    event.preventDefault();
+    largeImage.src = item.href;
+  });
+}
