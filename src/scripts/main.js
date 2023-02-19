04537d1c297b8644b907b2484669ac3508fecf99
mykolachupyak <chupyak@gmail.com>
2023-02-19 22:09:57 +0200

    improvement

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,9 +4,8 @@ const largeImage = document.querySelector('#largeImg');
 const smalImages = document.querySelectorAll('a');
 
 for (const item of smalImages) {
-  // eslint-disable-next-line no-shadow
-  item.addEventListener('click', function(event) {
-    event.preventDefault();
+  item.addEventListener('click', (myFunction) => {
+    myFunction.preventDefault();
     largeImage.src = item.href;
   });
 }
