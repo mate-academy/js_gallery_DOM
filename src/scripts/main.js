3c08a9f7e8b9587f5f8f759359202c46ce1db9ed
Yevhenii Hura <yevheniihura@gmail.com>
2023-02-20 20:17:27 +0200

    the task was solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const list = document.querySelector('.gallery__list');
+const bigImage = document.querySelector('.gallery__large-img');
+
+list.addEventListener('click', (occasion) => {
+  const item = occasion.target.closest('.list-item__link');
+
+  occasion.preventDefault();
+
+  if (!item || !list.contains(item)) {
+    return;
+  }
+
+  bigImage.src = item.href;
+});
