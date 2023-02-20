799afd129882aecf35e448271decab267c533ab6
mykolachupyak <chupyak@gmail.com>
2023-02-20 09:49:17 +0200

    improvement

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,8 +4,8 @@ const largeImage = document.querySelector('#largeImg');
 const smalImages = document.querySelectorAll('a');
 
 for (const item of smalImages) {
-  item.addEventListener('click', (myFunction) => {
-    myFunction.preventDefault();
+  item.addEventListener('click', (eventFunc) => {
+    eventFunc.preventDefault();
     largeImage.src = item.href;
   });
 }
