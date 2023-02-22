4ef34506c1dc92162b6519a7e27ccda569e9d53e
Dmytro Hubych <dh.hubych@gmail.com>
2023-02-23 00:39:20 +0200

    develop-second

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const list = document.querySelector('#thumbs');
+const links = [...list.querySelectorAll('a')];
+const largeImg = document.querySelector('#largeImg');
+
+links.forEach(el => {
+  el.removeAttribute('href');
+})
+
+list.addEventListener('click', (event) => {
+  const item = event.target;
+  const clicedImgLink = item.getAttribute('src');
+
+  largeImg.setAttribute('src', clicedImgLink)
+})
