56a91814199a0f6c7748ad3081b70cf9a10837a3
Dmytro Hubych <dh.hubych@gmail.com>
2023-02-23 12:39:32 +0200

    Fixes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,12 +5,16 @@ const links = [...list.querySelectorAll('a')];
 const largeImg = document.querySelector('#largeImg');
 
 links.forEach(el => {
+  const href = el.href;
+
+  el.setAttribute('data-link-img-large', href);
+  el.querySelector('img').setAttribute('data-link-img-large', href);
   el.removeAttribute('href');
-})
+});
 
-list.addEventListener('click', (event) => {
-  const item = event.target;
-  const clicedImgLink = item.getAttribute('src');
+list.addEventListener('click', (myEvent) => {
+  const item = myEvent.target;
+  const clicedImgLink = item.getAttribute('data-link-img-large');
 
-  largeImg.setAttribute('src', clicedImgLink)
-})
+  largeImg.setAttribute('src', clicedImgLink);
+});
