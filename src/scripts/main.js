5d2dbaf1dd3192c2f8839846c28b7f4b529af3ac
Nikitin Evgeny <ngenya81@gmail.com>
2023-02-23 15:47:24 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const ColectionList = document.querySelector('.gallery__list');
+const ImegLarge = document.querySelector('.gallery__large-img');
+
+ColectionList.addEventListener('click', (E) => {
+  const list = E.target.closest('a');
+
+  if (!list) {
+    return;
+  }
+
+  showGalery(list.href, list.title);
+  E.preventDefault();
+});
+
+function showGalery(href, title) {
+  ImegLarge.src = href;
+  ImegLarge.alt = title;
+}
