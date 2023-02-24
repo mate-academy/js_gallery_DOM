673955b14e1853e2d34242b2acfacc3b544e0944
Vlad <bojko.vladislav16@gmail.com>
2023-02-24 11:08:01 +0200

    Gallery was implemented

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
 'use strict';
+const largeImg = document.getElementById('largeImg');
+const galleryList = document.getElementById('thumbs');
+
+galleryList.addEventListener('click', e => {
+  const link = e.target.closest('a');
+
+  e.preventDefault();
+  largeImg.src = link.href;
+});
+
+galleryList.addEventListener('mouseover', e => {
+  if (e.target.tagName === 'IMG') {
+    e.target.style.boxShadow = '0 0 10px rgba(0, 0, 0, 0.5)';
+  }
+});
+
+galleryList.addEventListener('mouseout', e => {
+  if (e.target.tagName === 'IMG') {
+    e.target.style.boxShadow = '';
+  }
+});
