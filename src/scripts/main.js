50e79c548ed7a8d33ace8c06566719d02a2b00d2
Nikitin Evgeny <ngenya81@gmail.com>
2023-02-24 18:50:04 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,15 +3,15 @@
 const colectionList = document.querySelector('.gallery__list');
 const imegLarge = document.querySelector('.gallery__large-img');
 
-colectionList.addEventListener('click', (E) => {
-  const list = E.target.closest('a');
+colectionList.addEventListener('click', (e) => {
+  const list = e.target.closest('a');
 
   if (!list) {
     return;
   }
 
   showGalery(list.href, list.title);
-  E.preventDefault();
+  e.preventDefault();
 });
 
 function showGalery(href, title) {
