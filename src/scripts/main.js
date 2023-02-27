5a871a598e8ce2a7c144ce3c3345d39efd5f684a
Iryna Konovalova <iruna191020@gmail.com>
2023-02-27 19:25:42 +0100

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const mainImage = document.getElementById('largeImg');
+const list = document.getElementById('thumbs');
+const links = document.body.querySelectorAll('.list-item__link');
+
+links.forEach(link => {
+  link.onclick = function() {
+    return false;
+  };
+});
+
+// eslint-disable-next-line no-shadow
+list.addEventListener('click', function(event) {
+  const target = event.target.closest('.gallery__thumb');
+  const link = event.target.closest('.list-item__link');
+
+  if (!target) {
+    return;
+  };
+
+  if (!list.contains(target)) {
+    return;
+  };
+
+  mainImage.src = link.href;
+});
