77c1e02e28e494967a5e9e7903a7ec18a3289933
Mykola Huryn <mukola197022@gmail.com>
2023-03-01 12:46:33 +0100

    js_gallery_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const listGallery = document.getElementById('thumbs');
+const largeImage = document.getElementById('largeImg');
+
+listGallery.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const item = e.target.closest('.list-item__link');
+
+  largeImage.src = item.href;
+});
