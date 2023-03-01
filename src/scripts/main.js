49bb4679bfda4a902d1ec3b8c559806742a42e2e
Nataliia Batiukova <batiukova.nataliia@gmail.com>
2023-03-01 20:16:54 +0000

    the task was solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const mainImg = document.querySelector('#largeImg');
+const list = document.querySelector('#thumbs');
+
+list.addEventListener('click', () => {
+  const item = event.target.closest('.gallery__img');
+
+  mainImg.src = item.src;
+
+  event.preventDefault();
+});
