08c2e7cbc5d3e6dc9393e691e444b6702408761d
Mykola Butylkov <kokos.ibo.58@gmail.com>
2023-03-02 11:07:01 +0200

    Solution-1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,21 @@
 'use strict';
+
+// debugger;
+
+const largeImg = document.getElementById('largeImg');
+
+// const srcLargeImg = largeImg.getAttribute('src');
+
+// const linksListItem = document.querySelectorAll('.list-item__link');
+const imgesListItem = document.querySelectorAll('.gallery__img');
+
+for (const img of imgesListItem) {
+  img.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const srcObject = img.getAttribute('src');
+
+    largeImg.setAttribute('src', srcObject);
+    largeImg.style.height = '348px';
+  });
+}
