481294bb85564df3ec53f8e94a7b2fa4c8d793eb
Serhii Nikolin <sergeynikolin@gmail.com>
2023-03-03 15:25:01 +0200

    Ready

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
+/* eslint-disable no-shadow */
 'use strict';
+
+const list = document.getElementById('thumbs');
+
+function showImage(event) {
+  const targetLink = event.target.closest('.list-item__link');
+  const largeImage = document.querySelector('#largeImg');
+
+  if (!targetLink) {
+    return;
+  }
+
+  largeImage.src = targetLink.href;
+  event.preventDefault();
+};
+
+list.addEventListener('click', showImage);
