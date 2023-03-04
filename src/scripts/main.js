2f0d641f13009ef12e61a308376a1f0c9811b54e
Anastasiia Kuznetsova <an.stsii.k@gmail.com>
2023-03-04 16:34:49 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const largeImage = document.querySelector('#largeImg');
+const thumbs = document.querySelectorAll('.list-item');
+
+thumbs.forEach(img => {
+  img.addEventListener('click', ev => {
+    ev.preventDefault();
+    largeImage.src = ev.target.closest('.list-item__link').href;
+  });
+});
