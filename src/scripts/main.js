984a4460db46f61de343f928fb6cc89d98209210
Vadim Drobyazko <volandemortuwka@gmail.com>
2023-03-05 15:52:42 +0200

    gallery

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const listGallery = document.querySelector('.gallery__list');
+const largeImg = document.querySelector('.gallery__large-img');
+
+listGallery.addEventListener('click', (item) => {
+  item.preventDefault();
+
+  const slide = item.target.closest('.list-item__link');
+
+  largeImg.src = slide.href;
+});
