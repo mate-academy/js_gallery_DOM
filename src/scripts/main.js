b234a0b1c03dee79917ad3d97c4d2648a32232c4
Roman Kulchytskyi-Dashynych <kdrs0812@gmail.com>
2023-03-07 15:35:22 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const smallPictures = document.querySelector('#thumbs');
+const bigPicture = document.querySelector('#largeImg');
+
+smallPictures.addEventListener('click', (element) => {
+  element.preventDefault();
+
+  bigPicture.src = element.target.closest('.list-item__link').href;
+});
