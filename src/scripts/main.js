1353c5f55165ba1a30f20d3f5c549537ce34888a
Nazarii Piatnochka <nazar.piatnochka@gmail.com>
2023-03-08 19:28:53 +0200

    Task completed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const galleryList = document.querySelector('.gallery__list');
+const mainImage = document.querySelector('.gallery__large-img');
+
+galleryList.addEventListener('click', (ev) => {
+  const item = ev.target.closest('.list-item__link');
+
+  if (!item) {
+    return;
+  }
+
+  ev.preventDefault();
+  mainImage.src = item.href;
+});
