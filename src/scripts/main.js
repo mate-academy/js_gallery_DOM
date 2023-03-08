38ae5adcd83e236cab603f0dabd81599eafe1038
Tolik Petras <toliketras4@gmail.com>
2023-03-08 17:14:09 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const blockForImg = document.querySelector('#largeImg');
+
+const gallery = document.querySelector('#thumbs');
+
+gallery.addEventListener('click', (element) => {
+  element.preventDefault();
+
+  blockForImg.src = element.target.closest('.list-item__link').href;
+});
