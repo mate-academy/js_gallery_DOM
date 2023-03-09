d816faf42eea84f70bda3014e09e6d2365ef7b05
Andrew Litkovets <andriylao@gmail.com>
2023-03-09 22:41:40 +0200

    Task done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const thumbs = document.getElementById('thumbs');
+const largeImg = document.getElementById('largeImg');
+
+thumbs.onclick = (image) => {
+  const thumbItem = image.target.closest('a');
+
+  if (!thumbItem) {
+    return;
+  }
+
+  largeImg.src = thumbItem.href;
+  largeImg.alt = thumbItem.title;
+  image.preventDefault();
+};
