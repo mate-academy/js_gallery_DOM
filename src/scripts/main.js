5868b412b408de5940ce65cbd8b4ee0d609018e3
Ilona Romaniuk <ilona09041983@gmail.com>
2023-03-10 12:37:49 +0200

    Change image src in gallery with javascript

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const mainIcon = document.querySelector('#largeImg');
+const smallIconsList = document.querySelector('#thumbs');
+
+smallIconsList.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const icon = e.target.closest('.list-item');
+
+  if (!icon) {
+    return;
+  }
+
+  mainIcon.src = icon.querySelector('.list-item__link').href;
+});
