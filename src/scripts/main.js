f36f3343cb8e92aa3959322c9a8efccbef36dc50
Dmitriy Rybak <ryba@MacBook-Air-Dmitriy.local>
2023-03-12 12:46:02 -0500

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
+/* eslint-disable no-shadow */
 'use strict';
+
+const largeImg = document.querySelector('.gallery__large-img');
+const smallImgList = document.querySelector('.gallery__list');
+
+smallImgList.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  largeImg.src = event.target.closest('a').href;
+});
