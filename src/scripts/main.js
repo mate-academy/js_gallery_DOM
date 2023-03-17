d6e38e1767578622650d14dede45b01dc70e05c3
MarynaTovtyn <marina6782@gmail.com>
2023-03-17 21:59:18 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const galleryList = document.querySelector('.gallery__list');
+const mainImage = document.querySelector('.gallery__large-img');
+
+galleryList.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const item = e.target;
+
+  if (!item.matches('img')) {
+    return;
+  };
+
+  mainImage.src = item.parentElement.href;
+});
