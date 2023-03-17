9f2ed11e5405856e358146f5b956a274579773fd
Vladyslav Kulybaba <106146104+VladGraund@users.noreply.github.com>
2023-03-17 11:18:11 +0100

    complete task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const list = document.querySelector('.gallery__list');
+const container = document.querySelector('#largeImg').parentElement;
+
+list.addEventListener('click', (e) => {
+  const item = e.target;
+
+  e.preventDefault();
+  container.innerHTML = item.outerHTML;
+  container.firstChild.setAttribute('id', 'largeImg');
+  container.firstChild.classList.add('gallery__large-img');
+  container.firstChild.classList.remove('gallery__thumb');
+  container.firstChild.src = item.parentElement.href;
+});
