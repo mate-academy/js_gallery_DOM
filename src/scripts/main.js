3f6f739ed59c25bc2039bd9aa9235699bbae1ef1
Vladyslav Kulybaba <106146104+VladGraund@users.noreply.github.com>
2023-03-17 12:18:21 +0100

    fix task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,15 +1,11 @@
 'use strict';
 
 const list = document.querySelector('.gallery__list');
-const container = document.querySelector('#largeImg').parentElement;
+const container = document.querySelector('#largeImg');
 
 list.addEventListener('click', (e) => {
   const item = e.target;
 
   e.preventDefault();
-  container.innerHTML = item.outerHTML;
-  container.firstChild.setAttribute('id', 'largeImg');
-  container.firstChild.classList.add('gallery__large-img');
-  container.firstChild.classList.remove('gallery__thumb');
-  container.firstChild.src = item.parentElement.href;
+  container.src = item.parentElement.href;
 });
