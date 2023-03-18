ef85403e51acfc5fa150dd8ab166905eadc830ef
Max Zelenskyi <rio1391@gmail.com>
2023-03-18 15:36:56 +0100

    Image changer

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const pictures = document.querySelector('.gallery__list');
+const largePicture = document.querySelector('#largeImg');
+
+pictures.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const item = e.target.closest('.list-item');
+
+  if (!item || !pictures.contains(item)) {
+    return;
+  }
+
+  largePicture.src = item.querySelector('.list-item__link').href;
+});
