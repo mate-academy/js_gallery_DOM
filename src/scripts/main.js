6ac793f39040bfedb76f2a68885371bb339b0781
IvanVaverchak <ivanvaverchak@2000gmail.com>
2023-03-20 16:14:49 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const gallery = document.querySelector('.gallery__list');
+const mainPhoto = document.querySelector('#largeImg');
+
+gallery.addEventListener('click', (eve) => {
+  const link = eve.target.closest('list-item__link');
+  const item = eve.target.closest('.gallery__img');
+
+  eve.preventDefault(link);
+  mainPhoto.src = item.src;
+});
