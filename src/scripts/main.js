b4d2bae94528726d50bdbcd228dbb6dca67f4ac4
JulyaPetrovskaya <119932772+JulyaPetrovskaya@users.noreply.github.com>
2023-03-22 22:32:53 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const largeImage = document.querySelector('#largeImg');
+const galleryList = document.querySelector('.gallery__list');
+
+galleryList.addEventListener('click', function(el) {
+  el.preventDefault();
+
+  const item = el.target.closest('.list-item__link');
+
+  if (!item) {
+    return;
+  }
+
+  largeImage.src = item.href;
+});
