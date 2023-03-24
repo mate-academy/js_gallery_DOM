e6a52740b43bab76d2d3da4561a3f6a6ff7c5cd9
Sergey Lipikhin <football3@ukr.net>
2023-03-24 14:32:11 +0200

    Complete solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const largeImageElement = document.querySelector('#largeImg');
+const galleryListElement = document.querySelector('#thumbs');
+
+galleryListElement.addEventListener('click', (eventClick) => {
+  eventClick.preventDefault();
+
+  const thumbLink = eventClick.target.closest('.list-item__link');
+
+  if (
+    !thumbLink
+    || !galleryListElement.contains(thumbLink)
+  ) {
+    return;
+  }
+
+  largeImageElement.src = thumbLink.href;
+});
