c40d50b025025cab3757c7a59c9802320c47a1a6
Nazar Kharkevych <nkharkevych@gmail.com>
2023-04-02 15:16:37 +0300

    Fixes after code review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,9 +4,9 @@ const thumbs = document.querySelector('#thumbs');
 const largeImage = document.querySelector('#largeImg');
 
 thumbs.addEventListener('click', function(clickEvent) {
+  clickEvent.preventDefault();
+
   const link = clickEvent.target.closest('a');
 
   largeImage.src = link.href;
-
-  clickEvent.preventDefault();
 });
