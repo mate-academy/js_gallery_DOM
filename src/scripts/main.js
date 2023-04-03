0927953ccfbe108ee50bc8a7f2c32c79ef78389a
Roman Hrytsiuk <vasylguk7@gmail.com>
2023-04-03 22:56:24 +0300

    Initial commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const largeImage = document.querySelector('#largeImg');
+const list = document.querySelector('#thumbs');
+
+list.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const item = e.target;
+
+  if (item.classList.contains('gallery__img')) {
+    largeImage.src = item.parentNode.href;
+  }
+});
