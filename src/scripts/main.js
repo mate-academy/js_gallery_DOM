9c241b347f5d87c105b8634cdaf01953954727f7
stetsenko_viktoriia <stetsenko.viktoriia@gmail.com>
2023-04-03 17:28:25 +0300

    gallery added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const list = document.querySelector('.gallery__list');
+
+list.addEventListener('click', (_event) => {
+  _event.preventDefault();
+
+  const item = _event.target;
+
+  if (!item.classList.contains('gallery__thumb')) {
+    return;
+  }
+
+  document.querySelector('.gallery__large-img').src = item.src;
+});
