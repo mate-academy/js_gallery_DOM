2bac0d7714ce3f29152063f1cdbcef4dc7fc13c7
Mikhieiev Ivan <navigator79@gmail.com>
2023-04-07 15:05:04 +0300

    First task in Events

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const largeImg = document.getElementById('largeImg');
+const thumbsList = document.getElementById('thumbs');
+
+thumbsList.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const link = e.target.closest('.list-item__link');
+
+  largeImg.src = link.href;
+});
