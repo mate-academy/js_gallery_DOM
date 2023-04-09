0b2c8694a2a6d40e48c8f614332d256ec4230321
Pavlo Kostyshyn <kostishinpavlo24@gmail.com>
2023-04-09 16:31:31 +0300

    gallery was improved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const list = document.getElementById('thumbs');
+const largeImg = document.getElementById('largeImg');
+
+list.addEventListener('click', (listEvent) => {
+  const itemImg = listEvent.target.parentElement;
+
+  listEvent.preventDefault();
+  largeImg.src = itemImg.href;
+});
