028d0692084ad54a1503bcdab02871851346c4a2
Denys <kolbasin1990@gmail.com>
2023-04-09 14:01:51 +0300

    js_gallery_DOM_v_01

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const thumbs = document.getElementById('thumbs');
+const largeImg = document.getElementById('largeImg');
+
+thumbs.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const target = event.target;
+
+  if (target.tagName === 'IMG') {
+    largeImg.src = target.parentNode.href;
+  }
+});
