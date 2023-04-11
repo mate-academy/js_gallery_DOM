6ae21da47c773bf02e8d75882c0f9df93c4bd6b1
Anna Grechana <grechanaanna@gmial.com>
2023-04-11 23:20:16 +0300

    1st version

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const list = document.querySelector('.gallery__list');
+
+const picture = document.querySelector('.gallery__large-img');
+
+list.addEventListener('click', (events) => {
+  const item = events.target.closest('.list-item__link');
+
+  if (!item || !list.contains(item)) {
+    return;
+  }
+
+  events.preventDefault();
+
+  picture.src = item.href;
+});
