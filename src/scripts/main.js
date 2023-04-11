7a603ee19136410e701d83b16c6c9fc1c6c8aacc
Ilia Kolesnik <koleshnik160799@gmail.com>
2023-04-11 18:15:40 +0300

    Done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const thumbGallery = document.querySelectorAll('.gallery__thumb');
+
+thumbGallery.forEach(item =>
+  item.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const urlImage = e.path[1].getAttribute('href');
+    const largeImg = document.querySelector('#largeImg');
+
+    largeImg.setAttribute('src', urlImage);
+  })
+);
