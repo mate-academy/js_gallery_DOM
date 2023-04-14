59d03f5e3d6ef28705e6800fab2e11cbeda459a5
Pavlo Petrashevskyi <dragonthunderaddsdp@gmail.com>
2023-04-14 17:09:43 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const mainImage = document.querySelector('img');
+const list = document.querySelector('ul');
+
+list.addEventListener('click', (event) => {
+  const link = event.target.closest('.list-item__link');
+
+  mainImage.src = link.href;
+
+  event.preventDefault();
+});
