81eedcf1ae6498edbfad2ea0971e471c60e7906a
Pasha Moroz <pmoroz2004@gmail.com>
2023-04-17 12:33:07 +0300

    add my solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const list = document.querySelector('.gallery__list');
+const largeImg = document.querySelector('#largeImg');
+
+list.addEventListener('click', (e) => {
+  const link = e.target.closest('.list-item__link');
+
+  if (link) {
+    e.preventDefault();
+  }
+
+  largeImg.src = link.href;
+});
