8dc7dc1c2b82e908deefc2a501dc11abb01d0d5f
Ilia Kolesnik <koleshnik160799@gmail.com>
2023-04-19 22:41:42 +0300

    Fix method path

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,8 +5,12 @@ const thumbGallery = document.querySelectorAll('.gallery__thumb');
 thumbGallery.forEach(item =>
   item.addEventListener('click', (e) => {
     e.preventDefault();
+    
+    const urlImage = 
+    e.target.closest('.list-item__link').href
+    .split('http://localhost:8080')
+    .join('');
 
-    const urlImage = e.path[1].getAttribute('href');
     const largeImg = document.querySelector('#largeImg');
 
     largeImg.setAttribute('src', urlImage);
