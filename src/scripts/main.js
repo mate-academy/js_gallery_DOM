088830ded66a81920caef409cd38e48557e422f5
Irina Kulish <irinakulish20@gmail.com>
2023-04-20 13:59:41 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const img = document.querySelector('#largeImg');
+const list = document.querySelector('#thumbs');
+
+list.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const item = e.target.closest('.list-item__link');
+
+  img.src = item.href;
+});
