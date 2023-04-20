85e53bf88a5d5db7de4d23bb132528c6b95c2c9f
Mikołaj <ostermi@wp.pl>
2023-04-20 16:42:44 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,27 @@
 'use strict';
+
+function extractImageName(link) {
+  return link.split('/')
+    .slice(3)
+    .join('')
+    .split('-')
+    .slice(0, 2)
+    .join('-');
+}
+
+const largeImage = document.getElementById('largeImg');
+
+const thumbImages = document.querySelectorAll('.gallery__thumb');
+
+const thumbLinks = document.querySelectorAll('.list-item__link');
+
+thumbLinks.forEach(link => {
+  link.href = '#largeImg';
+});
+
+[...thumbImages]
+  .forEach(thumbImage => thumbImage.addEventListener('click', () => {
+    const imageName = extractImageName(thumbImage.src);
+
+    largeImage.src = `/images/${imageName}.png`;
+  }));
