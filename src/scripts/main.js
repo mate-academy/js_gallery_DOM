f30d5a4f7754c3dbbfb2affebaf43c08d15381b3
Nadiein Oleh <olegnadein1994@gmail.com>
2023-04-25 10:53:29 +0300

    Used delegation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const largeImage = document.getElementById('largeImg');
+const listImage = document.getElementById('thumbs');
+
+listImage.addEventListener('click', (e) => {
+  largeImage.src = e.target.src;
+});
+
+for (const link of document.querySelectorAll('.list-item__link')) {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+  });
+}
