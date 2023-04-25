bc64618499b8132640e9492dda35b6eb6afa3fd8
Nadiein Oleh <olegnadein1994@gmail.com>
2023-04-25 13:23:26 +0300

    changed src on href

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,10 @@
 'use strict';
 
 const largeImage = document.getElementById('largeImg');
-const listImage = document.getElementById('thumbs');
-
-listImage.addEventListener('click', (e) => {
-  largeImage.src = e.target.src;
-});
 
 for (const link of document.querySelectorAll('.list-item__link')) {
   link.addEventListener('click', (e) => {
     e.preventDefault();
+    largeImage.src = link.href;
   });
 }
