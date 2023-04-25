fd6ee400baa26fd4a113a968f636c8bb12395184
Victor Buldenko <gookidoo@gmail.com>
2023-04-25 19:35:23 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const mainImage = document.querySelector('#largeImg');
+const gallery = document.querySelector('.gallery');
+
+gallery.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  if (e.target.classList.contains('gallery__img')) {
+    mainImage.src = e.target.src;
+  }
+
+  if (e.target.classList.contains('list-item__link')) {
+    mainImage.src = e.target.querySelector('.gallery__img').src;
+  }
+});
