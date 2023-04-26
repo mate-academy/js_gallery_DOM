ea62090a961f43012e68675da7cfa1e1bdb99962
Victor Buldenko <gookidoo@gmail.com>
2023-04-26 19:52:04 +0300

    -.-

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,7 +6,9 @@ const gallery = document.querySelector('.gallery');
 gallery.addEventListener('click', (e) => {
   e.preventDefault();
 
-  if (e.target.closest('.list-item__link')) {
-    mainImage.src = e.target.closest('.list-item__link').href;
+  const elem = e.target.closest('.list-item__link');
+
+  if (elem) {
+    mainImage.src = elem.href;
   }
 });
