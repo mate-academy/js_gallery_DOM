29b328c46a289beec01714b8621de4f7dbd7b9c8
Victor Buldenko <gookidoo@gmail.com>
2023-04-26 15:55:45 +0300

    rework

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,11 +6,7 @@ const gallery = document.querySelector('.gallery');
 gallery.addEventListener('click', (e) => {
   e.preventDefault();
 
-  if (e.target.classList.contains('gallery__img')) {
-    mainImage.src = e.target.src;
-  }
-
-  if (e.target.classList.contains('list-item__link')) {
-    mainImage.src = e.target.querySelector('.gallery__img').src;
+  if (e.target.closest('.list-item__link')) {
+    mainImage.src = e.target.closest('.list-item__link').href;
   }
 });
