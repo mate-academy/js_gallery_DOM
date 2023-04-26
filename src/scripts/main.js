075933b04b569ad23e9d85252eb82dabdcb896fa
Oksana Tytanych <oksanatytanych@gmail.com>
2023-04-26 13:29:57 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const largeImg = document.querySelector('#largeImg');
+const gallery = [...document.querySelectorAll('.list-item__link')];
+
+gallery.forEach((img, index) => {
+  img.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    largeImg.src = img.href;
+  });
+});
+
+largeImg.addEventListener('click', () => {
+  let imgPosition = 0;
+
+  for (let i = 0; i < gallery.length; i++) {
+    if (largeImg.src === gallery[i].href && i !== gallery.length - 1) {
+      imgPosition = i + 1;
+    }
+  }
+
+  largeImg.src = gallery[imgPosition].href;
+});
