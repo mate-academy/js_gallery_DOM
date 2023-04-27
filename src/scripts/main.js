8ed35279739f1699478653c56de7190daf237fa9
Alla Gutsul <allagutsul1990@gmail.com>
2023-04-27 18:04:09 +0300

    create gallery

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const galleryList = document.querySelector('.gallery__list');
+const largeImg = document.querySelector('#largeImg');
+
+galleryList.addEventListener('click', function(e) {
+  e.preventDefault();
+
+  const itemLink = e.target.closest('.list-item__link');
+
+  largeImg.src = itemLink.href;
+});
