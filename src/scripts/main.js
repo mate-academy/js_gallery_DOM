f40ce976568176f89769bc6601d686649c422907
Oksana Tytanych <oksanatytanych@gmail.com>
2023-04-27 13:18:40 +0300

    code was changed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,23 +1,9 @@
 'use strict';
 
 const largeImg = document.querySelector('#largeImg');
-const gallery = [...document.querySelectorAll('.list-item__link')];
+const gallery = document.querySelector('.gallery__list');
 
-gallery.forEach((img, index) => {
-  img.addEventListener('click', (evt) => {
-    evt.preventDefault();
-    largeImg.src = img.href;
-  });
-});
-
-largeImg.addEventListener('click', () => {
-  let imgPosition = 0;
-
-  for (let i = 0; i < gallery.length; i++) {
-    if (largeImg.src === gallery[i].href && i !== gallery.length - 1) {
-      imgPosition = i + 1;
-    }
-  }
-
-  largeImg.src = gallery[imgPosition].href;
+gallery.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  largeImg.src = evt.target.closest('.list-item__link');
 });
