c2e66cf968ca81cdbda9424319c6ada9d89b0000
Zibi95 <sylwester.sob95@gmail.com>
2023-05-02 12:07:45 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
+/* eslint-disable no-useless-return */
 'use strict';
+
+const galleryBlock = document.querySelector('.gallery');
+const largeImg = galleryBlock.querySelector('#largeImg');
+
+galleryBlock.addEventListener('click', e => {
+  e.preventDefault();
+
+  const a = e.target.closest('a');
+
+  if (!a) {
+    return;
+  }
+
+  const img = a.href;
+
+  largeImg.src = img;
+});
