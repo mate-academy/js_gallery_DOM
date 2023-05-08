804ba7d867b290d33adb7a4c724ecda0461efa6f
Maria Shmakova <masha.shmakova@gmail.com>
2023-05-08 17:49:01 +0300

    gallery were created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const list = document.querySelector('.gallery__list');
+const bigImg = document.querySelector('#largeImg');
+
+list.addEventListener('click', (even) => {
+  even.preventDefault();
+
+  const item = even.target.closest('.list-item');
+
+  if (!item) {
+    return;
+  }
+
+  const link = item.querySelector('a');
+
+  bigImg.src = link.href;
+});
