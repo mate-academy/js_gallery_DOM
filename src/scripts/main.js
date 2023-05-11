77f7decb74e62288b72ad56accc971f64d7a8e66
metin <metinbicaksiz@gmail.com>
2023-05-11 11:48:54 +0300

    first attempt

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const thumbs = document.getElementById('thumbs');
+
+thumbs.addEventListener('click', function(e) {
+  e.preventDefault();
+
+  const target = e.target;
+
+  if (target.tagName === 'IMG' || target.tagName === 'A') {
+    const largeImg = document.getElementById('largeImg');
+    const newSrc = target.tagName === 'IMG' ? target.src : target.href;
+
+    largeImg.src = newSrc;
+  }
+});
