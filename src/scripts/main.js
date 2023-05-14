62839ee87c4b3ed05645cccb5c6f8dd37c4947bf
Veronika Pastushkova <d9169393507@gmai.com>
2023-05-14 13:56:16 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const list = document.getElementById('thumbs');
+const mainPicture = document.getElementById('largeImg');
+
+list.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const item = e.target.closest('.list-item__link');
+
+  if (!item || !list.contains(item)) {
+    return;
+  }
+
+  mainPicture.src = item.href;
+});
