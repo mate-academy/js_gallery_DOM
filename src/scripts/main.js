0073bd238824a003bd0b9ea7cab1ff4749f67f18
Vlad Danilenko <gabiolario@gmail.com>
2023-05-14 12:23:56 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const list = document.querySelector('.gallery__list');
+
+const largeImg = document.querySelector('#largeImg');
+
+list.addEventListener('click', (even) => {
+  const item = even.target.closest('.list-item__link');
+
+  even.preventDefault();
+
+  if (!item || !list.contains(item)) {
+    return;
+  }
+
+  largeImg.src = item.href;
+});
