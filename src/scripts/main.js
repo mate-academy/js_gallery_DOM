79bf15727c753b7ec90fd183148293a529cc3330
Yaroslav Liutyi <slavko5481@gmail.com>
2023-05-16 13:10:00 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const largeImg = document.getElementById('largeImg');
+const list = document.querySelector('.gallery__list');
+
+list.addEventListener('click', (e) => {
+  const item = e.target.closest('.list-item__link');
+
+  largeImg.src = item.firstElementChild.src;
+  e.preventDefault();
+});
