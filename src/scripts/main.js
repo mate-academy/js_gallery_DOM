2bb1b92adc9076d7c851b9e53883afb479aa1be3
vladimir tkachenko <vladimirscloud@icloud.com>
2023-05-18 22:07:11 +0300

    gallery_DOM commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const list = document.querySelector('#thumbs');
+const img = document.querySelector('#largeImg');
+
+list.addEventListener('click', (c) => {
+  c.preventDefault();
+
+  const closest = c.target.closest('.list-item__link');
+
+  if (!closest || list.contains(closest)) {
+    img.src = closest.href;
+  }
+});
