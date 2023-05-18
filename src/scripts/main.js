9271d64ed053ca3497d2501ad7cbfe2870f5c1b1
Vitalii Parkhomenko <poroh1991ukr@gmail.com>
2023-05-18 23:37:42 +0300

    js_gallery_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const gallery = document.querySelector('.gallery');
+  const galleryMainImage = gallery.querySelector('#largeImg');
+
+  gallery.addEventListener('click', (e) => {
+    const itemGallery = e.target.closest('.list-item');
+
+    if (!itemGallery) {
+      return;
+    }
+
+    const itemGalleryLink = itemGallery.querySelector('.list-item__link');
+
+    e.preventDefault();
+    galleryMainImage.src = itemGalleryLink.getAttribute('href');
+  });
+});
