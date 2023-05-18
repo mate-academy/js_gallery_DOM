a49ffc6a600f6b3302aaf26a58d1f24e2b19ffe6
Artem Kurinniy <artem.kyrinniy@gmail.com>
2023-05-18 13:36:04 +0300

    commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const mainImageBlock = document.querySelector('.gallery div');
+const mainImage = mainImageBlock.firstElementChild;
+const listItems
+  = document.querySelectorAll('.gallery__list .list-item');
+
+for (const listItem of listItems) {
+  const imgListItem = listItem.querySelector('.list-item__link .gallery__img');
+
+  listItem.addEventListener('click', (EVENT) => {
+    EVENT.preventDefault();
+
+    const littleImgSrc = imgListItem.getAttribute('src');
+    const largeImg = document.createElement('img');
+
+    largeImg.id = mainImage.id;
+
+    largeImg.src = './images'
+      + littleImgSrc.substring(0, littleImgSrc.lastIndexOf('-'))
+      + '.png';
+    largeImg.alt = mainImage.alt;
+    largeImg.className = mainImage.className;
+
+    mainImageBlock.firstElementChild.remove();
+    mainImageBlock.append(largeImg);
+  });
+}
