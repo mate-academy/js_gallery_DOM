dae3e660db575e1694419e6a9599265d93f8f424
Izzet Yusufov <17izzet@gmail.com12345Hello>
2023-05-23 16:26:07 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const listOfPictures = document.querySelector('#thumbs');
+const mainPicture = document.querySelector('#largeImg');
+
+listOfPictures.addEventListener('click', (picture) => {
+  picture.preventDefault();
+
+  const item = picture.target.closest('.list-item__link');
+
+  mainPicture.src = item.href;
+});
