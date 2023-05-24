1d0fbdf21d7fc3f9a0788d9d1d026f092facc208
Bogdan Parubchenko <bogdanparubchenko@gmail.com>
2023-05-24 19:51:31 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const pictures = document.querySelectorAll('.list-item__link');
+const valide = document.getElementById('largeImg');
+
+for (const picture of pictures) {
+  picture.addEventListener('click', (even) => {
+    even.preventDefault();
+
+    const item = even.target.closest('.list-item__link');
+
+    valide.src = item.children[0].src;
+  });
+}
