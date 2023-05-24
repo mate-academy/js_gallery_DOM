f6a98ff9d6ea2a28bfe265f2b9e67fa76be5f1d1
pasternakv19@gmail.com <pasternakv19@gmail.com>
2023-05-24 15:48:31 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const list = document.querySelector('.gallery__list');
+const large = document.querySelector('#largeImg');
+
+list.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const item = e.target.closest('.list-item__link');
+
+  large.src = item.href;
+});
