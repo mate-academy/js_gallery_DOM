e7374ab4586a3557cb6268de3533d11f902e807d
Oleksandr Chernikov <pjatochkin2013@gmail.com>
2023-05-29 23:30:56 +0300

    commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const list = document.querySelectorAll('li');
+const galleryImg = document.getElementById('largeImg');
+
+list.forEach(item => {
+  const link = item.querySelector('a');
+  const img = link.getAttribute('href');
+
+  item.addEventListener('click', (transition) => {
+    transition.preventDefault();
+    galleryImg.setAttribute('src', `${img}`);
+  });
+});
