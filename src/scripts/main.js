3b1d6beaadeec3bc6fe34a4b9721ecaf50a98c2e
[gord-oleg] <[kvb3.ol@gmail.com]>
2023-05-29 22:12:18 +0300

    Fixes after code review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const mainImg = document.querySelector('#largeImg');
+const smallImages = document.querySelectorAll('.gallery__thumb');
+
+smallImages.forEach(img => {
+  img.addEventListener('click', (a) => {
+    a.preventDefault();
+    mainImg.src = a.target.parentElement.href;
+  });
+});
