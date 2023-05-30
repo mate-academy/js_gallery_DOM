451464810ab7642479cfaffa060e4d2bacfb3b6c
AndriiYelieva <andrii.yelieva@gmail.com>
2023-05-30 21:45:05 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const mainPhoto = document.getElementById('largeImg');
+const list = document.querySelector('.gallery__list');
+
+list.addEventListener('click', (ev) => {
+  ev.preventDefault();
+
+  const image = ev.target.closest('.list-item__link');
+
+  mainPhoto.src = image.href;
+});
