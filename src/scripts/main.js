da0bb3b3d1f1016b04edcaf46866f23406502673
Antonina Klishch <tonya91t@gmail.com>
2023-05-31 15:56:00 +0300

    fixed the mistakes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,13 +3,13 @@
 const list = document.querySelector('.gallery__list');
 const largeImage = document.querySelector('#largeImg');
 
-list.addEventListener('click', (action) => {
-  const image = action.target.closest('.list-item__link');
+list.addEventListener('click', (e) => {
+  const image = e.target.closest('.list-item__link');
 
-  if (!image || !list.contains(image)) {
+  if (!image) {
     return;
   }
-  action.preventDefault();
+  e.preventDefault();
 
   largeImage.src = image.href;
 });
