487a8f498d15e789fd808d7ea8d9c4609279e5c4
Antonina Klishch <tonya91t@gmail.com>
2023-05-31 13:26:51 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const list = document.querySelector('.gallery__list');
+const largeImage = document.querySelector('#largeImg');
+
+list.addEventListener('click', (action) => {
+  const image = action.target.closest('.list-item__link');
+
+  if (!image || !list.contains(image)) {
+    return;
+  }
+  action.preventDefault();
+
+  largeImage.src = image.href;
+});
