914ccde56469e4017c6529568167ccf659655fc7
Halyna Rydasova <halyna.rydasova@gmail.com>
2023-06-02 10:40:54 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const largeImg = document.querySelector('#largeImg');
+const imgList = document.querySelector('#thumbs');
+
+imgList.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const item = e.target.closest('.list-item__link');
+
+  largeImg.src = item.href;
+});
