c1e5dd06caddafdcb90b446a9ac2362fdc234646
Bohdan Shumylo <injobs.kharkiv@gmail.com>
2023-06-02 23:36:19 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const thumbs = document.querySelectorAll('.gallery #thumbs img');
+const largeImage = document.getElementById('largeImg');
+
+thumbs.forEach(image => {
+  image.addEventListener('click', (eventPic) => {
+    eventPic.preventDefault();
+
+    const link = eventPic.target.closest('.list-item__link');
+
+    largeImage.src = link.href;
+  });
+});
