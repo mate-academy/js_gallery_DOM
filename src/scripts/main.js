fb15ad52e3bc1d264787b77cc52f5e1bd17e539b
Vitaliy Zviriuk <zvirvitalik1991@gmail.com>
2023-06-04 13:41:54 +0200

    solution2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,12 @@
 'use strict';
+
 const list = document.getElementById('thumbs');
 const bigItem = document.getElementById('largeImg');
 
-
 list.addEventListener('click', (event) => {
   event.preventDefault();
+
   const currentItemSrc = event.target.src;
-  console.log(currentItemSrc);
 
   bigItem.src = currentItemSrc;
-})
+});
