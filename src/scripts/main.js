192b78cb95e0a01652d3e8be5f55be6ed111ba58
Marina Olenchenko <marina.olenchenko2204@gmail.com>
2023-06-05 14:03:39 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const largeImg = document.getElementById('largeImg');
+const thumbs = document.getElementById('thumbs');
+
+thumbs.addEventListener('click', function(e) {
+  event.preventDefault();
+
+  const target = e.target.tagName.toLowerCase();
+
+  if (target === 'img' || target === 'a') {
+    const newSrc = e.target.parentNode.getAttribute('href');
+
+    largeImg.src = newSrc;
+  }
+});
