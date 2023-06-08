bf46db91009441642b53fb9e6f1713e04f9d170e
Hanna Balabukha <anna.aniskovets1992@ukr.net>
2023-06-08 16:46:34 +0300

    'done'

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const images = document.querySelectorAll('a');
+const mainImage = document.getElementById('largeImg');
+
+images.forEach((item) => {
+  item.addEventListener('click', e => {
+    e.preventDefault();
+    // let itemTarget = event.target.closest('finItem');
+    mainImage.src = item.href;
+  });
+});
