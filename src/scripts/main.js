33888edd18edb988ceb7706babe8e71ba37f04ea
domkab <dominykas.bobinas@gmail.com>
2023-06-10 19:10:29 +0300

    1.0.0

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const largeImg = document.querySelector('.gallery__large-img');
+const thumbnails = document.querySelectorAll('.gallery__thumb');
+
+thumbnails.forEach(thumbnail => {
+  thumbnail.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    largeImg.src = thumbnail.parentElement.href;
+  });
+});
