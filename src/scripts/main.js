73b87505e9e9f12e76827773c59c32ea4f626ce1
Oleksii Lavka <lavka.alexey37gmail.com>
2023-07-05 20:32:06 +0300

    add tsk solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const galleryList = document.querySelector('.gallery__list');
+const largeImage = document.querySelector('.gallery__large-img');
+
+// eslint-disable-next-line no-shadow
+galleryList.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const targetItem = event.target.closest('.list-item__link');
+
+  if (!targetItem) {
+    return;
+  }
+
+  largeImage.src = targetItem.href;
+});
