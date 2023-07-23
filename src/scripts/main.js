17785a7be7f71e6e0e07c7601d9e93c792db7828
Rufat Mammadov <rufat.mammadov.m@gmail.com>
2023-07-24 00:00:24 +0300

    js_gallery_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const smallImages = document.querySelectorAll('.gallery__thumb');
+const bigImage = document.querySelector('.gallery__large-img');
+
+smallImages.forEach(image => {
+  image.addEventListener('click', (element) => {
+    element.preventDefault();
+
+    if (element.target.tagName === 'IMG') {
+      bigImage.src = element.target.src;
+    } else if (element.target.tagName === 'A') {
+      bigImage.src = element.target.firstChildElement.src;
+    }
+  });
+});
