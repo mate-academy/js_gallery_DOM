306d6e6e2a653c7175389c465ba4ba3a7b53957b
Lukasz Warda <lukaszwarda1989@gmail.com>
2023-07-24 12:38:03 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,35 +1,11 @@
 'use strict';
 
-const links = document.querySelectorAll('.list-item__link');
-// const images = document.querySelectorAll('.gallery__thumb');
 const largeImg = document.querySelector('#largeImg');
 
-for (let i = 0; i < links.length; i++) {
-  links[i].removeAttribute('href');
+const list = document.getElementById('thumbs');
 
-  let number = '';
+list.addEventListener('click', (e) => {
+  const imageLink = e.target.closest('.list-item__link');
 
-  switch (i) {
-    case 0:
-      number = 'first';
-      break;
-    case 1:
-      number = 'second';
-      break;
-    case 2:
-      number = 'third';
-      break;
-    case 3:
-      number = 'fourth';
-      break;
-    case 4:
-      number = 'fifth';
-      break;
-  }
-
-  const source = `http://localhost:8080/images/landscape-${number}.png`;
-
-  links[i].addEventListener('click', () => {
-    largeImg.setAttribute('src', source);
-  });
-}
+  largeImg.setAttribute('src', imageLink.href);
+});
