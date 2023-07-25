a2fed2c3b14dccb75cc370b46aa2899f9d4ab5c2
spencerspe <spencerspe@gmail.com>
2023-07-25 08:59:01 +0100

    first solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const mainpic = document.getElementById('largeImg');
+const thumbs = document.getElementById('thumbs');
+
+thumbs.addEventListener('click', function(evt) {
+  evt.preventDefault();
+
+  let clicked = evt.target.src.slice(22, 38);
+
+  if (clicked[clicked.length - 1] === '-') {
+    clicked = clicked.slice(0, -1);
+  }
+
+  clicked = `./images/${clicked}.png`;
+
+  mainpic.src = clicked;
+});
