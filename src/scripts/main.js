fce05fbd99c25564d6e965870e441068ac2daed6
spencerspe <spencerspe@gmail.com>
2023-08-01 13:46:16 +0100

    second solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,13 +6,7 @@ const thumbs = document.getElementById('thumbs');
 thumbs.addEventListener('click', function(evt) {
   evt.preventDefault();
 
-  let clicked = evt.target.src.slice(22, 38);
-
-  if (clicked[clicked.length - 1] === '-') {
-    clicked = clicked.slice(0, -1);
-  }
-
-  clicked = `./images/${clicked}.png`;
+  const clicked = evt.target.closest('.list-item__link').href;
 
   mainpic.src = clicked;
 });
