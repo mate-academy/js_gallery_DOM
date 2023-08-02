d0f9b69f7661b6c645b4f9a3d589419b990c7c6e
GGsmou <svyatoslav.golovin.2005@gmail.com>
2023-08-02 14:38:49 +0300

    sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const thumbs = document.querySelector('#thumbs');
+const largeImg = document.querySelector('#largeImg');
+
+thumbs.addEventListener('click', ev => {
+  ev.preventDefault();
+
+  let target = ev.target;
+
+  if (target.tagName !== 'IMG' && target.tagName !== 'A') {
+    return;
+  }
+
+  if (target.tagName === 'A') {
+    target = target.children[0];
+  }
+
+  const srcParts = target.src.replace('-thumb', '')
+    .split('.')[0].split('/');
+
+  const src = `${srcParts[0]}//${srcParts[2]}/images/${srcParts[3]}.png`;
+
+  largeImg.src = src;
+});
