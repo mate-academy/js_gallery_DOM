f3393eba25e257bc879f3bbbf31ba187558f9e77
Edgar Kocuba <edgarkocuba@gmail.com>
2023-08-22 18:46:22 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const thumbImages = document.querySelectorAll('.list-item__link');
+const mainImage = document.getElementById('largeImg');
+
+thumbImages.forEach(thumbImage => thumbImage.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  mainImage.src = thumbImage.href;
+}));
