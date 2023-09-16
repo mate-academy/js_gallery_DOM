ded174ed48827516ebf917ac39fac1935848e8d8
Volodymyr Zadorozhnyi <Torsida@gmail.com>
2023-09-16 17:59:39 +0300

    first vrsion

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const largeImg = document.querySelector('#largeImg');
+
+const listLinks = document.querySelectorAll('.list-item__link');
+
+listLinks.forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+
+    largeImg.src = link.href;
+  }
+  );
+});
