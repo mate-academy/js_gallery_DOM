d92e4b104a3865c4bd5516463be76394de362170
Vitalii Hovenko <vet.hov.main@gmail.com>
2023-09-22 14:13:56 +0300

    renamed click event

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,10 +3,10 @@
 const mainImage = document.getElementById('largeImg');
 const secondaryImages = document.getElementById('thumbs');
 
-secondaryImages.addEventListener('click', (clickEvent) => {
-  clickEvent.preventDefault();
+secondaryImages.addEventListener('click', (event) => {
+  event.preventDefault();
 
-  const link = clickEvent.target.closest('.list-item__link');
+  const link = event.target.closest('.list-item__link');
 
   if (link.tagName === 'A') {
     mainImage.src = link.href;
