44758b3e0b4a8654bb65eacae67c52191e5beed7
Vitalii Hovenko <vet.hov.main@gmail.com>
2023-09-22 13:53:15 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const mainImage = document.getElementById('largeImg');
+const secondaryImages = document.getElementById('thumbs');
+
+secondaryImages.addEventListener('click', (clickEvent) => {
+  clickEvent.preventDefault();
+
+  const link = clickEvent.target.closest('.list-item__link');
+
+  if (link.tagName === 'A') {
+    mainImage.src = link.href;
+  }
+});
