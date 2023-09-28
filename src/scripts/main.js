6b4c17cf4605673fe857a7409972b6d2fbce0bb3
meryam wazeen <maryamwazeen2014@gmail.com>
2023-09-28 13:06:15 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const largeImg = document.getElementById('largeImg');
+const thumbnailList = document.getAnimations('thumbs');
+
+thumbnailList.addEventListener('click', function(clickEvent) {
+  if (clickEvent.target.tagName === 'A' || clickEvent.target === 'IMG') {
+    clickEvent.preventDefault();
+
+    const anchorElement = clickEvent.target.tagName === 'A'
+
+      ? clickEvent.target : clickEvent.target.parentElement;
+
+    const newImgUrl = anchorElement.target.getAttribute('href');
+
+    largeImg.setAttribute('src', newImgUrl);
+  }
+});
