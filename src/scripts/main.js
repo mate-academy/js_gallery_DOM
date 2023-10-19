9d4734ecf31b7807b09c3cb8fc505164ddb6daa9
Kristina Bekher <bekherkr@gmail.com>
2023-10-19 19:58:51 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+function selectImage(e) {
+  e.preventDefault();
+
+  const link = e.target.closest('a');
+
+  if (link) {
+    const largeImgElement = document.getElementById('largeImg');
+
+    largeImgElement.src = link.href;
+  }
+}
+
+const list = document.querySelector('.gallery__list');
+
+list.addEventListener('click', selectImage);
