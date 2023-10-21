82d727bca4224046e3cea0ac0a716e0586327bc9
Andrii Rykal <andreuka79@gmail.com>
2023-10-21 20:26:37 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const largeImg = document.getElementById('largeImg');
+const list = document.getElementById('thumbs');
+
+// eslint-disable-next-line no-shadow
+list.addEventListener('click', (event) => {
+  const item = event.target.closest('.list-item');
+
+  event.preventDefault();
+
+  if (!item || !list.contains(item)) {
+    return;
+  }
+
+  const src = item.querySelector('.list-item__link').href;
+
+  largeImg.src = src;
+});
