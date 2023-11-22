f09521d267276199e700cee585a2f81984297133
Patryk-Buczkowski <patryk.buczkowski@outlook.com>
2023-11-22 17:57:59 +0100

    sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,30 @@
 'use strict';
+
+const largeImg = document.getElementById('largeImg');
+const list = document.getElementById('thumbs');
+
+list.onclick = function(eventt) {
+  const target = eventt.target;
+
+  if (target.tagName !== 'IMG') {
+    return;
+  }
+
+  eventt.preventDefault();
+
+  showBigImg(target);
+
+  // return false;
+};
+
+function showBigImg(elem) {
+  const img = elem.closest('a');
+
+  const href = img.getAttribute('href');
+
+  if (largeImg) {
+    largeImg.src = '';
+  }
+
+  largeImg.src = href;
+}
