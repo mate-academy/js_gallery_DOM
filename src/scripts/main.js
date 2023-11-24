633fe40e91effa55e7d49eb80d19f448b4161927
Kozubowicz <55114447+Kozubowicz@users.noreply.github.com>
2023-11-24 23:53:29 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const links = document.querySelectorAll('.list-item__link');
+const bigImg = document.getElementById('largeImg');
+
+links.forEach(element => {
+  element.addEventListener('click', linkClick);
+});
+
+function linkClick(e) {
+  bigImg.src = e.currentTarget.getAttribute('href');
+  e.preventDefault();
+}
