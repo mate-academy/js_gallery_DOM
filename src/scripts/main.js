c1d935857563e432d6d5c1750779985b74168db6
Vladyslav Kolisnyk <vadyslavkolisnyk20@gmail.com>
2023-11-28 20:26:24 +0200

    add task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const galleryList = document.querySelector('.gallery__list');
+
+galleryList.addEventListener('click', (ev) => {
+  ev.preventDefault();
+
+  const target = ev.target.closest('.list-item__link');
+  const large = document.getElementById('largeImg');
+
+  if (!target || !galleryList.contains(target)) {
+    return;
+  }
+
+  large.src = target.href;
+});
