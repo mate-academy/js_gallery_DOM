accd324648b71a016da51b79b97433e59ac66a4b
Alex Rog <sasharg303@gmail.com>
2023-11-28 10:59:06 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const thumbs = document.querySelector('#thumbs');
+const largeImg = document.querySelector('#largeImg');
+
+thumbs.addEventListener('click', (eventus) => {
+  eventus.preventDefault();
+
+  const item = eventus.target.closest('.list-item');
+
+  if (!item) {
+    return;
+  }
+
+  const link = item.querySelector('.list-item__link').href;
+
+  largeImg.src = link;
+});
