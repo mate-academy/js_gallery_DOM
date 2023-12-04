e1082b15ba495c1a77a48b49012d84cdc3f84274
donizer <bogdan.ua45@gmail.com>
2023-12-04 13:15:39 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const baseUrl = window.location.href;
+
+document.addEventListener('DOMContentLoaded', function() {
+  const largeImg = document.querySelector('#largeImg');
+  const thumbsList = document.querySelectorAll('.list-item');
+
+  thumbsList.forEach((item) => {
+    item.addEventListener('click', handleThumbnailClick);
+  });
+
+  function handleThumbnailClick(e) {
+    e.preventDefault();
+
+    const newImgSrc = e.currentTarget.querySelector('a').getAttribute('href');
+
+    largeImg.setAttribute('src', baseUrl.slice(0, -1) + newImgSrc);
+  }
+});
