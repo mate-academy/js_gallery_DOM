51afe3ec10b53b03046434492ff57f3ebbc81d59
Joanna Konopacka <joanna.konopacka@hotmail.com>
2023-12-13 17:30:11 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,26 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const thumbsList = document.getElementById('thumbs');
+  const largeImg = document.getElementById('largeImg');
+
+  thumbsList.addEventListener('click', (e) => {
+    event.preventDefault();
+
+    let targetElement = e.target;
+
+    while (targetElement && targetElement.tagName !== 'A') {
+      targetElement = targetElement.parentNode;
+    }
+
+    if (targetElement && targetElement.tagName === 'A') {
+      const newImgSrc = targetElement.getAttribute('href');
+      const newImgAlt = targetElement.getAttribute('title');
+
+      if (newImgSrc) {
+        largeImg.src = newImgSrc;
+        largeImg.alt = newImgAlt;
+      }
+    }
+  });
+});
