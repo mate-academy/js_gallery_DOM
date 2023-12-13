53ec1b94870324f270f389ff3c8a7a37c5fdfb40
Nataliia Henda <nataliehenda1296@gmail.com>
2023-12-13 22:44:12 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const list = document.querySelectorAll('.list-item');
+const largeImg = document.querySelector('#largeImg');
+
+list.forEach(item => item.querySelector('.list-item__link')
+  .addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const current = e.currentTarget;
+    const url = window.location.href;
+
+    if (current.tagName === 'A') {
+      const link = current.getAttribute('href');
+      const newSrc = `${url.slice(0, -1)}${link}`;
+
+      largeImg.setAttribute('src', newSrc);
+    }
+  }));
