44d4b607362362bced2299bca18c9d137268eea8
Nataliia Henda <nataliehenda1296@gmail.com>
2023-12-19 12:27:13 +0200

    Fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const list = document.querySelectorAll('.list-item');
+const largeImg = document.querySelector('#largeImg');
+const url = window.location.href;
+
+list.forEach(item => item.querySelector('.list-item__link')
+  .addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const current = e.currentTarget;
+
+    if (current.tagName === 'A') {
+      const link = current.getAttribute('href');
+      const newSrc = `${url.slice(0, -1)}${link}`;
+
+      largeImg.setAttribute('src', newSrc);
+    }
+  }),
+);
