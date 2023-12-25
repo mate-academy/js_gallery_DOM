a2b520915eaf4eeff211aa1b50cd2725bcfadef5
Viktoriia Tkachyk <viko66666vs@gmail.com>
2023-12-25 21:46:02 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', function() {
+  const largeImg = document.getElementById('largeImg');
+  const listOfImgs = document.querySelectorAll('.list-item');
+
+  listOfImgs.forEach(item => item.addEventListener('click', handleImgClick));
+
+  function handleImgClick(e) {
+    e.preventDefault();
+
+    const newImgSrc = e.currentTarget.querySelector('a').getAttribute('href');
+
+    largeImg.setAttribute('src', newImgSrc);
+  }
+});
