4ed9df60fb7a0145df2d02b278bfe6547f30d06f
Vovchuk <kulivackijvoloda@gmail.com>
2024-01-07 20:48:56 +0200

    add

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
+/* eslint-disable no-shadow */
 'use strict';
+
+const mainImage = document.querySelector('#largeImg');
+const list = document.querySelector('#thumbs');
+
+list.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const clickedAnchorTag = event.target.closest('a');
+
+  if (!clickedAnchorTag) {
+    return;
+  }
+
+  const newImg = clickedAnchorTag.getAttribute('href');
+
+  mainImage.src = newImg;
+});
