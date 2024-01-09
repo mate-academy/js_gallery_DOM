3a12b0a3cddfc58546831203ea7823c060004523
Stephen Forde <sforde08@gmail.com>
2024-01-09 23:00:13 +0000

    gallarey soultion 2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const imgLinks = document.querySelectorAll('a');
+const mainImgDiv = document.querySelector('#largeImg').parentNode;
+
+// eslint-disable-next-line no-shadow
+imgLinks.forEach((link) => link.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  mainImgDiv.innerHTML = `
+    <img id="largeImg" src="${link.href}"
+    alt="main image" class="gallery__large-img gallery__img">
+  `;
+}));
