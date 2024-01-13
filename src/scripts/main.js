1cb8a63b61f3f30d309c551a53972ee2cdc6cfd9
Sergey Arnautov <sirius9312@ukr.net>
2024-01-13 16:30:18 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const list = document.querySelector('.gallery__list');
+const img = document.querySelector('.gallery__img');
+
+list.addEventListener('click', e => {
+  e.preventDefault();
+
+  const formatBigImage = e.target.src
+    .replace(/landscape/g, 'images/landscape')
+    .split('-')
+    .slice(0, e.target.src.split('-').length - 1)
+    .join('-') + '.png';
+
+  img.src = formatBigImage;
+});
