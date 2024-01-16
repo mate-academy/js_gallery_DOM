730d2f4d3cd5c643b242091cb1b7a6a0fd4cb99c
purplefade <tarovykanton@gmail.com>
2024-01-16 15:50:54 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const list = document.querySelector('.gallery__list');
+
+list.addEventListener('click', e => {
+  e.preventDefault();
+
+  const item = e.target.closest('.list-item__link');
+
+  if (!item || !list.contains(item)) {
+    return;
+  }
+
+  const largeImg = document.getElementById('largeImg');
+  const newSrc = item.getAttribute('href');
+
+  if (newSrc) {
+    largeImg.src = newSrc;
+  }
+});
