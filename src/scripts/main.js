2cd8ce303b19e807c6f4b47a283a0e82946074c0
Vitalii Fedusov <vitalii.fedusov@gmail.com>
2024-01-17 11:45:56 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const images = document.querySelector('#thumbs');
+const mainImage = document.querySelector('#largeImg');
+
+// eslint-disable-next-line no-shadow
+images.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const a = event.target.closest('.list-item__link');
+
+  mainImage.src = a.href;
+});
