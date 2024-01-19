3c9228d5f6c5554a4a354874027503d702558d64
Mykola Plekan <zikzar@gmail.com>
2024-01-19 18:45:06 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const bigImage = document.querySelector('#largeImg');
+const items = document.querySelector('#thumbs');
+
+items.addEventListener('click', (clickEvent) => {
+  clickEvent.preventDefault();
+
+  const item = clickEvent.target.closest('.list-item__link');
+
+  if (!item || !items.contains(item)) {
+    return;
+  }
+
+  const imgWay = '.' + item.attributes.href.nodeValue;
+
+  bigImage.attributes.src.nodeValue = imgWay;
+});
