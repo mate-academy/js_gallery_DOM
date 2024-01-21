bce1e02ccf72e11ab89c9f3a2663827cab6de437
Viktoriia Vynohradova <viktoriia.vynohradova8@gmail.com>
2024-01-21 19:35:05 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const largeImg = document.getElementById('largeImg');
+
+const list = document.getElementById('thumbs');
+
+// eslint-disable-next-line no-shadow
+list.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const item = event.target;
+
+  if (!item.classList.contains('gallery__img')) {
+    return false;
+  }
+
+  largeImg.src = item.parentElement.href;
+});
