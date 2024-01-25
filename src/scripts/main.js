ad2907ff49e78549c908cba800231bbb62d6eb6f
AgnieszkaWw <agnieszkawie82@onet.pl>
2024-01-25 10:25:43 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const images = document.querySelector('#thumbs');
+const mainImage = document.querySelector('#largeImg');
+
+images.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const a = e.target.closest('.list-item__link');
+
+  mainImage.src = a.href;
+});
