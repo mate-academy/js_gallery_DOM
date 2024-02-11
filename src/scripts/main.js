d6d14165eca773d505413733fcc63b6d569235ed
Misha Siv <0Sivcov0@gmail.com>
2024-02-11 16:21:56 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const acvive = document.querySelector('#largeImg');
+const links = document.querySelectorAll('.list-item__link');
+
+for (const item of links) {
+  item.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    acvive.src = item.getAttribute('href');
+  });
+}
+
