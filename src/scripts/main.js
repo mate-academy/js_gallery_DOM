f398235c4ac23684701b1b038d015c2afd6980d9
Siarhei Tsishkavets <kenny.sergey@gmail.com>
2024-02-12 14:42:40 +0100

    Add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const largeImg = document.getElementById('largeImg');
+
+// eslint-disable-next-line no-shadow
+function clickGallarySmallImg(event) {
+  largeImg.setAttribute('src', this.href);
+
+  event.preventDefault();
+};
+
+const galleryLinks = document.querySelectorAll('.list-item__link');
+
+galleryLinks.forEach((link) => {
+  link.addEventListener('click', clickGallarySmallImg);
+});
