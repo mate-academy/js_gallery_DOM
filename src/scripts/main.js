45404f6f12e22b4554ab06516d9160060c009c83
Dmitriy Matvichuk <dmytromatviichuk1979@gmail.com>
2024-02-16 11:41:27 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const largeImg = document.getElementById('largeImg');
+const ul = document.getElementById('thumbs');
+
+ul.addEventListener('click', (evt) => {
+  evt.preventDefault();
+
+  const item = evt.target.closest('.gallery__thumb');
+
+  if (!item) {
+    return;
+  }
+
+  const link = item.closest('a');
+
+  if (!link) {
+    return;
+  }
+
+  largeImg.src = link.href;
+});
