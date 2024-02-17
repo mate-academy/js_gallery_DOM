404798a1d3fb9d29eb8dd3817bd2561434b55174
oleksandrshtonda <oleksandrshtonda@gmail.com>
2024-02-17 12:22:10 +0100

    implemented gallery solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const largeImg = document.querySelector('.gallery__large-img');
+const smallImgsList = document.querySelector('.gallery__list');
+
+smallImgsList.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const itemOfList = e.target.closest('a');
+
+  if (!itemOfList) {
+    return;
+  }
+
+  largeImg.src = itemOfList.href;
+});
