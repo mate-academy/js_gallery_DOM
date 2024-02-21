2c3ae19b4f26a2e19a577b5ded6f556754c5265a
Horizon-git <apbreloaded947@gmail.com>
2024-02-21 23:26:54 +0200

    completed task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const largeImage = document.getElementById('largeImg');
+const smallImages = document.getElementById('thumbs');
+
+smallImages.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const item = e.target.closest('.list-item__link');
+
+  if (!item) {
+    return;
+  }
+
+  largeImage.src = item.href;
+});
