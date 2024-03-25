7cded5e91d844dfec9ec0c51a0a96d8eeb54e1c8
Oleksandr Kosenko <d1izel@ukr.net>
2024-03-25 19:36:48 +0200

    add gallery-DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const largeImg = document.getElementById('largeImg');
+const links = document.querySelectorAll('.list-item__link');
+
+links.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    largeImg.setAttribute('src', link.href);
+  });
+});
