c410f63c7123272fe260786fb026e9368e2fec56
Mateusz Oślizło <amti4osl@gmail.com>
2024-03-28 12:55:58 +0100

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const largeImage = document.querySelector('.gallery__large-img');
+const thumbnails = document.querySelectorAll('.gallery__thumb');
+
+function changeImage(imageEvent) {
+  let href = imageEvent.target.href;
+
+  if (!href) {
+    href = imageEvent.target.parentElement.href;
+  }
+
+  imageEvent.preventDefault();
+  largeImage.src = href;
+}
+
+thumbnails.forEach((thumbnail) => {
+  thumbnail.addEventListener('click', changeImage);
+  thumbnail.parentElement.addEventListener('click', changeImage);
+});
