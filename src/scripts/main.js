627a49a2cf0392a62bcd1d07e94f4e7dcdec2792
Emil Sukiennik <ewilemil@gmail.com>
2024-04-06 20:54:22 +0200

    initial commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const largeImg = document.querySelector('#largeImg');
+
+const list = document.querySelector('.gallery__list');
+
+list.addEventListener('click', (listener) => {
+  listener.preventDefault();
+
+  const properLink = listener.target.closest('a');
+
+  largeImg.src = properLink.href;
+});
