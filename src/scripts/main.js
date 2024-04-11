93e44564719a861f3088272ae65177e56088876d
vadymboichenkoAcupower <vadym.boichenko@acupower.co.uk>
2024-04-11 19:50:02 +0300

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const buttons = document.getElementById('thumbs');
+
+const imageNeedToChange = document.getElementById('largeImg');
+
+buttons.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const targetItem = e.target.closest('.gallery__thumb');
+
+  if (!targetItem) {
+    return;
+  }
+
+  const link = targetItem.closest('a');
+
+  if (!link) {
+    return;
+  }
+
+  imageNeedToChange.src = link.href;
+});
