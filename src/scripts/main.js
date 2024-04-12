14e7eb67690638d565083656ec4d046be3d061b3
Pavlo Mykhalov <pavlomihaliov@gmail.com>
2024-04-12 16:35:51 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const gallery = document.querySelector('div.gallery');
+const largeImg = document.getElementById('largeImg');
+
+gallery.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const target = e.target.tagName === 'IMG' ? e.target.parentNode : e.target;
+
+  const href = target.getAttribute('href');
+
+  largeImg.setAttribute('src', href);
+});
