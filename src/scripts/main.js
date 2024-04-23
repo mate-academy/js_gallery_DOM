cde04abc5a1bb967e6100a2dee9f65cee6bd07f2
Marta Chobaniuk <marta.chobaniuk@gmail.com>
2024-04-23 14:16:54 +0300

    added gallery DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
+/* eslint-disable no-shadow */
 'use strict';
+
+const list = document.querySelector('#thumbs');
+const largeImage = document.querySelector('#largeImg');
+
+list.addEventListener('click', (event) => {
+  const item = event.target.closest('.list-item__link');
+
+  event.preventDefault();
+  largeImage.src = item.href;
+});
