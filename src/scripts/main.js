751e37271d151a6f0823e044b86e633d9e6d4085
Vladyslav Starodub <starodubvlad08@gmail.com>
2024-04-24 17:14:55 +0300

    Implement changing main photo gallery

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,8 @@
 'use strict';
+
+document.querySelector('#thumbs').addEventListener('click', (e) => {
+  e.preventDefault();
+
+  document.querySelector('#largeImg').src =
+    e.target.closest('.list-item__link').href;
+});
