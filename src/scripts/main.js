b45f2618303b71eb2ce31b7aabb7067d51cd0873
Dmytro Manankin <dmitry.manankin@gmail.com>
2024-04-29 10:57:15 +0300

    task done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const bigImage = document.querySelector('#largeImg');
+const imagesList = document.querySelector('.gallery__list');
+
+imagesList.addEventListener('click', photoChange);
+
+function photoChange(e) {
+  e.preventDefault();
+
+  const currentImage = e.target.closest('.list-item__link');
+
+  bigImage.src = currentImage.href;
+}
