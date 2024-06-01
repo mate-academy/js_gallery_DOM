71a248cd23e8943757e8207347452b3ed4bd1568
IvanGrabovsky <ivan.grabovsky.ua@gmail.com>
2024-06-01 10:08:07 +0300

    add absolute url for src image attribute

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,8 +6,9 @@ const largeImg = document.querySelector('.gallery__large-img');
 images.addEventListener('click', (e) => {
   e.preventDefault();
 
+  const baseUrl = window.location.origin;
   const link = e.target.closest('.list-item__link');
-  const newSrc = link.getAttribute('href');
+  const absoluteSrc = new URL(link.getAttribute('href'), baseUrl).href;
 
-  largeImg.setAttribute('src', newSrc.slice(1));
+  largeImg.setAttribute('src', absoluteSrc);
 });
