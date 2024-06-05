4a449e683010523222d3640621ba09a0ccc0f748
Krzysztof <krzysiek.gugala@gmail.com>
2024-06-05 09:52:38 +0200

    solutionV1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const imageList = document.getElementById('thumbs');
+const largeImage = document.getElementById('largeImg');
+
+imageList.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  let target = e.target;
+
+  target = target.parentElement;
+  largeImage.src = target.href;
+});
