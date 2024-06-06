10af0452879135946258ff644c4bd84da5c3b25e
Nazar Oshchepkov <nazzarin565@gmail.com>
2024-06-06 10:42:02 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const largeImg = document.getElementById('largeImg');
+
+const ul = document.getElementById('thumbs');
+
+ul.addEventListener('click', (e) => {
+  // e.preventDefault();
+  // const link = e.target.closest('a');
+  // if (!link) {
+  //   return;
+  // }
+  // largeImg.setAttribute('src', link.href);
+
+  const link = e.target.closest('.list-item__link');
+
+  e.preventDefault();
+
+  largeImg.setAttribute('src', link.href);
+});
