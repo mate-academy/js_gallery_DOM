5494b049af289670f33845935a2c8ede9a46bc9e
Nazar Oshchepkov <nazzarin565@gmail.com>
2024-06-06 10:42:54 +0300

    add solution2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,13 +5,6 @@ const largeImg = document.getElementById('largeImg');
 const ul = document.getElementById('thumbs');
 
 ul.addEventListener('click', (e) => {
-  // e.preventDefault();
-  // const link = e.target.closest('a');
-  // if (!link) {
-  //   return;
-  // }
-  // largeImg.setAttribute('src', link.href);
-
   const link = e.target.closest('.list-item__link');
 
   e.preventDefault();
