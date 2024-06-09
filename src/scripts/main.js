74a7f97b580e79b2f7b3b0e2c04576a90f67e15c
Vladyslav <vladbuldenko@gmail.com>
2024-06-09 11:45:43 +0300

    VB solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const largeImg = document.querySelector('#largeImg');
+const thumbList = document.querySelector('#thumbs');
+
+thumbList.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const newLargeImgLink = e.target.closest('a');
+
+  largeImg.src = newLargeImgLink.href;
+});
