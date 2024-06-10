d5260fa7a1034d2544d2eaaf5d75f9ddd651bb85
SrTrace <levashov04sergey@gmail.com>
2024-06-10 13:25:16 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const mainImg = document.getElementById('largeImg');
+
+const thumbs = [...document.getElementById('thumbs').children];
+
+thumbs.forEach((item) => {
+  item.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const imgSrc = item.querySelector('a').href;
+
+    mainImg.src = imgSrc;
+  });
+});
