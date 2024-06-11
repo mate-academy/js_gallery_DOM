1f2d29ef4a017e328dbb64198881998618b76d1f
Viktoriya Riabets <vikamalik30@gmail.com>
2024-06-11 19:43:21 -0400

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const largeImg = document.getElementById('largeImg');
+
+  const thumbs = document.getElementById('thumbs');
+
+  thumbs.addEventListener('click', (e) => {
+    event.preventDefault();
+
+    if (e.target.tagName === 'IMG' && e.target.parentElement.tagName === 'A') {
+      const newSrc = e.target.parentElement.href;
+
+      largeImg.src = newSrc;
+    }
+  });
+});
