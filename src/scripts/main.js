874478fde5377079cc0a7c547653b8a6ad3eded9
Oleksii Bidiak <oleksii.bidiak@gmail.com>
2024-06-14 13:21:39 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,21 @@
 'use strict';
+
+window.addEventListener('DOMContentLoaded', setApp);
+
+function setApp() {
+  const gallery = document.querySelector('.gallery');
+  const clickImgHandler = (e) => {
+    const link = e.target.closest('.list-item__link');
+
+    if (link) {
+      e.preventDefault();
+
+      const largeImg = document.getElementById('largeImg');
+      const newSrc = link.getAttribute('href');
+
+      largeImg.src = newSrc;
+    }
+  };
+
+  gallery.addEventListener('click', clickImgHandler);
+}
