a97a2d5d54616d61e96b04a21e9a87d43200459f
Dmytro Boiko <iamboikodmytro@gmail.com>
2024-06-16 12:35:28 +0300

    task complete

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const showBlock = document.querySelector('#largeImg');
+
+const links = document.querySelectorAll('.list-item__link');
+
+links.forEach((link) => {
+  link.addEventListener('click', function (even) {
+    even.preventDefault();
+
+    showBlock.src = link.href;
+  });
+});
