9da679e2a55655b6d9f13d448085fe1eff341819
Viktor Talashko <viktortalashko@gmail.com>
2024-06-16 14:45:06 +0300

    my solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const largeImg = document.querySelector('#largeImg');
+const thumbs = document.querySelector('#thumbs');
+
+thumbs.addEventListener('click', function (evt) {
+  evt.preventDefault();
+
+  const target = evt.target.closest('.list-item');
+
+  if (target) {
+    const img = target.querySelector('a');
+
+    if (img) {
+      const imgUrl = img.getAttribute('href');
+      const imgAlt = img.getAttribute('title');
+
+      largeImg.setAttribute('src', imgUrl);
+      largeImg.setAttribute('alt', imgAlt);
+    }
+  }
+});
