6c69381877820486c734205bb0be67498c046ef5
getchards <kazansasha02@gmail.com>
2024-06-17 21:50:50 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const largeImage = document.getElementById('largeImg');
+const thumbsContainer = document.getElementById('thumbs');
+
+thumbsContainer.addEventListener('click', (e) => {
+  event.preventDefault();
+
+  const target = e.target;
+
+  if (target.tagName === 'IMG' && target.parentElement.tagName === 'A') {
+    largeImage.src = target.parentElement.href;
+  } else if (target.tagName === 'A') {
+    largeImage.src = target.href;
+  }
+});
