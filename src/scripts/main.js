74ea1f4b6b0ed2bf4d9db8fe1b3979584620b442
mikezhylka <115790814+mikezhylka@users.noreply.github.com>
2024-06-17 17:50:16 +0200

    add js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', function () {
+  const thumbs = document.querySelector('#thumbs');
+  const mainImg = document.querySelector('#largeImg');
+
+  thumbs.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    let target = e.target;
+
+    // make sure we target link tag
+    if (e.target.tagName === 'IMG') {
+      target = target.parentNode;
+    }
+
+    // change src attribute for main image
+    mainImg.setAttribute('src', target.getAttribute('href'));
+  });
+});
