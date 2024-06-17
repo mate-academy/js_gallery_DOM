3f61d9ba644e24c3f31734b6009277320456a03a
Hanna Svirina <26anna07@ukr.net>
2024-06-17 16:28:01 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const largeImg = document.getElementById('largeImg');
+const ul = document.getElementById('thumbs');
+
+ul.addEventListener('click', (action) => {
+  action.preventDefault();
+
+  const newLargeImg = action.target.closest('.list-item__link');
+
+  largeImg.src = newLargeImg.href;
+});
