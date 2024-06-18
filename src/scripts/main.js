b3809ca3408376cc7f2a729233e706a0b2ad8de7
Yevhen Protasov <protasovyevhen@gmail.com>
2024-06-18 16:37:57 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const gallerylist = document.querySelector('.gallery__list');
+const largeImg = document.getElementById('largeImg');
+
+gallerylist.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const thumb = e.target.closest('.list-item__link');
+
+  if (!thumb) {
+    return;
+  }
+
+  const newSrc = thumb.href;
+
+  largeImg.setAttribute('src', newSrc);
+});
