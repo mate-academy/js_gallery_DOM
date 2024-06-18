c8405c07e7317b2f19bd475243e7bc1764459562
Oleksandr Lytovchenko <jaster93him@gmail.com>
2024-06-18 21:05:01 +0300

    added event

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const largeImg = document.getElementById('largeImg');
+const conteiner = document.getElementById('thumbs');
+
+conteiner.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const target = e.target;
+
+  if (target.classList.contains('gallery__thumb')) {
+    largeImg.src = target.parentElement.href;
+  } else {
+    largeImg.src = target.href;
+  }
+});
