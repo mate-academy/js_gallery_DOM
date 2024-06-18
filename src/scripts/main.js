f58ef1e21a5ca01c526435fd9cf92aab1b1ead8a
ZakZh <38009936+ZakZh@users.noreply.github.com>
2024-06-18 20:31:51 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const largeImg = document.querySelector('#largeImg');
+const galleryContainer = document.querySelector('#thumbs');
+
+galleryContainer.addEventListener('click', (ev) => {
+  ev.preventDefault();
+  largeImg.src = ev.target.closest('.list-item__link').href;
+});
