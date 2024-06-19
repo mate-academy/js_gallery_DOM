cb6122f896af1b4c07a789f4bd4cfb3a83959255
Rusłan Karaniewski <karane88vka@gmail.com>
2024-06-19 18:51:27 +0200

    My task' solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const mainPhoto = document.querySelector('#largeImg');
+const images = document.querySelectorAll('.list-item');
+
+images.forEach((el) => {
+  el.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const imageSrc = e.target.parentElement.href;
+
+    mainPhoto.src = imageSrc;
+  });
+});
