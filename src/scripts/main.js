c62961d3b96c07e72dbd41225090d287c80334f9
Yaroslav Pazynenko <yaroslavpazynenko@gmail.com>
2024-06-19 15:44:45 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const list = document.querySelector('.gallery__list');
+
+list.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const link = e.target.closest('.list-item__link');
+
+  if (!link) {
+    return;
+  }
+
+  const mainImage = document.getElementById('largeImg');
+
+  mainImage.src = link.href;
+});
