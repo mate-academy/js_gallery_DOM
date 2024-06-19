952cf4dcdbe74568dfccc25d1bff71184c2492c8
Arthur Tolkachov <babanjerelo@gmail.com>
2024-06-19 13:15:29 +0300

    [Feature] added gallery

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const thumbs = document.getElementById('thumbs');
+const mainImage = document.getElementById('largeImg');
+
+thumbs.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const { target } = e;
+
+  const { href } = target.parentElement;
+
+  mainImage.src = href;
+});
