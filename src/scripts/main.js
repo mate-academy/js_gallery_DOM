e38de56f6239e279165d8e6d2375898af7cd7b09
Oleksii Kyslytsyn <kislitsyn.aleksey@gmail.com>
2024-06-20 21:05:54 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const galleryList = document.getElementById('thumbs');
+const largeImg = document.getElementById('largeImg');
+
+galleryList.addEventListener('click', (action) => {
+  action.preventDefault();
+
+  const img = action.target.closest('.list-item__link');
+
+  if (!img) {
+    return;
+  }
+
+  largeImg.src = img.href;
+});
