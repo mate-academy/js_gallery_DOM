8dd3914dacfb66cc69a3c241db889af7d90dd345
Roman Romanchuk <roma.roman4uk.48@gmail.com>
2024-06-20 18:47:42 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const thumbsContainer = document.getElementById('thumbs');
+
+thumbsContainer.addEventListener('click', (ev) => {
+  ev.preventDefault();
+
+  let target = ev.target;
+
+  if (target.tagName === 'IMG') {
+    target = target.parentElement;
+  }
+
+  if (target.tagName === 'A') {
+    const mainImg = document.getElementById('largeImg');
+    const newSrc = target.getAttribute('href');
+
+    mainImg.setAttribute('src', newSrc);
+  }
+});
