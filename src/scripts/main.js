a9aa75164a446b537bc0a347f55dc0055f23df86
Dmytro Bashlai <imaxs2016@gmail.com>
2024-06-20 22:49:11 +0300

    The gallery has been created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const gallery = document.querySelector('.gallery__list');
+const largeImage = document.querySelector('.gallery__large-img');
+
+if (gallery && largeImage) {
+  gallery.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const linkHref = e.target
+      .closest('.list-item')
+      .querySelector('.list-item__link').href;
+
+    largeImage.setAttribute('src', linkHref);
+  });
+}
