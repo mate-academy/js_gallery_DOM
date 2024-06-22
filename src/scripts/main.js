108f903815e5b821fd7df154aa0386a73690b2f6
Voloshynd <Voloshynd13@gmail.com>
2024-06-22 20:14:10 +0200

    add solution of task js_gallery_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const largeImg = document.querySelector('#largeImg');
+const list = document.querySelector('.gallery__list');
+
+list.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const element = e.target;
+
+  if (element.tagName === 'A') {
+    largeImg.src = getReference(element.firstElementChild.getAttribute('src'));
+  } else {
+    largeImg.src = getReference(element.getAttribute('src'));
+  }
+});
+
+function getReference(str) {
+  const idx = str.lastIndexOf('-');
+
+  return `./images${str.slice(0, idx)}.png`;
+}
