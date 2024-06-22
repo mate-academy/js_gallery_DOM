bbabd56ccc890a10b70f08341a512a41e26ba6b6
verjane23 <verjane23.gmail.com>
2024-06-22 15:15:55 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const list = document.querySelector('ul');
+const largeImg = document.getElementById('largeImg');
+
+list.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const img = e.target.closest('.list-item__link');
+
+  if (!img) {
+    return;
+  }
+
+  const link = img.closest('a');
+
+  largeImg.src = link.href;
+});
