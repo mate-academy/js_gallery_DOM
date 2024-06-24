fba602f41bc3a860f58de9d82a3d48cddb482836
Oleh Hanenko <hello.isaev@gmail.com>
2024-06-24 19:20:09 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const largeImg = document.querySelector('#largeImg');
+const thumbs = document.querySelector('#thumbs');
+
+thumbs.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const targetLink = e.target.closest('a');
+
+  if (targetLink) {
+    largeImg.setAttribute('src', targetLink.getAttribute('href'));
+  }
+});
