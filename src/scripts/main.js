4a572f0e4f14a71598180091e71cc7c30df2e5f0
Marian <getmoodyup@gmail.com>
2024-06-25 20:25:52 +0300

    slider2000 is made

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const thumbs = document.getElementById('thumbs');
+const largeImg = document.getElementById('largeImg');
+
+thumbs.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const link = event.target.closest('a');
+
+  if (thumbs.contains(link)) {
+    largeImg.src = link.href;
+    largeImg.alt = link.title;
+  }
+});
