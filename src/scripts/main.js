a2f086ba16b7c1db59118cf1c7cb06df2e96d54e
Pavlo Svinarchuk <shokkontent2004@gmail.com>
2024-06-25 20:48:11 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const gallery = document.querySelector('.gallery');
+const largeImg = gallery.querySelector('#largeImg');
+const smallImg = gallery.querySelector('.gallery__list');
+
+// eslint-disable-next-line no-shadow
+smallImg.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const link = event.target.closest('.list-item__link');
+
+  if (!link) {
+    return;
+  }
+
+  const linkHref = link.getAttribute('href');
+
+  largeImg.setAttribute('src', linkHref);
+});
