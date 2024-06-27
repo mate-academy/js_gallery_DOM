edf3771f574589960d8cd555da4c8df5c20ccbc9
Denis Reznik <reznik.d.o12@gmail.com>
2024-06-27 03:32:27 +0300

    add base url

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const largeImg = document.querySelector('#largeImg');
+const baseURL = window.location.origin;
 
 const gallery = document.querySelector('#thumbs');
 
@@ -10,5 +11,5 @@ gallery.addEventListener('click', (e) => {
   const link = e.target.closest('.list-item__link');
   const imgLink = link.getAttribute('href');
 
-  largeImg.setAttribute('src', imgLink);
+  largeImg.setAttribute('src', `${baseURL}/${imgLink}`);
 });
