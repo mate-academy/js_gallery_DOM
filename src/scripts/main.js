50b1558427b8739ed27c6ac929558764cade5c43
Denis Reznik <reznik.d.o12@gmail.com>
2024-06-27 03:22:28 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const largeImg = document.querySelector('#largeImg');
+const baseURL = window.location.origin;
+
+const gallery = document.querySelector('#thumbs');
+
+gallery.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const link = e.target.closest('.list-item__link');
+  const imgLink = link.getAttribute('href');
+
+  largeImg.setAttribute('src', `${baseURL}${imgLink}`);
+});
