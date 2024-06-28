cb07b1ba7f0fb582a2116aac836687ec926f07ba
MacBook Pro 13 <macbookpro13@MacBook-Pro-MacBook.local>
2024-06-28 16:43:48 +0300

    add test

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
+/* eslint-disable no-shadow */
 'use strict';
+
+const sideImg = document.querySelectorAll('img');
+
+Array.from(sideImg)
+  .slice(1)
+  .forEach((img) => {
+    const currentImg = img.getAttribute('src').split('-')[1];
+
+    img.addEventListener('click', (event) => {
+      event.preventDefault();
+
+      Array.from(sideImg)
+        .slice(0, 1)[0]
+        .setAttribute('src', `/images/landscape-${currentImg}.png`);
+    });
+  });
