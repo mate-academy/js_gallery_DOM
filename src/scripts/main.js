12e187b28dd2b2e5c5bad805afaad89c2e4ffa12
myemets <for.education1809@gmail.com>
2024-06-28 18:18:34 +0300

    solution js_gallery_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const thumbs = document.getElementById('thumbs');
+  const largeImg = document.getElementById('largeImg');
+
+  thumbs.addEventListener('click', (eventClick) => {
+    eventClick.preventDefault();
+
+    let target = eventClick.target;
+
+    if (target.tagName === 'img') {
+      target = target.parentElement;
+    }
+
+    if (target.tagName === 'a') {
+      const newSrc = target.getAttribute('href');
+
+      largeImg.setAttribute('src', newSrc);
+    }
+  });
+});
