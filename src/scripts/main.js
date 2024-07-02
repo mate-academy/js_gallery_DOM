1c2f02e000748e9a733c39d0bd4a5c4bb63f1e6d
smachyloooo <smachyloooo@gmail.com>
2024-07-02 17:03:41 +0200

    add solution
    --no-verify

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const mainPicture = document.getElementById('largeImg');
+const allLi = [...document.querySelectorAll('.list-item')];
+
+
+allLi.map(el => {
+    const anchor = el.querySelector('a');
+    anchor.addEventListener('click', (e) => {
+        e.preventDefault();
+        mainPicture.setAttribute('src', anchor.href);
+    });
+});
