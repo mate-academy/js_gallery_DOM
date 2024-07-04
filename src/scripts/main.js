7153ab499def9a28995be4238028a40f95926a01
Ivan Lavrinenko <lavrinenkoivan51@gmail.com>
2024-07-04 16:29:50 +0200

    corrected code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,15 +6,5 @@ const listGallery = document.querySelector('.gallery__list');
 listGallery.addEventListener('click', (evt) => {
   evt.preventDefault();
 
-  let target = evt.target;
-
-  if (target.tagName === 'IMG') {
-    target = target.parentElement;
-  }
-
-  if (target.tagName === 'A') {
-    const newSrc = target.getAttribute('href');
-
-    largeImg.setAttribute('src', newSrc);
-  }
+  largeImg.src = evt.target.closest('.list-item__link').href;
 });
