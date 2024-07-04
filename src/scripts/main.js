de36ff196ceaafe10dd312b35aa1498e87df54ea
Vadym Bryvka <vadym.bryvkaa@gmail.com>
2024-07-03 21:50:29 -0500

    gallery

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const images = document.querySelectorAll('a');
+
+images.forEach((item) => {
+  item.addEventListener('click', (even) => {
+    even.preventDefault();
+
+    if (even.target.tagName === 'IMG' || even.target.tagName === 'A') {
+      const src = item.getAttribute('href');
+      const absoluteSrc = new URL(src, window.location.origin).href;
+
+      document
+        .getElementById('largeImg')
+        .setAttribute('src', `${absoluteSrc}`);
+    }
+  });
+});
