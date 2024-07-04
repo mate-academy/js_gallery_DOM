f3d6f0a18f9e5e1c2cc15efe5aec4e22f8d4a8b4
Vadym Bryvka <vadym.bryvkaa@gmail.com>
2024-07-03 22:06:15 -0500

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,16 +3,14 @@
 const images = document.querySelectorAll('a');
 
 images.forEach((item) => {
-  item.addEventListener('click', (even) => {
-    even.preventDefault();
+  item.addEventListener('click', (e) => {
+    e.preventDefault();
 
-    if (even.target.tagName === 'IMG' || even.target.tagName === 'A') {
+    if (e.target.tagName === 'IMG' || e.target.tagName === 'A') {
       const src = item.getAttribute('href');
       const absoluteSrc = new URL(src, window.location.origin).href;
 
-      document
-        .getElementById('largeImg')
-        .setAttribute('src', `${absoluteSrc}`);
+      document.getElementById('largeImg').setAttribute('src', `${absoluteSrc}`);
     }
   });
 });
