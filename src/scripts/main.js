8d8de253af2538e4f590cb5f34a7a9f80ee1cacd
Ivan Lavrinenko <lavrinenkoivan51@gmail.com>
2024-07-04 14:56:56 +0200

    corrected version of code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,22 +1,20 @@
 'use strict';
 
-document.addEventListener('DOMContentLoaded', () => {
-  const largeImg = document.getElementById('largeImg');
-  const thumbsContainer = document.getElementById('thumbs');
+const largeImg = document.getElementById('largeImg');
+const listGallery = document.querySelector('.gallery__list');
 
-  thumbsContainer.addEventListener('click', (evt) => {
-    evt.preventDefault();
+listGallery.addEventListener('click', (evt) => {
+  evt.preventDefault();
 
-    let target = evt.target;
+  let target = evt.target;
 
-    if (target.tagName === 'IMG') {
-      target = target.parentElement;
-    }
+  if (target.tagName === 'IMG') {
+    target = target.parentElement;
+  }
 
-    if (target.tagName === 'A') {
-      const newSrc = target.getAttribute('href');
+  if (target.tagName === 'A') {
+    const newSrc = target.getAttribute('href');
 
-      largeImg.setAttribute('src', newSrc);
-    }
-  });
+    largeImg.setAttribute('src', newSrc);
+  }
 });
