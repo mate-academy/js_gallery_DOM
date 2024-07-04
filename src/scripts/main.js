598afff59af35fac78e56c0035964f486fa00193
Daria Sydorenko <dashasidorenko123@gmail.com>
2024-07-04 15:18:40 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+document.addEventListener('click', (ev) => {
+  ev.preventDefault();
+
+  const largeImg = document.getElementById('largeImg');
+
+  const thumbnailLink = ev.target.closest('.list-item__link');
+
+  if (!thumbnailLink) {
+    return;
+  }
+
+  largeImg.src = thumbnailLink.href;
+});
