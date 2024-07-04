c4b87966539df5f241f85f223a9ddab900320cc8
Ivan Lavrinenko <lavrinenkoivan51@gmail.com>
2024-07-04 14:27:29 +0200

    used event delegation to make images open

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const largeImg = document.getElementById('largeImg');
+  const thumbsContainer = document.getElementById('thumbs');
+
+  thumbsContainer.addEventListener('click', (evt) => {
+    evt.preventDefault();
+
+    let target = evt.target;
+
+    if (target.tagName === 'IMG') {
+      target = target.parentElement;
+    }
+
+    if (target.tagName === 'A') {
+      const newSrc = target.getAttribute('href');
+
+      largeImg.setAttribute('src', newSrc);
+    }
+  });
+});
