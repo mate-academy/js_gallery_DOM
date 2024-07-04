769c0fbf07cbd69a4f60d16e4c72b6ca08093f52
Heorhii Savostikov <georgesavostikov@gmail.com>
2024-07-04 23:03:40 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const ulELement = document.querySelector('ul');
+const imgElement = document.querySelector('div > img');
+
+ulELement.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const link = e.target.closest('a');
+
+  if (!link) {
+    return;
+  }
+
+  imgElement.setAttribute('src', link.href);
+});
