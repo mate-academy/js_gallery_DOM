d0d932c78f3338c2d2ae33bff22b322a5a165d5e
Seva Podolskiy <s_podolskiy@live.com>
2024-07-05 18:03:51 -0400

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const galleryCont = document.querySelector('.gallery__list');
+const galleryImgLarge = document.querySelector('#largeImg');
+
+// clear default action on a tag
+document.querySelectorAll('.list-item__link').forEach((link) => {
+  link.setAttribute('onClick', 'event.preventDefault()');
+});
+
+galleryCont?.addEventListener('click', function (eventClick) {
+  const imgTarget = eventClick.target.getAttribute('src');
+
+  galleryImgLarge?.setAttribute(
+    'src',
+    location.origin + '/images' + imgTarget.split('-thumb')[0] + '.png',
+  );
+});
