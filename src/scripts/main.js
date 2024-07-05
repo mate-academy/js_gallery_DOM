f8fe1352d76441ac1e5f270edac45de04beb32b8
under-a <mymail15@i.ua>
2024-07-05 11:36:42 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const gallery = document.querySelector('.gallery');
+
+gallery.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  if (e.target.classList.contains('gallery__thumb')) {
+    const thumbPath = new URL(e.target.src);
+    const bigImage = thumbPath.pathname
+      .split('-')
+      .slice(0, 2)
+      .join('-')
+      .slice(1);
+
+    const galleryImage = gallery.firstElementChild.firstElementChild;
+
+    const newPathGallery = galleryImage.src.split('/');
+
+    newPathGallery.splice(-1, 1, `${bigImage}.png`);
+    galleryImage.src = newPathGallery.join('/');
+  }
+});
