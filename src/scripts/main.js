3679f1ff9de913870633c4caea5562e909a2f841
Anastasiia <vasilisandreeva00@gmail.com>
2024-07-05 16:28:51 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,35 @@
 'use strict';
+
+const container = document.querySelector('.gallery__list');
+
+container.addEventListener(
+  'click',
+  (eventTarget) => {
+    const targetListItem = eventTarget.target.closest('li');
+
+    if (!targetListItem) {
+      return;
+    }
+
+    if (!container.contains(targetListItem)) {
+      return;
+    }
+
+    const targetLink = targetListItem.querySelector('a');
+
+    if (targetLink) {
+      eventTarget.preventDefault();
+    }
+
+    const source = targetLink.href;
+
+    changeBigImage(source);
+  },
+  { capture: true },
+);
+
+function changeBigImage(image) {
+  const newImage = document.querySelector('.gallery__large-img');
+
+  newImage.src = image;
+}
