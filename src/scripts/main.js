1018c5b6c26e74241931f6667cbc722f1f74db3b
under-a <mymail15@i.ua>
2024-07-05 12:36:54 +0300

    Simplifying code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,24 +1,10 @@
 'use strict';
 
 const gallery = document.querySelector('.gallery');
+const largeImg = document.querySelector('.gallery__large-img');
 
 gallery.addEventListener('click', (e) => {
   e.preventDefault();
 
-  if (e.target.classList.contains('gallery__thumb')) {
-    const thumbPath = new URL(e.target.src);
-    const bigImage = thumbPath.pathname
-      .split('/')
-      .at(-1)
-      .split('-')
-      .slice(0, 2)
-      .join('-');
-
-    const galleryImage = gallery.firstElementChild.firstElementChild;
-
-    const newPathGallery = galleryImage.src.split('/');
-
-    newPathGallery.splice(-1, 1, `${bigImage}.png`);
-    galleryImage.src = newPathGallery.join('/');
-  }
+  largeImg.src = e.target.closest('.list-item__link').href;
 });
