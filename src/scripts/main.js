1fb55b922a16c28ed1ca8651bb58d3bd772a0b41
Kirill <ovoo1339@gmail.com>
2024-07-09 11:07:12 +0300

    work

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const largeImg = document.getElementById('largeImg');
+const parentList = document.getElementById('thumbs');
+
+parentList.addEventListener('click', (evt) => {
+  if (evt.target) {
+    evt.preventDefault();
+    largeImg.setAttribute('src', evt.target.src);
+  }
+});
