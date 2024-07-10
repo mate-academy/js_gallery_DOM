f65bec553ce71f02532c173805ae3b5ad4677f44
Jane Shavrukova <eva.shavrukova@gmail.com>
2024-07-09 21:50:32 -0700

    Add Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const largeImg = document.getElementById('largeImg');
+const pickedImg = document.getElementById('thumbs');
+
+pickedImg.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  if (e.target.tagName === 'IMG' && e.target.parentElement.tagName === 'A') {
+    const linkElement = e.target.parentElement;
+    const imageUrl = new URL(
+      linkElement.getAttribute('href'),
+      window.location.href,
+    );
+
+    largeImg.setAttribute('src', imageUrl);
+  }
+});
