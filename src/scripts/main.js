883a632657afe7337cdb74d79249ffad68c5fc89
roman-logos-frontend <romanst235@gmail.com>
2024-07-10 18:29:13 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const ulItems = document.querySelector('#thumbs');
+const mainImage = document.querySelector('#largeImg');
+
+ulItems.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  mainImage.src = event.target.closest('.list-item__link').href;
+});
