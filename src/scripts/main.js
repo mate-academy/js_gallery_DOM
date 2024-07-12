5f8216b1d5b520c06e3db78c5de3b4a6c86295c0
Iryna Stefaniuk <irinagoyan@gmail.com>
2024-07-12 15:40:54 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const largeImg = document.querySelector('#largeImg');
+const galeryImg = document.querySelector('ul');
+
+galeryImg.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const newLargeImg = e.target.closest('.gallery__thumb');
+
+  largeImg.setAttribute('src', newLargeImg.getAttribute('src'));
+});
