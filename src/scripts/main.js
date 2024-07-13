841889433d471699e2459acec490c0b52c9069b5
Oleksandr Sushko <alexandrsushko25@gmail.com>
2024-07-13 22:25:40 +0200

    commit message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,12 +6,11 @@ const mainPhoto = document.querySelector('.gallery__large-img');
 galery.addEventListener('click', (e) => {
   e.preventDefault();
 
-  const item = e.target.closest('.list-item__link'); // есть href
+  const item = e.target.closest('.list-item__link');
 
-  // check if click was inside a `.nav__link`
   if (!item) {
     return;
   }
 
-  mainPhoto.src = item.href; // Изменение `src` основного изображения
+  mainPhoto.src = item.href;
 });
