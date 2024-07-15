ba5ca4325bf9f42f2b11ecb801960c6cb5d0f485
Andrii <andriykostiuk1@gmail.com>
2024-07-15 21:52:53 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const largeImg = document.querySelector('#largeImg');
+const thumbs = document.querySelector('#thumbs');
+
+thumbs.addEventListener('click', (evt) => {
+  evt.preventDefault();
+
+  const href = evt.target.closest('a').getAttribute('href');
+  const domain = window.location.origin;
+
+  if (href) {
+    largeImg.src = domain + href;
+  }
+});
