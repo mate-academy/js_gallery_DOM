f04b985731196d89c1259586a0c8143f9b3538e2
Artem Pinchuk <103887088+Artem193@users.noreply.github.com>
2024-07-15 07:33:12 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const listImg = document.getElementById('thumbs');
+const largeImg = document.getElementById('largeImg');
+
+listImg.addEventListener('click', (evt) => {
+  let target = evt.target;
+
+  evt.preventDefault();
+
+  if (target.tagName === 'IMG') {
+    target = target.parentElement;
+  }
+
+  largeImg.setAttribute('src', target.href);
+});
