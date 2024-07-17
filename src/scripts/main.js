c86b538ebef883f3b67873c1b27154b2e992d1f1
Ivan <tixehack@gmail.com>
2024-07-17 14:10:42 +0300

    feat: add logic to complete task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const mainImg = document.getElementById('largeImg');
+const gallery = document.querySelector('.gallery__list');
+
+gallery.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const linkToSmallPhoto = event.target.closest('a');
+
+  if (linkToSmallPhoto) {
+    mainImg.src = linkToSmallPhoto;
+  }
+});
