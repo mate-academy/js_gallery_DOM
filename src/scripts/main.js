2febca116fd5a2a6f3ebe2548199856ca9a02d26
Olha Rypich <olha.rypich@gmail.com>
2024-07-17 22:24:26 +0300

    js_gallery_DOM_solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const gallery = document.querySelector('.gallery__list');
+const largeImg = document.getElementById('largeImg');
+
+gallery.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  largeImg.src = e.target.closest('a').href;
+});
