da178edb25cdea121f13e13d60091f1e08c7f619
Anatoly Olkhovsky <anatoltbcm@gmail.com>
2024-07-19 23:51:10 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const bigImg = document.querySelector('#largeImg');
+const thumbsList = document.querySelector('#thumbs');
+
+thumbsList.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const link = event.target.closest('.list-item__link');
+
+  bigImg.src = link.href;
+});
