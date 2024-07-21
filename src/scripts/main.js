9dc01a1e80c784d191bfe108c9f23b94fe9a0846
Iryna Yehorova <yehorova.dev@gmail.com>
2024-07-21 09:52:36 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const thumbList = document.querySelector('.gallery__list');
+const largeImage = document.querySelector('.gallery__large-img');
+
+thumbList.addEventListener('click', showImage);
+
+function showImage(e) {
+  e.preventDefault();
+
+  const target = e.target;
+
+  if (target.className.includes('gallery__thumb')) {
+    const thumbUrl = target.src;
+
+    largeImage.src = getLargeImgUrl(thumbUrl);
+  }
+}
+
+function getLargeImgUrl(thumbUrl) {
+  const thumbUrlParts = thumbUrl.split('/');
+  const nameThumbFile = thumbUrlParts[thumbUrlParts.length - 1].split('.')[0];
+
+  return `../images/${nameThumbFile.replace('-thumb', '')}.png`;
+}
