d814c62c01f855d1225f92f90f6af1ee18648d6b
Igor Novikov <novikov.igor.20@gmail.com>
2024-07-22 21:07:32 +0300

    add task gallery DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const galleryList = document.querySelector('.gallery__list');
+const galleryLargeImg = document.querySelector('.gallery__large-img');
+
+galleryList.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const link = e.target.closest('.list-item__link');
+
+  if (link) {
+    galleryLargeImg.src = link.href;
+  }
+});
