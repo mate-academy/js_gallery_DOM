70e8b762c6b864da76cd01177d33e409d8acc396
Viktoriia <vkkishko@gmail.com>
2024-07-22 15:45:41 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const gallery = document.querySelector('.gallery');
+const mainImg = document.querySelector('.gallery__large-img');
+
+const clickHnadler = (event) => {
+  event.preventDefault();
+
+  const target = event.target;
+
+  mainImg.src = target.parentNode.href;
+};
+
+gallery.addEventListener('click', clickHnadler);
