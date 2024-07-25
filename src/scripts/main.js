71487ca1f6490665294052e6700555635b2e65d0
Maksym_enKO <finside41@gmail.com>
2024-07-25 14:28:59 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
-'use strict';
+function ready() {
+  const mainImg = document.querySelector('#largeImg');
+  const imagesList = document.querySelector('#thumbs');
+
+  imagesList.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const link = e.target.closest('a');
+
+    if (!link) {
+      return;
+    }
+
+    mainImg.src = link.href;
+  });
+}
+
+document.addEventListener('DOMContentLoaded', ready);
