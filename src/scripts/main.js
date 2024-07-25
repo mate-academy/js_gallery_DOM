2538282e9e9f56cb14a4a9fb535a171a507afe6a
mrsvolodya <vovamursky@gmail.com>
2024-07-25 16:42:48 +0300

    gallery-dom

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const largeImage = document.getElementById('largeImg');
+
+document.getElementById('thumbs').addEventListener('mousedown', (ev) => {
+  const currentLink = ev.target.closest('.list-item__link');
+  const currentImage = ev.target.closest('.gallery__thumb');
+
+  if (currentImage) {
+    currentLink.addEventListener('click', (e) => {
+      e.preventDefault();
+    });
+
+    const newSrc = event.target.getAttribute('src');
+
+    largeImage.setAttribute('src', `${newSrc}`);
+  }
+});
