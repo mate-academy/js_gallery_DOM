473d3583b80f215ee426ea4354fd0a118a978530
Roma Zuravliov <janmorozov1988@gmail.com>
2024-07-27 12:17:44 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const mainImg = document.getElementById('largeImg');
+const thumbsnailGallery = document.getElementById('thumbs');
+
+thumbsnailGallery.addEventListener('click', (e) => {
+  const target = e.target;
+
+  if (target.tagName === 'IMG' || target.tagName === 'A') {
+    const thumbnailLink =
+      target.tagName === 'A' ? target : target.parentElement;
+    const newSrc = thumbnailLink.href;
+
+    mainImg.src = newSrc;
+
+    e.preventDefault();
+  }
+});
