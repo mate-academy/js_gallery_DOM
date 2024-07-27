72e1384191034094fcb6d8e6776a4fbcad1a7ff8
mobiledaisy <morana110900@gmail.com>
2024-07-27 10:58:24 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,32 @@
 'use strict';
+
+const mainImage = document.querySelector('#largeImg');
+const arrayOfLinks = document.querySelectorAll('.list-item__link');
+
+arrayOfLinks.forEach((link) => {
+  link.addEventListener('click', clickHandler);
+
+  const img = link.querySelector('img');
+
+  if (img) {
+    img.addEventListener('click', clickHandler);
+  }
+});
+
+function clickHandler(ev) {
+  ev.preventDefault();
+
+  const link = ev.currentTarget.closest('.list-item__link');
+
+  const srcValue = toAbsoluteUrl(link.getAttribute('href'));
+
+  mainImage.setAttribute('src', srcValue);
+}
+
+function toAbsoluteUrl(relativePath) {
+  const a = document.createElement('a');
+
+  a.href = relativePath;
+
+  return a.href;
+}
