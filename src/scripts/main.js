52c26552e325a291cd713c7ba2ca6c4ddb02c7e7
Vasyl Chukhrai <wasik1995@mail.com>
2024-07-27 18:08:44 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const galleryList = document.querySelector('.gallery__list');
+const largeImg = document.querySelector('.gallery__large-img');
+const links = document.querySelectorAll('.list-item__link');
+
+links.forEach((link) => (link.onclick = (events) => events.preventDefault()));
+
+galleryList.onclick = (events) =>
+  (largeImg.src = events.target.closest('.list-item__link').href);
