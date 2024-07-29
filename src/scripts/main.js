c55a8c51717c2a9536327cb0854f555406747c4b
Mykola Bilenkiy <nikolosknv@gmail.com>
2024-07-29 23:14:01 +0300

    make task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const thumbs = document.querySelector('#thumbs');
+const largeImg = document.querySelector('#largeImg');
+
+thumbs.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const target = e.target.closest('a');
+
+  if (target) {
+    largeImg.src = target.href;
+  }
+});
