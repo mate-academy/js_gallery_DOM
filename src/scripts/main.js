dd955f388fd395d1ce3ba3d44b74ecfd789055c0
Kirill Gasan <dp160190gkj@gmail.com>
2024-08-05 11:00:14 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const imgDiv = document.querySelector('#largeImg');
+const ul = document.querySelector('ul');
+
+function changeImage(e) {
+  if (e.target.tagName === 'IMG' || e.target.tagName === 'A') {
+    e.preventDefault();
+
+    const necessaryA = e.target.closest('a');
+    const newSrc = necessaryA.getAttribute('href');
+
+    imgDiv.setAttribute('src', newSrc);
+  }
+}
+
+ul.addEventListener('click', changeImage);
