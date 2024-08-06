71863b625d7a6d88c189d2cf64333d5f8bb61cc0
NataliaDavida <nataliadavida88@gmail.com>
2024-08-06 22:04:41 +0300

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
-'use strict';
+const largeImgEl = document.getElementById('largeImg');
+const thumbs = document.getElementById('thumbs');
+
+thumbs.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const linkEl = e.target.closest('.list-item__link');
+
+  if (thumbs.contains(linkEl)) {
+    const newSrc = linkEl.getAttribute('href');
+
+    largeImgEl.setAttribute(
+      'src',
+      new URL(newSrc, window.location.origin).href,
+    );
+  }
+});
