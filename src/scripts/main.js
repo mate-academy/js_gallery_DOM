098257b5443a34a371e08ef37eec7d7dbc0703c4
Oleksii Symonets <alex.simonets@gmail.com>
2024-08-07 21:43:06 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const galery = document.querySelector('#largeImg');
+const listLinks = document.querySelector('#thumbs');
+
+listLinks.addEventListener('click', (ev) => {
+  ev.preventDefault();
+
+  const link = ev.target.closest('a');
+
+  galery.setAttribute(
+    'src',
+    'http://' + window.location.host + link.getAttribute('href'),
+  );
+});
