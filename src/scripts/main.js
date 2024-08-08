581dda891674cdbb64ddf8ef3a2f31d072a4d484
Olexander <v.h.olexander@gmail.com>
2024-08-08 10:23:07 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const mainImg = document.getElementById('largeImg');
+const imgList = document.querySelector('.gallery__list');
+
+imgList.addEventListener('click', (even) => {
+  even.preventDefault();
+
+  const image = even.target.closest('.list-item__link');
+
+  if (image) {
+    mainImg.src = image.href;
+  }
+});
