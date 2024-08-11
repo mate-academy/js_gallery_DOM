faeb8e2ae40ecc8a51755dabd157c9f8d8d73b63
Victor Komara <vitioklss@gmail.com>
2024-08-11 18:54:44 +0300

    add task second solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,33 +3,14 @@
 const largeImg = document.querySelector('#largeImg');
 const nav = document.querySelector('.gallery__list');
 
-nav.addEventListener('click', (event) => {
-  event.preventDefault();
+nav.addEventListener('click', (e) => {
+  e.preventDefault();
 
-  const link = event.target.closest('.list-item');
-  const title = link.querySelector('a').title;
+  const link = e.target.closest('.list-item__link');
 
   if (!link) {
     return;
   }
 
-  if (title === 'Image 1') {
-    largeImg.src = './images/landscape-first.png';
-  }
-
-  if (title === 'Image 2') {
-    largeImg.src = './images/landscape-second.png';
-  }
-
-  if (title === 'Image 3') {
-    largeImg.src = './images/landscape-third.png';
-  }
-
-  if (title === 'Image 4') {
-    largeImg.src = './images/landscape-fourth.png';
-  }
-
-  if (title === 'Image 5') {
-    largeImg.src = './images/landscape-fifth.png';
-  }
+  largeImg.src = link.href;
 });
