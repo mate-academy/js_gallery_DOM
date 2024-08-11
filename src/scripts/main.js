162c7edeabc082119609b697c90a8b66e537d294
Mikhailo Koshilka <mkosilka@gmail.com>
2024-08-11 10:05:49 +0100

    Add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const mainPicture = document.getElementById('largeImg');
+const container = document.getElementById('thumbs');
+
+container.addEventListener('click', (evt) => {
+  evt.preventDefault();
+
+  const link = evt.target.closest('.list-item__link');
+  const newPicture = link.getAttribute('href');
+
+  mainPicture.src = newPicture;
+});
