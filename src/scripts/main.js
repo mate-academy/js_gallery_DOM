a7b8340f32b9ff668e56dbe1fd5c5d1de47bea92
Burchak Dmitry <burchak.dmitry@icloud.com>
2024-08-18 13:54:20 +0300

    task is done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const mainImg = document.querySelector('.gallery__large-img');
+const itemLink = [...document.querySelectorAll('.list-item__link')];
+
+itemLink.forEach((img, index) => {
+  img.addEventListener('click', () => {
+    mainImg.src = `${location.origin}${itemLink[index].getAttribute('href')}`;
+  });
+});
