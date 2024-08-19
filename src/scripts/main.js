acd2393b0edd02a2e824ed27071d8a13f0e110f8
Vlad Tkachuk <vlad.tkachuk.04@gmail.com>
2024-08-19 21:51:09 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const largeImg = document.querySelector('#largeImg');
+const thumbnailLinks = document.querySelectorAll('.list-item__link');
+
+thumbnailLinks.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    largeImg.src = link.href;
+  });
+});
