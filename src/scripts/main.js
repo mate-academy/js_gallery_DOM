69a83be1ecebb3d234d66740bc0c8022f7082635
Olena Horovenko <elena.gorovenko@gmail.com>
2024-08-19 17:23:22 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const largeImg = document.querySelector('#largeImg');
+const thumbCollection = document.querySelector('.gallery__list');
+
+thumbCollection.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const target = e.target.closest('a');
+
+  if (!target) {
+    return;
+  }
+
+  if (!thumbCollection.contains(target)) {
+    return;
+  }
+
+  selectThumb(target);
+});
+
+function selectThumb(thumb) {
+  largeImg.src = thumb.href;
+}
