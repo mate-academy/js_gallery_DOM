e46a0990df8356ebb0d54fe01bb526765ed401d8
Hlib Vakhrameiev <g.vakhrameev@icloud.com>
2024-08-20 16:36:56 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const generalPicture = document.getElementById('largeImg');
+const pictures = document.querySelectorAll('a');
+
+pictures.forEach((picture) => {
+  picture.addEventListener('click', (e) => {
+    e.preventDefault();
+    generalPicture.src = `${picture.href}`;
+  });
+});
