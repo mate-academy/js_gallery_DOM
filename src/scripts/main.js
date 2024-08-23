a8955bf20e141b92ca7bbf2ed7b0e4ed9281e3db
unknown <maksoleksyuk@gmail.com>
2024-08-23 11:16:27 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const mainImage = document.querySelector('#largeImg');
+
+const thumbs = document.querySelectorAll('.list-item__link');
+
+thumbs.forEach((thumb) => {
+  thumb.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const newSrc = thumb.getAttribute('href');
+
+    mainImage.setAttribute('src', newSrc);
+  });
+});
