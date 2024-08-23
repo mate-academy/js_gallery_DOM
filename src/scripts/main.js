3d3b4541419cfc710b52bc8589184a6c32aa7bdb
Oleksandr Robenko <oleksandr.robenko@gmail.com>
2024-08-24 01:52:27 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const largeImg = document.getElementById('largeImg');
+const thumbsLinks = document.querySelector('.gallery__list');
+
+thumbsLinks.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const link = e.target.closest('a');
+
+  if (!link) {
+    return;
+  }
+
+  const imgSrc = link.getAttribute('href');
+
+  largeImg.src = imgSrc;
+});
