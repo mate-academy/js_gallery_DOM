39418131763da07700b966b7a5671e226507abf6
Serhii-Khobotov <hobbitura@gmail.com>
2024-08-24 15:27:01 +0300

    Gallery ready

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const largeImg = document.querySelector('.gallery__large-img');
+const links = document.querySelectorAll('.list-item__link');
+
+for (const link of links) {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    largeImg.src = link.href;
+  });
+}
