913e94454c17d2cd1be00c7eb6806b54e1db6b76
Yehor Gordiienko <egordienko319@gmail.com>
2024-08-24 18:19:33 +0300

    first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
-'use strict';
+const links = document.querySelectorAll('.list-item__link');
+const mainFoto = document.querySelector('.gallery__large-img');
+
+for (const link of links) {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    mainFoto.src = link.href;
+  });
+}
