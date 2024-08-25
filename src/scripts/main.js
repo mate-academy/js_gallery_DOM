12a0491d824bb6c15867c0df80ca116922dfac5c
Daria Nastas <bycinska@gmail.com>
2024-08-25 14:29:37 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+document.getElementById('thumbs').addEventListener('click', (event) => {
+  event.preventDefault();
+
+  let target = event.target;
+
+  if (target.tagName === 'IMG') {
+    target = target.parentElement;
+  }
+
+  if (target.tagName === 'A') {
+    const largeImg = document.getElementById('largeImg');
+
+    largeImg.src = target.href;
+  }
+});
