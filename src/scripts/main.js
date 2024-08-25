20e3f76178b66d50490a3e57cc11719fd8198078
Yurii Kramar <y.kramar15@gmail.com>
2024-08-25 16:12:51 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const list = document.querySelector('.gallery__list');
+const mainImg = document.querySelector('#largeImg');
+
+list.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  mainImg.src = e.target.parentElement.href;
+});
