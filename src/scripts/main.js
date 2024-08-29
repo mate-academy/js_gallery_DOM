4782172181d5adbdee7158d78d9773d25b69c032
Ostap Kushnir <kushnirostap04@gmail.com>
2024-08-29 18:34:20 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const li = document.querySelectorAll('li');
+const largeImg = document.querySelector('#largeImg');
+
+li.forEach((element) => {
+  const link = element.querySelector('a');
+
+  link.addEventListener('click', (eventOfClick) => {
+    eventOfClick.preventDefault();
+
+    const href = link.getAttribute('href');
+
+    largeImg.setAttribute('src', `http://localhost:3001${href}`);
+  });
+});
