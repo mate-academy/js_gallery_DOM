f02a84ef2f5d72fcf0de6453c0021b8ee7334b20
Stas <staskapica93@gmail.com>
2024-09-02 21:07:41 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const galleryList = document.getElementById('thumbs');
+
+galleryList.addEventListener('click', (obj) => {
+  obj.preventDefault();
+
+  const link = obj.target.closest('a');
+
+  if (!link) {
+    return;
+  }
+
+  if (!link.contains(obj.target)) {
+    return;
+  }
+
+  const largeImg = document.getElementById('largeImg');
+
+  largeImg.src = link.href;
+});
