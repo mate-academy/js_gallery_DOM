774426a6c534789b541343aa131d62fa001716d2
Dmytro Ivasenko <dmtivasenko@gmail.com>
2024-09-06 00:06:20 +0700

    add js code for gallery

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const listOfImages = document.querySelector('.gallery__list');
+const mainImg = document.getElementById('largeImg');
+let imgPath = '';
+
+listOfImages.addEventListener('click', (e) => {
+  const { target } = e;
+
+  e.preventDefault();
+
+  if (target.tagName.toLowerCase() === 'img') {
+    imgPath = target.closest('a').href;
+    mainImg.src = imgPath;
+  }
+
+  if (target.tagName.toLowerCase() === 'a') {
+    imgPath = target.href;
+    mainImg.src = imgPath;
+  }
+});
