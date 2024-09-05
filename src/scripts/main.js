3ae12fd4dc468936fa37625d07d6322df4786956
Nataliia <koreniuknataliia@gmail.com>
2024-09-05 17:29:38 +0300

    updated after review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,30 +3,9 @@
 const largeImg = document.getElementById('largeImg');
 const linkList = document.getElementsByClassName('list-item__link');
 
-const linkListArray = [...linkList];
-
-for (const link of linkListArray) {
+[...linkList].forEach((link) => {
   link.addEventListener('click', (e) => {
     e.preventDefault();
-
-    switch (link.title) {
-      case 'Image 1':
-        largeImg.src = './images/landscape-first.png';
-        break;
-      case 'Image 2':
-        largeImg.src = './images/landscape-second.png';
-        break;
-      case 'Image 3':
-        largeImg.src = './images/landscape-third.png';
-        break;
-      case 'Image 4':
-        largeImg.src = './images/landscape-fourth.png';
-        break;
-      case 'Image 5':
-        largeImg.src = './images/landscape-fifth.png';
-        break;
-      default:
-        largeImg.src = '';
-    }
+    largeImg.src = e.target.closest('.list-item__link').href;
   });
-}
+});
