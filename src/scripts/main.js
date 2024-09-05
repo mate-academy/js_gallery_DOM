b8ecd5b9a1d555b483e771955aa8388d54a4f738
Diana Darienko <diana.darienko21@gmail.com>
2024-09-05 14:50:44 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const largeImg = document.getElementById('largeImg');
+const list = document.querySelector('.gallery__list');
+
+list.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const link = e.target.closest('.list-item__link');
+  const linkHref = link.getAttribute('href');
+
+  largeImg.setAttribute('src', linkHref);
+});
