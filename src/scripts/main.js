892e7966b984eaed8246218b8f7f538c0f44d280
voidmain69 <photoreznichenko@gmail.com>
2024-09-09 00:44:27 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
-'use strict';
+document.addEventListener('DOMContentLoaded', () => {
+  const largeImg = document.querySelector('#largeImg');
+  const thumbs = document.querySelector('#thumbs');
+
+  const handleThumbnailClick = (e) => {
+    e.preventDefault();
+
+    const target = e.target.closest('a');
+
+    if (target) {
+      const newImgSrc = target.getAttribute('href');
+
+      updateMainImage(newImgSrc);
+    }
+  };
+
+  const updateMainImage = (src) => {
+    largeImg.setAttribute('src', src);
+  };
+
+  thumbs.addEventListener('click', handleThumbnailClick);
+});
