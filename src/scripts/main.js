c081c326e331d35614a81d6569e9e37571019a0e
Kamil <kamilbykowski99@gmail.com>
2024-09-08 19:08:29 +0200

    removed empty {}

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,17 +3,14 @@
 const largeImg = document.querySelector('#largeImg');
 const smallImg = document.querySelectorAll('.list-item__link');
 
-const imagesLinksArray = Array.from(smallImg).map(
-  (item) => item.getAttribute('href'),
-  {},
-);
+const imagesLinksArray = Array.from(smallImg).map((item) => {
+  item.getAttribute('href');
+});
 
-smallImg.forEach(
-  (item, i) =>
-    item.addEventListener('click', function (e) {
-      e.preventDefault();
+smallImg.forEach((item, i) => {
+  item.addEventListener('click', function (e) {
+    e.preventDefault();
 
-      largeImg.setAttribute('src', `${imagesLinksArray[i]}`);
-    }),
-  {},
-);
+    largeImg.setAttribute('src', `${imagesLinksArray[i]}`);
+  });
+});
