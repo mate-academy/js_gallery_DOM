67c52edb484307f4b4f1559713cac13119b4df43
Kamil <kamilbykowski99@gmail.com>
2024-09-08 18:17:21 +0200

    Gallery_DOM(events)

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const largeImg = document.querySelector('#largeImg');
+const smallImg = document.querySelectorAll('.list-item__link');
+
+const arr = Array.from(smallImg).map(
+  (item) => item.getAttribute('href').replace(/\/images\/landscape-|.png/g, ''),
+  {},
+);
+
+smallImg.forEach(
+  (item, i) =>
+    item.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      largeImg.src = `./images/landscape-${arr[i]}.png`;
+    }),
+  {},
+);
