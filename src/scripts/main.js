6235e4d7c501620e164cc95f542260a1da66305c
yursprava <yursprava@gmail.com>
2024-09-07 22:20:17 -0400

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const smallImgCollection = document.querySelectorAll('.gallery__thumb');
+
+smallImgCollection.forEach((elem) => {
+  const hrefValue = elem.parentNode.getAttribute('href');
+
+  elem.setAttribute('mydata', hrefValue);
+  elem.parentNode.href = '';
+});
+
+const gallery = document.querySelector('.gallery');
+
+const largeImgElem = document.querySelector('.gallery__large-img')
+
+gallery.addEventListener('click', function (ev) {
+  if (ev.target.tagName === 'IMG') {
+    ev.preventDefault();
+    ev.stopPropagation();
+
+    const route = ev.target.getAttribute('mydata');
+
+    if (route) {
+      largeImgElem.setAttribute('src', route);
+    }
+  }
+});
