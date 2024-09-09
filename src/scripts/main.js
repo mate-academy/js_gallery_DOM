663343a37119aa3efe828d1dfebc8a8f54be6cdd
Iryna Zahorodnia <irishas25@ukr.net>
2024-09-09 20:11:38 +0300

    gallery

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const list = document.querySelector('.gallery__list');
+const largeImg = document.getElementById('largeImg');
+
+list.addEventListener('click', (clickEvent) => {
+  const linkImg = clickEvent.target.closest('.list-item__link');
+
+  clickEvent.preventDefault();
+
+  if (linkImg) {
+    largeImg.src = linkImg.href;
+  }
+});
