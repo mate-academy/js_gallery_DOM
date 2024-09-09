ae0e246311c717e4dfbb0cb07524488732958786
Artem-Brui <artembryj@gmail.com>
2024-09-09 13:26:11 +0200

    update path

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -14,7 +14,7 @@ el.container.addEventListener('click', (ev) => {
     if (isClickFound) {
       ev.preventDefault();
 
-      const path = `.${image.closest('a').getAttribute('href')}`;
+      const path = `${image.closest('a').getAttribute('href')}`;
 
       el.largeImg.setAttribute('src', path);
     }
