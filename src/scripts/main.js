26a3511570aa2781089f96e70d56b15994593557
Artem-Brui <artembryj@gmail.com>
2024-09-09 13:21:56 +0200

    solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const el = {
+  container: document.querySelector('.gallery'),
+  largeImg: document.getElementById('largeImg'),
+  smallImgs: document.getElementsByClassName('gallery__thumb'),
+};
+
+el.container.addEventListener('click', (ev) => {
+  for (const image of el.smallImgs) {
+    const isClickFound =
+      ev.target === image || ev.target === image.closest('a');
+
+    if (isClickFound) {
+      ev.preventDefault();
+
+      const path = `${window.location.origin}${image.closest('a').getAttribute('href')}`;
+
+      el.largeImg.setAttribute('src', path);
+    }
+  }
+});
