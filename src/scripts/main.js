f95464b6418b65340e1f716fdb83fc042b300f8e
Ren <buher1994@gmail.com>
2024-09-10 22:13:43 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const largeImg = document.getElementById('largeImg');
+const thumbsUl = document.getElementById('thumbs');
+
+thumbsUl.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const link = e.target.closest('a');
+
+  if (link) {
+    largeImg.src = link.href;
+  }
+});
