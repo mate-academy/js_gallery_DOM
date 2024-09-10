d9de11b4cf5071391823c098dbc40ccfd79c5b02
olshum <olshum8@gmail.com>
2024-09-10 20:34:27 +0300

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const ul = document.querySelector('.gallery__list');
+const largeImage = document.querySelector('#largeImg');
+
+// event were renamed due to eslit error
+ul.addEventListener('click', (evnt) => {
+  const link = evnt.target.closest('a');
+
+  largeImage.src = link.href;
+  evnt.preventDefault();
+});
