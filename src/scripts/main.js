deb60f3ba3c333c60d05a13fbf36b4d74e8fe0a4
Ivan <fanmetalheavy@gmail.com>
2024-09-11 17:27:46 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const thumbs = document.getElementById('thumbs');
+const largeImg = document.getElementById('largeImg');
+
+thumbs.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const link = e.target.closest('.list-item__link');
+  const href = link.getAttribute('href');
+
+  largeImg.setAttribute('src', `${href}`);
+});
