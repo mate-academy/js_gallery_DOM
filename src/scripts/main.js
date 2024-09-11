e4a14eb4e1796c49e18951bc9a8837def4c03f09
Maksym Fedorchuk <maxeffect.apollo@gmail.com>
2024-09-11 19:55:33 +0300

    done, i guess

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const gallery = document.querySelector('.gallery');
+
+gallery.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const imgLink = e.target.closest('.list-item__link');
+
+  if (!imgLink) {
+    return;
+  }
+
+  const imgSrc = new URL(imgLink.getAttribute('href'), window.location.origin)
+    .href;
+  const imgTitle = imgLink.getAttribute('title');
+  const largeImg = document.querySelector(
+    '#largeImg.gallery__large-img.gallery__img',
+  );
+
+  largeImg.src = imgSrc;
+  largeImg.title = imgTitle;
+});
