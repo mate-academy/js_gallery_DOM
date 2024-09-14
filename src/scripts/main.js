af02fb3244c1f6ac246c62a53943513c415dce27
Hryniuk_Taras <gritaras484@gmail.com>
2024-09-14 15:16:32 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const mainImage = document.getElementById('largeImg');
+const thumbs = document.getElementById('thumbs');
+
+thumbs.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const thumb = e.target.closest('.gallery__thumb');
+
+  if (!thumb) {
+    return;
+  }
+
+  const link = thumb.closest('a');
+
+  if (link) {
+    mainImage.src = link.href;
+  }
+});
