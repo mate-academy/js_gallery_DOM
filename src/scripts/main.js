2583c5a7e75d92d08eafbe859fe00ffdf2c68c23
Eduard Nerkararian <mr.nerkararyna99@gmail.com>
2024-09-15 12:33:50 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const listItemLink = document.querySelectorAll('.list-item__link');
+const largeImg = document.querySelector('#largeImg');
+
+listItemLink.forEach((smallImg) => {
+  smallImg.addEventListener('click', (e) => {
+    e.preventDefault();
+    largeImg.src = smallImg.href;
+  });
+});
