1632b3668bdb71a2696735edd4c6d55d66b5afdf
Mariia <maria.gula0307@gmail.com>
2024-09-16 14:49:50 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const gallery = document.querySelector('.gallery');
+const largeImg = document.getElementById('largeImg');
+
+gallery.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const imgLink = e.target.closest('.list-item__link');
+
+  if (!imgLink) {
+    return;
+  }
+
+  largeImg.src = imgLink.href;
+});
