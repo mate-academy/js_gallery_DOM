1d8a7c35479c495298e98339be6ac039fe3f67b2
Tetyana Chuhai <chugaytatyana@gmail.&git config --global user.email chugaytatyana@gmail.>
2024-09-17 15:36:23 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const largeImg = document.getElementById('largeImg');
+const thumbnails = document.getElementById('thumbs');
+
+thumbnails.addEventListener('click', (ev) => {
+  const thumbnail = ev.target.closest('a');
+
+  if (!thumbnail) {
+    return;
+  }
+
+  showImage(thumbnail.href, thumbnail.title);
+  ev.preventDefault();
+});
+
+function showImage(href, title) {
+  largeImg.src = href;
+  largeImg.alt = title;
+}
