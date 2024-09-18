6a1f0da729eb6e297903e34f3bccd371b70da18d
Roman Martseniuk <roman.martseniuk.developer@gmail.com>
2024-09-18 19:30:32 +0300

    complete task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const btns = Array.from(document.querySelectorAll('.list-item__link'));
+
+btns.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    document.querySelector('#largeImg').src = btn.href;
+  });
+});
