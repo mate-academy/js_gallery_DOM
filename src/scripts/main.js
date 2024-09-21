5248e22a99999b3f42909b1f656961b5f3f00433
Heorhii Lytvynenko <104064704+Grifion@users.noreply.github.com>
2024-09-21 11:26:08 +0200

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,8 +3,10 @@
 const gallery = document.getElementById('largeImg');
 const thumbs = document.getElementById('thumbs');
 
-thumbs.addEventListener('click', (click) => {
-  const target = click.target.closest('.list-item');
+thumbs.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const target = e.target.closest('.list-item');
   const img = target.children[0];
 
   gallery.src = img.href;
