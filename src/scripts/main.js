682ff52ab498128ed5fbb51f9fda58df9504f7af
Heorhii Lytvynenko <104064704+Grifion@users.noreply.github.com>
2024-09-21 10:42:19 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const gallery = document.getElementById('largeImg');
+const thumbs = document.getElementById('thumbs');
+
+thumbs.addEventListener('click', (click) => {
+  const target = click.target.closest('.list-item');
+  const img = target.children[0];
+
+  gallery.src = img.href;
+});
