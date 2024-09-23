d5de760833604216c4c580986cb8a2157e2d11e8
Alex Popov <alexpopovdm@outlook.com>
2024-09-23 21:02:03 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const largeImg = document.querySelector('#largeImg');
+const thumbs = document.querySelector('#thumbs');
+
+thumbs.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  let target = e.target;
+
+  if (target.tagName === 'IMG') {
+    target = target.closest('a');
+  }
+
+  if (target.tagName === 'A') {
+    const newSrc = target.href;
+
+    largeImg.src = newSrc;
+  }
+});
