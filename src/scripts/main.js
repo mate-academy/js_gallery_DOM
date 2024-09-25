a7f904123adcb44671bd0c4ff6d40513e7181da3
Korshun <dmitriy.kor91@gmail.com>
2024-09-25 10:11:43 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const allGallery = document.querySelector('ul');
+
+allGallery.addEventListener('click', (even) => {
+  const link = even.target.closest('a');
+
+  if (link) {
+    even.preventDefault();
+
+    const mainPhoto = document.getElementById('largeImg');
+
+    mainPhoto.src = link.href;
+  }
+});
