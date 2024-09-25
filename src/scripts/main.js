08f89acc05b2abaf011cfbb2a57b226eb94ce432
Kostya Vasilenko <kostyantin.vasilenko.03@gmail.com>
2024-09-25 21:30:37 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,32 +1,9 @@
-const one = document.querySelectorAll('.list-item__link')[0];
-const two = document.querySelectorAll('.list-item__link')[1];
-const three = document.querySelectorAll('.list-item__link')[2];
-const four = document.querySelectorAll('.list-item__link')[3];
-const five = document.querySelectorAll('.list-item__link')[4];
+const thumbs = document.querySelectorAll('.list-item__link');
+const largeImg = document.querySelector('#largeImg');
 
-const title = document.querySelector('#largeImg');
-
-one.addEventListener('click', (ev) => {
-  ev.preventDefault();
-  title.src = one.href;
-});
-
-two.addEventListener('click', (ev) => {
-  ev.preventDefault();
-  title.src = two.href;
-});
-
-three.addEventListener('click', (ev) => {
-  ev.preventDefault();
-  title.src = three.href;
-});
-
-four.addEventListener('click', (ev) => {
-  ev.preventDefault();
-  title.src = four.href;
-});
-
-five.addEventListener('click', (ev) => {
-  ev.preventDefault();
-  title.src = five.href;
-});
+for (let i = 0; i < thumbs.length; i++) {
+  thumbs[i].addEventListener('click', (ev) => {
+    ev.preventDefault();
+    largeImg.src = thumbs[i].href;
+  });
+}
