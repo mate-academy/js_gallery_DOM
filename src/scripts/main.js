8300a37d7e220349252870b8b5cab4555fd66442
Viktoriia Dereza <viktoriiadereza7@gmail.com>
2024-09-26 21:06:04 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const mainImage = document.getElementById('largeImg');
+const thumbs = document.getElementById('thumbs');
+
+thumbs.addEventListener('click', (ev) => {
+  ev.preventDefault();
+
+  const link = ev.target.closest('a');
+
+  if (!link) {
+    return;
+  }
+
+  const largeImageSrc = link.href;
+
+  mainImage.src = largeImageSrc;
+});
