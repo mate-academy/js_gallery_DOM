7185e48b9d91357f59d1f45506e6a0345e9f8315
Kseniia Karpenko <ksushakarpenko635@gmail.com>
2024-09-28 17:32:48 +0200

    photos task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const galleryList = document.querySelector('ul');
+
+galleryList.addEventListener('click', (incident) => {
+  const link = incident.target.closest('a');
+
+  if (link) {
+    incident.preventDefault();
+
+    const lafgeImg = document.getElementById('largeImg');
+
+    lafgeImg.src = link.href;
+  }
+});
