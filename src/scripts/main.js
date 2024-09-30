7fad69fa063af9d93e83d0befb46b33c9fd3ddc0
Ivan Dovzhanyn <ivan.dovzhanyn@gmail.com>
2024-09-30 09:53:43 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const thumbList = document.querySelector('ul');
+
+thumbList.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  let target = e.target;
+
+  if (target.tagName === 'IMG') {
+    target = target.parentElement;
+  }
+
+  const largeImg = document.getElementById('largeImg');
+
+  largeImg.src = target.href;
+});
