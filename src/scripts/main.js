ccf51cc315dfce7b256e08c0e3c97cde02df884e
kovaltar <kovaltar@gmail.com>
2024-10-02 02:46:06 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const largeImg = document.getElementById('largeImg');
+const galleryList = document.getElementById('thumbs');
+
+galleryList.addEventListener('click', changeImg);
+
+function changeImg(clickEvent) {
+  clickEvent.preventDefault();
+
+  if (clickEvent.target.classList.contains('gallery__thumb')) {
+    if (largeImg.src !== clickEvent.target.parentNode.href) {
+      largeImg.src = clickEvent.target.parentNode.href;
+    }
+  } else if (clickEvent.target.classList.contains('list-item__link')) {
+    if (largeImg.src !== clickEvent.target.href) {
+      largeImg.src = clickEvent.target.href;
+    }
+  }
+}
