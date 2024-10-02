8faed84d6f15d93dd356016584b193119edca9f4
Nazar Pastuschak <kns19pna@gmail.com>
2024-10-02 23:44:23 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const largeImg = document.getElementById('largeImg');
+const thumbs = document.getElementById('thumbs');
+
+// eslint-disable-next-line no-shadow
+thumbs.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const target = event.target;
+
+  if (target.tagName === 'IMG') {
+    const newSrc = target.getAttribute('href');
+
+    largeImg.src = newSrc;
+  } else if (target.tagName === 'A') {
+    const newSrc = target.getAttribute('href');
+
+    largeImg.src = newSrc;
+  }
+
+  if (largeImg.src.startsWith(window.location.origin)) {
+    largeImg.src = largeImg.src.replace(window.location.origin, '');
+  }
+});
