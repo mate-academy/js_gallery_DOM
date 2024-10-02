aa50edf9ebb2d426475e9e0bf8e5a7601a242b05
Gennadiy Boreyko <gennadiyboreyko0@gmail.com>
2024-10-02 18:32:16 +0300

    creation of the gallery

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,29 @@
 'use strict';
+
+const mainImg = document.getElementById('largeImg');
+const galleryList = document.getElementById('thumbs');
+
+const images = document.querySelectorAll('.gallery__thumb');
+const links = document.querySelectorAll('.list-item__link');
+
+galleryList.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const checkClick = e.target.closest('.list-item__link');
+
+  if (!checkClick) {
+    return;
+  }
+
+  images.forEach((image) => {
+    if (image === e.target) {
+      mainImg.src = image.closest('.list-item__link').href;
+    }
+  });
+
+  links.forEach((link) => {
+    if (link === e.target) {
+      mainImg.src = link.href;
+    }
+  });
+});
