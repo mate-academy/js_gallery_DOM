d3f66d5e5ae4d3318425dc469e230931705f6ddc
Oleksandr Kovalchuk <kov280396@gmail.com>
2024-10-02 11:05:52 +0300

    Add solutionf

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const ul = document.querySelector('#thumbs');
+const largeImg = document.querySelector('#largeImg');
+
+ul.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const link = e.target.closest('.list-item__link');
+
+  largeImg.src = link.href;
+});
