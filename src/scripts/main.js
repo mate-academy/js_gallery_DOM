bd9d3d5c04e996d886c97afb7bac37807c02c579
Gennadiy Boreyko <gennadiyboreyko0@gmail.com>
2024-10-02 21:24:19 +0300

    the way of my solving the task was changed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,21 +3,14 @@
 const mainImg = document.getElementById('largeImg');
 const galleryList = document.getElementById('thumbs');
 
-const images = document.querySelectorAll('.gallery__thumb');
-const links = document.querySelectorAll('.list-item__link');
-
 galleryList.addEventListener('click', (e) => {
   e.preventDefault();
 
-  images.forEach((image) => {
-    if (image === e.target) {
-      mainImg.src = image.closest('.list-item__link').href;
-    }
-  });
+  const imgLink = e.target.closest('.list-item__link');
 
-  links.forEach((link) => {
-    if (link === e.target) {
-      mainImg.src = link.href;
-    }
-  });
+  if (!imgLink) {
+    return;
+  }
+
+  mainImg.src = imgLink.href;
 });
