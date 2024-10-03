cc52af4d9c6283f5703c3ad4ad3d5bbb397c14d8
Vika Ruda <rudaviktoria0@gmail.com>
2024-10-03 13:09:46 +0300

    New solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const getGallery = document.querySelector('.gallery');
+
+// eslint-disable-next-line no-shadow
+getGallery.addEventListener('click', function (event) {
+  // Перевіряємо, чи клацнули на елемент посилання
+  const target = event.target.closest('.list-item__link');
+
+  if (target) {
+    event.preventDefault();
+
+    const mainImage = document.querySelector('#largeImg');
+    const getSrcTarget = target.getAttribute('href');
+
+    mainImage.src = getSrcTarget;
+    mainImage.alt = target.title;
+  }
+});
