caeeaa5b36fcf5501c79728da8f439c12fc4ef52
Oleksii Karpov <alneon1@gmail.com>
2024-10-04 19:51:34 +0300

    Implemented solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const bigItem = document.querySelector('.gallery__large-img');
+
+const smallItem = document.querySelector('.gallery__list');
+
+smallItem.addEventListener('click', function (e) {
+  const link = e.target.closest('.list-item__link');
+
+  if (!link) {
+    return;
+  }
+
+  bigItem.src = link.href;
+  e.preventDefault();
+});
