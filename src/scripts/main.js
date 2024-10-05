0c1d412cb52239d65779dd86a6457c9c5edb4c80
associate2coder <olatsko@gmail.com>
2024-10-05 13:52:40 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+document.querySelector('#thumbs').addEventListener('click', (clickEvent) => {
+  clickEvent.preventDefault();
+
+  const link = clickEvent.target.closest('a.list-item__link');
+
+  if (link) {
+    document
+      .querySelector('#largeImg')
+      .setAttribute('src', link.getAttribute('href'));
+  }
+});
