70f9e0081dedef966fd74b5529d11a92f236f633
AndriiZhovtko <andriy.zhovtko@gmail.com>
2024-10-06 19:41:23 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const largeImage = document.querySelector('#largeImg');
+const smallImage = document.querySelector('#thumbs');
+
+smallImage.addEventListener('click', (f) => {
+  f.preventDefault();
+
+  const imageLink = f.target.closest('.list-item__link');
+
+  if (imageLink) {
+    largeImage.src = imageLink.href;
+  }
+});
