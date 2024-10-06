c44c81284a92e817612356c9c6f3a018a6c667c5
ab3MN <ab3mn3@gmail.com>
2024-10-06 21:46:14 +0300

    feat:implemented solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const galleryLargeImag = document.querySelector('#largeImg');
+const galleryThumbs = document.querySelector('#thumbs');
+
+galleryThumbs.onclick = (e) => {
+  e.preventDefault();
+
+  const { href } = e.target.closest('.list-item__link');
+
+  if (href) {
+    galleryLargeImag.src = href;
+  }
+};
