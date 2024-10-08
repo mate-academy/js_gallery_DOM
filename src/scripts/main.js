df8c529cb4a327ae27ad4f60bc9998a9e7b382b8
lublubuterbrodi <lublubuterbrodi@gmail.com>
2024-10-08 13:02:14 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const largeImg = document.getElementById('largeImg');
+const thumbs = document.getElementById('thumbs');
+
+thumbs.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const link = e.target.closest('a');
+
+  if (link) {
+    largeImg.src = link.href;
+  }
+});
