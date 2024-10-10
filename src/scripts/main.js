f54a55edfbd24741029fd7ddbe9f7d2f344863a9
SerhiiSemennikov <semennik@hotmail.com>
2024-10-10 14:46:20 +0200

    v1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const images = document.querySelector('#thumbs');
+const largeImg = document.querySelector('#largeImg');
+
+images.onclick = function () {
+  event.preventDefault();
+
+  const linkToImage = event.target.closest('.list-item__link');
+
+  if (linkToImage) {
+    largeImg.src = linkToImage.href;
+  }
+};
