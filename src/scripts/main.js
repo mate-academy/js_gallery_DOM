bf9bc19cd92ae8b0dd3ff591f9375097a22aa309
Ivan Paiareli <ipayareli@gmail.com>
2024-10-27 22:25:52 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const largeImg = document.getElementById('largeImg');
+const thumbs = document.getElementById('thumbs');
+
+thumbs.addEventListener('click', (evento) => {
+  evento.preventDefault();
+
+  if (evento.target.closest('a')) {
+    const link = evento.target.closest('a');
+    largeImg.src = link.href;
+  }
+});
