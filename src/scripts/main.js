38fe52509112b8c54d3c7b8549fcaefeccfc51b2
gorbachovam00 <gorbachovamaria00@gmail.com>
2024-11-27 12:43:53 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,23 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const largeImg = document.getElementById('largeImg');
+  const thumbs = document.getElementById('thumbs');
+
+  // eslint-disable-next-line no-shadow
+  thumbs.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const target = event.target;
+
+    if (target.tagName === 'IMG') {
+      const parentLink = target.closest('a');
+
+      if (parentLink && parentLink.href) {
+        largeImg.src = parentLink.href;
+      }
+    } else if (target.tagName === 'A') {
+      largeImg.src = target.href;
+    }
+  });
+});
