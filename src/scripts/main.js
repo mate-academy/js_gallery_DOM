0f7b4c0256e8d6a45cc6a27a401cf5164514de32
RomanHasiuk <romanhasiuk1@gmail.com>
2025-01-03 15:44:31 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const thumbs = document.getElementById('thumbs');
+  const largeImg = document.getElementById('largeImg');
+
+  thumbs.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    let target = e.target;
+
+    if (target.tagName === 'IMG') {
+      target = target.closest('a');
+    }
+
+    if (target && target.tagName === 'A') {
+      const newSrc = target.getAttribute('href');
+
+      largeImg.src = newSrc;
+    }
+  });
+});
