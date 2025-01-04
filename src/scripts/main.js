d5de79bc2f7577dc6bedb24beab39530cfd96809
Inga <ingatkachuk8@gmail.com>
2025-01-04 11:47:44 +0500

    Solution task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const pictureElement = document.querySelector('#largeImg');
+const thumbsElement = document.querySelector('#thumbs');
+
+// eslint-disable-next-line no-shadow
+thumbsElement.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const target = event.target;
+
+  if (target.tagName === 'IMG') {
+    const link = target.closest('a');
+
+    if (link) {
+      pictureElement.src = link.href;
+    }
+  }
+});
