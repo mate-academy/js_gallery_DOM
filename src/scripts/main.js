0068b78e97cca988f78c935103bc9be5f73ae039
Oleh Chernov <nineuito@mail.com>
2025-01-06 23:40:53 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const largeImg = document.querySelector('#largeImg');
+const thumbs = document.querySelector('#thumbs');
+
+const changeLargeImage = (e) => {
+  e.preventDefault();
+
+  if (e.target.tagName === 'IMG') {
+    const link = e.target.parentNode;
+    const newUrlLink = link.href;
+
+    largeImg.src = newUrlLink;
+    largeImg.alt = link.title;
+  }
+};
+
+thumbs.addEventListener('click', changeLargeImage);
