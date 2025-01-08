0691978aa770b5f3ad7ce1c1cbb4a41db8e5ec05
Valentyna <adm@adms-MacBook-Pro.local>
2025-01-08 20:06:02 +0200

    add gallery dom

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const largeImg = document.getElementById('largeImg');
+const thumbs = document.getElementById('thumbs');
+
+thumbs.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const target = e.target;
+
+  if (target.tagName === 'IMG' || target.tagName === 'A') {
+    const newImgSrc =
+      target.tagName === 'IMG' ? target.parentElement.href : target.href;
+
+    largeImg.src = newImgSrc;
+  }
+});
