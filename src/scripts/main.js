d10e222e3252a58e9c69847c2c37464ccc35c3ef
Vladyslava Proskura <vladyslava.proskura@gmail.com>
2025-01-09 18:24:06 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const largeImage = document.getElementById('largeImg');
+const thumbNail = document.getElementById('thumbs');
+
+thumbNail.addEventListener('click', (ev) => {
+  if (ev.target.tagName.toLowerCase() === 'img') {
+    const imgLink = ev.target.closest('a');
+
+    ev.preventDefault();
+
+    largeImage.src = imgLink.href;
+  }
+});
