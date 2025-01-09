18cd64b9fa05049d7368a8d758675a7e65b41d2d
Sasha Popovych <sashapopovych98@gmail.com>
2025-01-09 12:24:44 +0100

    test

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
-'use strict';
+const mainImg = document.querySelector('#largeImg');
+
+const list = document.querySelector('#thumbs');
+
+list.addEventListener('click', (e) => {
+  const link = e.target.closest('a');
+
+  if (list && list.contains(link)) {
+    e.preventDefault();
+    mainImg.src = link.href;
+  }
+});
