2542abe6bd3b27b13597985090db8cc6447b648a
ArtemHryb <artem.mushroom.gh@gmail.com>
2025-01-13 16:49:35 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const largeImage = document.querySelector('.gallery__large-img');
+const gallery = document.querySelector('.gallery__list');
+
+gallery.addEventListener('click', (evt) => {
+  evt.preventDefault();
+
+  let link;
+
+  if (evt.target.tagName === 'IMG') {
+    link = evt.target.closest('a');
+  } else if (evt.target.tagName === 'A') {
+    link = evt.target;
+  }
+
+  if (link) {
+    largeImage.src = link.href;
+  }
+});
