238cb61a3eda53569b8800df57c3f60fcafc4dd1
Volodymyr Vynnyk <vinnikvvv@gmail.com>
2025-01-15 16:17:13 +0200

    add gallery list of photos

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const mainImg = document.querySelector('.gallery__large-img');
+const galleryList = document.querySelector('.gallery__list');
+
+if (galleryList) {
+  galleryList.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const previewImg = e.target.closest('.list-item__link');
+
+    if (previewImg && mainImg) {
+      mainImg.src = previewImg.href;
+    }
+  });
+}
