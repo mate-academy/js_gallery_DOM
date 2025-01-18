7f97cfdf254ae80d75c6451409a3fe6f20478bf5
Dmytro <dmytrokovaliak@gmail.com>
2025-01-18 16:02:16 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const images = [...document.querySelectorAll('a')];
+const mainImage = document.querySelector('#largeImg');
+
+function changeImage(e, image) {
+  e.preventDefault();
+  mainImage.src = image.href;
+}
+
+images.map((image) => {
+  image.addEventListener('click', (e) => changeImage(e, image));
+});
