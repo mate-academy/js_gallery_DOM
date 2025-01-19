aad6dfebec2c5720ca6611805cc009cb9813080c
Anton Tur <ant.tur85@gmail.com>
2025-01-19 14:14:45 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const mainImg = document.querySelector('#largeImg');
+const galleryImgs = document.querySelectorAll('.gallery__thumb');
+
+galleryImgs.forEach((image) => {
+  image.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const newSrc = e.currentTarget.parentNode.href;
+
+    mainImg.src = newSrc;
+  });
+});
