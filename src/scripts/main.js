13eb8f31ec400cc214ef39d641665cd8d9da8644
Lidiia Shvets <lepoki051003@gmail.com>
2025-01-19 10:49:27 +0100

    gallery

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const thumbsElements = document.querySelector('#thumbs');
+const largeImg = document.querySelector('#largeImg');
+
+thumbsElements.addEventListener('click', (e) => {
+  const thumbsLink = e.target.closest('a');
+
+  if (!thumbsLink) {
+    return;
+  }
+  e.preventDefault();
+
+  const newImage = thumbsLink.href;
+
+  largeImg.src = newImage;
+});
