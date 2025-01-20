c44aea9ba90811fc5b054be78e35917925928d90
Artur <artur2005456@gmail.com>
2025-01-20 20:25:26 +0200

    Add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const mainImage = document.querySelector('#largeImg');
+const ul = document.querySelector('ul');
+
+ul.addEventListener('click', (e) => {
+  const link = e.target.closest('.list-item__link');
+
+  if (!link) {
+    return;
+  }
+  e.preventDefault();
+  mainImage.src = link.href;
+});
