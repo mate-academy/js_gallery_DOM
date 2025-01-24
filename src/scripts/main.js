3af6c6326875134302c88122e4819ad05e640c3d
Albina <yakunina.albina@gmail.com>
2025-01-24 17:33:48 +0100

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const largeImg = document.getElementById('largeImg');
+const thumbs = document.getElementById('thumbs');
+
+thumbs.addEventListener('click', (events) => {
+  events.preventDefault();
+
+  const link = events.target.closest('.list-item__link');
+
+  if (link) {
+    const newSrc = link.getAttribute('href');
+
+    largeImg.setAttribute('src', newSrc);
+  }
+});
