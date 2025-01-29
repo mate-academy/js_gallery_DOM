508911b6895a50e94ffe44af7ec994de25f28e97
Oleh Yavoriv <oleh.yavoriv@gmail.com>
2025-01-29 10:50:04 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const largeImage = document.getElementById('largeImg');
+const thumbs = document.getElementById('thumbs');
+
+thumbs.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const link = e.target.closest('a');
+
+  if (link) {
+    largeImage.src = link.href;
+  }
+});
