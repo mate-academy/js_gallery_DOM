1b2c22ab7bba1bf63563a907de759ec34b1927ed
Anton Olifan <olifananton@gmail.com>
2025-01-29 21:55:33 +0100

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const largeImg = document.getElementById('largeImg');
+const thumbs = document.getElementById('thumbs');
+
+thumbs.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  let target = e.target;
+
+  if (target.tagName === 'IMG') {
+    target = target.closest('a');
+  }
+
+  if (target && target.tagName === 'A') {
+    largeImg.src = target.href;
+  }
+});
