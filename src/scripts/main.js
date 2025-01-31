cf1b4b4b3c0aa1bbcbf4010544ce9d77e0921dac
Che <rererdom@gmail.com>
2025-01-31 19:28:31 +0000

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const thumbs = document.getElementById('thumbs');
+const largeImg = document.getElementById('largeImg');
+
+thumbs.addEventListener('click', (action) => {
+  action.preventDefault();
+
+  const target = action.target;
+
+  if (target.tagName.toLowerCase() === 'img') {
+    const parentAnchor = target.parentElement;
+
+    largeImg.src = parentAnchor.href;
+  } else if (target.tagName.toLowerCase() === 'a') {
+    largeImg.src = target.href;
+  }
+});
