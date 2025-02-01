89e2e72b81a4d4ddc5d3b0d87a8c3da976a89672
Oleksandr <dedikad@gmail.com>
2025-02-01 20:12:26 +0100

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const thumbs = document.getElementById('thumbs');
+const largeImg = document.getElementById('largeImg');
+
+thumbs.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const target = e.target;
+
+  if (target.tagName.toLowerCase() === 'img') {
+    const parentElement = target.parentElement;
+
+    largeImg.src = parentElement.href;
+  } else if (target.tagName.toLowerCase() === 'a') {
+    largeImg.src = target.href;
+  }
+});
