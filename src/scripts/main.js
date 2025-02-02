e651462b63c664ed253ec508f9b6ca7695b01cb3
Mykhailo <kurochkinmy@gmail.com>
2025-02-02 23:45:15 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const container = document.querySelector('.gallery__list');
+const mainPicture = document.querySelector('#largeImg');
+
+container.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  mainPicture.src = e.target.closest('a').href;
+});
