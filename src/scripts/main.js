ae3c29e04c470f97f2015101088cbcbf2e29b839
Nadiia Bulmak <nadgaval@gmail.com>
2025-02-07 18:32:51 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const listThumbnails = document.querySelector('.gallery__list');
+const bigImg = document.querySelector('.gallery__large-img');
+
+listThumbnails.onclick = (e) => {
+  e.preventDefault();
+
+  if (e.target.tagName !== 'IMG') {
+    return;
+  }
+
+  const link = e.target.closest('a');
+
+  if (link) {
+    const largeImgSrc = link.href;
+
+    bigImg.setAttribute('src', largeImgSrc);
+  }
+};
