c6277aecbd96f40c8400b8fa7c5ddf15433d1ad0
nnalinn <alina.vysotskayaaa@gmail.com>
2025-02-09 21:26:14 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const largeImg = document.getElementById('largeImg');
+const thumbs = document.getElementById('thumbs');
+
+thumbs.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  let target = e.target;
+
+  if (target.tagName === 'IMG') {
+    target = target.parentElement;
+  }
+
+  if (target.tagName === 'A') {
+    largeImg.src = target.href;
+  }
+});
