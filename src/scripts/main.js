54ed606aa0d9cad9f7d811708dcf81631522be76
iteamfox <avlisunenko@gmail.com>
2025-02-09 13:06:15 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const largImg = document.getElementById('largeImg');
+const thumbs = document.getElementById('thumbs');
+
+thumbs.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const target = event.target;
+  let newSrc;
+
+  if (target.tagName === 'IMG') {
+    newSrc = target.parentElement.href;
+  } else if (target.tagName === 'A') {
+    newSrc = target.href;
+  }
+
+  if (newSrc) {
+    largImg.src = newSrc;
+  }
+});
