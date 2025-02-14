bcc8c36a0b8a457e796319f127f2560fbc6269a4
Vladyslav Tsarenko <vaaaltaaasaaar@gmail.com>
2025-02-14 09:40:26 -0600

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const galeryList = document.querySelector('.gallery__list');
+const largeImg = document.querySelector('.gallery__large-img');
+
+galeryList.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const closestListItem = e.target.closest('.list-item');
+
+  if (closestListItem) {
+    largeImg.src = closestListItem.querySelector('a').href;
+  }
+});
