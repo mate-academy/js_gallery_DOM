524cd66408bb1661f1847d1c2cb3f3ee04889690
Nataliia Kozyriatska <kozyriatskanataliia@gmail.com>
2025-02-16 18:19:10 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const gallery = document.querySelector('.gallery');
+
+gallery.addEventListener('click', (e) => {
+  const listItem = e.target.closest('.list-item');
+
+  if (listItem) {
+    const image = listItem.querySelector('img').src;
+    const largeImage = document.querySelector('#largeImg');
+
+    largeImage.src = image;
+  }
+});
