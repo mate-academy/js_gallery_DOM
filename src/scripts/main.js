1783415c3e5155993b13407ac28a8744998d6224
troleksii <aliosha123418@gmail.com>
2025-02-16 01:00:13 +0100

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const gallery = document.getElementById('thumbs')
+const largeImg = document.getElementById('largeImg')
+console.log(largeImg)
+
+gallery.addEventListener('click', function (event) {
+  event.preventDefault();
+
+  const target = event.target;
+  const link = target.closest('a')
+  console.log(link)
+
+  largeImg.src = link.href;
+  largeImg.alt = link.title
+})
+
