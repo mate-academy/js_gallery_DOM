c1f38cddedfba369a20fa4540785736db2ab16a1
Alina <alina220380@gmail.com>
2025-02-22 13:48:42 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const thumbs = document.querySelector('#thumbs');
+
+const mainImage = document.querySelector('#largeImg');
+
+thumbs.addEventListener('click', (events) => {
+  events.preventDefault();
+
+  let target = events.target;
+
+  if (target.tagName === 'IMG') {
+    target = target.parentElement;
+  }
+
+  if (target.tagName === 'A') {
+    const newSrc = target.getAttribute('href');
+
+    mainImage.src = newSrc;
+  }
+});
