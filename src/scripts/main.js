44af80c60fb91de7b43df8e98aec3a911efe75f1
Vasyl <priymak17ukrvaha@gmail.com>
2025-02-25 14:41:27 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const mainImage = document.querySelector('.gallery__img');
+const allImages = Array.from(document.querySelectorAll('.list-item'));
+
+for (const image of allImages) {
+  const imageLink = image.firstElementChild.getAttribute('href');
+
+  image.addEventListener('click', (something) => {
+    something.preventDefault();
+    mainImage.setAttribute('src', imageLink);
+  });
+}
