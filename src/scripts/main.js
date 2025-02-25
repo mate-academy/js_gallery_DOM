e0a2253ab8e180412ae54f45b7339bd03cccf521
Ivan Ryabukha <ryabukhaivan98@gmail.com>
2025-02-25 15:41:00 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const listItem = Array.from(document.querySelectorAll('.list-item'));
+
+listItem.forEach((element) => {
+  element.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const li = e.currentTarget;
+    const href = li.querySelector('a').getAttribute('href');
+
+    const mainImg = document.querySelector('#largeImg');
+
+    mainImg.setAttribute('src', href);
+  });
+});
