d66d4efb920bd8529253714b9adb476f73101273
WalentynaSadowa <walentynasadowa28@gmail.com>
2025-03-02 09:59:40 +0100

    gallery

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
-'use strict';
+document.addEventListener('DOMContentLoaded', () => {
+  const gallery = document.querySelector('.gallery');
+  const mainImage = document.querySelector('.main-image img');
+
+  gallery.addEventListener('click', (e) => {
+    if (
+      e.target &&
+      e.target.tagName === 'IMG' &&
+      e.target.parentElement.tagName === 'A'
+    ) {
+      mainImage.src = e.target.src;
+    }
+  });
+});
