5dcefe465f3d02710d6bc70367a58709b4b8fd00
Anton Polishchuk <hardixxxxx@gmail.com>
2025-03-03 20:35:34 +0100

    complete task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const galleryList = document.querySelector('.gallery__list');
+
+galleryList.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const galleryImg = document.querySelector('.gallery__img');
+  const link = e.target.closest('.list-item__link').href;
+
+  if (!link) {
+    return;
+  }
+
+  galleryImg.src = link;
+});
