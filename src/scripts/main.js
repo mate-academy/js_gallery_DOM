4eff55c715a2998066be75e285e583820045767d
Eduard Sukhomlyn <eduard.sukhomlyn@gmail.com>
2025-03-04 16:44:38 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const largeImg = document.getElementById('largeImg');
+const thumbs = document.getElementById('thumbs');
+
+thumbs.addEventListener('click', function (picture) {
+  picture.preventDefault();
+
+  const link = picture.target.closest('a');
+
+  if (!link) {
+    return;
+  }
+
+  largeImg.src = link.href;
+});
