5747b9ec8ddf0ff2cf11960e92939d651fe157f5
Daniil Trusov <daniiltrusov00@gmail.com>
2025-03-05 14:00:26 +0200

    create solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const largeImage = document.getElementById('largeImg');
+const thumbs = document.getElementById('thumbs');
+
+thumbs.addEventListener('click', handleImageClick);
+
+function handleImageClick(image) {
+  image.preventDefault();
+
+  const link = image.target.closest('a');
+
+  largeImage.src = link.href;
+}
