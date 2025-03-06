18c8bb7e12aa0fbba2c29c84023cd43fd6869a05
Owk <kkoctuk2005@gmail.com>
2025-03-06 17:41:26 +0200

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const gallery = document.querySelector('.gallery');
+
+gallery.addEventListener('click', (e) => {
+  const mainImg = document.querySelector('.gallery__large-img');
+  const link = e.target.closest('.gallery__thumb');
+
+  if (link) {
+    e.preventDefault();
+    mainImg.src = link.src;
+  }
+});
