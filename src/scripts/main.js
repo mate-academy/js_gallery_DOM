7c90d313c6da8f7fc6e3d60a5afe807f5da49364
George <george.boujhenitsa@gmail.com>
2025-03-06 20:19:08 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const ul = document.getElementById('thumbs');
+const largeImg = document.getElementById('largeImg');
+
+ul.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  largeImg.src = e.target.closest('a').href;
+});
