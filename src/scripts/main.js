b2ddf04e2e9713a0711373e27f4a9afb99349db7
Yaroslav <yarmoshyk96@gmail.com>
2025-03-11 20:36:07 +0200

    Add gallery DOM solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const largeImage = document.getElementById('largeImg');
+const galleryList = document.getElementById('thumbs');
+
+// eslint-disable-next-line no-shadow
+galleryList.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const link = event.target.closest('.list-item__link');
+
+  if (!link) {
+    return;
+  }
+
+  largeImage.src = link.href;
+});
