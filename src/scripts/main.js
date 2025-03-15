d5967fa2742676a5b8c394504fe94a3ad1441ddb
Lera <valeriamelesenko4@gmail.com>
2025-03-15 15:07:43 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const list = document.querySelector('.gallery__list');
+const large = document.getElementById('largeImg');
+
+// eslint-disable-next-line no-shadow
+list.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const listItem = event.target.closest('.list-item');
+
+  const link = listItem.querySelector('.list-item__link');
+
+  large.src = link.href;
+  large.alt = link.title;
+});
