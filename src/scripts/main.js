fffe93837b0e5187818f6a6636d1de28128ef64a
Ivan Sierov <iserov1989@gmail.com>
2025-03-17 04:38:45 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const largeImg = document.getElementById('largeImg');
+  const thumbs = document.getElementById('thumbs');
+
+  thumbs.addEventListener('click', (ev) => {
+    ev.preventDefault();
+
+    let target = ev.target;
+
+    if (target.tagName === 'IMG') {
+      target = target.parentElement;
+    }
+
+    if (target.tagName === 'A') {
+      largeImg.src = target.href;
+    }
+  });
+});
