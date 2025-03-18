20d8bb756015917b28c6ac9265eeb750d2f94ba0
Ostap Danyliv <danyliv.ostap@gmail.com>
2025-03-18 09:21:07 -0500

    gallary impl

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const thumbs = document.getElementById('thumbs');
+const largeImage = document.getElementById('largeImg');
+
+thumbs.addEventListener('click', (ev) => {
+  ev.preventDefault();
+
+  let target = ev.target;
+
+  if (target.tagName === 'IMG') {
+    target = target.parentElement;
+  }
+
+  if (target.tagName === 'A') {
+    largeImage.src = target.href;
+  }
+});
