91a17985d517a0f58b218482642d97154a981775
Ivan Huzhva <iwan_guzhwa@gmail.com>
2025-03-20 10:14:58 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const images = document.querySelector('.gallery__list');
+
+// eslint-disable-next-line no-shadow
+images.addEventListener('click', (event) => {
+  const li = event.target.closest('li');
+
+  if (!li || !images.contains(li)) {
+    return;
+  }
+
+  event.preventDefault();
+
+  const liLink = li.querySelector('a');
+  const liImage = liLink.getAttribute('href');
+
+  const imgDiv = document.querySelector('.gallery__large-img');
+
+  imgDiv.setAttribute('src', liImage);
+});
