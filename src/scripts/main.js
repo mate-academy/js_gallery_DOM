c017a5980b0df776d3b4356b118f070575b835ea
Maria <m.abeeva.ma@gmail.com>
2025-03-25 17:26:54 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const gallery = document.querySelector('.gallery__list');
+
+gallery.onclick = (mouseEvent) => {
+  mouseEvent.preventDefault();
+
+  const mainImage = document.getElementById('largeImg');
+  const link = mouseEvent.target.closest('.list-item__link').href;
+
+  if (!link) {
+    return;
+  }
+
+  mainImage.src = link;
+};
