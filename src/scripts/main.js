1d4f3adae7db0733d82a0ad0b15a378368481a6b
user <mariiaantoniv@gmail.com>
2025-03-26 11:05:45 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const mainTmage = document.getElementById('largeImg');
+  const container = document.getElementById('thumbs');
+
+  container.addEventListener('click', (events) => {
+    const target = events.target;
+
+    if (target.tagName === 'IMG') {
+      const parentLink = target.parentElement;
+
+      if (parentLink && parentLink.tagName === 'A') {
+        events.preventDefault();
+        mainTmage.src = parentLink.href;
+      }
+    }
+  });
+});
