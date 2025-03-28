5f5a1cb0b25140bc4a2c6535b001d2ce15602e78
Iryna <kapustyak.ira97@gmail.com>
2025-03-28 08:59:40 +0100

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const divEl = document.getElementById('thumbs'); 
+
+divEl.addEventListener('click', (event) => {
+    const aEl = event.target.closest('a').getAttribute('href'); 
+    largeImg.src = aEl; 
+    event.preventDefault(); 
+  });
