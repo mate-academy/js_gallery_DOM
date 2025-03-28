6253a2c813a0047f007eed3c2725ad2008ea9e2e
Iryna <kapustyak.ira97@gmail.com>
2025-03-28 08:59:49 +0100

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,11 @@
 'use strict';
 
-const divEl = document.getElementById('thumbs'); 
+const divEl = document.getElementById('thumbs');
+const largeImg = document.getElementById('largeImg');
 
-divEl.addEventListener('click', (event) => {
-    const aEl = event.target.closest('a').getAttribute('href'); 
-    largeImg.src = aEl; 
-    event.preventDefault(); 
-  });
+divEl.addEventListener('click', (e) => {
+  const aEl = e.target.closest('a').getAttribute('href');
+
+  largeImg.src = aEl;
+  e.preventDefault();
+});
