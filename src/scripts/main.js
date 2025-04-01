23e872ce838395060d4226442787c73e621d97fc
Ivan Ilnytskyy <ilniskyi2602@gmail.com>
2025-04-02 00:06:08 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const gallery = document.querySelector('.gallery');
+  const mainImage = document.querySelector('.main-image');
+
+  if (gallery && mainImage) {
+    gallery.addEventListener('click', (e) => {
+      e.preventDefault();
+
+      const target = e.target.closest('a');
+
+      if (target && target.querySelector('img')) {
+        mainImage.src = target.href;
+      }
+    });
+  }
+});
