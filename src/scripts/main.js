ad1be05200cd3f06b8a89e24896b925a132ea52d
BoGoBooM <myPikcha@gmail.com>
2025-04-04 14:41:52 +0300

    gallery was created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const image = document.querySelector('#thumbs');
+const largeImg = document.querySelector('#largeImg');
+
+image.addEventListener('click', (events) => {
+  const link = events.target.closest('a');
+
+  if (!link) {
+    return;
+  }
+
+  events.preventDefault();
+
+  const href = link.href;
+
+  largeImg.src = href;
+});
