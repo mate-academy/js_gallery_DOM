0c29a1aa508300bb631c77afd779b3e0575bf86e
Yurii Chain <yuriichaiun@gmail.com>
2025-04-07 16:03:02 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const mainImage = document.querySelector('#largeImg');
+const galleryList = document.querySelector('#thumbs');
+
+galleryList.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  if (e.target.tagName === 'A') {
+    mainImage.src = e.target.href;
+  }
+
+  if (e.target.tagName === 'IMG') {
+    mainImage.src = e.target.parentElement.href;
+  }
+});
