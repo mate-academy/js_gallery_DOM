cb5108bb18d64f2e627e1b309ec8dfc4d3449d66
Pavlo Stelmashchuk <stelmaschukpavlo3@gmail.com>
2025-04-07 20:09:48 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const smallImage = document.querySelectorAll('.gallery__img');
+const imageSrc = ['first', 'second', 'third', 'fourth', 'fifth'];
+const baseUrl = 'http://localhost:3001';
+
+smallImage.forEach((img, index) => {
+  img.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const bigImage = document.getElementById('largeImg');
+
+    bigImage.src = `${baseUrl}/images/landscape-${imageSrc[index - 1]}.png`;
+  });
+});
