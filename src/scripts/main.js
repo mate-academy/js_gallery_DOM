ae6ac8480aa5af4446f17b615259e37d8b23b04d
Galyna Maikovych <gmaik.com@gmail.com>
2025-04-08 20:07:36 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const list = document.querySelector('ul');
+const mainImage = document.querySelector('#largeImg');
+
+list.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  mainImage.src = e.target.closest('a').href;
+});
