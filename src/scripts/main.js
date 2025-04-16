d465f0e60fa3db0c8ce3a5466430ac66baed3dcd
Yuliia Babych <jl.babych29@gmail.com>
2025-04-16 15:33:40 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const bigImg = document.querySelector('#largeImg');
+const gallery = document.querySelector('.gallery');
+
+gallery.addEventListener('click', () => {
+  event.preventDefault();
+
+  const target = event.target;
+
+  if (target.tagName === 'IMG') {
+    const link = target.closest('a');
+
+    if (link) {
+      bigImg.src = link.href;
+    }
+  }
+});
