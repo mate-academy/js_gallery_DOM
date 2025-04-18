8cbe2f3805809e9218e1208c306b5f241c32fdfa
apr1ori <vladgolenok4@gmail.com>
2025-04-18 21:26:12 +0200

    task 1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const images = document.querySelector('ul');
+const bigImage = document.querySelector('#largeImg');
+
+images.addEventListener('click', (targetEvent) => {
+  targetEvent.preventDefault();
+  bigImage.src = targetEvent.target.src;
+});
