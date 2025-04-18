680aaa740e76c2a438b5632165e0f7182c3faae8
Danylo Kachaniuk <logistik2004@ukr.net>
2025-04-18 12:04:23 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const showElements = document.querySelector('.gallery');
+
+const moverElement = (element) => {
+  element.preventDefault();
+
+  const listItem = element.target.closest('a');
+
+  if (listItem && listItem.href) {
+    const prevElement = document.querySelector('.gallery__large-img');
+
+    prevElement.src = listItem.href;
+  }
+};
+
+showElements.addEventListener('click', moverElement);
