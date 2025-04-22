fb3cd0480cc98dc27de97de47933c68cbdb1939b
Nikola57 <nikola.sribniy@gmail.com>
2025-04-22 17:56:59 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', function () {
+  const thumbs = document.getElementById('thumbs');
+  const largeImg = document.getElementById('largeImg');
+
+  thumbs.addEventListener('click', function (eventt) {
+    eventt.preventDefault(); // щоб не переходити за посиланням
+
+    // Знаходимо <a>, навіть якщо клік був по <img>
+    const link = eventt.target.closest('a');
+
+    if (!link || !thumbs.contains(link)) {
+      return;
+    }
+
+    // Змінюємо src великого зображення на href посилання
+    largeImg.src = link.href;
+  });
+});
