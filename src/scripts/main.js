0154fd963a260c8281d9d52ebc40d6149d6f155c
petrushinsky-kirill <petrushinsky@gmail.com>
2025-05-01 11:47:04 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const mainImage = document.querySelector('#largeImg');
+const thumbsContainer = document.querySelector('#thumbs');
+
+thumbsContainer.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  let target = e.target;
+
+  // If the clicked element is <img>, get its parent <a>
+  if (target.tagName.toLowerCase() === 'img') {
+    target = target.parentElement;
+  }
+
+  // Check if the target is an <a> element
+  if (target.tagName.toLowerCase() === 'a') {
+    mainImage.src = target.href;
+  }
+});
