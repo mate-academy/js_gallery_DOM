0418af76d51dc6490ba00d7ea7a807332619adf5
Oksana Sidanych <sasha3sashkova@gmail.com>
2025-05-02 18:52:17 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+document.querySelector('.gallery').onclick = function () {
+  const img = event.target.closest('img');
+
+  if (!img) {
+    return;
+  }
+  document.querySelector('.main-image').src = img.src;
+};
