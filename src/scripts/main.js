9893f7b93468326baa2709c5928744486fd18e62
Maryna Kravchuk <marinalinkova02@gmail.com>
2025-05-15 15:39:44 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
+/* eslint-disable no-shadow */
 'use strict';
+
+const mainImage = document.querySelector('#largeImg');
+const thumbs = document.querySelector('#thumbs');
+
+thumbs.addEventListener('click', function (event) {
+  event.preventDefault();
+
+  const link = event.target.closest('a');
+
+  if (!link || !thumbs.contains(link)) {
+    return;
+  }
+
+  const largeImgUrl = link.getAttribute('href');
+
+  mainImage.src = largeImgUrl;
+});
