f83dbfa632ab8743d6e4f92436f262df9c31de37
Viktor Pastushenko <viktor.pastushenko@udelphi.com>
2025-05-16 18:02:13 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,26 @@
 'use strict';
+
+document
+  .getElementById('thumbs')
+  .addEventListener('click', (ev) => {
+    ev.preventDefault();
+
+    const target = ev.target;
+    const largeImg = document.getElementById('largeImg');
+    let largeImgSrc = '';
+
+    switch (target.tagName.toLowerCase()) {
+      case 'img':
+        largeImgSrc = target.closest('a').getAttribute('href');
+        break;
+
+      case 'a':
+        largeImgSrc = target.getAttribute('href');
+        break;
+
+      default:
+        return;
+    }
+
+    largeImg.setAttribute('src', `${window.location.href}${largeImgSrc.slice(1)}`);
+  });
