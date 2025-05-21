8f8bb0ce825cfab294a39af1fe02e127230427df
Oleksandr Voronkin <94705289+Alexraiv@users.noreply.github.com>
2025-05-21 18:50:08 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const gallery = document.querySelector('.gallery');
+const largeImg = document.querySelector('#largeImg');
+
+gallery.addEventListener('click', (imageClick) => {
+  imageClick.preventDefault();
+
+  const link = imageClick.target.closest('a');
+
+  if (!link || !gallery.contains(link)) {
+    return;
+  }
+
+  largeImg.src = link.href;
+});
