1b89309f961e77d4a6c6b86df63fd4f6d1954708
Nikita Doroshenko <nikitokd05@gmail.com>
2025-05-23 13:52:02 +0300

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const mainImg = document.querySelector('#largeImg');
+
+const list = document.querySelector('.gallery__list');
+
+// eslint-disable-next-line no-shadow
+list.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const link = event.target.closest('.list-item__link');
+
+  mainImg.src = link.href;
+});
