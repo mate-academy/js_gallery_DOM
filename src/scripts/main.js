e10d60d35d69ee2a35fb4ad6b7254aec88cf27f5
Vlad Mitlosh <vlad.mitlosh@gmail.com>
2025-05-26 13:24:04 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const listItems = document.querySelectorAll('.list-item');
+const largeImg = document.getElementById('largeImg');
+
+listItems.forEach((item) => {
+  item.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    largeImg.src = item.querySelector('.list-item__link').href;
+  });
+});
