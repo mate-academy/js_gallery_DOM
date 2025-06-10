b1b22298fde8bf482c17503b31059f09fbd888d1
Sofiia13 <sophiyastanish@gmail.com>
2025-06-10 15:56:43 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const mainBlock = document.querySelector('.gallery');
+const largeImg = document.getElementById('largeImg');
+
+mainBlock.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const linkElement = e.target.closest('.list-item__link');
+
+  if (linkElement) {
+    const clickedImage = linkElement.querySelector('img');
+
+    if (clickedImage) {
+      largeImg.src = clickedImage.src;
+    }
+  }
+});
