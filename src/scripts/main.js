c5fbc54ae0a09854592d49318d4ddf74d0f29a3e
Roman Shevchyshyn <romanshevchyshyn1998@gmail.com>
2025-06-14 21:49:39 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const list = document.getElementById('thumbs');
+const largeImg = document.getElementById('largeImg');
+
+list.addEventListener('click', (e) => {
+  const link = e.target.closest('.list-item__link');
+
+  if (!link) {
+    return;
+  }
+
+  e.preventDefault();
+
+  const newImgSrc = link.getAttribute('href');
+
+  largeImg.src = new URL(newImgSrc, window.location.href).href;
+  largeImg.alt = link.title || 'main image';
+});
