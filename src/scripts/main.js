f12a7af25c19d27d3e640e0e63e553b9854f14cf
Oleksii Myronenko <oleksiimrnko@gmail.com>
2025-06-18 23:33:21 +0300

    add test solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const thumbs = document.getElementById('thumbs');
+  const largeImg = document.getElementById('largeImg');
+
+  thumbs.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const link = e.target.closest('a');
+
+    if (!link || !thumbs.contains(link)) {
+      return;
+    }
+
+    const newSrc = link.getAttribute('href');
+    const newAlt = link.getAttribute('title');
+
+    largeImg.src = newSrc;
+    largeImg.alt = newAlt;
+  });
+});
