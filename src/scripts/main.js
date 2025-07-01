849b152b1699b132d57f1c2e129cfae9bdd7c8dd
tymchukmr <184266140+tymchukmr@users.noreply.github.com.>
2025-07-01 17:45:38 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const thumbs = document.getElementById('thumbs');
+const largeImg = document.getElementById('largeImg');
+
+thumbs.addEventListener('click', (ev) => {
+  ev.preventDefault();
+
+  let link = null;
+
+  if (ev.target.tagName === 'IMG') {
+    link = ev.target.closest('a');
+  } else if (ev.target.tagName === 'A') {
+    link = ev.target;
+  }
+
+  if (link) {
+    const href = link.getAttribute('href');
+    const absoluteSrc = window.location.origin + href;
+
+    largeImg.setAttribute('src', absoluteSrc);
+  }
+});
