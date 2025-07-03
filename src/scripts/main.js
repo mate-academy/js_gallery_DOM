698fc7c299ad9f2b68f976ef56ed180f163124df
Bernavschi Natalia <natb@list.ru>
2025-07-03 10:28:25 +0300

    add task solution for js_gallery_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const thumbs = document.getElementById('thumbs');
+
+thumbs.addEventListener('click', (e) => {
+  const link = e.target.closest('a');
+
+  if (!link) {
+    return;
+  }
+
+  const newSrc = link.getAttribute('href');
+
+  e.preventDefault();
+
+  const largeImg = document.getElementById('largeImg');
+
+  largeImg.src = newSrc;
+});
