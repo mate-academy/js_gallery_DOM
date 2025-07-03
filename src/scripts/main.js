d739b6ea92586a3491de875813fa78c9dbc6f19b
Yura <yurii.st11@gmail.com>
2025-07-03 18:11:25 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const largeImg = document.getElementById('largeImg');
+
+const links = document.querySelectorAll('.list-item__link');
+
+links.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    largeImg.src = link.href;
+  });
+});
