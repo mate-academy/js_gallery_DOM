88716a4e12ea09350e730ea51317e334835834e5
Sofiia Kogut <kogutsofia288@gmail.com>
2025-07-08 14:43:11 +0300

    added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const mainImage = document.getElementById('largeImg');
+
+const listImages = document.getElementById('thumbs');
+
+listImages.addEventListener('click', (e) => {
+  const link = e.target.closest('a');
+
+  if (!link) {
+    return;
+  }
+
+  const href = link.href;
+
+  e.preventDefault();
+
+  mainImage.src = href;
+});
