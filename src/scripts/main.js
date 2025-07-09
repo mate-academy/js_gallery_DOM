b5c94d1f3a59ec6bbdc20a527c3bbfaf49d5344e
Ruslan <gnatisin.ruslan1988@gmail.com>
2025-07-09 19:50:34 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const largeImg = document.getElementById('largeImg');
+const list = document.getElementById('thumbs');
+
+list.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const link = e.target.closest('a');
+
+  if (!link || !list.contains(link)) {
+    return;
+  }
+
+  const imgSrc = link.href;
+
+  largeImg.src = imgSrc;
+});
