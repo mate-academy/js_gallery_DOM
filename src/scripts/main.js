a065899ded5588984928f73f9dcc6737374ad4b8
Drsgon <oksanaleschenko.ol@gmail.com>
2025-07-11 17:35:06 +0300

    add gallery solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', function () {
+  const mainImg = document.querySelector('.gallery__large-img');
+  const listImgs = document.getElementById('thumbs');
+
+  listImgs.addEventListener('click', function (ev) {
+    ev.preventDefault();
+
+    let link;
+
+    if (ev.target.tagName === 'IMG') {
+      link = ev.target.closest('a');
+    } else {
+      link = ev.target;
+    }
+
+    mainImg.src = link.href;
+  });
+});
