8757ae0022ecbde8af1dcac473185763417e5cb6
Н  е андрей <oleksandrlib4@gmail.com>
2025-07-16 09:59:47 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const thumbs = document.getElementById('thumbs');
+const largeImg = document.getElementById('largeImg');
+
+thumbs.addEventListener('click', function (e) {
+  const link = e.target.closest('a');
+
+  if (!link || !thumbs.contains(link)) {
+    return;
+  }
+
+  e.preventDefault();
+
+  const largeImageUrl = link.href;
+
+  largeImg.src = largeImageUrl;
+});
