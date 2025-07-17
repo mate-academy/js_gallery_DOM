12d882b9302b0d71f2a5f39a14a2a964c6af24e3
Taras Niklevych <niklevychtaras@gmail.com>
2025-07-17 14:43:56 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const thumbs = document.getElementById('thumbs');
+const mainImage = document.getElementById('largeImg');
+
+thumbs.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const image = e.target.closest('a');
+
+  if (!image) {
+    return;
+  }
+
+  showImg(image.href);
+});
+
+function showImg(img) {
+  mainImage.src = img;
+}
