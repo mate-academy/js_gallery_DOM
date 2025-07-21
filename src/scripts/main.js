18fb3c55bae6f9e03ca8728eb6252589ecfeea36
Pavlo <marlana08022022@gmail.com>
2025-07-21 15:52:04 +0300

    Photo galey

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const imageBoss = document.querySelector('#largeImg');
+
+const list = document.querySelector('#thumbs');
+
+list.addEventListener('click', (events) => {
+  const targetImg = events.target.closest('img');
+  const targetLink = events.target.closest('a');
+
+  if (targetImg) {
+    const largeImg = targetImg.parentNode.href;
+
+    imageBoss.src = largeImg;
+    events.preventDefault();
+  } else if (targetLink) {
+    const largeImg = targetLink.href;
+
+    imageBoss.src = largeImg;
+    events.preventDefault();
+  }
+});
