43e60f162195e7b5d63f99f6dfc1063ba6376e06
Denis <denchick1546450@gmail.com>
2025-07-22 15:55:23 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const thumbs = document.getElementById('thumbs');
+const largeImg = document.getElementById('largeImg');
+
+thumbs.addEventListener('click', function (event1) {
+  event1.preventDefault();
+
+  const target = event1.target;
+
+  // click IMG
+
+  if (target.tagName === 'IMG') {
+    const link = target.closest('a');
+
+    if (link) {
+      largeImg.src = link.href;
+    }
+  }
+
+  if (target.tagName === 'A') {
+    largeImg.src = target.href;
+  }
+});
