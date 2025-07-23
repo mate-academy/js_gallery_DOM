59947aea2bda46f280ed823ea07210b257e31a44
Denis <denchick1546450@gmail.com>
2025-07-23 14:51:16 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,22 +3,20 @@
 const thumbs = document.getElementById('thumbs');
 const largeImg = document.getElementById('largeImg');
 
-thumbs.addEventListener('click', function (event1) {
-  event1.preventDefault();
-
-  const target = event1.target;
-
-  // click IMG
+thumbs.addEventListener('click', function (e) {
+  const target = e.target;
 
   if (target.tagName === 'IMG') {
     const link = target.closest('a');
 
     if (link) {
+      e.preventDefault();
       largeImg.src = link.href;
     }
   }
 
   if (target.tagName === 'A') {
+    e.preventDefault();
     largeImg.src = target.href;
   }
 });
