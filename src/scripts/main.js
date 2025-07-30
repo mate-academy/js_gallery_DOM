6d70bfcbfc4ac65c787fdabdb9b93a414abad56c
Oleksandr <sasha16092007@gmail.com>
2025-07-31 00:30:19 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const mainPhoto = document.querySelector('.gallery__large-img');
+const littlePhoto = document.querySelector('.gallery__list');
+
+littlePhoto.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  if (e.target.classList.contains('list-item__link')) {
+    mainPhoto.src = e.target.href;
+  }
+
+  if (e.target.classList.contains('gallery__img')) {
+    mainPhoto.src = e.target.parentElement.href;
+  }
+});
