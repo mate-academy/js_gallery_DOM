f023828ffc906cb51f2424687a66c9409ea81bca
Ivan Denys <denisivan06007@gmail.com>
2025-08-02 15:26:53 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const ul = document.querySelector('.gallery__list');
+const largeImg = document.querySelector('.gallery__large-img');
+
+ul.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const link = e.target.closest('.list-item__link');
+
+  if (!link) {
+    return;
+  }
+  largeImg.src = link.href;
+});
