c1aa550265a97c963e78201156cf8470e2dc47f8
CwaR228 <sosok2gt@gmail.com>
2025-08-04 20:36:48 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const list = document.getElementById('thumbs');
+const mainImg = document.getElementById('largeImg');
+
+list.addEventListener('click', (selecting) => {
+  const link = selecting.target.closest('.list-item a');
+
+  selecting.preventDefault();
+
+  mainImg.src = link.href;
+});
