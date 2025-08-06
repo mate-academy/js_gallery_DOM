847160830934ee3d431b6f23e986056bd2a3beb6
Dariia Hryshchukova <dariagrisukova@gmail.com>
2025-08-06 19:17:26 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const galery = document.querySelector('.gallery__list');
+
+galery.addEventListener('click', (e) => {
+  const click = e.target.closest('a');
+
+  e.preventDefault();
+
+  if (!click) {
+    return;
+  }
+
+  document.getElementById('largeImg').src = click.href;
+});
