b186f74d089a4e1e1f104d4273ea02fee07b669e
Nazariy <nazariypanochko@gmail.com>
2025-08-10 12:20:50 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const generalImg = document.getElementById('largeImg');
+const listImg = document.getElementById('thumbs');
+
+listImg.addEventListener('click', (evt) => {
+  evt.preventDefault();
+
+  let target = evt.target;
+
+  if (target.tagName === 'IMG') {
+    target = target.closest('a');
+  }
+
+  if (target && target.tagName === 'A') {
+    generalImg.src = target.href;
+  }
+});
