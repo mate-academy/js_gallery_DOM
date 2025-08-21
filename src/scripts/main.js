f05a6b241ffefa18510e6543d9d5b2be2f1d68c0
Nikita Chupahin <nik123chupahin@gmail.com>
2025-08-21 09:05:44 +0300

    added task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const largeImg = document.getElementById('largeImg');
+const thumbs = document.getElementById('thumbs');
+
+thumbs.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  let target = e.target;
+
+  if (target.tagName !== 'A') {
+    target = target.closest('a');
+  }
+
+  if (target) {
+    const hrefAttrValue = target.href;
+
+    largeImg.src = hrefAttrValue;
+  }
+});
