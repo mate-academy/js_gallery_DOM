d38d8a6a92d28c89cd4b71086989befe0bf9a3ed
Nazar <nazar.shevchuk.121@gmail.com>
2025-08-21 11:17:53 +0300

    sol1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const imgListBlock = document.querySelector('#thumbs');
+const mainImg = document.querySelector('#largeImg');
+
+imgListBlock.addEventListener('click', (item) => {
+  item.preventDefault();
+
+  let link;
+
+  if (item.target.tagName === 'IMG') {
+    link = item.target.closest('a');
+  } else if (item.target.tagName === 'A') {
+    link = item.target;
+  }
+
+  if (link) {
+    const hrefVal = link.getAttribute('href');
+
+    mainImg.src = hrefVal;
+  }
+});
