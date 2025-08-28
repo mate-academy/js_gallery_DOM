1a8a67019cfe3eddf3cf982facc166218474b1af
Denys Piddubnyi <iforgotmypincode@gmail.com>
2025-08-28 14:04:55 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const mainImage = document.querySelector('#largeImg');
+const container = document.querySelector('#thumbs');
+
+container.addEventListener('click', (e) => {
+  const picture = e.target.tagName;
+
+  if (picture === 'IMG') {
+    mainImage.src = e.target.parentNode.href;
+  }
+
+  if (picture === 'A') {
+    mainImage.src = e.target.href;
+  }
+});
+
+const click = document.querySelectorAll('a');
+
+click.forEach((item) => {
+  item.addEventListener('click', (e) => {
+    e.preventDefault();
+  });
+});
