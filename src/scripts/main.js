e997e5455ead168eeba1bd6a2a3dd4423acd99e0
kirky359 <lol783176@gmail.com>
2025-08-29 20:04:15 +0300

    task completed waiting for rejection

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const ul = document.querySelector('ul');
+const mainPhoto = document.querySelector('#largeImg');
+
+ul.addEventListener('click', (e) => {
+  const li = e.target.closest('.list-item');
+
+  e.preventDefault();
+
+  if (!li) {
+    return;
+  }
+
+  const link = li.querySelector('a');
+
+  mainPhoto.src = link.href;
+});
