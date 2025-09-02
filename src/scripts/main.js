413d9caee87af4ec95eae0d0f7f599d335401f29
Gennadiy99 <login841@gmail.com>
2025-09-02 23:26:45 +0200

    finished project

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const mainImg = document.getElementById('largeImg');
+const ul = document.getElementById('thumbs');
+
+ul.addEventListener('click', function (even) {
+  even.preventDefault();
+
+  const a = even.target.closest('a');
+  const targetHref = a.href;
+
+  mainImg.src = targetHref;
+});
