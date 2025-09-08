3b0bd1f463cc23b54da563e810500038195bc37a
anastasiiadns <nastyadenisenko17@gmail.com>
2025-09-08 19:16:41 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const mainImage = document.querySelector('div img');
+const thumbnailImages = document.querySelector('.gallery');
+
+thumbnailImages.addEventListener('click', (events) => {
+  const a = events.target.closest('a');
+
+  if (events.target.tagName === 'IMG') {
+    mainImage.src = a.href;
+    events.preventDefault();
+  }
+});
