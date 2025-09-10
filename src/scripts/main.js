faab84433ff2e03351ccd09514e65d50f0e88913
Mykola Kuzmenko <kolua.kuzmenko@gmail.com>
2025-09-10 19:26:50 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const mainImage = document.getElementById('largeImg');
+const galleryList = document.querySelector('.gallery__list');
+
+galleryList.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const link = e.target.closest('.list-item__link');
+
+  if (!link) {
+    return;
+  }
+
+  mainImage.src = link.href;
+});
