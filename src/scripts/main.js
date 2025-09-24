c85bb6e8ad70550187dc27f17a9fd2e94b22e661
Yuliia Kulish <kulish.yulechka@gmail.com>
2025-09-24 15:48:27 +0300

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,19 +1,30 @@
 'use strict';
 
-const largeImg = document.getElementById('largeImg');
-const thumbs = document.getElementById('thumbs');
+const changeImg = () => {
+  const largeImg = document.getElementById('largeImg');
+  const thumbs = document.getElementById('thumbs');
 
-thumbs.addEventListener('click', (evt) => {
-  evt.preventDefault();
-
-  const link = evt.target.closest('a');
-
-  if (!link) {
+  if (!largeImg || !thumbs) {
     return;
   }
 
-  const relativeSrc = link.getAttribute('href');
-  const absoluteSrc = new URL(relativeSrc, window.location.origin).href;
+  thumbs.addEventListener('click', (evt) => {
+    if (!(evt.target instanceof Element)) {
+      return;
+    }
 
-  largeImg.src = absoluteSrc;
-});
+    const link = evt.target.closest('a');
+
+    if (!link) {
+      return;
+    }
+    evt.preventDefault();
+
+    const relativeSrc = link.getAttribute('href');
+    const absoluteSrc = new URL(relativeSrc, window.location.origin).href;
+
+    largeImg.src = absoluteSrc;
+  });
+};
+
+document.addEventListener('DOMContentLoaded', changeImg);
