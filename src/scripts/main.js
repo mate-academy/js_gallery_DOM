2795190655722223fed0510e3a9846896e708388
Yuliia Kulish <kulish.yulechka@gmail.com>
2025-09-24 15:34:43 +0300

    add tast solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const largeImg = document.getElementById('largeImg');
+const thumbs = document.getElementById('thumbs');
+
+thumbs.addEventListener('click', (evt) => {
+  evt.preventDefault();
+
+  const link = evt.target.closest('a');
+
+  if (!link) {
+    return;
+  }
+
+  const relativeSrc = link.getAttribute('href');
+  const absoluteSrc = new URL(relativeSrc, window.location.origin).href;
+
+  largeImg.src = absoluteSrc;
+});
