06d933b585d9ec25d95d908a3b505d1771049e58
Denys Piddubnyi <iforgotmypincode@gmail.com>
2025-09-25 13:23:43 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const largePhoto = document.getElementById('largeImg');
+const smallPhoto = document.getElementById('thumbs');
+
+smallPhoto.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const target = e.target.closest('.list-item__link');
+
+  if (target) {
+    largePhoto.src = target.href;
+  }
+});
