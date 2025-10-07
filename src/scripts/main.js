93b87eeb33552efcab4515753004022afad25924
Iryna <Yurchak.Iryna2017@gmail.com>
2025-10-07 21:44:51 +0300

    New Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,15 +4,16 @@ const thumbs = document.getElementById('thumbs');
 const large = document.getElementById('largeImg');
 
 thumbs.addEventListener('click', (e) => {
-  e.preventDefault();
-
   const link = e.target.closest('.list-item__link');
 
   if (!link) {
     return;
   }
+  e.preventDefault();
 
   const newSrc = link.getAttribute('href');
+  const img = link.querySelector('img');
 
   large.src = newSrc;
+  large.alt = img?.alt || '';
 });
