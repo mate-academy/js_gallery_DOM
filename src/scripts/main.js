715d2d3946debf89d3301995778f6b2d5293ab05
Iryna <Yurchak.Iryna2017@gmail.com>
2025-10-07 21:09:50 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const thumbs = document.getElementById('thumbs');
+const large = document.getElementById('largeImg');
+
+thumbs.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const link = e.target.closest('.list-item__link');
+
+  if (!link) {
+    return;
+  }
+
+  const newSrc = link.getAttribute('href');
+
+  large.src = newSrc;
+});
