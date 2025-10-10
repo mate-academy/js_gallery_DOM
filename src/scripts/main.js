48890c4609cc3176219428023ce8f387564a9df0
chumachenkoUA <kirito22122005@gmail.com>
2025-10-10 20:48:24 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const galleryList = document.querySelector('.gallery__list');
+const largeImage = document.querySelector('#largeImg');
+
+galleryList.addEventListener('click', (e) => {
+  const link = e.target.closest('a');
+
+  if (!link || !largeImage) {
+    return;
+  }
+
+  const image = link.querySelector('img');
+
+  e.preventDefault();
+
+  largeImage.src = link.href;
+  largeImage.alt = image.alt;
+});
