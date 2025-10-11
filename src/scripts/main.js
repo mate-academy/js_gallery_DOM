41011db8e63898753c4f06a327d7a5478c5073d0
Yaroslav Molchanov <y.molchanoff@gmail.com>
2025-10-11 20:21:29 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const thumbs = document.getElementById('thumbs');
+const largeImg = document.getElementById('largeImg');
+
+thumbs.addEventListener('click', function (e) {
+  e.preventDefault(); // запобігаємо переходу за посиланням
+
+  // Знаходимо найближчий <a> навіть якщо клік був по <img>
+  const link = e.target.closest('a');
+
+  if (!link || !thumbs.contains(link)) {
+    return;
+  }
+
+  // Змінюємо велике зображення
+  largeImg.src = link.href;
+  largeImg.alt = link.title;
+});
