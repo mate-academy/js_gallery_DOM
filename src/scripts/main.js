ec1bc2386b30002bd655c61e01fb0c9909ec3b49
Bohdan Pidhrebelnyi <bodiiiiic@gmail.com>
2025-10-11 12:23:57 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', function () {
+  const imgGallery = document.getElementById('largeImg');
+  const links = document.querySelectorAll('.gallery ul li a');
+
+  links.forEach((link) => {
+    if (link) {
+      link.addEventListener('click', (e) => {
+        if (link.hasAttribute('href')) {
+          const largeImg = link.getAttribute('href');
+
+          imgGallery.setAttribute('src', largeImg);
+          e.preventDefault();
+        }
+      });
+    }
+  });
+});
