fb1410c0634f4f77e9a6bbccbd733b972c91cd68
Roma Ivashchenko <rrrrgvvggh@gmail.com>
2025-10-12 14:08:09 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const bigImage = document.querySelector('#largeImg');
+const container = document.querySelector('.gallery__list');
+
+container.addEventListener('click', (events) => {
+  const link = events.target.closest('a');
+
+  if (!link) {
+    return;
+  }
+
+  events.preventDefault();
+  bigImage.src = link.href;
+  bigImage.alt = link.title;
+});
