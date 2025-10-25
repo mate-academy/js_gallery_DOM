ce5f0c799d49ab864b6e3d86dda958a874407816
Luna <yanka16247@mail.com>
2025-10-25 19:33:21 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const list = document.getElementById('thumbs');
+const mainImg = document.getElementById('largeImg');
+
+list.addEventListener('click', (ev) => {
+  ev.preventDefault();
+
+  const target = ev.target;
+  const link = target.closest('a');
+
+  if (!link || !list.contains(link)) {
+    return;
+  }
+  mainImg.src = link.href;
+});
