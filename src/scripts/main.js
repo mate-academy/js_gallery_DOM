793d4dc6e2e36644f0fc16f6c40bdf5f84e076b2
wzfxc <maksohrimenko1@gmail.com>
2025-10-26 18:35:05 +0200

    v1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const gallery = document.querySelector('#thumbs');
+const largeImg = document.querySelector('#largeImg');
+
+gallery.addEventListener('click', (eventik) => {
+  eventik.preventDefault();
+
+  const link = eventik.target.closest('a');
+
+  if (!link) {
+    return;
+  }
+
+  largeImg.src = link.href;
+});
