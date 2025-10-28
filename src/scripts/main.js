9e9788390bf41fc6317fcd66e6398a2c06510eea
yahohulia <yaroslavhohulia@gmail.com>
2025-10-28 14:00:17 +0200

    added js solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const imgContainer = document.querySelector('ul');
+
+imgContainer.addEventListener('click', (eventer) => {
+  const largeImg = document.querySelector('#largeImg');
+  const smallImg = eventer.target.closest('a');
+
+  eventer.preventDefault();
+
+  if (!smallImg) {
+    return;
+  }
+
+  largeImg.src = smallImg.href;
+});
