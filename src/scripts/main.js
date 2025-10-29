d0315af6e2fe14fad57b6e39b7515388a1b6529b
Dmytruk Serhii <dmitruksl92@gmail.com>
2025-10-29 13:34:33 +0200

    Solutions

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const list = document.querySelector('.gallery__list');
+const largeImg = document.querySelector('.gallery__large-img');
+
+list.addEventListener('click', (e) => {
+  const link = e.target.closest('.list-item__link');
+  const imgSrcNew = link.getAttribute('href');
+
+  if (!link) {
+    return;
+  }
+
+  e.preventDefault();
+
+  largeImg.setAttribute('src', imgSrcNew);
+});
