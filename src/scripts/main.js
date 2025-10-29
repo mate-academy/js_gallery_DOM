08b54c5ad5649f12c2b8b9fdbc2e20fab1932053
Dmytruk Serhii <dmitruksl92@gmail.com>
2025-10-29 13:41:54 +0200

    Fix Remarks

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,11 +5,12 @@ const largeImg = document.querySelector('.gallery__large-img');
 
 list.addEventListener('click', (e) => {
   const link = e.target.closest('.list-item__link');
-  const imgSrcNew = link.getAttribute('href');
 
   if (!link) {
     return;
   }
+
+  const imgSrcNew = link.getAttribute('href');
 
   e.preventDefault();
 
