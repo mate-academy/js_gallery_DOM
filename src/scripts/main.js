f34b627989864906418301e167e8e850930809b3
Oksana Rubel <okhymych480@gmail.com>
2025-10-30 13:58:27 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const gallery = document.querySelector('.gallery');
+const largeImg = document.querySelector('.gallery__large-img');
+
+gallery.addEventListener('click', onClick);
+
+function onClick(eve) {
+  const link = eve.target.closest('.list-item__link');
+
+  if (!link) {
+    return;
+  }
+  eve.preventDefault();
+
+  const href = link.getAttribute('href');
+
+  if (href) {
+    largeImg.src = href;
+  }
+}
