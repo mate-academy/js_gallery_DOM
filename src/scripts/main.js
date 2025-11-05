a3a0aea115f91e89d96c8c124ff6a4cd05921145
Sertavr <pampuh.net@gmail.com>
2025-11-05 22:32:26 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const galleryList = document.querySelector('#thumbs');
+
+galleryList.addEventListener('click', (e) => {
+  const largeImg = document.querySelector('#largeImg');
+
+  if (e.target.tagName === 'IMG') {
+    const link = e.target.closest('a');
+
+    largeImg.src = link.href;
+  }
+
+  if (e.target.tagName === 'A') {
+    largeImg.src = e.target.href;
+  }
+  e.preventDefault();
+});
