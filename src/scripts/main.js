f1b7e7aa4701e10a57bd39a13c867d940efce118
Vasyl Levitskyi <leva1990vale@gmail.com>
2025-11-05 10:36:42 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const largeImg = document.querySelector('#largeImg');
+const thumbs = document.querySelector('#thumbs');
+
+thumbs.addEventListener('click', (e) => {
+  const link = e.target.closest('a');
+
+  if (link === null) {
+    return;
+  }
+
+  e.preventDefault();
+  largeImg.src = link.href;
+});
