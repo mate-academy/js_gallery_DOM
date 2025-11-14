38fc2bbcd6c3c3b5b10b949e96facf408ddd789c
Sofiia <sofiiamuz@gmail.com>
2025-11-14 21:12:48 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const elements = document.querySelectorAll('a.list-item__link');
+
+elements.forEach((element) => {
+  element.addEventListener('click', (elementEvent) => {
+    const image = document.querySelector('#largeImg');
+
+    image.src = element.href;
+    elementEvent.preventDefault();
+  });
+});
