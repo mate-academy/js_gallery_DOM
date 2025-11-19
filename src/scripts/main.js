c539390396a09e1ecd4e4d8f36dd7140a05194a0
mpodaniev <mpodanev@gmail.com>
2025-11-19 23:41:58 +0200

    Add image preview functionality to gallery links in `main.js`

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const galleryElement = document.querySelector('.gallery');
+
+const galleryLinks = galleryElement.querySelectorAll('a');
+const largeImg = document.querySelector('#largeImg');
+
+galleryLinks.forEach((link) => {
+  link.addEventListener('click', function (evnt) {
+    evnt.preventDefault();
+    largeImg.src = this.href;
+  });
+});
