27668707542924eb913f7e293b2f04a1fe72de78
mpodaniev <mpodanev@gmail.com>
2025-11-19 23:57:49 +0200

    Refactor gallery click handling to use event delegation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,14 @@
 'use strict';
 
 const galleryElement = document.querySelector('.gallery');
-
-const galleryLinks = galleryElement.querySelectorAll('a');
 const largeImg = document.querySelector('#largeImg');
 
-galleryLinks.forEach((link) => {
-  link.addEventListener('click', function (evnt) {
-    evnt.preventDefault();
-    largeImg.src = this.href;
-  });
+galleryElement.addEventListener('click', function (evnt) {
+  evnt.preventDefault();
+
+  const parentLink = evnt.target.closest('a');
+
+  if (parentLink) {
+    largeImg.src = parentLink.href;
+  }
 });
