68759928749b51bbbf09585e270e15fd17b444f1
horbenkoOleksandr <horbenko.oleksandr.v@gmail.com>
2025-11-25 00:18:16 +0200

    Gallery-dom

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const ulElemGallery = document.querySelector('ul.gallery__list');
+const initialImg = document.querySelector('#largeImg');
+
+ulElemGallery.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const receivedItem = e.target.closest('a');
+
+  if (!receivedItem) {
+    return;
+  }
+
+  const currentImg = receivedItem.href;
+
+  initialImg.src = currentImg;
+});
