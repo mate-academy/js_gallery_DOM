71dddd3b0679d4e7d530dc98c3dbcdc2d64d4193
Olga Wukelan <olga.wukelan@gmail.com>
2025-11-25 20:53:56 +0400

    solution-1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const largeImage = document.getElementById('largeImg');
+const galleryList = document.getElementById('thumbs');
+
+// eslint-disable-next-line no-shadow
+galleryList.addEventListener('click', (event) => {
+  const link = event.target.closest('.list-item__link');
+
+  if (!link) {
+    return;
+  }
+
+  event.preventDefault();
+
+  const newImgSrc = link.href;
+
+  largeImage.src = newImgSrc;
+});
