345d5e8e1b5cdec072e3d133cb528f1d2c610824
Tymofii <zrozik@MacBook-Air-Tymofii.local>
2025-11-29 10:22:50 +0200

    commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const largeImg = document.getElementById('largeImg');
+const thumbs = document.getElementById('thumbs');
+
+thumbs.addEventListener('click', (event) => {
+  if (event.target.tagName === 'IMG' || event.target.tagName === 'a') {
+    event.preventDefault();
+
+    let newSrc;
+
+    if (event.target.tagName === 'IMG') {
+      newSrc = event.target.parentElement.href;
+    } else {
+      newSrc = event.target.href;
+    }
+
+    largeImg.src = newSrc;
+  }
+});
