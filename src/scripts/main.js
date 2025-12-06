30d848360d8147a1f89ffa91c6fc6b519b532817
Alex <1inboxna@gmail.com>
2025-12-06 15:54:23 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const mainImg = document.querySelector('#largeImg'); // велике зображення
+const galleryList = document.querySelector('#thumbs'); // галерея
+
+galleryList.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const linkImg = e.target.closest('.list-item__link');
+  const galleryImg = e.target.closest('.gallery__img');
+
+  if (!linkImg && !galleryImg) {
+    return;
+  }
+
+  const newSrc = linkImg.href;
+
+  mainImg.src = newSrc;
+});
