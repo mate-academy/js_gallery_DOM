138c4a52fa0e019db44fd683c087c1fe92be8542
Maksym Halushko <gallikillag@gmail.com>
2025-12-06 18:26:38 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const largeImg = document.querySelector('#largeImg');
+const thumbs = document.querySelector('#thumbs');
+
+thumbs.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const link = e.target.closest('a');
+
+  if (!link) {
+    return;
+  }
+
+  largeImg.src = link.href;
+  largeImg.alt = link.title;
+});
