65cf37098597ee4c90ccae84f40d486afad65443
Vladislav-Korsun <vladosik385@gmail.com~>
2025-12-16 15:49:57 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const thumbsList = document.querySelector('#thumbs');
+const mainImage = document.querySelector('#largeImg');
+
+thumbsList.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  let link = e.target;
+
+  if (link.tagName === 'IMG') {
+    link = link.closest('a');
+  }
+
+  if (!link || link.tagName !== 'A') {
+    return;
+  }
+
+  mainImage.src = link.href;
+});
