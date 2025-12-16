3296641a2d94575b8038bc2c605ec2ea03eeaae6
Ivan <igrytsaenko@gmail.com>
2025-12-16 20:56:42 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const gallery = document.querySelector('ul');
+const preview = document.querySelector('.gallery__large-img');
+
+gallery.addEventListener('click', (e) => {
+  const link = e.target.closest('a');
+
+  if (!link) {
+    return;
+  }
+
+  e.preventDefault();
+
+  const bigSrc = link.getAttribute('href');
+
+  preview.setAttribute('src', bigSrc);
+
+  const active = document.querySelector('.thumb-active');
+
+  if (active) {
+    active.classList.remove('thumb-active');
+  }
+
+  link.classList.add('thumb-active');
+});
