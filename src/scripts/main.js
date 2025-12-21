c8dc43eee36f08279e472edf9b4c2627514244cc
Yurii Kumpanenko <yuriikumpanenko@ukr.net>
2025-12-21 14:30:12 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const imageList = document.getElementById('thumbs');
+const mainImage = document.getElementById('largeImg');
+
+imageList.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const link = e.target.closest('.list-item__link');
+
+  if (link) {
+    mainImage.src = link.href;
+  }
+});
