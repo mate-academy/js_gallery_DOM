64b5ac074c56bc02f41420eec39367a66bf26d56
Dmytro <dimusgamer2.0@gmail.com>
2025-12-21 18:48:25 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const largeImage = document.getElementById('largeImg');
+const thumbs = document.getElementById('thumbs');
+
+thumbs.addEventListener('click', (e) => {
+  const link = e.target.closest('a');
+
+  if (link) {
+    e.preventDefault();
+
+    largeImage.src = link.href;
+  }
+});
