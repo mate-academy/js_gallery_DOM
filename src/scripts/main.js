e161162b6362f7006e2af3f4cf08a01b5e1d67ee
Evg3n22 <evgen.gulevaty@gmail.com>
2025-12-22 20:28:45 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,30 @@
 'use strict';
+
+const images = document.querySelectorAll('.list-item__link');
+const bigImage = document.querySelector('#largeImg');
+
+images.forEach((image) => {
+  image.addEventListener('click', () => {
+    event.preventDefault();
+
+    const imageId = +image.title.split(' ')[1];
+
+    switch (imageId) {
+      case 1:
+        bigImage.src = '/images/landscape-first.png';
+        break;
+      case 2:
+        bigImage.src = '/images/landscape-second.png';
+        break;
+      case 3:
+        bigImage.src = '/images/landscape-third.png';
+        break;
+      case 4:
+        bigImage.src = '/images/landscape-fourth.png';
+        break;
+      case 5:
+        bigImage.src = '/images/landscape-fifth.png';
+        break;
+    }
+  });
+});
