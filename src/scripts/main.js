3c38af187e3623fe2e702ebe122058c16c3368a9
Evg3n22 <evgen.gulevaty@gmail.com>
2025-12-22 20:49:24 +0200

    use event.target as solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,30 +1,9 @@
 'use strict';
 
-const images = document.querySelectorAll('.list-item__link');
+const imageUl = document.querySelector('#thumbs');
 const bigImage = document.querySelector('#largeImg');
 
-images.forEach((image) => {
-  image.addEventListener('click', () => {
-    event.preventDefault();
-
-    const imageId = +image.title.split(' ')[1];
-
-    switch (imageId) {
-      case 1:
-        bigImage.src = '/images/landscape-first.png';
-        break;
-      case 2:
-        bigImage.src = '/images/landscape-second.png';
-        break;
-      case 3:
-        bigImage.src = '/images/landscape-third.png';
-        break;
-      case 4:
-        bigImage.src = '/images/landscape-fourth.png';
-        break;
-      case 5:
-        bigImage.src = '/images/landscape-fifth.png';
-        break;
-    }
-  });
+imageUl.addEventListener('click', () => {
+  event.preventDefault();
+  bigImage.src = `/images/landscape-${event.target.src.split('-')[1]}.png`;
 });
