d446ee8715a84171c1f0191d4b93ff4edb5902e4
Dima Sidenko <sidenkodima2202@gmail.com>
2026-01-01 15:50:44 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const list = [...document.querySelectorAll('.list-item__link')];
+const img = document.querySelector('#largeImg');
+
+list.forEach((link) => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    img.src = link.href;
+  });
+});
