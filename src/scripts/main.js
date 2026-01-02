d42396cd7e82b8a781a2983aad4a02ff814db9b9
Pasha <balanenko12@gmail.com>
2026-01-02 08:23:30 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const largeImg = document.getElementById('largeImg');
+const thumbs = document.getElementById('thumbs');
+
+// eslint-disable-next-line no-shadow
+thumbs.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const link = event.target.closest('a');
+
+  largeImg.src = link.href;
+});
