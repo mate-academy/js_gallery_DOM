db40d6c479f555db15897d94acdccf95920874f3
vladiksanl <vladislav.liashenko1@gmail.com>
2026-01-05 13:05:12 +0200

    solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const mainImg = document.getElementById('largeImg');
+
+const li = document.querySelectorAll('ul li');
+
+const arrayLiOfLi = [...li];
+
+for (const item of arrayLiOfLi) {
+  item.querySelector('a').addEventListener('click', () => {
+    event.preventDefault();
+    mainImg.setAttribute('src', item.querySelector('a').getAttribute('href'));
+  });
+}
