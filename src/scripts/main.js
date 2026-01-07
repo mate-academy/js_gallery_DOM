b1532c03bd2db7d910ece8d5e739e993dda8cac3
Nazarii Korol <nazarkorol3684@gmail.com>
2026-01-07 13:04:29 +0200

    Task js-gallery-dom done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const largeImg = document.getElementById('largeImg');
+const thumbnails = document.getElementById('thumbs');
+
+thumbnails.addEventListener('click', (e) => {
+  const link = e.target.closest('a');
+
+  if (!link) {
+    return;
+  }
+  e.preventDefault();
+  largeImg.src = link.href;
+});
