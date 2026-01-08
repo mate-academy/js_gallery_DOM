71cfbd20c2ea73dac74e89365d55546a20008830
Nazarii Lesniak <naziksay@gmail.com>
2026-01-08 13:31:10 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const galleryList = document.querySelector('.gallery__list');
+
+galleryList.addEventListener('click', (clickEvent) => {
+  clickEvent.preventDefault();
+
+  const link = clickEvent.target.closest('a');
+  const image = document.querySelector('.gallery__large-img');
+
+  if (!link) {
+    return;
+  }
+
+  const url = link.href;
+
+  image.src = url;
+});
