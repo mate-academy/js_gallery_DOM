fb9d8d4b6a7960fd6f0a153778b830c7b18a105c
Yaroslav Olieniev <olieniev.od.ua@gmail.com>
2026-01-09 11:40:14 +0100

    added task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const list = document.querySelector('.gallery__list');
+
+const mainImage = document.querySelector('.gallery__large-img');
+
+list.addEventListener('click', (action) => {
+  const link = action.target.closest('.list-item__link');
+
+  if (!link) {
+    return;
+  }
+
+  action.preventDefault();
+
+  mainImage.src = link.href;
+});
