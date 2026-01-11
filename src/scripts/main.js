e1d1f394f084f9bab2576fe3a90db4a67ca5d655
Bohdan <bodiiiiic@gmail.com>
2026-01-11 13:31:21 +0000

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const gallery = document.querySelector('.gallery');
+
+  if (!gallery) {
+    return;
+  }
+
+  const largeImg = gallery.querySelector('.gallery__large-img');
+
+  gallery.addEventListener('click', (ev) => {
+    const listItem = event.target.closest('.list-item__link');
+
+    if (!listItem) {
+      return;
+    }
+
+    ev.preventDefault();
+    largeImg.src = listItem.href;
+  });
+});
