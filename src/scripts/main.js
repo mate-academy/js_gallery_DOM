a4347ab6b34b3bfef91a1babda82e92f6b64f30a
DevT <dev.tymofiichuk@gmail.com>
2026-01-13 15:01:38 +0200

    Gallery task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const thumbs = document.querySelector('#thumbs');
+
+thumbs.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const thumb = e.target.closest('.list-item__link');
+
+  if (!thumb) {
+    return;
+  }
+
+  document
+    .querySelector('#largeImg')
+    .setAttribute('src', thumb.getAttribute('href'));
+});
