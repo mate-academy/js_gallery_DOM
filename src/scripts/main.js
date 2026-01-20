bc7f2b16c0d83b0a07fd98a834715865f77deebb
BeginerAlex <aleksander9677@gmail.com>
2026-01-21 00:14:31 +0100

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+document.querySelector('.gallery__list').addEventListener('click', (e) => {
+  const link = e.target.closest('.list-item__link');
+
+  if (link) {
+    e.preventDefault();
+
+    const imageLink = link.href;
+    const imageAlt = link.querySelector('img').getAttribute('alt');
+    const mainImage = document.querySelector('#largeImg');
+
+    mainImage.setAttribute('src', imageLink);
+    mainImage.setAttribute('alt', imageAlt);
+  }
+});
