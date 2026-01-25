3a33f2bc05a11d483aa2d31affc96ad890cf3cf0
Andriy <andriit35@gmail.com>
2026-01-25 19:40:51 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const imagesArray = document.querySelectorAll('.gallery__thumb');
+const bigImage = document.querySelector('.gallery__large-img');
+
+imagesArray.forEach((image) => {
+  image.addEventListener('click', (events) => {
+    events.preventDefault();
+
+    const parentLink = image.closest('a');
+
+    if (parentLink) {
+      bigImage.src = parentLink.href;
+    } else {
+      bigImage.src = image.src;
+    }
+  });
+});
