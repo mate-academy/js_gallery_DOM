0094136749fbacaa038834474f5bef381b168994
Vlad Chudin <chudintttks@gmail.com>
2026-01-25 11:04:48 +0100

    complete

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const gallary = document.querySelector('.gallery__list');
+const bigImg = document.querySelector('.gallery__img');
+
+const callback = (events) => {
+  const img = events.target.closest('img');
+
+  if (!img) {
+    return;
+  }
+
+  const link = img.closest('a');
+
+  if (!link) {
+    return;
+  }
+
+  event.preventDefault();
+
+  bigImg.src = link.href;
+};
+
+gallary.addEventListener('click', callback);
