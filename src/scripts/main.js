6316ba8ce51c70e981eb04754f094dddbb04d019
Illia Safonov <safonov.illia@gmail.com>
2026-01-26 20:39:33 +0100

    gallery_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,16 +3,14 @@
 const thumbs = document.querySelector('#thumbs');
 const largeImg = document.querySelector('#largeImg');
 
-thumbs.onclick = function (action) {
+thumbs.addEventListener('click', (action) => {
+  action.preventDefault();
+
   const link = action.target.closest('a');
 
-  if (!link) {
+  if (!link || !thumbs.contains(link)) {
     return;
   }
 
-  action.preventDefault();
-
-  const newSrc = link.getAttribute('href');
-
-  largeImg.setAttribute('src', newSrc);
-};
+  largeImg.src = link.href;
+});
