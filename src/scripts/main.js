a586489cfd127c8665cd700b2c1bdea0d8ab35aa
unknown <dkuz6323@gmail.com>
2026-01-27 14:55:05 +0200

    fix bugs

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,13 +3,17 @@
 const container = document.querySelector('.gallery');
 
 container.addEventListener('click', (events) => {
-  const img = events.target.closest('img');
+  events.preventDefault();
 
-  if (img === null) {
+  const a = events.target.closest('a');
+
+  if (a === null) {
     return;
   }
 
+  const thatImg = a.querySelector('img');
+
   const largeImg = document.querySelector('.gallery__large-img');
 
-  largeImg.src = img.src;
+  largeImg.src = thatImg.src;
 });
