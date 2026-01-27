b15dad48f6cf9a8d5af779ab1740b47351c65037
unknown <dkuz6323@gmail.com>
2026-01-27 14:47:58 +0200

    completed tasks

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const container = document.querySelector('.gallery');
+
+container.addEventListener('click', (events) => {
+  const img = events.target.closest('img');
+
+  if (img === null) {
+    return;
+  }
+
+  const largeImg = document.querySelector('.gallery__large-img');
+
+  largeImg.src = img.src;
+});
