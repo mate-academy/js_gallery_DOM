fd1ce30b42b5014433cf54e3bd7ae2190b8c733e
Oleksandr Ratseburzhynskyi <ratseburzhynskyi@gmail.com>
2026-02-01 13:50:27 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const gallery = document.querySelector('.gallery');
+
+gallery.addEventListener('click', (e) => {
+  const link = event.target.closest('a');
+
+  if (!link) {
+    return;
+  }
+
+  e.preventDefault();
+
+  const path = link.href;
+
+  const mainImage = document.querySelector('.gallery__large-img');
+
+  mainImage.src = path;
+});
