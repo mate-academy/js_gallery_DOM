2f599326888b32f8da86990edc3f02be2a884d19
Oleksii <olexijukraina@gmail.com>
2026-02-04 17:18:08 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const gallery = document.querySelector('.gallery');
+const mainImg = document.querySelector('.gallery__large-img');
+
+gallery.addEventListener('click', (e) => {
+  const link = e.target.closest('a');
+
+  if (!link) {
+    return;
+  }
+
+  e.preventDefault();
+  mainImg.src = link.href;
+});
