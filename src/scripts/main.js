f59d2933cba4cd01988a6970dc9f574e12102c39
Anastasia Sutkova <nxxqar@gmail.com>
2026-02-04 23:11:25 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const thumbs = document.getElementById('thumbs');
+const largeImg = document.getElementById('largeImg');
+
+thumbs.addEventListener('click', (ev) => {
+  const thumbnailLink = ev.target.closest('a');
+
+  if (!thumbnailLink || !thumbs.contains(thumbnailLink)) {
+    return;
+  }
+
+  ev.preventDefault();
+
+  largeImg.src = thumbnailLink.href;
+  largeImg.alt = thumbnailLink.title;
+});
