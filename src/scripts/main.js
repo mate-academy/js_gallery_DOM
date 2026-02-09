431f2c95cf421420ce7fed63aceda4487d7c4a72
Maksum Nemchenko <maksnemch2004@gmail.com>
2026-02-09 23:44:55 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const largeImage = document.querySelector('#largeImg');
+const galleryImages = document.querySelector('.gallery__list');
+
+galleryImages.addEventListener('click', (e) => {
+  const target = e.target;
+  const link =
+    target.tagName === 'IMG'
+      ? target.closest('a')
+      : target.tagName === 'A'
+        ? target
+        : null;
+
+  if (!link) {
+    return;
+  }
+  e.preventDefault();
+
+  const bigUrl = link.href;
+
+  largeImage.src = bigUrl;
+});
