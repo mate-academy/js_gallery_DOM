1db1505322919fcae9bbcf11e0b7cd1623956ec6
Taras Borshch <tarasborshch4@gmail.com>
2026-02-13 18:09:44 +0200

    Develop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,26 +1,20 @@
 'use strict';
 
-'use strict';
-
 const largeImg = document.getElementById('largeImg');
 const thumbs = document.getElementById('thumbs');
 
-if (!largeImg || !thumbs) {
-  return;
+if (largeImg && thumbs) {
+  thumbs.addEventListener('click', (e) => {
+    if (!(e.target instanceof Element)) {
+      return;
+    }
+
+    const link = e.target.closest('a');
+    if (!link) {
+      return;
+    }
+
+    e.preventDefault();
+    largeImg.src = link.href;
+  });
 }
-
-thumbs.addEventListener('click', (e) => {
-  if (!(e.target instanceof Element)) {
-    return;
-  }
-
-  const link = e.target.closest('a');
-  if (!link) {
-    return;
-  }
-
-  e.preventDefault();
-
-  largeImg.src = link.href;
-});
-
