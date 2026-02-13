00b27507f13c087fa9f1d7586ba94e050d83e452
Taras Borshch <tarasborshch4@gmail.com>
2026-02-13 17:56:31 +0200

    Develop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const largeImg = document.getElementById("largeImg")
+const thumbs = document.getElementById("thumbs")
+
+thumbs.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  const link = event.target.closest("a")
+  console.log(link)
+  if (!link) return;
+
+  largeImg.src = link.href;
+})
