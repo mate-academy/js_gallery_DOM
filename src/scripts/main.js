68f6246aac396544b77e68430038e295456a5b36
Mykola Fatkullin <fatkullinmykola@gmail.com>
2026-02-14 19:01:19 +0200

    added solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,21 +2,18 @@
 
 const gallery = document.querySelector('.gallery');
 const galleryList = gallery.querySelector('.gallery__list');
-const listItems = galleryList.querySelectorAll('.list-item');
 const largeImg = gallery.querySelector('.gallery__large-img');
 
 const galleryEvent = (e) => {
-  e.preventDefault();
-
   const chosenLink = e.target.closest('a');
+
+  if (!chosenLink) {
+    return
+  }
+
+  e.preventDefault();
 
   largeImg.src = chosenLink.href;
 };
 
-[...listItems].forEach((item) => {
-  setEvent(item.querySelector('img'));
-});
-
-function setEvent(element) {
-  element.addEventListener('click', galleryEvent);
-}
+listItems.addEventListener('click', galleryEvent);
