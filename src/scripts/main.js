6b3d05c04a132cc183f7114d4b7f9549a8d8292d
freelinex <freelinex7@gmail.com>
2026-02-16 21:26:55 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const mainImage = document.querySelector('.gallery__large-img');
+const gallery = document.querySelectorAll('.list-item a');
+
+if (mainImage) {
+  gallery.forEach((link) => {
+    link.addEventListener('click', (target) => {
+      target.preventDefault();
+      mainImage.src = target.currentTarget.href;
+    });
+  });
+}
