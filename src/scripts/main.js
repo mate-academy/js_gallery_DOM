cbdc6d70e6b9558bec0bca3c9babf6140501889e
Roman <hpp1ovh@gmail.com>
2026-02-17 21:38:06 +0200

    complete

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const mainImage = document.getElementById('largeImg');
+const thumbsList = document.getElementById('thumbs');
+
+thumbsList.addEventListener('click', (e) => {
+  const link = e.target.closest('a');
+
+  if (link) {
+    e.preventDefault();
+  }
+
+  const item = e.target.closest('.list-item');
+
+  if (!item) {
+    return;
+  }
+
+  const largeImageUrl = link.href;
+
+  if (largeImageUrl) {
+    mainImage.src = largeImageUrl;
+  }
+});
