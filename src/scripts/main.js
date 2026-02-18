e42d8292e3ee6367b07230dbd614f10cd2938143
naviailpach <ivan.chaplya13@gmail.com>
2026-02-18 12:07:05 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const largeImg = document.querySelector('#largeImg');
+const gallery = document.querySelector('#thumbs');
+
+if (gallery && largeImg) {
+  gallery.addEventListener('click', (clickEvent) => {
+    clickEvent.preventDefault();
+
+    const link = clickEvent.target.closest('a');
+
+    if (!link) {
+      return;
+    }
+
+    largeImg.src = link.href;
+  });
+}
