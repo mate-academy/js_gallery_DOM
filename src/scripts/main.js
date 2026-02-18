98969745b4f6ba1fe455581918f15f3b793fea28
GabrielSpelchuk <gabrielspelciuc24@gmail.com>
2026-02-18 13:41:44 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const galleryElements = document.querySelector('.gallery__list');
+const preview = document.querySelector('.gallery__large-img');
+
+galleryElements.addEventListener('click', onThumbnailClick);
+
+function onThumbnailClick(events) {
+  events.preventDefault();
+
+  const target = events.target;
+
+  if (target.tagName === 'IMG') {
+    preview.src = target.parentElement.href;
+  }
+
+  if (target.tagName === 'A') {
+    preview.src = target.href;
+  }
+}
