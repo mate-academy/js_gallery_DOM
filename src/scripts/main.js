5e8211872cece29ae15f351335d07ac6e423c039
yuliana111007-debug <yuliana111007@gmail.com>
2026-02-24 22:18:32 +0200

    add silution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const gallery = document.querySelector('.gallery__list');
+const preview = document.querySelector('.gallery__large-img');
+
+gallery.addEventListener('click', (e) => {
+  const link = e.target.closest('a');
+
+  if (!link) {
+    return;
+  }
+  e.preventDefault();
+
+  const newSrc = link.dataset.source || link.href;
+
+  if (!newSrc) {
+    return;
+  }
+
+  if (preview) {
+    preview.src = newSrc;
+  }
+});
