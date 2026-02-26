5cd3fb4c884cdaf040d4f2af94ed794f497fc95d
Anatolii Lutai <toliklutai@gmail.com>
2026-02-26 12:36:04 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const thumbs = document.querySelector('#thumbs');
+const largeImg = document.querySelector('#largeImg');
+
+if (thumbs && largeImg) {
+  thumbs.addEventListener('click', function (evt) {
+    const target = evt.target;
+    const link = target.closest('a');
+
+    if (!link || !thumbs.contains(link)) {
+      return;
+    }
+    evt.preventDefault();
+
+    const largeImgUrl = link.href;
+
+    if (!largeImgUrl) {
+      return;
+    }
+
+    largeImg.src = largeImgUrl;
+  });
+}
