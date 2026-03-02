b74f323d2b26629be3c913ff80b0b8be3433fca4
Oleksandr Shyshenok <shyshenok81@gmail.com>
2026-03-02 23:31:30 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const thumbs = document.getElementById('thumbs');
+const largeImg = document.getElementById('largeImg');
+
+thumbs.addEventListener('click', function (evt) {
+  const link = evt.target.closest('a');
+
+  if (!link) {
+    return;
+  }
+
+  if (!thumbs.contains(link)) {
+    return;
+  }
+
+  evt.preventDefault();
+
+  const newSrc = link.getAttribute('href');
+
+  largeImg.setAttribute('src', newSrc);
+});
