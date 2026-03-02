86f6961011a39f26583d7e2177abe35303d81d37
uselessmorning <shtoykoigor@ukr.net>
2026-03-02 20:58:11 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const previousContainer = document.getElementById('thumbs');
+const mainImage = document.getElementById('largeImg');
+
+previousContainer.addEventListener('click', (e) => {
+  const link = e.target.closest('a');
+
+  if (!link) {
+    return;
+  }
+
+  e.preventDefault();
+  mainImage.src = link.href;
+});
