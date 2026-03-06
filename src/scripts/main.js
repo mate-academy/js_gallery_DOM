b35f07068c6f0cfd9190fb6367ca3ede8ff1ec77
Dmytro Halieba <dmytro.halyeba@gmail.com>
2026-03-07 00:15:32 +0200

    feat: eventHandler for thumbnails for switching photos

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const gallery = document.querySelector('#thumbs');
+const mainImg = document.querySelector('#largeImg');
+
+gallery.addEventListener('click', (e) => {
+  const link = e.target.closest('.list-item__link');
+
+  if (link) {
+    e.preventDefault();
+    mainImg.src = link.href;
+  }
+});
