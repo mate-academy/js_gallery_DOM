cf63c4367fc6aab12b692e8f9406fe52603037b6
Ola <aleksandra-chomicz@outlook.com>
2026-03-09 19:21:33 +0100

    change the large image source on thumbnail click.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const smallImages = document.querySelectorAll('.gallery__thumb');
+const largeImage = document.querySelector('#largeImg');
+
+smallImages.forEach((image) => {
+  image.addEventListener('click', (change) => {
+    change.preventDefault();
+
+    const parentLink = image.parentElement;
+
+    largeImage.src = parentLink.href;
+  });
+});
