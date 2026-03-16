d7445cc78a7dc4ad0cee6884cb567525838aa864
Danylo Skepskyi <skepskyidanylo@gmail.com>
2026-03-16 21:48:37 +0100

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const mainImage = document.querySelector('#largeImg');
+const ul = document.querySelector('ul');
+
+ul.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const listItem = e.target.closest('.list-item');
+
+  if (!listItem) {
+    return;
+  }
+
+  const a = listItem.querySelector('a');
+
+  mainImage.src = a.href;
+});
