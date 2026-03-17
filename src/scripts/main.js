fc094064e38e2fd3e325db77109f11852f7872d8
ikseloi <gdalexgo@gmail.com>
2026-03-17 06:23:43 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const mainImage = document.querySelector('#largeImg');
+const thumbnails = document.querySelector('#thumbs');
+
+const onThumbnailsClick = (e) => {
+  const thumb = e.target.closest('a');
+
+  if (!thumb) {
+    return;
+  }
+
+  e.preventDefault();
+  mainImage.src = thumb.href;
+};
+
+(() => {
+  if (!mainImage || !thumbnails) {
+    return;
+  }
+
+  thumbnails.addEventListener('click', (e) => {
+    onThumbnailsClick(e);
+  });
+})();
