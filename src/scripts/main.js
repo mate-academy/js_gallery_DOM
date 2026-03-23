a1fae5749c22361550bf9dca45d3cfb7ff64a89e
Olena Sobolieva <soboleva13as@gmail.com>
2026-03-23 20:52:05 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,30 @@
 'use strict';
+
+const galleryItems = document.querySelector('.gallery');
+const bigImg = document.querySelector('.gallery__large-img');
+
+const firstLink = galleryItems.querySelector('.list-item__link');
+
+firstLink.classList.add('list-item__link-active');
+bigImg.src = firstLink.href || firstLink.dataset.full;
+
+galleryItems.addEventListener('click', (ev) => {
+  const link = ev.target.closest('a');
+  const prev = galleryItems.querySelector('.list-item__link-active');
+
+  if (!link) {
+    return;
+  }
+  ev.preventDefault();
+
+  if (prev) {
+    prev.classList.remove('list-item__link-active');
+  }
+  link.classList.add('list-item__link-active');
+
+  const newSrc = link.href || link.dataset.full;
+
+  if (newSrc) {
+    bigImg.src = newSrc;
+  }
+});
