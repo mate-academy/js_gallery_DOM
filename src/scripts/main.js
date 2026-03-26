8c92836022032855b6a1beae5887bd8c39a0ec45
Grigoriy <m9258923028@gmail.com>
2026-03-26 13:29:52 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const gallery = document.getElementById('largeImg');
+const listThumbs = document.getElementById('thumbs');
+
+listThumbs.addEventListener('click', (currThumb) => {
+  const currLink = currThumb.target.closest('.list-item__link');
+
+  if (!currLink) {
+    return;
+  }
+
+  currThumb.preventDefault();
+
+  gallery.src = currLink.href;
+});
