71461e6ba2b19d97edf6a66e51274d5dbc39df07
Valentyna Bahyrly <valentynabahyrly@gmail.com>
2026-03-29 14:27:00 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const galleryContainer = document.querySelector('.gallery');
+const mainImage = document.querySelector('#largeImg');
+
+galleryContainer.addEventListener('click', function (e) {
+  const clickedLink = e.target.closest('a');
+
+  if (!clickedLink) {
+    return;
+  }
+
+  e.preventDefault();
+
+  const newSrc = clickedLink.href;
+
+  mainImage.src = newSrc;
+});
