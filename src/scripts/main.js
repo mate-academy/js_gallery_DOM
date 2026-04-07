0191c44063f717f3b4c30b2689c747dbef7faf89
kennyprog <overbafer08@gmail.com>
2026-04-07 12:46:11 +0300

    solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const mainImg = document.getElementById('largeImg');
+const images = [...document.querySelectorAll('.gallery__thumb')];
+const links = [...document.querySelectorAll('.list-item__link')]
+
+images.forEach(img => img.addEventListener('click', (event) => {
+  const eventImg = event.currentTarget.src;
+  mainImg.src = eventImg;
+}))
+
+links.forEach(link => link.addEventListener('click', (event) => {
+  event.preventDefault();
+  mainImg.src = event.currentTarget.href;
+}))
+
