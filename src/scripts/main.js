6f254bdd8f9004a64007ccfd7635fde57a1cd6d0
Heorhii Minesko <heorhii.minesko@gmail.com>
2026-04-07 13:40:36 +0300

    added the solution of the task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const largeImage = document.getElementById('largeImg');
+const tumbsCollection = document.querySelectorAll('ul li');
+
+tumbsCollection.forEach((li) => {
+  const liLink = li.firstElementChild;
+  const liImg = liLink.firstElementChild;
+  const linkContent = liLink.href;
+
+  if (liLink || liImg) {
+    liLink.addEventListener('click', () => {
+      liLink.href = '#';
+      largeImage.src = linkContent;
+    });
+
+    liImg.addEventListener('click', () => {
+      largeImage.src = liLink.href;
+    });
+  }
+});
