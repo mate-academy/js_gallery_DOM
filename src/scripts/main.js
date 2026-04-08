47187d475a0ce142f0b1b24c2f0ddb74a2f0a40b
Tymur <timurradkevic84@mail.com>
2026-04-08 10:40:41 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const mainImg = document.querySelector('#largeImg');
+const imgs = document.querySelectorAll('.list-item__link');
+
+for (const img of imgs) {
+  img.addEventListener('click', (ev) => {
+    ev.preventDefault();
+    mainImg.src = img.href;
+  });
+}
