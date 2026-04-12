1950ef74a09f0985df669b9b7a5aa61f06e3d780
Bohdan <felbohdan@gmail.com>
2026-04-12 22:23:55 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const thumbnails = document.querySelectorAll('.list-item');
+const largeImage = document.querySelector('.gallery__large-img');
+
+thumbnails.forEach((thumbnail) => {
+  thumbnail.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    largeImage.src = thumbnail.firstElementChild.href;
+  });
+});
