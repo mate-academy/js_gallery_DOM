0680661ba3924fd9ec2728e8ba1d90cb35cdf5fa
Maksym <maxim.golya@gmail.com>
2026-04-13 19:35:26 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const container = document.querySelector('.gallery');
+const mainImage = document.querySelector('img');
+
+container.addEventListener('click', (evt) => {
+  const link = evt.target.closest('a');
+
+  if (!link) {
+    return;
+  }
+
+  evt.preventDefault();
+
+  const newSrc = link.href;
+
+  mainImage.src = newSrc;
+});
