61dad6dbc1f18d86030e1d28e37f2815b259659d
Nika Shevchuk <niksh170307@gmail.com>
2026-04-20 13:19:39 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const gallery = document.querySelector('.gallery');
+const mainImage = document.querySelector('.gallery__img');
+
+gallery.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  let link;
+
+  if (e.target.tagName === 'IMG') {
+    link = e.target.closest('a');
+  }
+
+  if (e.target.tagName === 'A') {
+    link = e.target;
+  }
+
+  if (!link) {
+    return;
+  }
+
+  const newSrc = link.href;
+
+  mainImage.src = newSrc;
+});
