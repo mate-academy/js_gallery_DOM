15a228372339bcdf44a94a6f96ebff3317606304
Oksana Shtelma <oshtelma@gmail.com>
2026-04-22 15:04:33 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
-'use strict';
+const gallery = document.querySelector('.gallery');
+const largeImg = document.querySelector('#largeImg');
+
+// eslint-disable-next-line no-shadow
+gallery.addEventListener('click', function (event) {
+  if (event.target.tagName === 'img') {
+    const targetSrc = event.target.src;
+
+    largeImg.src = targetSrc;
+  }
+
+  if (event.target.tagName === 'a') {
+    const imgInsideA = event.target.querySelector('img');
+
+    largeImg.src = imgInsideA.src;
+  }
+});
