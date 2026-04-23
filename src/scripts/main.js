6f5c3a0d3c1904ef48641fafdefb3794d3d13a81
Sergey Ivanov <ivanovsereza26@gmail.com>
2026-04-23 23:04:47 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const thumbs = document.querySelector('#thumbs');
+const largeImg = document.querySelector('#largeImg');
+
+thumbs.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const link = e.target.closest('a');
+
+  if (!link || !thumbs.contains(link)) {
+    return;
+  }
+
+  largeImg.src = link.href;
+});
