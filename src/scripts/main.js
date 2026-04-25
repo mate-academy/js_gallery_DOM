e4e92387b5108b63fafe2d76a7736c869fd0024e
Viktor Meleshko <vectormeleshko@gmail.com>
2026-04-25 12:22:01 +0300

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const largeImg = document.querySelector('#largeImg');
+
+document.querySelector('#thumbs').addEventListener('click', function (e) {
+  const link = event.target.closest('a');
+
+  if (!link) {
+    return;
+  }
+  event.preventDefault();
+  largeImg.src = link.href;
+});
