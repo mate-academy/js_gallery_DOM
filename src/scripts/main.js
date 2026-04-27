052c85432557d38f661e5447ae923dceb434a269
Ihor Tutak <igor2tak2212@gmail.com>
2026-04-27 20:16:58 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const photos = document.querySelector('#thumbs');
+const largePhoto = document.querySelector('#largeImg');
+
+photos.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const link = e.target.closest('a');
+
+  if (!link) {
+    return;
+  }
+
+  largePhoto.src = link.href;
+});
