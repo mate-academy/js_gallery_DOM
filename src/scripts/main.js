55259d9a9e0e2fa62646fb3afe58f7adce46f2af
Inna K <innakonopatcka@gmail.com>
2026-04-27 17:02:50 +0300

    added solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const thumbs = document.querySelector('#thumbs');
+const largeImg = document.querySelector('#largeImg');
+
+thumbs.addEventListener('click', (ev) => {
+  const link = event.target.closest('a');
+
+  if (!link) {
+    return;
+  }
+  event.preventDefault();
+  largeImg.src = link.href;
+});
