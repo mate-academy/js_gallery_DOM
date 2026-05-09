041add3948778506eb2d4098ad1c54bd00ed9651
Roman <yamshchikovr11@gmail.com>
2026-05-09 04:57:38 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const thumbsEl = document.querySelector('#thumbs');
+const largeImgEl = document.querySelector('#largeImg');
+
+thumbsEl.addEventListener('click', (ev) => {
+  ev.preventDefault();
+
+  if (ev.target.tagName === 'A') {
+    largeImgEl.src = ev.target.href;
+  }
+
+  if (ev.target.tagName === 'IMG') {
+    largeImgEl.src = ev.target.parentElement.href;
+  }
+
+  if (ev.target.tagName === 'LI') {
+    largeImgEl.src = ev.target.querySelector('a').href;
+  }
+});
