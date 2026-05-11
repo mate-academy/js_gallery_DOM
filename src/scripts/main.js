cb50a56f41bce2dc3ec7971486515e395305c550
Maksim Vashuk <maximka.vashuk@gmail.com>
2026-05-11 22:19:39 +0200

    galery

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const bilder = document.querySelector('#largeImg');
+const listImg = document.querySelector('#thumbs');
+
+listImg.addEventListener('click', (e) => {
+  const item = e.target.closest('.list-item__link');
+
+  if (!item) {
+    return;
+  }
+
+  e.preventDefault();
+
+  bilder.src = item.href;
+});
