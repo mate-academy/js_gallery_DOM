f51517551dde66b04d91488cc4d1643cf8a84e47
Rostyslav` <hrostyslav@ukr.net`>
2026-05-17 16:55:11 +0300

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const gallery = document.querySelector('.gallery');
+const mainImage = document.querySelector('.gallery__img');
+
+gallery.addEventListener('click', function (eve) {
+  if (eve.target.tagName === 'IMG') {
+    mainImage.src = eve.target.src;
+  } else {
+    mainImage.src = eve.target.href;
+  }
+});
