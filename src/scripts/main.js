7324a2a5f94d29b4f0d9812bc7e072e93b0b8a0a
ILLYA SMYK <smykillya@gmail.com>
2026-06-07 21:40:05 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const largeImg = document.querySelector('#largeImg');
+const gallery = document.querySelector('#thumbs');
+
+gallery.addEventListener('click', changeImg);
+
+function changeImg(ev) {
+  const link = event.target.closest('a');
+
+  if (!link) {
+    return;
+  }
+  event.preventDefault();
+  largeImg.src = link.href;
+}
