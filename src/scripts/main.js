5956a187be7f680780762b191cf108e10291cd5c
Ihor Balakhmei <ihor@MacBook-Pro-Ihor.local>
2026-06-11 22:09:31 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const largeImg = document.querySelector('.gallery__large-img');
+const galleryList = document.querySelector('.gallery__list');
+
+function clickOnGallery(evt) {
+  const listItem = evt.target.closest('.list-item');
+
+  if (listItem) {
+    evt.preventDefault();
+
+    const link = listItem.querySelector('.list-item__link');
+    const itemSource = link.href;
+
+    largeImg.setAttribute('src', itemSource);
+  }
+}
+
+galleryList.addEventListener('click', clickOnGallery);
