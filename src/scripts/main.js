2178de5f774b574fac3f2f323701bc7dd1cf08d8
Timur K <timurkakandzo@gmail.com>
2026-06-15 11:41:22 +0300

    final1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const largeImage = document.querySelector('#largeImg');
+const smallImages = document.querySelector('#thumbs');
+
+smallImages.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const target = e.target.closest('.list-item__link');
+
+  if (!target) {
+    return;
+  }
+
+  largeImage.src = target.href;
+  largeImage.alt = target.title;
+});
