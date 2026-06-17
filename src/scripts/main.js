ea970c70348e8578edbfb5571c686113dc4f4eaa
Denys Boiko <big4ever6996@gmail.com>
2026-06-17 17:52:31 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const mainImg = document.getElementById('largeImg');
+const thumbs = document.getElementById('thumbs');
+
+thumbs.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const clickedImg = e.target.closest('a');
+
+  if (clickedImg) {
+    const newImg = clickedImg.href;
+
+    mainImg.src = newImg;
+  }
+});
