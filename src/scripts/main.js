3fca8149d2f4250ec24610bda1b8865eb9b78b46
Vitalii <vtysiachnyk@gmail.com>
2026-06-30 18:57:54 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const gallery = document.querySelector('#thumbs');
+const largeImg = document.querySelector('#largeImg');
+
+if (gallery && largeImg) {
+  gallery.addEventListener('click', (e) => {
+    const thumbnailLink = e.target.closest('a');
+
+    if (!thumbnailLink || !gallery.contains(thumbnailLink)) {
+      return;
+    }
+
+    e.preventDefault();
+
+    largeImg.src = thumbnailLink.href;
+    largeImg.alt = thumbnailLink.title || '';
+  });
+}
