bf2a168d099b0fcbf7c021627f5231be739198a1
Skoulskiy <yaskevychilya@gmail.com>
2026-07-02 12:16:18 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const mainImg = document.getElementById('largeImg');
+
+const links = document.querySelector('.gallery__list');
+
+links.addEventListener('click', (e) => {
+  const link = e.target.closest('a');
+
+  e.preventDefault();
+
+  if (!link) {
+    return;
+  }
+
+  mainImg.src = link.getAttribute('href');
+});
