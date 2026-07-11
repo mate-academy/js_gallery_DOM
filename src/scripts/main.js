85fb30bb19d925cb301784b03f84a0345b353f09
Danyliuk Yaroslav <unityismylove@gmail.com>
2026-07-11 20:55:08 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const item = document.querySelector('.gallery__list');
+const mainImg = document.querySelector('.gallery__large-img');
+
+item.addEventListener('click', (eventObj) => {
+  eventObj.preventDefault();
+
+  if (eventObj.target.alt === 'thumb') {
+    mainImg.setAttribute('src', eventObj.target.parentElement.href);
+  } else {
+    mainImg.setAttribute('src', eventObj.target.href);
+  }
+});
