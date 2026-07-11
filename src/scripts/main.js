fa5d1618508babf6d075cce31f567e0bafbaab4f
Danyliuk Yaroslav <unityismylove@gmail.com>
2026-07-11 21:02:11 +0300

    sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,7 +6,7 @@ const mainImg = document.querySelector('.gallery__large-img');
 item.addEventListener('click', (eventObj) => {
   eventObj.preventDefault();
 
-  if (eventObj.target.alt === 'thumb') {
+  if (eventObj.target.class === 'gallery__thumb') {
     mainImg.setAttribute('src', eventObj.target.parentElement.href);
   } else if (eventObj.target.class === 'list-item__link') {
     mainImg.setAttribute('src', eventObj.target.href);
