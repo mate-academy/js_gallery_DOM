58ad2f9f03d656fa0decd8b18bdbd8ccaa9dea12
Anna Pryimak <sapfira03@gmail.com>
2026-07-12 14:31:37 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const gallery = document.querySelector('.gallery');
+const mainImage = document.querySelector('.gallery__large-img');
+
+gallery.addEventListener('click', () => {
+  const image = event.target.closest('img');
+  const link = event.target.closest('a');
+
+  if (!image && !link) {
+    return;
+  }
+
+  if (image) {
+    mainImage.src = image.src;
+  }
+
+  if (link) {
+    event.preventDefault();
+    mainImage.src = link.href;
+  }
+});
