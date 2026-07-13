2238d4a0c6f222eaa4bb9f622a12557be820e629
Mariana <zh.yu.st22@gmail.com>
2026-07-13 23:25:33 +0300

    fixed issues

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,9 +4,11 @@ const bigPic = document.querySelector('#largeImg');
 const galleryCon = document.querySelector('#thumbs');
 
 galleryCon.addEventListener('click', (even) => {
-  even.preventDefault();
-
   const link = even.target.closest('a');
 
+  if (!link) {
+    return;
+  }
+  even.preventDefault();
   bigPic.src = link.href;
 });
