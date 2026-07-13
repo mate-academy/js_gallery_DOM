fea442b729dbab7c17295eed13cba5ecda9b8b9f
Mariana <zh.yu.st22@gmail.com>
2026-07-13 22:57:30 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const bigPic = document.querySelector('#largeImg');
+const galleryCon = document.querySelector('#thumbs');
+
+galleryCon.addEventListener('click', (even) => {
+  even.preventDefault();
+
+  const link = even.target.closest('a');
+
+  bigPic.src = link.href;
+});
