341983f8e38597cb3d60e8e5bc64e0d4080bf4fc
Danyliuk Yaroslav <unityismylove@gmail.com>
2026-07-13 12:13:55 +0300

    soluu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,11 +4,13 @@ const item = document.querySelector('.gallery__list');
 const mainImg = document.querySelector('.gallery__large-img');
 
 item.addEventListener('click', (eventObj) => {
+  const link = eventObj.target.closest('a');
+
   eventObj.preventDefault();
 
-  if (eventObj.target.class === 'gallery__thumb') {
+  if (link) {
     mainImg.setAttribute('src', eventObj.target.parentElement.href);
-  } else if (eventObj.target.class === 'list-item__link') {
-    mainImg.setAttribute('src', eventObj.target.href);
+  } else {
+    return null;
   }
 });
