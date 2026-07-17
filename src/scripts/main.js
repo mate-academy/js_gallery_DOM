456b116377af3b6b6f2f3708732669a281addc79
Svitlana Slavets <slavets.sa@gmail.com>
2026-07-17 23:17:58 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const thumbs = document.querySelector('#thumbs');
+const largeImg = document.querySelector('#largeImg');
+
+thumbs.addEventListener('click', (e) => {
+  const link = e.target.closest('a');
+
+  if (!link) {
+    return;
+  }
+
+  e.preventDefault();
+
+  largeImg.src = link.href;
+});
