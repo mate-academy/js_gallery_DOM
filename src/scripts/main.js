6c603f8a1bcb1430252a67c01dbf957f4e47899a
darvexon <darvexon.w@proton.me>
2026-07-24 13:58:50 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const largeImg = document.getElementById('largeImg');
+const thumbs = document.getElementById('thumbs');
+
+thumbs.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const newImg = e.target.closest('a');
+
+  if (!newImg) {
+    return;
+  }
+
+  const imgLink = newImg.getAttribute('href');
+
+  largeImg.src = imgLink;
+});
