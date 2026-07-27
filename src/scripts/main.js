bd56438c78bee81fd34e58ac495536eca2cbb6e6
Yelyzaveta Romanovska <lizarom2608@gmail.com>
2026-07-27 21:38:26 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const thumbs = document.querySelector('#thumbs');
+const largeImg = document.querySelector('#largeImg');
+
+thumbs.addEventListener('click', (clickEvent) => {
+  const target = clickEvent.target;
+
+  const link = target.closest('.list-item__link');
+
+  if (!link) {
+    return;
+  }
+
+  clickEvent.preventDefault();
+  largeImg.src = link.getAttribute('href');
+  largeImg.alt = link.getAttribute('title');
+});
