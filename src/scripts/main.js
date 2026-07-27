adfa47a4e5a425d41f2ff1769c08ee0fe10c56c9
Victoria Neustroeva <neustrvk@gmail.com>
2026-07-27 19:10:03 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const galleryListElement = document.querySelector('#thumbs');
+const largeImgElement = document.querySelector('#largeImg');
+
+galleryListElement.addEventListener('click', (evt) => {
+  evt.preventDefault();
+
+  const link = evt.target.closest('.list-item__link');
+
+  if (!link) {
+    return;
+  }
+
+  largeImgElement.src = link.href;
+});
