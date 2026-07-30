bbed5396d7a1d0febe63cdde438617af48a74fb8
pparys93 <pparys93@gmail.com>
2026-07-30 20:34:08 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const gallery = document.querySelector('.gallery');
+const largeImage = document.querySelector('#largeImg');
+
+gallery.addEventListener('click', (e) => {
+  const thumbnail = e.target.closest('a');
+
+  if (thumbnail) {
+    e.preventDefault();
+
+    const src = thumbnail.getAttribute('href');
+
+    largeImage.src = src;
+  }
+});
