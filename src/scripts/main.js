1158ab6df84e0b5f12526184d0de2773413532ca
Alexander Okhotnychenko <shashaohota@gmail.com>
2026-07-31 12:41:27 +0300

    solution: gallety DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,33 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+  function handleClickGallery(selector, preview, list, target) {
+    const gallery = document.querySelector(selector);
+    const galleryPreview = gallery?.querySelector(preview);
+    const galleryList = gallery?.querySelector(list);
+
+    if (!gallery || !galleryPreview || !galleryList) {
+      return;
+    }
+
+    galleryList.addEventListener('click', (evt) => {
+      evt.preventDefault();
+
+      const element = evt.target.closest(target);
+
+      if (!element) {
+        return;
+      }
+
+      const link = element.closest('a');
+
+      if (link) {
+        const fullImageUrl = link.href;
+
+        galleryPreview.src = fullImageUrl;
+      }
+    });
+  }
+
+  handleClickGallery('.gallery', '#largeImg', '#thumbs', '.gallery__img');
+});
