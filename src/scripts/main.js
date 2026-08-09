722c48df523e01c5c7062d1b7c9a04e28185c843
Taras Peteichuk <taraskull123@gmail.com>
2026-08-09 15:37:58 +0300

    added solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const images = document.querySelector('.gallery ul');
+
+images.addEventListener('click', (e) => {
+  const link = e.target.closest('.list-item__link');
+
+  if (link) {
+    e.preventDefault();
+
+    const img = e.target.closest('a');
+
+    document.getElementById('largeImg').setAttribute('src', `${img}`);
+  }
+});
