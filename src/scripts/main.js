4b9e510f78777b63e4378ab120b44e456f7a35bd
Yevheniia Biliaieva <biliaieva99@gmail.com>
2026-08-13 18:06:58 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const galleryList = document.querySelector('.gallery__list');
+const bigImage = document.querySelector('.gallery__large-img');
+
+galleryList.addEventListener('click', (e) => {
+  const clickedItem = e.target.closest('.gallery__img');
+
+  if (!clickedItem) {
+    return;
+  }
+
+  e.preventDefault();
+
+  bigImage.src = clickedItem.parentElement.href;
+});
