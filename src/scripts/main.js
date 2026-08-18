a8d889cc091f2e2fa3a162ebd6ca8ac6ded153aa
polina3107 <polinadavidenko3107@gmail.com>
2026-08-18 17:51:10 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const galleryList = document.querySelector('.gallery ul');
+const main = document.querySelector('img#largeImg');
+
+galleryList.addEventListener('click', () => {
+  event.preventDefault();
+
+  if (event.target.tagName === 'IMG') {
+    const value = event.target.parentElement.href;
+
+    main.src = value;
+  } else if (event.target.tagName === 'A') {
+    const value = event.target.href;
+
+    main.src = value;
+  }
+});
