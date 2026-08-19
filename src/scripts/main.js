e9fb55b882749fe551a42873a87959d993761ab0
Olya <fialkaolya08@gmail.com>
2026-08-19 18:34:45 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const thumbs = document.querySelector('#thumbs');
+
+const oldImg = document.querySelector('.gallery__large-img');
+
+thumbs.addEventListener('click', () => {
+  event.preventDefault();
+
+  if (event.target.tagName === 'A') {
+    oldImg.setAttribute('src', event.target.getAttribute('href'));
+  } else {
+    const closest = event.target.closest('.list-item__link');
+
+    oldImg.setAttribute('src', closest.getAttribute('href'));
+  }
+});
