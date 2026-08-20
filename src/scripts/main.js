c8031aeb5c248537bbeb555afc301e193a09c3aa
Natalia Onufrienko <natalia_anamalia@icloud.com>
2026-08-20 13:33:10 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const gallery = document.querySelector('.gallery');
+const largeImg = document.querySelector('#largeImg');
+
+gallery.addEventListener('click', (e) => {
+  const link = e.target.closest('a');
+
+  if (!link) {
+    return;
+  }
+
+  largeImg.src = link.href;
+
+  e.preventDefault();
+});
