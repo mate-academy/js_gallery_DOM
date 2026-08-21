7d8732563cd8c5c59702d67055f11cf75bca6882
Vladyslav <vlad.lozynsky@gmail.com>
2026-08-21 14:59:49 +0300

    Task completed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,12 +6,9 @@ galleryList.addEventListener('click', (e) => {
   e.preventDefault();
 
   const itemLink = e.target.closest('.list-item__link');
-  const image = e.target.closest('.gallery__thumb');
   const mainImage = document.getElementById('largeImg');
 
-  if (image && itemLink) {
-    mainImage.setAttribute('src', itemLink.getAttribute('href'));
-  } else if (itemLink) {
-    mainImage.setAttribute('src', itemLink.getAttribute('href'));
+  if (itemLink) {
+    mainImage.src = itemLink.href;
   }
 });
