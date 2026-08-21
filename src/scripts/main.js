bfbd40cc853e4b0be578f580e19681a44cea6fdf
ALex <sasha.hivuk@gmail.com>
2026-08-21 13:36:11 +0300

    event

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const big = document.querySelector('#largeImg');
+const list = document.querySelector('#thumbs');
+
+list.addEventListener('click', (e) => {
+  const link = e.target.closest('a');
+
+  if (link) {
+    e.preventDefault();
+
+    const src = link.getAttribute('href');
+
+    big.src = src;
+  }
+});
