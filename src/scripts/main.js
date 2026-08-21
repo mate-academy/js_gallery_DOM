8a67a819b21813e87ab6e125ae97469b35fe98ea
Vladyslav <vlad.lozynsky@gmail.com>
2026-08-21 14:47:41 +0300

    Task solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const galleryList = document.querySelector('.gallery__list');
+
+galleryList.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const itemLink = e.target.closest('.list-item__link');
+  const image = e.target.closest('.gallery__thumb');
+  const mainImage = document.getElementById('largeImg');
+
+  if (image && itemLink) {
+    mainImage.setAttribute('src', itemLink.getAttribute('href'));
+  } else if (itemLink) {
+    mainImage.setAttribute('src', itemLink.getAttribute('href'));
+  }
+});
