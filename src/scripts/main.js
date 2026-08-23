94c2c28795838981d0a4760fd86fc02f8b404cd3
Gregory Lyon <ggylyon@gmail.com>
2026-08-23 20:03:40 +0300

    Added the solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const gallery = document.querySelector('.gallery');
+
+const thumbs = gallery.querySelector('.gallery__list');
+
+const mainImage = gallery.querySelector('#largeImg');
+
+thumbs.addEventListener('click', (e) => {
+  const image = e.target.closest('.list-item');
+
+  e.preventDefault();
+
+  if (!image) {
+    return;
+  }
+
+  mainImage.src = e.target.parentNode.href;
+});
