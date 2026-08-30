eb8bf88c7078b1940c319daaab4212c6a2f03a37
Jonereh <gnomushka0@gmail.com>
2026-08-30 18:47:28 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const mainImage = document.querySelector('#largeImg');
+const thumbs = document.querySelector('#thumbs');
+
+thumbs.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const link = e.target.closest('a');
+
+  mainImage.src = link.href;
+});
