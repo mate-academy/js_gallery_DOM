cac25fd860b2ab575a38ad77b1a9b079d32d82cb
Anastasi Bielikova <anastasiabelikova39@gmail.com>
2026-09-03 20:39:17 +0300

    add

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const largeImg = document.querySelector('#largeImg');
+const thumbs = document.querySelector('#thumbs');
+
+thumbs.addEventListener('click', (e) => {
+  const link = e.target.closest('a');
+
+  if (!link || !thumbs.contains(link)) {
+    return;
+  }
+
+  e.preventDefault();
+
+  largeImg.src = link.href;
+});
